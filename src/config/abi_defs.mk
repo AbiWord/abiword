0d89707779366194349943719009e4fae6352ffb
Jeff Hostetler <jeff@svn.abisource.com>
1998-08-18 18:33:23 +0000

    Added Copyright headers to everything else.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@170 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/abi_defs.mk
+++ src/config/abi_defs.mk
@@ -1,4 +1,24 @@
 #! gmake
+ 
+##
+## The contents of this file are subject to the AbiSource Public
+## License Version 1.0 (the "License"); you may not use this file
+## except in compliance with the License. You may obtain a copy
+## of the License at http://www.abisource.com/LICENSE/ 
+## 
+## Software distributed under the License is distributed on an
+## "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+## implied. See the License for the specific language governing
+## rights and limitations under the License. 
+## 
+## The Original Code is AbiSource Utilities.
+## 
+## The Initial Developer of the Original Code is AbiSource, Inc.
+## Portions created by AbiSource, Inc. are Copyright (C) 1998 AbiSource, Inc. 
+## All Rights Reserved. 
+## 
+## Contributor(s):
+##  
 
 ##################################################################
 ##################################################################
@@ -68,7 +88,6 @@ EXTRA_LIBS=	-L$(DIST)/lib 							\
 		$(addprefix -l,$(addsuffix $(MOD_VERSION),$(ABI_OTHLIBS)))	\
 		$(addprefix -l,$(ABI_LIBS))	\
 		`gtk-config --libs`
-		
 endif
 
 ##################################################################
