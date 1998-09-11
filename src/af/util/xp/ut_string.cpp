0f1d1b43d0a2caed94897dc5899c4caa3f9b4b00
Paul Rohr <paul@svn.abisource.com>
1998-09-11 16:22:47 +0000

    Fixed Unix build.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@231 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_string.cpp
+++ src/af/util/xp/ut_string.cpp
@@ -82,5 +82,5 @@ UT_Bool UT_XML_cloneString(XML_Char *& rszDest, const XML_Char * szSource)
 UT_sint32 UT_XML_stricmp(const XML_Char * sz1, const XML_Char * sz2)
 {
 	UT_ASSERT(sizeof(char) == sizeof(XML_Char));
-	return stricmp(sz1,sz2);
+	return UT_stricmp(sz1,sz2);
 }
