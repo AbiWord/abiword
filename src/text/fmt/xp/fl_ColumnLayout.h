4c21845474e6a789bcb261de4371205fe2e3b3f8
Jeff Hostetler <jeff@svn.abisource.com>
1998-09-25 16:51:40 +0000

    Added copyright notices to all new files.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@294 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fl_ColumnLayout.h
+++ src/text/fmt/xp/fl_ColumnLayout.h
@@ -1,3 +1,24 @@
+ 
+/*
+** The contents of this file are subject to the AbiSource Public
+** License Version 1.0 (the "License"); you may not use this file
+** except in compliance with the License. You may obtain a copy
+** of the License at http://www.abisource.com/LICENSE/ 
+** 
+** Software distributed under the License is distributed on an
+** "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+** implied. See the License for the specific language governing
+** rights and limitations under the License. 
+** 
+** The Original Code is AbiWord.
+** 
+** The Initial Developer of the Original Code is AbiSource, Inc.
+** Portions created by AbiSource, Inc. are Copyright (C) 1998 AbiSource, Inc. 
+** All Rights Reserved. 
+** 
+** Contributor(s):
+**  
+*/
 
 #ifndef FL_COLUMNLAYOUT_H
 #define FL_COLUMNLAYOUT_H
