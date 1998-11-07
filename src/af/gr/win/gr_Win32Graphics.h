4627b8ce781b41c6a9516990fd95ee185a3470c0
Paul Rohr <paul@svn.abisource.com>
1998-11-07 00:32:20 +0000

    Fixed build
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@450 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/gr/win/gr_Win32Graphics.h
+++ src/af/gr/win/gr_Win32Graphics.h
@@ -53,7 +53,8 @@ public:
 		const char* pszFontVariant, 
 		const char* pszFontWeight, 
 		const char* pszFontStretch, 
-		const char* pszFontSize);
+		const char* pszFontSize,
+		const char* pszXLFD); /* ignored for non X platforms */
 	virtual UT_uint32 getFontAscent();
 	virtual UT_uint32 getFontDescent();
 	virtual void drawLine(UT_sint32, UT_sint32, UT_sint32, UT_sint32);
