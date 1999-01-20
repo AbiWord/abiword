feda4545c09c4b11fdaf963881c448014ab842a6
Jeff Hostetler <jeff@svn.abisource.com>
1999-01-20 22:59:45 +0000

    Missed a file in my last checkin.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@841 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/win/ap_Win32Frame.h
@@ -0,0 +1,65 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#ifndef AP_WIN32FRAME_H
+#define AP_WIN32FRAME_H
+
+#include "xap_Win32Frame.h"
+
+/*****************************************************************/
+
+class AP_Win32Frame : public XAP_Win32Frame
+{
+public:
+	AP_Win32Frame(AP_Win32App * app);
+	AP_Win32Frame(AP_Win32Frame * f);
+	virtual ~AP_Win32Frame(void);
+
+	virtual UT_Bool				initialize(void);
+	virtual	XAP_Frame *			cloneFrame(void);
+	virtual UT_Bool				loadDocument(const char * szFilename);
+	virtual UT_Bool				initFrameData(void);
+	virtual void				killFrameData(void);
+
+	virtual void				setXScrollRange(void);
+	virtual void				setYScrollRange(void);
+
+	static UT_Bool				RegisterClass(AP_Win32App * app);
+
+protected:
+	virtual HWND				_createDocumentWindow(HWND hwndParent,
+													  UT_uint32 iLeft, UT_uint32 iTop,
+													  UT_uint32 iWidth, UT_uint32 iHeight);
+	UT_Bool						_loadDocument(const char * szFilename);
+	UT_Bool						_showDocument(void);
+	static void					_scrollFunc(void * pData, UT_sint32 xoff, UT_sint32 yoff);
+	static LRESULT CALLBACK		_ContainerWndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK		_TopRulerWndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK		_LeftRulerWndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK		_DocumentWndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+	HWND						m_hwndTopRuler;
+	HWND						m_hwndLeftRuler;
+	HWND						m_hwndDeadLowerRight;
+	HWND						m_hwndVScroll;
+	HWND						m_hwndHScroll;
+	HWND						m_hwndDocument;	/* the actual document window */
+};
+
+#endif /* AP_WIN32FRAME_H */
