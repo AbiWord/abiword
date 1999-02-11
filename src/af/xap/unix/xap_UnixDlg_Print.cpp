b880c1bebc61816618bbb1bfbb4d3b2e883b1e65
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-02-11 20:36:58 +0000

    Removed #includes for C++ stream functions, since they're no longer used.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@992 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/unix/xap_UnixDlg_Print.cpp
+++ src/af/xap/unix/xap_UnixDlg_Print.cpp
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fstream.h>
 #include "ut_debugmsg.h"
 #include "ut_string.h"
 #include "ut_assert.h"
--- src/af/xap/unix/xap_UnixFontManager.cpp
+++ src/af/xap/unix/xap_UnixFontManager.cpp
@@ -20,7 +20,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fstream.h>
 
 #include "ut_types.h"
 #include "ut_assert.h"
