fec57d2e4658991f616372ca2eb5baaa3957107c
Jeff Hostetler <jeff@svn.abisource.com>
1999-03-26 19:56:21 +0000

    Inserted Thomas's BeOS port into the tree.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1311 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/util/beos/ut_BeOSTimer.h
@@ -0,0 +1,44 @@
+/* AbiSource Program Utilities
+ * Copyright (C) 1998 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+ 
+
+
+#ifndef UT_BEOSTIMER_H
+#define UT_BEOSTIMER_H
+
+#include "ut_timer.h"
+
+class UT_BEOSTimer : public UT_Timer
+{
+public:
+	UT_BEOSTimer(UT_TimerCallback pCallback, void* pData);
+	virtual ~UT_BEOSTimer();
+
+	virtual UT_sint32 set(UT_uint32 iMilliseconds);
+	virtual void stop(void);
+	virtual void start(void);
+	virtual void resetIfStarted(void);
+	
+protected:
+	UT_sint32 m_iMilliseconds;
+	UT_Bool m_bStarted;
+};
+
+#endif /* UT_BEOSTIMER_H */
+
