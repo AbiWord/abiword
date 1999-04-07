92601e8d352dde561ccf98e3863362e23cff6fed
Paul Rohr <paul@svn.abisource.com>
1999-04-07 02:09:57 +0000

    Applying block styles clears character-level properties.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1393 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/ptbl/xp/pt_PT_ChangeStrux.cpp
+++ src/text/ptbl/xp/pt_PT_ChangeStrux.cpp
@@ -27,6 +27,7 @@
 #include "ut_growbuf.h"
 #include "pt_PieceTable.h"
 #include "pf_Frag.h"
+#include "pf_Frag_Object.h"
 #include "pf_Frag_Strux.h"
 #include "pf_Frag_Strux_Block.h"
 #include "pf_Frag_Strux_Section.h"
@@ -120,40 +121,131 @@ UT_Bool pt_PieceTable::changeStruxFmt(PTChangeFmt ptc,
 	// NOTE: endMultiStepGlob() before we return -- otherwise,
 	// NOTE: the undo/redo won't be properly bracketed.
 
-	UT_Bool bSimple = (pfs_First == pfs_End);
+	UT_Bool bApplyStyle = (ptc == PTC_AddStyle);
+	UT_Bool bSimple = (!bApplyStyle && (pfs_First == pfs_End));
 	if (!bSimple)
 		beginMultiStepGlob();
 
 	pf_Frag * pf = pfs_First;
 	UT_Bool bFinished = UT_FALSE;
-	while (!bFinished)
+
+	if (!bApplyStyle)
 	{
-		switch (pf->getType())
+		// simple loop for normal strux change
+		while (!bFinished)
 		{
-		case pf_Frag::PFT_EndOfDoc:
-		default:
-			UT_ASSERT(0);
-			return UT_FALSE;
-			
-		case pf_Frag::PFT_Strux:
+			switch (pf->getType())
 			{
-				pf_Frag_Strux * pfs = static_cast<pf_Frag_Strux *>(pf);
-				if (pfs->getStruxType() == pts)
+			case pf_Frag::PFT_EndOfDoc:
+			default:
+				UT_ASSERT(0);
+				return UT_FALSE;
+				
+			case pf_Frag::PFT_Strux:
 				{
-					UT_Bool bResult = _fmtChangeStruxWithNotify(ptc,pfs,attributes,properties);
-					UT_ASSERT(bResult);
+					pf_Frag_Strux * pfs = static_cast<pf_Frag_Strux *>(pf);
+					if (pfs->getStruxType() == pts)
+					{
+						UT_Bool bResult = _fmtChangeStruxWithNotify(ptc,pfs,attributes,properties);
+						UT_ASSERT(bResult);
+					}
+					if (pfs == pfs_End)
+						bFinished = UT_TRUE;
 				}
-				if (pfs == pfs_End)
-					bFinished = UT_TRUE;
+				break;
+
+			case pf_Frag::PFT_Object:
+			case pf_Frag::PFT_Text:
+				break;
 			}
-			break;
 
-		case pf_Frag::PFT_Object:
-		case pf_Frag::PFT_Text:
-			break;
+			pf = pf->getNext();
 		}
+	}
+	else
+	{
+		// when applying a block-level style, we also need to clear 
+		// any props at the frag level, which might trigger coalescing, 
+		// thus this version of the loop is more complex.
 
-		pf = pf->getNext();
+		PT_DocPosition dpos = getFragPosition(pfs_First);
+		pf_Frag_Strux * pfsContainer = pfs_First;
+		pf_Frag * pfNewEnd;
+		UT_uint32 fragOffsetNewEnd;
+		
+		UT_Bool bEndSeen = UT_FALSE;
+
+		while (!bFinished)
+		{
+			UT_uint32 lengthThisStep = pf->getLength();
+			
+			switch (pf->getType())
+			{
+			case pf_Frag::PFT_EndOfDoc:
+				UT_ASSERT(bEndSeen);
+				bFinished = UT_TRUE;
+				break;
+
+			default:
+				UT_ASSERT(0);
+				return UT_FALSE;
+				
+			case pf_Frag::PFT_Strux:
+				{
+					pfNewEnd = pf->getNext();
+					fragOffsetNewEnd = 0;
+					pfsContainer = static_cast<pf_Frag_Strux *> (pf);
+					if (!bEndSeen && (pfsContainer->getStruxType() == pts))
+					{
+						UT_Bool bResult = _fmtChangeStruxWithNotify(ptc,pfsContainer,attributes,properties);
+						UT_ASSERT(bResult);
+					}
+
+					if (pfsContainer == pfs_End)
+						bEndSeen = UT_TRUE;
+					else if (bEndSeen)
+						bFinished = UT_TRUE;
+				}
+				break;
+
+			case pf_Frag::PFT_Text:
+				{
+					UT_Bool bResult
+						= _fmtChangeSpanWithNotify(ptc,static_cast<pf_Frag_Text *>(pf),
+												   0,dpos,lengthThisStep,
+												   NULL,NULL,
+												   pfsContainer,&pfNewEnd,&fragOffsetNewEnd);
+					UT_ASSERT(bResult);
+					UT_ASSERT(fragOffsetNewEnd == 0);
+				}
+				break;
+
+			case pf_Frag::PFT_Object:
+				{
+					UT_Bool bResult
+						= _fmtChangeObjectWithNotify(ptc,static_cast<pf_Frag_Object *>(pf),
+													 0,dpos,lengthThisStep,
+													 NULL,NULL,
+													 pfsContainer,&pfNewEnd,&fragOffsetNewEnd);
+					UT_ASSERT(bResult);
+					UT_ASSERT(fragOffsetNewEnd == 0);
+				}
+				break;
+			}
+
+			dpos += lengthThisStep;
+			
+			// since _fmtChangeSpanWithNotify(), can delete pf, mess with the
+			// fragment list, and does some aggressive coalescing of
+			// fragments, we cannot just do a pf->getNext() here.
+			// to advance to the next fragment, we use the *NewEnd variables
+			// that each of the cases routines gave us.
+
+			pf = pfNewEnd;
+			if (!pf)
+				bFinished = UT_TRUE;
+//			fragOffset_First = fragOffsetNewEnd;
+		}
 	}
 
 	if (!bSimple)
