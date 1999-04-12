c785ab0e9dee122d0286a4031eecd659b2c60fc9
Jeff Hostetler <jeff@svn.abisource.com>
1999-04-12 19:29:23 +0000

    Fixed somebody's build (maybe BeOS maybe Linux).
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1434 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/other/libmswordview/strerror.c
+++ src/other/libmswordview/strerror.c
@@ -1,11 +1,11 @@
 
-#include <stdio.h>
-
 char *strerror(int errnum)
 {
 #ifdef NO_SYS_ERRLIST
-	// TODO replace this hack.
-	// TODO i couldn't find the library with sys_errlist in it.
+	/*
+	** TODO replace this hack.
+	** TODO i couldn't find the library with sys_errlist in it. 
+	*/
 	static char buf[128];
 	sprintf(buf,"System Error Number %d.",errnum);
 	return buf;
