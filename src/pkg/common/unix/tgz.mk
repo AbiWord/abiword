097fc133d96c5f5bfb018276e7a68471e46ee3cd
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-05-12 18:21:09 +0000

    Added common tgz unix makefiles
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1571 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/pkg/common/unix/tgz.mk
@@ -0,0 +1,109 @@
+#! gmake
+
+## AbiSource Applications
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+## 02111-1307, USA.
+
+##################################################################
+# THIS MAKEFILE IS A STANDARD SET OF INSTALL TARGETS AND WILL BE 
+# INCLUDED BY ACTUAL PLATFORM PKG MAKEFILES.  REFERENCES TO SHARED
+# (COMMON) RESOURCES SHOULD BE DONE FROM A LOCATION "INSIDE" THE
+# INCLUDING MAKEFILE.  BE SURE TO INCLUDE THIS MAKEFILE AFTER 
+# YOU HAVE ALREADY INCLUDED THE ABI_DEFS.MK AND ABI_RULES.MK.
+##################################################################	
+
+# tgz fonts are handled by these rules
+ABI_DEPTH=../..
+include $(ABI_DEPTH)/pkg/common/unix/tgzfonts.mk
+
+##################################################################
+## tgz -- this is a very simple tree of the essentials.  this is
+##		just a tar of everything and can be put in /usr/local
+##		and with a few symbolic links should work just fine....
+##		the install script included with this package should
+##		handle installation issues for all Unix platforms.
+
+PKGBASENAME_DYNAMIC_NOFONTS	= $(PKGBASENAME)_dynamic_nofonts
+PKGBASENAME_STATIC_NOFONTS	= $(PKGBASENAME)_static_nofonts
+PKGBASENAME_DYNAMIC_FONTS	= $(PKGBASENAME)_dynamic_fonts
+PKGBASENAME_STATIC_FONTS	= $(PKGBASENAME)_static_fonts
+
+# the dirs from the src/[yourplatformstringhere] directory where
+# everything got put after the compile
+tgz_dirs=	bin AbiSuite
+
+# one requirement of this target is the fonts package, then all the
+# different combinations of binary packages
+tgz:	tgz_fonts tgz_dynamic_nofonts tgz_dynamic_fonts tgz_static_nofonts tgz_static_fonts
+
+tgz_dynamic_nofonts:
+	@echo Building .tar.gz package [dynamic,nofonts] ...
+	@$(subst xxxx,$(DIST),$(VERIFY_DIRECTORY))
+	@$(subst xxxx,$(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS),$(VERIFY_DIRECTORY))
+	(cp ../common/unix/install.sh $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS))
+	(cd $(OUTDIR); tar cf - $(tgz_dirs)) | (cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); tar xf -)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); mv -f bin AbiSuite)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); rm -rf AbiSuite/bin/*_s)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); rm -rf AbiSuite/fonts)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); strip AbiSuite/bin/*_d;)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS)/AbiSuite; tar cf ../data.tar .)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_NOFONTS); rm -rf AbiSuite)
+	(cd $(DIST); tar cf - $(PKGBASENAME_DYNAMIC_NOFONTS) | gzip - -9 - > $(PKGBASENAME_DYNAMIC_NOFONTS).tar.gz)
+	(cd $(DIST); rm -rf $(PKGBASENAME_DYNAMIC_NOFONTS))
+
+tgz_dynamic_fonts:
+	@echo Building .tar.gz package [dynamic,fonts] ...
+	@$(subst xxxx,$(DIST),$(VERIFY_DIRECTORY))
+	@$(subst xxxx,$(DIST)/$(PKGBASENAME_DYNAMIC_FONTS),$(VERIFY_DIRECTORY))
+	(cp ../common/unix/install.sh $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS))
+	(cd $(OUTDIR); tar cf - $(tgz_dirs)) | (cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS); tar xf -)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS); mv -f bin AbiSuite)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS); rm -rf AbiSuite/bin/*_s)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS); strip AbiSuite/bin/*_d;)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS)/AbiSuite; tar cf ../data.tar .)
+	(cd $(DIST)/$(PKGBASENAME_DYNAMIC_FONTS); rm -rf AbiSuite)
+	(cd $(DIST); tar cf - $(PKGBASENAME_DYNAMIC_FONTS) | gzip - -9 - > $(PKGBASENAME_DYNAMIC_FONTS).tar.gz)
+	(cd $(DIST); rm -rf $(PKGBASENAME_DYNAMIC_FONTS))
+
+tgz_static_nofonts:
+	@echo Building .tar.gz package [static,nofonts] ...
+	@$(subst xxxx,$(DIST),$(VERIFY_DIRECTORY))
+	@$(subst xxxx,$(DIST)/$(PKGBASENAME_STATIC_NOFONTS),$(VERIFY_DIRECTORY))
+	(cp ../common/unix/install.sh $(DIST)/$(PKGBASENAME_STATIC_NOFONTS))
+	(cd $(OUTDIR); tar cf - $(tgz_dirs)) | (cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); tar xf -)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); mv -f bin AbiSuite)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); rm -rf AbiSuite/bin/*_d)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); rm -rf AbiSuite/fonts)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); strip AbiSuite/bin/*_s;)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS)/AbiSuite; tar cf ../data.tar .)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_NOFONTS); rm -rf AbiSuite)
+	(cd $(DIST); tar cf - $(PKGBASENAME_STATIC_NOFONTS) | gzip - -9 - > $(PKGBASENAME_STATIC_NOFONTS).tar.gz)
+	(cd $(DIST); rm -rf $(PKGBASENAME_STATIC_NOFONTS))
+
+tgz_static_fonts:
+	@echo Building .tar.gz package [static,fonts] ...
+	@$(subst xxxx,$(DIST),$(VERIFY_DIRECTORY))
+	@$(subst xxxx,$(DIST)/$(PKGBASENAME_STATIC_FONTS),$(VERIFY_DIRECTORY))
+	(cp ../common/unix/install.sh $(DIST)/$(PKGBASENAME_STATIC_FONTS))
+	(cd $(OUTDIR); tar cf - $(tgz_dirs)) | (cd $(DIST)/$(PKGBASENAME_STATIC_FONTS); tar xf -)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_FONTS); mv -f bin AbiSuite)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_FONTS); rm -rf AbiSuite/bin/*_d)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_FONTS); strip AbiSuite/bin/*_s;)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_FONTS)/AbiSuite; tar cf ../data.tar .)
+	(cd $(DIST)/$(PKGBASENAME_STATIC_FONTS); rm -rf AbiSuite)
+	(cd $(DIST); tar cf - $(PKGBASENAME_STATIC_FONTS) | gzip - -9 - > $(PKGBASENAME_STATIC_FONTS).tar.gz)
+	(cd $(DIST); rm -rf $(PKGBASENAME_STATIC_FONTS))
---
+++ src/pkg/common/unix/tgzfonts.mk
@@ -0,0 +1,52 @@
+#! gmake
+
+## AbiSource Applications
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+## 02111-1307, USA.
+
+##################################################################
+# THIS MAKEFILE IS A STANDARD SET OF INSTALL TARGETS AND WILL BE 
+# INCLUDED BY ACTUAL PLATFORM PKG MAKEFILES.  REFERENCES TO SHARED
+# (COMMON) RESOURCES SHOULD BE DONE FROM A LOCATION "INSIDE" THE
+# INCLUDING MAKEFILE.  BE SURE TO INCLUDE THIS MAKEFILE AFTER 
+# YOU HAVE ALREADY INCLUDED THE ABI_DEFS.MK AND ABI_RULES.MK.
+##################################################################	
+
+##################################################################
+## tgzfonts -- the Unix fonts package in .tar.gz format.  this
+##			package is kind of an oddball since it
+##			only commonly builds .tar.gz for all Unix
+##			platforms (no deb, rpm, slp, etc.)
+
+# get the package names we'll use to build with
+ABI_DEPTH=../..
+include $(ABI_DEPTH)/pkg/common/unix/allfonts.mk
+
+font_files = 	README \
+	  	INSTALL \
+		readme.abw \
+		COPYING \
+		install.sh \
+		fonts
+
+tgz_fonts:
+	@echo Building Unix fonts package [.tar.gz format] ...
+	@$(subst xxxx,$(DIST),$(VERIFY_DIRECTORY))
+	@$(subst xxxx,$(DIST)/$(ABI_FONTS_PKGBASENAME),$(VERIFY_DIRECTORY))
+	(cd $(OUTDIR)/AbiSuite/fonts; tar cf - $(font_files))| (cd $(DIST)/$(ABI_FONTS_PKGBASENAME); tar xf -)
+	(cd $(DIST); tar cf - $(ABI_FONTS_PKGBASENAME) | gzip - -9 - > $(ABI_FONTS_PKGBASENAME).tar.gz)
+	(cd $(DIST); rm -rf $(ABI_FONTS_PKGBASENAME))
