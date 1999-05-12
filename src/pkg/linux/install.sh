dde765b4a0484384367e9bd83461ac2862b99556
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-05-12 01:06:45 +0000

    does symlinks
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1556 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/pkg/linux/install.sh
+++ src/pkg/linux/install.sh
@@ -61,6 +61,12 @@ do
     echo -n "Installation path for AbiSuite software [${DEFAULT_PREFIX}/${DEFAULT_HOME}]: "
     read INSTALL_BASE
 
+    # did they use tildes for home dirs?
+    if [ ! -z "`echo ${INSTALL_BASE} | grep '~'`" ]
+    then
+	INSTALL_BASE = `echo ${INSTALL_BASE} | sed "s:~:${HOME}:"`
+    fi
+
     # did they just hit enter?
     if [ -z "${INSTALL_BASE}" ]
     then
@@ -125,7 +131,7 @@ tar xf ${INSTALL_DATA_FILE}
 #then
 #    cd ${INSTALL_BASE}/fonts
 #    makepsres 1>/dev/null 2>/dev/null
-#endif
+#fi
 
 ########################################################################
 # Dynamically construct a wrapper for AbiSuite binaries
@@ -137,7 +143,7 @@ cat >${INSTALL_BASE}/bin/AbiWord<<EOF
 #!/bin/sh
 
 # AbiWord wrapper script.
-b
+
 # Change this if you move the AbiSuite tree.
 ABISUITE_HOME=${INSTALL_BASE}
 
@@ -147,8 +153,8 @@ ABISUITE_FONTHOME=\$ABISUITE_HOME/fonts
 # Set run-time font path
 if [ -d \$ABISUITE_FONT_HOME ]
 then
-    xset fp+ \$ABISUITE_FONT_HOME
-endif
+    xset fp+ \$ABISUITE_FONT_HOME 1>/dev/null 2>/dev/null
+fi
 
 # Figure out which binary to run
 if [ -f \$ABISUITE_HOME/bin/AbiWord_d ]
@@ -162,19 +168,109 @@ else
     echo "    \$ABISUITE_HOME/bin/AbiWord_d"
     echo "    \$ABISUITE_HOME/bin/AbiWord_s"
     echo ""
-    echo "Where did they go?"
+    echo "They should be in [${INSTALL_DATA_FILE}]. Where did they go?"
     exit
-endif
+fi
 
 # Set post run-time font path
 if [ -d \$ABISUITE_FONT_HOME ]
 then
-    xset fp- \$ABISUITE_FONT_HOME
-endif
+    xset fp- \$ABISUITE_FONT_HOME 1>/dev/null 2>/dev/null
+fi
 EOF
 
 chmod 755 ${INSTALL_BASE}/bin/AbiWord
 
+########################################################################
+# Ask the user if he would like to set up symlinks to the script
+# we just created.
+########################################################################
+
+cat <<EOF
+
+  AbiSuite programs are now installed:
+      ${INSTALL_BASE}/bin/AbiWord
+
+  As a convenience, I can install symbolic links to the installed
+  executables so you and other users do not have to modify
+  your paths to execute the AbiSuite programs.  For example, if you
+  proceed and specify "${DEFAULT_LINK_DIR}", I will create symbolic links like
+  "${DEFAULT_LINK_DIR}/AbiWord" and point them to the executable I just
+  previously installed.
+
+EOF
+
+echo -n "Do you want to provide a directory for these symbolic links? (y/n) [y]: "
+read MAKE_LINKS
+if [ "${MAKE_LINKS}" = "n" -o "${MAKE_LINKS}" = "N" ]
+then
+    echo ""
+    echo "Installation complete."
+    echo ""
+    exit
+fi
+
+# go for symlinks
+GO=1
+while test ${GO} -eq 1
+do
+    echo ""
+    echo "In which directory shall I install the symbolic links?"
+    echo -n "[${DEFAULT_LINK_DIR}]: "
+    read LINK_DIR
+
+    # did they use tildes for home dirs?
+    if [ ! -z "`echo ${LINK_DIR} | grep '~'`" ]
+    then
+	LINK_DIR = `echo ${LINK_DIR} | sed "s:~:${HOME}:"`
+    fi
+
+    # did they just hit enter?
+    if [ -z "${LINK_DIR}" ]
+    then
+	LINK_DIR = "${DEFAULT_LINK_DIR}"
+    fi
+
+    # GO off, passed tests
+    GO=0
+    if [ ! -d ${LINK_DIR} ]
+    then
+	echo ""
+	echo -n "The directory [${LINK_DIR}] does not exist; would you like to create it now? (y/n) [y]: "
+	read CREATE_DIR
+	if [ "${CREATE_DIR}" = "n" -o "${CREATE_DIR}" = "N" ]
+	then
+	    GO=1
+	else
+	    mkdir -p ${LINK_DIR}
+	    if [ $? -ne 0 ]
+	    then
+		echo ""
+		echo "I couldn't create [${LINK_DIR}].  You are probably seeing this error"
+ 		echo "because you do not have sufficient permission to perform this operation."
+		echo "You will most likely have to run this script as superuser to write to"
+		echo "system directories."
+		echo ""
+		echo "You can manually create these links at a later time.  You can run"
+		echo "the executables in ${INSTALL_BASE}/bin for now, or run this install"
+		echo "script again with sufficient permissions."
+		exit 2
+	    fi
+	fi
+    fi
+done
+
+# Do the linkage to the installed, dynamically-generated SCRIPTS,
+# not the actual executable binaries.  We install two links, one with 
+# proper caps and one for the strong proponents of lowercase naming.
+#
+# Add more pairs here for any more binaries we install.
+ln -fs ${INSTALL_BASE}/bin/AbiWord ${LINK_DIR}/AbiWord
+ln -fs ${INSTALL_BASE}/bin/AbiWord ${LINK_DIR}/abiword
+
+########################################################################
+# Bye!
+########################################################################
 echo ""
-echo "Done!"
+echo "Installation complete and symbolic links installed."
 echo ""
