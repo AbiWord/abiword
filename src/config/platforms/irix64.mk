32bd4a0f9b809109c58d8ce1c9717c0b1bd3b54b
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-05-25 15:03:17 +0000

    Added platform configuration scripts from Philippe.Defert@cern.ch
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1673 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/config/platforms/irix64.mk
@@ -0,0 +1,98 @@
+#! gmake
+
+## AbiSource Program Utilities
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+## 02111-1307, USA.
+
+##############################################################################
+## SGI IRIX 64 platform defines, courtesy of Philippe Defert 
+## (Philippe.Defert@cern.ch)
+##############################################################################
+
+##################################################################
+##################################################################
+## The main makefile and/or this file requires that OS_ARCH be set
+## to something to describe which chip that this OS is running on.
+## This can be used to change which tools are used and/or which
+## compiler/loader options are used.  It will probably also be used
+## in constructing the name object file destination directory.
+
+OS_ARCH		:= $(shell uname -m)
+
+# Define tools
+CC		= gcc
+#CC		= cc
+CCC		= g++
+#CCC		= CC
+#RANLIB		= ranlib
+RANLIB		= true
+
+# Suffixes
+OBJ_SUFFIX	= o
+LIB_SUFFIX	= a
+DLL_SUFFIX	= so
+AR		= ar cr $@
+
+# Compiler flags
+# NOTE:  Using both "-Wall" and "-W" turn on every single darn message
+# NOTE:  GCC can throw, and it turns quiet compiles into raging rivers of
+# NOTE:  warnings.  -Wall includes the very useful warnings, -W includes
+# NOTE:  more stylistic warnings.  -pedantic just gets really picky about
+# NOTE:  ANSI things.
+ifdef ABI_OPT_DEBUG
+OPTIMIZER	= -g -Wall -W -ansi -pedantic
+DEFINES		= -DDEBUG -UNDEBUG
+OBJ_DIR_SFX	= DBG
+else
+# NOTE:  In some instances, GCC can only know about truly unused variables
+# NOTE:  when optimizations are enabled in the compilation.  For this reason,
+# NOTE:  building with optimizations may reveal further warnings not 
+# NOTE:  visible without any -O[number] option.
+OPTIMIZER	= -O2 -Wall -W -ansi -pedantic
+DEFINES		=
+OBJ_DIR_SFX	= OBJ
+endif
+
+# Includes
+OS_INCLUDES		=
+G++INCLUDES		= -I/usr/include/g++
+
+# Compiler flags
+PLATFORM_FLAGS		= 
+PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE
+OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
+
+PLATFORM_FLAGS		+=
+PORT_FLAGS		+= 
+
+# Shared library flags
+MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
+
+# Which links can this platform create.  Define one or
+# both of these options. (what can IRIX do?  someone
+# with an SGI set the right one and mail a patch)
+UNIX_CAN_BUILD_DYNAMIC=1
+UNIX_CAN_BUILD_STATIC=1
+
+# Compiler options for static and dynamic linkage
+DL_LIBS			= dl
+STATIC_FLAGS		= -static
+
+ABI_NATIVE	= unix
+ABI_FE		= Unix
+
+# End of IRIX 64 defs
