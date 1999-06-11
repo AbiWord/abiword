2acbc9430f269a1f223cae12601e075332c78279
Paul Rohr <paul@svn.abisource.com>
1999-06-11 00:53:46 +0000

    Updated german translation by Christian Terboven <cterboven@gmx.net>.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@1749 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TB_LabelSet_de-DE.h
+++ src/wp/ap/xp/ap_TB_LabelSet_de-DE.h
@@ -42,47 +42,47 @@
 
 BeginSet(DeDE)
 
-	ToolbarLabel(AP_TOOLBAR_ID__BOGUS1__,		NULL,		NoIcon,			NULL,NULL)
+	ToolbarLabel(AP_TOOLBAR_ID__BOGUS1__,		NULL,		NoIcon,			NULL,		NULL)
 
-	//          (id, 		                    szLabel,	IconName,     	szToolTip,      szStatusMsg)
+	//          (id, 		                    szLabel,	IconName,	    szToolTip,	szStatusMsg)
 
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Neu", 		tb_new_xpm,		NULL, "Neues Dokument erstellen")	
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN,		"Öffnen",	tb_open_xpm,	        NULL, "Vorhandenes Dokument öffnen")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE,		"Speichern", 	tb_save_xpm,    	NULL, "Aktuelles Dokument speichern")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS,		"Speichern unter", 	tb_save_as_xpm,	NULL, "Aktuelles Dokument unter einem anderen Namen speichern")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT,		"Drucken",	tb_print_xpm,	        NULL, "Dokument oder Teile davon drucken")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Neu",		tb_new_xpm,		NULL,		"Neues Dokument erstellen")	
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN,		"Öffnen",	tb_open_xpm,	NULL,		"Vorhandenes Dokument öffnen")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE,		"Speichern", 	tb_save_xpm,NULL,		"Aktuelles Dokument speichern")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS,		"Speichern unter", 	tb_save_as_xpm,	NULL,	"Aktuelles Dokument unter einem anderen Namen speichern")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT,		"Drucken",	tb_print_xpm,	NULL,		"Dokument oder Teile davon drucken")
 
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_UNDO,		"Rückgängig",	tb_undo_xpm,      	NULL, "Letzte Aktion rückgängig machen")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_REDO,		"Wiederholen",	tb_redo_xpm,    	NULL, "Rückgängig gemachte Aktion wiederholen")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_CUT,		"Ausschneiden",	tb_cut_xpm,		NULL, "Markierung löschen und in die Zwischenablage kopieren")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_COPY,		"Kopieren",	tb_copy_xpm,    	NULL, "Markierung in die Zwischenablage kopieren")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_PASTE,		"Einfügen",	tb_paste_xpm,   	NULL, "Inhalt der Zwischenablage einfügen")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_UNDO,		"Rückgängig",	tb_undo_xpm,NULL,		"Letzten Befehl rückgängig machen")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_REDO,		"Wiederholen",	tb_redo_xpm,NULL,		"Letzten Befehl wiederholen")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_CUT,		"Ausschneiden",	tb_cut_xpm,	NULL,		"Markierung löschen und in die Zwischenablage kopieren")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_COPY,		"Kopieren",	tb_copy_xpm,	NULL,		"Markierung in die Zwischenablage kopieren")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_PASTE,		"Einfügen",	tb_paste_xpm,	NULL,		"Inhalt der Zwischenablage einfügen")
 
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_STYLE,		"Style",	NoIcon,			NULL, "TODO: insert description")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_FONT,		"Schriftart",   NoIcon,			NULL, "Schriftart")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_SIZE,		"Schriftgröße", NoIcon,         	NULL, "Schriftgröße")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_BOLD,		"Fett",		tb_text_bold_xpm,	NULL, "Fett")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_ITALIC,		"Kursiv",	tb_text_italic_xpm,	NULL, "Kursiv")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_UNDERLINE,	"Unterstrichen",tb_text_underline_xpm,	NULL, "Unterstrichen")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_STRIKE,		"Durchgestrichen", tb_text_strikeout_xpm, NULL, "Durchgestrichen")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_STYLE,		"Stil",		NoIcon,			NULL,		"Stil")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_FONT,		"Schriftart",   NoIcon,		NULL,		"Schriftart")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_SIZE,		"Schriftgröße", NoIcon,		NULL,		"Schriftgröße")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_BOLD,		"Fett",		tb_text_bold_xpm,	NULL,	"Fett")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_ITALIC,		"Kursiv",	tb_text_italic_xpm,	NULL,	"Kursiv")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_UNDERLINE,	"Unterstrichen",tb_text_underline_xpm,	NULL,	"Unterstrichen")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_STRIKE,		"Durchgestrichen", tb_text_strikeout_xpm,	NULL,	"Durchgestrichen")
 
 	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_LEFT,		"Links",	tb_text_align_left_xpm, NULL, "Links ausrichten")
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_CENTER,	"Zentriert",	tb_text_center_xpm,	NULL, "Zentriert ausrichten")
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_CENTER,	"Zentriert",tb_text_center_xpm,		NULL, "Zentriert ausrichten")
 	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_RIGHT,		"Rechts",	tb_text_align_right_xpm,NULL, "Rechts ausrichten")
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_JUSTIFY,	"Justify",	tb_text_justify_xpm,	NULL, "TODO: insert description")
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_JUSTIFY,	"Ausrichten",	tb_text_justify_xpm,	NULL, "Ausrichten")
 
-	ToolbarLabel(AP_TOOLBAR_ID_PARA_0BEFORE,	"None before",		tb_para_0before_xpm,	NULL, "TODO: insert description")
-	ToolbarLabel(AP_TOOLBAR_ID_PARA_12BEFORE,	"12 pt before",		tb_para_12before_xpm,	NULL, "TODO: insert description")
+	ToolbarLabel(AP_TOOLBAR_ID_PARA_0BEFORE,	"Keinen",	tb_para_0before_xpm,	NULL,	"Keinen Zeilenabstand")
+	ToolbarLabel(AP_TOOLBAR_ID_PARA_12BEFORE,	"12 pt",	tb_para_12before_xpm,	NULL,	"Zeilenabstand: 12 pt")
 
-	ToolbarLabel(AP_TOOLBAR_ID_SINGLE_SPACE,	"Einfacher Zeilenabstand", tb_line_single_space_xpm,	NULL, "Einfacher Zeilenabstand")
+	ToolbarLabel(AP_TOOLBAR_ID_SINGLE_SPACE,	"Einfacher Zeilenabstand",	tb_line_single_space_xpm,	NULL,	"Einfacher Zeilenabstand")
 	ToolbarLabel(AP_TOOLBAR_ID_MIDDLE_SPACE,	"1.5 Zeilenabstand",	   tb_line_middle_space_xpm,	NULL, "1.5 Zeilenabstand")
 	ToolbarLabel(AP_TOOLBAR_ID_DOUBLE_SPACE,	"Doppelter Zeilenabstand", tb_line_double_space_xpm,	NULL, "Doppelter Zeilenabstand")
 
-	ToolbarLabel(AP_TOOLBAR_ID_1COLUMN,		"1 Spalte",	tb_1column_xpm,		NULL, "1 Spalte")
-	ToolbarLabel(AP_TOOLBAR_ID_2COLUMN,		"2 Spalten",	tb_2column_xpm,		NULL, "2 Spalten")
-	ToolbarLabel(AP_TOOLBAR_ID_3COLUMN,		"3 Spalten",	tb_3column_xpm,		NULL, "3 Spalten")
+	ToolbarLabel(AP_TOOLBAR_ID_1COLUMN,		"1 Spalte",		tb_1column_xpm,	NULL,	"1 Spalte")
+	ToolbarLabel(AP_TOOLBAR_ID_2COLUMN,		"2 Spalten",	tb_2column_xpm,	NULL,	"2 Spalten")
+	ToolbarLabel(AP_TOOLBAR_ID_3COLUMN,		"3 Spalten",	tb_3column_xpm,	NULL,	"3 Spalten")
 
-	ToolbarLabel(AP_TOOLBAR_ID_ZOOM,		"Zoom",		NoIcon,			NULL, "Zoom")
+	ToolbarLabel(AP_TOOLBAR_ID_ZOOM,		"Zoom",			NoIcon,			NULL,	"Zoom")
 	
 	// ... add others here ...
 
