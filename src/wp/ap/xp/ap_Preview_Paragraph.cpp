46b7ef191f6c21d13be75164a881721afaa165a6
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-10-20 10:56:43 +0000

    Made MULTIPLE math more precise (made it work)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2006 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Preview_Paragraph.cpp
+++ src/wp/ap/xp/ap_Preview_Paragraph.cpp
@@ -202,8 +202,8 @@ void AP_Preview_Paragraph_Block::setFormat(AP_Dialog_Paragraph::tAlignState alig
 			// TODO : is this right?  I think we just use what they asked for
 			// TODO : unless it's less than the height of a line
 			if ((UT_uint32) (UT_convertToInches(lineSpacing) * (double) DIMENSION_INCH_SCALE_FACTOR) > m_fontHeight)
-				m_lineSpacing = (UT_uint32) (UT_convertToInches(lineSpacing)
-											 * (double) DIMENSION_INCH_SCALE_FACTOR) - (double) m_fontHeight;
+				m_lineSpacing = (UT_uint32) ((UT_convertToInches(lineSpacing)
+											  * (double) DIMENSION_INCH_SCALE_FACTOR) - (double) m_fontHeight);
 			break;
 		case AP_Dialog_Paragraph::spacing_EXACTLY:
 			// TODO : is this right?  I think we just use the spacing they asked
@@ -212,7 +212,8 @@ void AP_Preview_Paragraph_Block::setFormat(AP_Dialog_Paragraph::tAlignState alig
 										 * (double) DIMENSION_INCH_SCALE_FACTOR);
 			break;
 		case AP_Dialog_Paragraph::spacing_MULTIPLE:
-			m_lineSpacing = m_fontHeight * (UT_uint32) (UT_convertDimensionless(lineSpacing) - (double) 1);
+			m_lineSpacing = (UT_uint32) ((double) m_fontHeight
+										 * (UT_convertDimensionless(lineSpacing) - (double) 1));
 			break;
 		}
 	}
