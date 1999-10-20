27d410b419bc3f55ceb545324a7d595546748aff
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-10-20 13:25:52 +0000

    FreeBSD package script mods to work with build server
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2011 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/pkg/freebsd/scripts/mkplist.sh
+++ src/pkg/freebsd/scripts/mkplist.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# remove the static binary and strip the dynamic one
+ls -Flags $prefix/AbiSuite/bin
+strip $prefix/AbiSuite/bin/AbiWord_d
+rm -f $prefix/AbiSuite/bin/AbiWord_s
+# list the files now
 echo "@cwd $prefix" > $OUTFILE
 echo "@pkgdep `/bin/ls /var/db/pkg | grep gtk-`" >> $OUTFILE
 echo "@pkgdep `/bin/ls /var/db/pkg | grep png-`" >> $OUTFILE
@@ -7,5 +12,6 @@ cd $TARGET
 find . -type f | sort | sed 's/^./AbiSuite/g' >> $OUTFILE
 cd $BINDIR
 find . -name "[Aa]bi[Ww]ord*" | sort | sed 's/^./bin/g' >> $OUTFILE
+# find . -name "[Aa]bi[Ww]ord" | sort | sed 's/^./bin/g' >> $OUTFILE
 cd $TARGET
 find . -type d | sort -r | sed 's/^./@dirrm AbiSuite/g' >> $OUTFILE
