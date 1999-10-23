5ee32136bf9e706ef11e1614ae729db218b12f37
Shaw Terwilliger <sterwill@svn.abisource.com>
1999-10-23 22:46:40 +0000

    FreeBSD patch by Ming-I Hsieh to better detect GTK and library versions for native packages
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2018 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/pkg/freebsd/scripts/mkplist.sh
+++ src/pkg/freebsd/scripts/mkplist.sh
@@ -3,15 +3,31 @@
 ls -Flags $prefix/AbiSuite/bin
 strip $prefix/AbiSuite/bin/AbiWord_d
 rm -f $prefix/AbiSuite/bin/AbiWord_s
-# list the files now
+
+# check and list the depend package
 echo "@cwd $prefix" > $OUTFILE
-echo "@pkgdep `/bin/ls /var/db/pkg | grep gtk-`" >> $OUTFILE
-echo "@pkgdep `/bin/ls /var/db/pkg | grep png-`" >> $OUTFILE
-echo "@pkgdep `/bin/ls /var/db/pkg | grep ispell-`" >> $OUTFILE
+SHPWD=`pwd`
+cd /var/db/pkg
+if [ `/bin/ls -d gtk-* | wc -l` -eq 1 ]; then
+	echo @pkgdep `/bin/ls -d gtk-*` >> $OUTFILE
+else
+	cat $SHPWD/scripts/fbsdwarn
+fi
+if [ `/bin/ls -d png-* | wc -l` -eq 1 ]; then
+	echo @pkgdep `/bin/ls -d png-*` >> $OUTFILE
+else
+	sed 's/gtk/png/g' $SHPWD/scripts/fbsdwarn
+fi
+
+# list the files in $TARGET(/usr/local/AbiSuite)
 cd $TARGET
 find . -type f | sort | sed 's/^./AbiSuite/g' >> $OUTFILE
+
+# list the symbolic link files in $BINDIR(/usr/local/bin)
 cd $BINDIR
 find . -name "[Aa]bi[Ww]ord*" | sort | sed 's/^./bin/g' >> $OUTFILE
-# find . -name "[Aa]bi[Ww]ord" | sort | sed 's/^./bin/g' >> $OUTFILE
+
+# list the directories ino $TARGET(/usr/local/AbiSuite)
+# They will be remove if you remove AbiSuite package.
 cd $TARGET
 find . -type d | sort -r | sed 's/^./@dirrm AbiSuite/g' >> $OUTFILE
