546439e5b63b626662f6d1b2f009e651d99740d4
Thomas Fletcher <thomasf@qnx.com>
1999-12-13 15:35:43 +0000

    Added qnx/neutrino photon support
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2147 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/config/platforms/nto.mk
@@ -0,0 +1,121 @@
+#! gmake
+
+## AbiSource Program Utilities
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+## 02111-1307, USA.
+
+##############################################################################
+## QNX platform defines ... for Neutrino
+##############################################################################
+
+##################################################################
+##################################################################
+## The main makefile and/or this file requires that OS_ARCH be set
+## to something to describe which chip that this OS is running on.
+## This can be used to change which tools are used and/or which
+## compiler/loader options are used.  It will probably also be used
+## in constructing the name object file destination directory.
+
+#OS_ARCH		:=`uname -m`
+OS_ARCH		:=x86pc
+
+# Compiler defaults should be fine for Intel.
+i386_ARCH_FLAGS		=
+PPC_ARCH_FLAGS		=
+ALPHA_ARCH_FLAGS 	= 
+SPARC_ARCH_FLAGS 	= 
+
+# Define tools (should gcc be cc/qcc ... same for ar?)
+CC		= qcc
+CCC		= qcc
+RANLIB		= touch
+
+# Suffixes
+OBJ_SUFFIX	= o
+LIB_SUFFIX	= a
+DLL_SUFFIX	= so
+AR		= qcc -a $@
+
+# Compiler flags
+# NOTE:  Using both "-Wall" and "-W" turn on every single darn message
+# NOTE:  GCC can throw, and it turns quiet compiles into raging rivers of
+# NOTE:  warnings.  -Wall includes the very useful warnings, -W includes
+# NOTE:  more stylistic warnings.  -pedantic just gets really picky about
+# NOTE:  ANSI things.
+ifdef ABI_OPT_DEBUG
+OPTIMIZER	= -g 
+DEFINES		= -DDEBUG -UNDEBUG
+OBJ_DIR_SFX	= DBG
+else
+# NOTE:  In some instances, GCC can only know about truly unused variables
+# NOTE:  when optimizations are enabled in the compilation.  For this reason,
+# NOTE:  building with optimizations may reveal further warnings not 
+# NOTE:  visible without any -O[number] option.
+OPTIMIZER	= -O2 
+DEFINES		=
+OBJ_DIR_SFX	= OBJ
+endif
+
+# Includes
+OS_INCLUDES	=
+G++INCLUDES	=
+
+# Compiler flags
+PLATFORM_FLAGS	= 
+#PORT_FLAGS	= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE -D__USE_XOPEN_EXTENDED
+PORT_FLAGS	= 
+OS_CFLAGS	= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
+
+# Architecture-specific flags
+ifeq ($(OS_ARCH), x86pc)
+PLATFORM_FLAGS	+= $(i386_ARCH_FLAGS)
+OS_ENDIAN	= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), ppc)
+PLATFORM_FLAGS	+= $(PPC_ARCH_FLAGS)
+OS_ENDIAN	= BigEndian32
+endif
+
+# ...
+
+GLIB_CONFIG		=
+GTK_CONFIG		=
+
+# Shared library flags
+MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
+
+# Which links can this platform create.  Define one or
+# both of these options.
+QNX_CAN_BUILD_DYNAMIC=0
+QNX_CAN_BUILD_STATIC=1
+
+# Compiler options for static and dynamic linkage
+DL_LIBS		=
+STATIC_FLAGS	= -static
+
+ABI_NATIVE	=qnx
+ABI_FE		=QNX
+
+##################################################################
+## ABIPKGDIR defines the directory containing the Makefile to use to
+## build a set of distribution archives (.deb, .rpm, .tgz, .exe, etc)
+## This is relative to $(ABI_ROOT)/src/pkg
+
+ABIPKGDIR	= 
+
+# End of qnx defs
---
+++ src/config/platforms/qnx.mk
@@ -0,0 +1,138 @@
+#! gmake
+
+## AbiSource Program Utilities
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+## 02111-1307, USA.
+
+##############################################################################
+## QNX platform defines
+##############################################################################
+
+##################################################################
+##################################################################
+## The main makefile and/or this file requires that OS_ARCH be set
+## to something to describe which chip that this OS is running on.
+## This can be used to change which tools are used and/or which
+## compiler/loader options are used.  It will probably also be used
+## in constructing the name object file destination directory.
+
+OS_ARCH		:=pc
+#OS_RELEASE	:= `uname -v`
+
+
+# Define architecture-specific flags (L. Kollar, 3 Nov 1998)
+# These are (probably) optional for your platform.
+
+# Compiler defaults should be fine for Intel.
+i386_ARCH_FLAGS		=
+PPC_ARCH_FLAGS		=
+ALPHA_ARCH_FLAGS 	= 
+SPARC_ARCH_FLAGS 	= 
+
+# Define tools
+CC		= cc
+CCC		= cc
+RANLIB	= touch
+
+# Suffixes
+OBJ_SUFFIX	= o
+LIB_SUFFIX	= a
+DLL_SUFFIX	= so
+AR		= cc -a $@
+
+# Compiler flags
+# NOTE:  Using both "-Wall" and "-W" turn on every single darn message
+# NOTE:  GCC can throw, and it turns quiet compiles into raging rivers of
+# NOTE:  warnings.  -Wall includes the very useful warnings, -W includes
+# NOTE:  more stylistic warnings.  -pedantic just gets really picky about
+# NOTE:  ANSI things.
+ifdef ABI_OPT_DEBUG
+OPTIMIZER	= -g 
+DEFINES		= -DDEBUG -UNDEBUG
+OBJ_DIR_SFX	= DBG
+else
+# NOTE:  In some instances, GCC can only know about truly unused variables
+# NOTE:  when optimizations are enabled in the compilation.  For this reason,
+# NOTE:  building with optimizations may reveal further warnings not 
+# NOTE:  visible without any -O[number] option.
+OPTIMIZER	= -O2 
+DEFINES		=
+OBJ_DIR_SFX	= OBJ
+endif
+
+# Includes
+OS_INCLUDES		=
+G++INCLUDES		=
+
+# Compiler flags
+PLATFORM_FLAGS	= 
+PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE -D__USE_XOPEN_EXTENDED
+OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
+
+# Architecture-specific flags
+ifeq ($(OS_ARCH), i386)
+PLATFORM_FLAGS	+= $(i386_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), ppc)
+PLATFORM_FLAGS	+= $(PPC_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+ifeq ($(OS_ARCH), alpha)
+PLATFORM_FLAGS	+= $(ALPHA_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), sparc)
+PLATFORM_FLAGS		+= $(SPARC_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+# Endian for M68K from Roman Hodek <Roman.Hodek@informatik.uni-erlangen.de>
+ifeq ($(OS_ARCH), m68k)
+PLATFORM_FLAGS		+= $(M68K_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+GLIB_CONFIG		=
+GTK_CONFIG		=
+
+# Shared library flags
+MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
+
+# Which links can this platform create.  Define one or
+# both of these options.
+QNX_CAN_BUILD_DYNAMIC=0
+QNX_CAN_BUILD_STATIC=1
+
+# Compiler options for static and dynamic linkage
+DL_LIBS			=
+STATIC_FLAGS	= 
+
+ABI_NATIVE	=qnx
+ABI_FE		=QNX
+
+##################################################################
+## ABIPKGDIR defines the directory containing the Makefile to use to
+## build a set of distribution archives (.deb, .rpm, .tgz, .exe, etc)
+## This is relative to $(ABI_ROOT)/src/pkg
+
+ABIPKGDIR	= 
+
+# End of qnx defs
