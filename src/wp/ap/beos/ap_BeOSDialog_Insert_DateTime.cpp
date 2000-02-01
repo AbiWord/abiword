f3687bf11ee444de01f14828c029ba570ea30938
Shaw Terwilliger <sterwill@svn.abisource.com>
2000-02-01 09:27:03 +0000

    Added stubs for insert date/time dialog
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2281 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/beos/ap_BeOSDialog_Insert_DateTime.cpp
@@ -0,0 +1,56 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "ut_string.h"
+#include "ut_assert.h"
+#include "ut_debugmsg.h"
+
+#include "xap_App.h"
+#include "xap_BeOSApp.h"
+#include "xap_BeOSFrame.h"
+
+#include "ap_Strings.h"
+#include "ap_Dialog_Id.h"
+#include "ap_Dialog_Insert_DateTime.h"
+#include "ap_BeOSDialog_Insert_DateTime.h"
+
+#include "ut_Rehydrate.h"
+
+XAP_Dialog * AP_BeOSDialog_Insert_DateTime::static_constructor(XAP_DialogFactory * pFactory,
+													 XAP_Dialog_Id id)
+{
+	AP_BeOSDialog_Insert_DateTime * p = new AP_BeOSDialog_Insert_DateTime(pFactory,id);
+	return p;
+}
+
+AP_BeOSDialog_Insert_DateTime::AP_BeOSDialog_Insert_DateTime(XAP_DialogFactory * pDlgFactory,
+					 XAP_Dialog_Id id) 
+		   : AP_Dialog_Insert_DateTime(pDlgFactory,id) { 
+} 
+
+AP_BeOSDialog_Insert_DateTime::~AP_BeOSDialog_Insert_DateTime(void) {
+}
+
+/*****************************************************************/
+
+void AP_BeOSDialog_Insert_DateTime::runModal(XAP_Frame * pFrame)
+{
+	UT_ASSERT(UT_NOT_IMPLEMENTED);
+}
+
