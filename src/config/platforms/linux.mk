33c25994f60fe396f62fd3c9c9ad864d0ec56966
Shaw Terwilliger <sterwill@svn.abisource.com>
2000-02-29 15:06:29 +0000

    Endian patch for ARM from Jim Studt <jim@federated.com>
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2377 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/linux.mk
+++ src/config/platforms/linux.mk
@@ -99,6 +99,12 @@ PLATFORM_FLAGS		+= $(SPARC_ARCH_FLAGS)
 OS_ENDIAN		= BigEndian32
 endif
 
+# Endian for ARM from Jim Studt <jim@federated.com>
+ifeq ($(OS_ARCH), arm)
+PLATFORM_FLAGS		+= $(ARM_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
 # Endian for M68K from Roman Hodek <Roman.Hodek@informatik.uni-erlangen.de>
 ifeq ($(OS_ARCH), m68k)
 PLATFORM_FLAGS		+= $(M68K_ARCH_FLAGS)
