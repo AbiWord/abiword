7574df705167d710e8a75a193d22d186a6ea9bf5
Mike Nordell <tamlin@algonet.se>
2000-05-12 05:40:53 +0000

    Killed compiler warnings
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2612 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fp_PageSize.cpp
+++ src/text/fmt/xp/fp_PageSize.cpp
@@ -19,32 +19,32 @@ struct private_pagesize_sizes
 const private_pagesize_sizes
 	pagesizes[fp_PageSize::_last_predefined_pagesize_dont_use_] =
 {
-	 841.0, 1189.0, fp_PageSize::mm,		// A0
-	 594.0,  841.0, fp_PageSize::mm,		// A1
-	 420.0,  594.0, fp_PageSize::mm,		// A2
-	 297.0,  420.0, fp_PageSize::mm,		// A3
-	 210.0,  297.0, fp_PageSize::mm,		// A4
-	 148.0,  210.0, fp_PageSize::mm,		// A5
-	 105.0,  148.0, fp_PageSize::mm,		// A6
-	1000.0, 1414.0, fp_PageSize::mm,		// B0
-	 707.0, 1000.0, fp_PageSize::mm,		// B1
-	 500.0,  707.0, fp_PageSize::mm,		// B2
-	 353.0,  500.0, fp_PageSize::mm,		// B3
-	 250.0,  353.0, fp_PageSize::mm,		// B4
-	 176.0,  250.0, fp_PageSize::mm,		// B5
-	 125.0,  176.0, fp_PageSize::mm,		// B6
-	   8.5,   14.0, fp_PageSize::inch,		// Legal
-	   8.5,   13.0, fp_PageSize::inch,		// Folio
-	   8.5,   11.0, fp_PageSize::inch,		// Letter
+	{ 841.0, 1189.0, fp_PageSize::mm	},		// A0
+	{ 594.0,  841.0, fp_PageSize::mm	},		// A1
+	{ 420.0,  594.0, fp_PageSize::mm	},		// A2
+	{ 297.0,  420.0, fp_PageSize::mm	},		// A3
+	{ 210.0,  297.0, fp_PageSize::mm	},		// A4
+	{ 148.0,  210.0, fp_PageSize::mm	},		// A5
+	{ 105.0,  148.0, fp_PageSize::mm	},		// A6
+	{1000.0, 1414.0, fp_PageSize::mm	},		// B0
+	{ 707.0, 1000.0, fp_PageSize::mm	},		// B1
+	{ 500.0,  707.0, fp_PageSize::mm	},		// B2
+	{ 353.0,  500.0, fp_PageSize::mm	},		// B3
+	{ 250.0,  353.0, fp_PageSize::mm	},		// B4
+	{ 176.0,  250.0, fp_PageSize::mm	},		// B5
+	{ 125.0,  176.0, fp_PageSize::mm	},		// B6
+	{   8.5,   14.0, fp_PageSize::inch	},		// Legal
+	{   8.5,   13.0, fp_PageSize::inch	},		// Folio
+	{   8.5,   11.0, fp_PageSize::inch	}	// Letter
 };
 
 const double ScaleFactors[fp_PageSize::_last_predefined_unit_dont_use_] =
 {
-	1.0,							// mm
-	10.0,							// cm
-	25.4,							// inch
-	25.4 / UT_PAPER_UNITS_PER_INCH,	// PaperUnit
-	25.4 / UT_LAYOUT_UNITS			// LayoutUnit
+	{ 1.0								},		// mm
+	{ 10.0								},		// cm
+	{ 25.4								},		// inch
+	{ 25.4 / UT_PAPER_UNITS_PER_INCH	},		// PaperUnit
+	{ 25.4 / UT_LAYOUT_UNITS			}		// LayoutUnit
 };
 
 fp_PageSize::fp_PageSize(Predefined preDef)
