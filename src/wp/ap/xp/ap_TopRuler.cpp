5d55583e6afb793ab50e78ad4d05cf1b8aafc5eb
Aaron Lehmann <aaronl@vitelus.com>
2000-06-20 07:08:02 +0000

    I'm glad I found this before Paul Egli did!
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2787 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TopRuler.cpp
+++ src/wp/ap/xp/ap_TopRuler.cpp
@@ -1217,13 +1217,12 @@ void AP_TopRuler::mousePress(EV_EditModifierState /* ems */, EV_EditMouseButton 
 
 void AP_TopRuler::mouseRelease(EV_EditModifierState /* ems */, EV_EditMouseButton /* emb */, UT_sint32 x, UT_sint32 y)
 {
+	m_bValidMouseClick = UT_FALSE;
 	if (!m_bValidMouseClick || (m_bEventIgnored && m_draggingWhat != DW_TABSTOP))
 	{
 		m_draggingWhat = DW_NOTHING;
 		return;
 	}
-
-	m_bValidMouseClick = UT_FALSE;
 
 	// if they drag vertically off the ruler, we ignore the whole thing.
 
