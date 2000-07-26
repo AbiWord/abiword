d274b7dc5cf52d1056917aaeb5e613b2e08b4ab4
Martin Sevior <martines@unimelb.edu.au>
2000-07-26 01:38:40 +0000

    Daily lists bugfix. This fix stops the grey line appearing where the list field was after stopping a list.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@2951 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fl_BlockLayout.cpp
+++ src/text/fmt/xp/fl_BlockLayout.cpp
@@ -530,8 +530,12 @@ void fl_BlockLayout::clearScreen(GR_Graphics* /* pG */)
 	fp_Line* pLine = m_pFirstLine;
 	while (pLine)
 	{
-		pLine->clearScreen();
- 
+		fp_Run * pRun = pLine->getFirstRun();
+		while(pRun)
+		{
+		        pRun->clearScreen();
+		        pRun = pRun->getNext();
+		}
 		pLine = pLine->getNext();
 	}
 }
