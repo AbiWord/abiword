1f375dde4ef1cc696b5c2f3195dbe1b3e4b7738d
Hubert Figuiere <hub@figuiere.net>
2000-09-22 13:25:28 +0000

    Added Files:  	xap_MacDlg_MessageBox.r xap_MacResources.r  	xap_Mac_TB_CFactory.h
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3143 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/xap/mac/xap_MacDlg_MessageBox.r
@@ -0,0 +1,100 @@
+/* AbiSource Application Framework
+ * Copyright (C) 2000 Hubert Figuiere <hfiguiere@teaser.fr>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+/*
+	This file is resource file for AbiWord for MacOS
+
+	$Id$
+*/
+
+#include <Controls.r>
+#include <Types.r>
+
+#define __INCLUDING_REZ__
+#include "ap_Mac_ResID.h"
+
+
+resource 'ALRT' (RES_ALRT_OK) {
+	{ 0, 0, 100, 250 },
+	RES_DITL_ALRT_OK,
+	{
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent
+	},
+	centerParentWindow
+};
+
+
+
+resource 'ALRT' (RES_ALRT_OKCANCEL) {
+	{ 0, 0, 100, 250 },
+	RES_ALRT_OKCANCEL,
+	{
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent
+	},
+	centerParentWindow
+};
+
+
+resource 'ALRT' (RES_ALRT_YESNO) {
+	{ 0, 0, 100, 250 },
+	RES_ALRT_YESNO,
+	{
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent
+	},
+	centerParentWindow
+};
+
+
+resource 'ALRT' (RES_ALRT_YESNOCANCEL) {
+	{ 0, 0, 100, 250 },
+	RES_ALRT_YESNOCANCEL,
+	{
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent,
+		OK, visible, silent
+	},
+	centerParentWindow
+};
+
+
+resource 'DITL' (RES_DITL_ALRT_OK) {
+	{
+		{64, 189, 87, 237},
+		Button {
+			enabled,
+			"OK"
+		},
+		{13, 78, 51, 237},
+		StaticText {
+			disabled,
+			""
+		}
+	}
+};
+
---
+++ src/af/xap/mac/xap_MacResources.r
@@ -0,0 +1,93 @@
+/* AbiSource Application Framework
+ * Copyright (C) 2000 Hubert Figuiere <hfiguiere@teaser.fr>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+/*
+	This file is resource file for AbiWord for MacOS
+
+	$Id$
+*/
+
+
+#include <Types.r>
+#include <Controls.r>
+#include <Balloons.r>
+
+#define __INCLUDING_REZ__
+#include "ap_Mac_ResID.h"
+
+#define APPLICATION_SIGNATURE	'AbiW'
+#define MAJOR_VERSION			0
+#define MINOR_VERSION			7
+#define RELEASE_KIND 			beta
+#define PATCH_LEVEL				11
+#define CURRENT_VERSION_TEXT	"0.7.11"
+
+#define APP_NAME 				"AbiWord"
+
+type APPLICATION_SIGNATURE {
+	pstring;
+};
+
+resource 'vers' (1) {
+	MAJOR_VERSION,
+	MINOR_VERSION,
+	RELEASE_KIND,
+	PATCH_LEVEL,
+	1,
+	APP_NAME" "CURRENT_VERSION_TEXT,
+	APP_NAME" "CURRENT_VERSION_TEXT" - ©1998-2000 AbiSource, Inc. and other contributors."
+};
+
+
+resource 'vers' (2) {
+	MAJOR_VERSION,
+	MINOR_VERSION,
+	RELEASE_KIND,
+	PATCH_LEVEL,
+	1,
+	"",
+	APP_NAME" - ©1998-2000 AbiSource, Inc. and other contributors."
+};
+
+
+resource 'STR ' (HFDR_STR_ID) {
+	APP_NAME" "CURRENT_VERSION_TEXT
+	" - ©1998-2000 AbiSource, Inc. and other contributors.\n\n"
+	"This application is a free word processor."
+};
+
+
+resource APPLICATION_SIGNATURE (128) {
+	APP_NAME" "CURRENT_VERSION_TEXT" - ©1998-2000 AbiSource, Inc. and other contributors."
+};
+
+
+resource 'hfdr' (-5696) {
+	2,
+	0,
+	0,
+	0,
+	{	/* array HFdrArray: 1 elements */
+		/* [1] */
+		HMSTRResItem {
+			HFDR_STR_ID
+		}
+	}
+};
+
