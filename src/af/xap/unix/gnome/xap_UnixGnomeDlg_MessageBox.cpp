54dc800249dda0776d16750b08f2a8a51b77ea76
Dom Lachowicz <domlachowicz@gmail.com>
2000-11-22 04:15:27 +0000

    Changed gnome message boxes to be more gnomeish (added icons).
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3441 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/unix/gnome/xap_UnixGnomeDlg_MessageBox.cpp
+++ src/af/xap/unix/gnome/xap_UnixGnomeDlg_MessageBox.cpp
@@ -64,14 +64,15 @@ void XAP_UnixGnomeDialog_MessageBox::runModal(XAP_Frame * pFrame)
 	UT_ASSERT(pApp);
 
 	const char * szCaption = pApp->getApplicationTitleForTitleBar();
-	
+
 	switch (m_buttons)
 	{
 	case b_O:
-	    // OK   TODO: Me queda por ver si gnome reconoce automaticamente los _ como aceleradores
-		dialog_window = gnome_dialog_new (szCaption,
-										  GNOME_STOCK_BUTTON_OK,
-										  NULL);
+		// we never put up an ok dialog if it's not an error condition
+	        // TODO: make a b_error so we can make the distinction between "OK" Ok and "Error" Ok
+		dialog_window = gnome_message_box_new(m_szMessage, GNOME_MESSAGE_BOX_ERROR, 
+						      GNOME_STOCK_BUTTON_OK, NULL);
+
 		ok_button = GTK_WIDGET (g_list_last (GNOME_DIALOG (dialog_window)->buttons)->data);
 		gtk_widget_grab_focus (ok_button);
 		answer[0] = XAP_Dialog_MessageBox::a_OK;
@@ -79,11 +80,11 @@ void XAP_UnixGnomeDialog_MessageBox::runModal(XAP_Frame * pFrame)
 		break;
 
 	case b_OC:
-		// OK - CANCEL
-		dialog_window = gnome_dialog_new (szCaption,
-										  GNOME_STOCK_BUTTON_OK,
-										  GNOME_STOCK_BUTTON_CANCEL,
-										  NULL);
+	        // OK - Cancel
+	        // most certainly a question
+		dialog_window = gnome_message_box_new(m_szMessage, GNOME_MESSAGE_BOX_QUESTION,
+						      GNOME_STOCK_BUTTON_OK, GNOME_STOCK_BUTTON_CANCEL,
+						      NULL);
 
 		ok_button = GTK_WIDGET (g_list_first (GNOME_DIALOG (dialog_window)->buttons)->data);
 		cancel_button = GTK_WIDGET (g_list_last (GNOME_DIALOG (dialog_window)->buttons)->data);
@@ -101,10 +102,10 @@ void XAP_UnixGnomeDialog_MessageBox::runModal(XAP_Frame * pFrame)
 
 	case b_YN:
 		// YES - NO
-		dialog_window = gnome_dialog_new (szCaption,
-										  GNOME_STOCK_BUTTON_YES,
-										  GNOME_STOCK_BUTTON_NO,
-										  NULL);
+	        // most certainly a question
+	        dialog_window = gnome_message_box_new(m_szMessage, GNOME_MESSAGE_BOX_QUESTION,
+						      GNOME_STOCK_BUTTON_YES, GNOME_STOCK_BUTTON_NO,
+						      NULL);
 
 		yes_button = GTK_WIDGET (g_list_first (GNOME_DIALOG (dialog_window)->buttons)->data);
 		no_button = GTK_WIDGET (g_list_last (GNOME_DIALOG (dialog_window)->buttons)->data);
@@ -122,11 +123,12 @@ void XAP_UnixGnomeDialog_MessageBox::runModal(XAP_Frame * pFrame)
 
 	case b_YNC:
 		// YES - NO - CANCEL
-		dialog_window = gnome_dialog_new (szCaption,
-										  GNOME_STOCK_BUTTON_YES,
-										  GNOME_STOCK_BUTTON_NO,
-										  GNOME_STOCK_BUTTON_CANCEL,
-										  NULL);
+        	// this is used for saving files. the message: Unsaved files. Warning looks good here.
+	        dialog_window = gnome_message_box_new(m_szMessage, GNOME_MESSAGE_BOX_WARNING,
+						      GNOME_STOCK_BUTTON_YES,
+						      GNOME_STOCK_BUTTON_NO,
+						      GNOME_STOCK_BUTTON_CANCEL,
+						      NULL);
 
 		yes_button = GTK_WIDGET (g_list_first (GNOME_DIALOG (dialog_window)->buttons)->data);
 		no_button = GTK_WIDGET (g_list_next (g_list_first (GNOME_DIALOG (dialog_window)->buttons))->data);
@@ -151,22 +153,14 @@ void XAP_UnixGnomeDialog_MessageBox::runModal(XAP_Frame * pFrame)
 		UT_ASSERT(UT_SHOULD_NOT_HAPPEN);
 	}
 
-	// Add our label string to the dialog in the message area
-	GtkWidget * label = gtk_label_new (m_szMessage);
-	gtk_misc_set_padding (GTK_MISC (label), 10, 10);
-	gtk_box_pack_start (GTK_BOX (GNOME_DIALOG (dialog_window)->vbox),
-						label, TRUE, TRUE, 0);
-	gtk_widget_show (label);
-
 	// get top level window and it's GtkWidget *
 	XAP_UnixFrame * frame = static_cast<XAP_UnixFrame *>(pFrame);
 	UT_ASSERT(frame);
 	GtkWidget * parent = frame->getTopLevelWindow();
 	UT_ASSERT(parent);
 
-	// center it
-    centerDialog(parent, dialog_window);
-	gtk_window_set_transient_for(GTK_WINDOW(dialog_window), GTK_WINDOW(parent));
+	gtk_window_set_title(GTK_WINDOW(dialog_window), szCaption);
+	centerDialog(parent, dialog_window);
 
 	m_answer = answer[gnome_dialog_run_and_close (GNOME_DIALOG (dialog_window)) + 1];
 
