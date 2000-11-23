a04ebc80721cd48eee65e33433d99addb7809e55
Mike Nordell <tamlin@algonet.se>
2000-11-23 22:05:18 +0000

    MSVC compiler warning
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3455 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/win/ap_Win32Dialog_Columns.cpp
+++ src/wp/ap/win/ap_Win32Dialog_Columns.cpp
@@ -44,6 +44,10 @@ XAP_Dialog * AP_Win32Dialog_Columns::static_constructor(XAP_DialogFactory * pFac
 	return p;
 }
 
+#ifdef _MSC_VER	// MSVC++ warns about using 'this' in initializer list.
+#pragma warning(disable: 4355)
+#endif
+
 AP_Win32Dialog_Columns::AP_Win32Dialog_Columns(XAP_DialogFactory * pDlgFactory,
 											   XAP_Dialog_Id id)
 	: AP_Dialog_Columns(pDlgFactory,id), _win32Dialog(this)
