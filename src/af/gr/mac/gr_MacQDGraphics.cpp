b31818ab2ec72050784f6bd017976ef902cc9c5e
Hubert Figuiere <hub@figuiere.net>
2000-11-24 15:57:57 +0000

    Added Files:  	src/af/gr/mac/gr_MacQDGraphics.cpp  	src/af/gr/mac/gr_MacQDGraphics.h
    implementation for QuickDraw.
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3476 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/gr/mac/gr_MacQDGraphics.cpp
@@ -0,0 +1,76 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * Copyright (C) 1999 John Brewer DBA Jera Design
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+
+#include "gr_MacGraphics.h"
+
+#include "ut_debugmsg.h"
+#include "ut_assert.h"
+
+
+
+MacGraphics::MacGraphics(GrafPtr grafPort, XAP_App * app)
+      : GR_Graphics ()
+{
+      UT_ASSERT (grafPort != NULL);
+
+      m_GrafPort = grafPort;
+}
+
+MacGraphics::~MacGraphics ()
+{
+
+}
+
+
+/*
+void MacGraphics::drawChars(const UT_UCSChar* pChars, 
+		int iCharOffset, int iLength, UT_sint32 xoff, UT_sint32 yoff)
+void setFont(GR_Font* pFont);
+
+UT_uint32 getFontAscent();
+UT_uint32 getFontDescent();
+UT_uint32 getFontHeight();
+*/
+
+// Code below borrowed to gr_BeOSGraphics.cpp... FIXIT
+//////////////////////////////////////////////////////////////////
+// This is a static method in the GR_Font base class implemented
+// in platform code.
+//////////////////////////////////////////////////////////////////
+void GR_Font::s_getGenericFontProperties(const char * szFontName,
+										 FontFamilyEnum * pff,
+										 FontPitchEnum * pfp,
+										 UT_Bool * pbTrueType)
+{
+	// describe in generic terms the named font.
+
+	// Note: most of the unix font handling code is in abi/src/af/xap/unix
+	// Note: rather than in the graphics class.  i'm not sure this matters,
+	// Note: but it is just different....
+
+	// TODO add code to map the given font name into one of the
+	// TODO enums in GR_Font and set *pff and *pft.
+
+	*pff = FF_Unknown;
+	*pfp = FP_Unknown;
+	*pbTrueType = UT_TRUE;
+}
+
