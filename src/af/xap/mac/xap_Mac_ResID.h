2621e12e1d0fd389a1c122222039581d39fa468a
Hubert Figuiere <hub@figuiere.net>
2001-01-22 19:50:34 +0000

    First Checked In.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3695 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/xap/mac/xap_Mac_ResID.h
@@ -0,0 +1,59 @@
+/* AbiSource Application Framework
+ * Copyright (C) 1998 AbiSource, Inc.
+ * Copyright (C) 2000-2001 Hubert Figuiere <hfiguiere@teaser.fr>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+ 
+ 
+#ifndef __XAP_MAC_RESID_H__
+#define __XAP_MAC_RESID_H__
+
+/* 'ALRT' resource IDs */
+#define RES_ALRT_OK				128
+#define RES_ALRT_OKCANCEL		129
+#define RES_ALRT_YESNO			130
+#define RES_ALRT_YESNOCANCEL	131
+
+#define RES_ALRT_ASSERT			1000
+
+/* ID of first AP ALRT resource */
+#define AP_FIRST_ALRT			2000
+
+/* 'DITL' resource IDs */
+#define RES_DITL_ALRT_OK 		RES_ALRT_OK
+#define RES_DITL_ALRT_OKCANCEL	RES_ALRT_OKCANCEL
+#define RES_DITL_ALRT_YESNO		RES_ALRT_YESNO
+#define RES_DITL_ALRT_YESNOCANCEL	RES_ALRT_YESNOCANCEL
+
+#define RES_DITL_ALRT_ASSERT	RES_ALRT_ASSERT
+
+/* ID of first AP DITL resource */
+#define AP_FIRST_DITL			2000
+
+/* 'STR ' resource IDs */
+#define HFDR_STR_ID				128
+
+/* ID of first AP STR resource */
+#define AP_FIRST_STR			2000
+
+
+#ifndef __INCLUDING_REZ__
+/* section for C code */
+
+#endif
+
+#endif
