d39e316c5c6cdf680cf05a7ec25cc47e8c730823
Hubert Figuiere <hub@figuiere.net>
2001-01-29 23:09:21 +0000

    Changed class name from ap_UnixDialog_PageSetup to ap_MacDialog...Initial commit typo.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3737 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/mac/ap_MacDialog_PageSetup.h
+++ src/wp/ap/mac/ap_MacDialog_PageSetup.h
@@ -22,7 +22,7 @@
 
 #include "ap_Dialog_PageSetup.h"
 
-class AP_UnixDialog_PageSetup : public AP_Dialog_PageSetup
+class AP_MacDialog_PageSetup : public AP_Dialog_PageSetup
 {
 public:
 	AP_MacDialog_PageSetup (XAP_DialogFactory *pDlgFactory, XAP_Dialog_Id id);
