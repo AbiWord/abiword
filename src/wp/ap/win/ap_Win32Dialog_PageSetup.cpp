2a209898cec63e7bf46559b04bf27650fbdb9ad9
Mike Nordell <tamlin@algonet.se>
2001-02-01 01:17:23 +0000

    Basic maintenance
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3766 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/win/ap_Win32Dialog_PageSetup.cpp
+++ src/wp/ap/win/ap_Win32Dialog_PageSetup.cpp
@@ -1,5 +1,5 @@
 /* AbiWord
- * Copyright (C) 2001 AbiSource, Inc.
+ * Copyright (C) 2001 Mike Nordell
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,13 @@
 #include <commdlg.h>
 
 
+// fwd. decl. for documentation purposes.
+static const POINT	_getPaperSizeFromDlg(const AP_Dialog_PageSetup& dlg);
+static const RECT	_getMarginRectFromDlg(const AP_Dialog_PageSetup& dlg);
+static void			_initDevModeFromDlg(HGLOBAL hDevMode, const AP_Dialog_PageSetup& dlg);
+static void			_setDlgFromPSD(AP_Dialog_PageSetup& dlg, const PAGESETUPDLG& psd);
+
+
 AP_Win32Dialog_PageSetup::AP_Win32Dialog_PageSetup(	XAP_DialogFactory* pDlgFactory,
 													XAP_Dialog_Id id)
 :	AP_Dialog_PageSetup (pDlgFactory, id)
@@ -33,10 +40,126 @@ AP_Win32Dialog_PageSetup::~AP_Win32Dialog_PageSetup()
 {
 }
 
-XAP_Dialog * AP_Win32Dialog_PageSetup::static_constructor(XAP_DialogFactory* pDlgFactory, XAP_Dialog_Id id)
+XAP_Dialog* AP_Win32Dialog_PageSetup::static_constructor(XAP_DialogFactory* pDlgFactory, XAP_Dialog_Id id)
 {
 	AP_Win32Dialog_PageSetup* dlg = new AP_Win32Dialog_PageSetup (pDlgFactory, id);
 	return dlg;
+}
+
+
+//
+// Free standing functions that have no business in the class' interface since
+// these *can* be free-standing. Positive effect: less compile time
+// dependencies and a smaller class interface. But, take note, following
+// the "Interface Priciple" these *are* logically part of the
+// AP_Dialog_PageSetup interface.
+//
+
+static const POINT _getPaperSizeFromDlg(const AP_Dialog_PageSetup& dlg)
+{
+	UT_Bool bPageIsInches	= dlg.getPageUnits() == fp_PageSize::inch;
+	float pageScaleFactor	= bPageIsInches ? 1000.0f : 100.0f;
+	fp_PageSize::Unit pageUnit   = bPageIsInches ? fp_PageSize::inch : fp_PageSize::mm;
+
+	const POINT ptPaperSize =
+	{
+		(LONG)(dlg.getPageSize().Width(pageUnit)	* pageScaleFactor),
+		(LONG)(dlg.getPageSize().Height(pageUnit)	* pageScaleFactor)
+	};
+
+	return ptPaperSize;
+}
+
+static const RECT _getMarginRectFromDlg(const AP_Dialog_PageSetup& dlg)
+{
+	UT_Bool bMarginIsInches	= dlg.getMarginUnits()	== fp_PageSize::inch;
+	float marginScaleFactor	= bMarginIsInches ? 1000.0f : 100.0f;
+
+	const RECT rcMargin =
+	{
+		(LONG)(dlg.getMarginLeft()		* marginScaleFactor),
+		(LONG)(dlg.getMarginTop()		* marginScaleFactor),
+		(LONG)(dlg.getMarginRight()		* marginScaleFactor),
+		(LONG)(dlg.getMarginBottom()	* marginScaleFactor)
+	};
+
+	return rcMargin;
+}
+
+static void _initDevModeFromDlg(HGLOBAL hDevMode, const AP_Dialog_PageSetup& dlg)
+{
+	if (!hDevMode)
+	{
+		return;
+	}
+
+	DEVMODE* pDM = (DEVMODE*)GlobalLock(hDevMode);
+	UT_ASSERT(pDM);
+	if (pDM->dmFields & DM_ORIENTATION)
+	{
+		pDM->dmOrientation = dlg.getPageOrientation() == AP_Dialog_PageSetup::PORTRAIT ?
+								DMORIENT_PORTRAIT : DMORIENT_LANDSCAPE;
+	}
+	if (pDM->dmFields & DM_SCALE)
+	{
+		pDM->dmScale = (short)dlg.getPageScale();
+	}
+
+	pDM->dmFields &= ~(DM_PAPERSIZE | DM_FORMNAME);
+	pDM->dmPaperSize = 0;
+	pDM->dmFormName[0] = '\0';
+	const char* pszName = dlg.getPageSize().getPredefinedName();
+	if (pszName)
+	{
+		pDM->dmFields |= DM_FORMNAME;
+		lstrcpyn((char*)pDM->dmFormName, pszName, CCHFORMNAME);
+	}
+
+	// The following two are in tenths of mm
+	pDM->dmPaperLength = (short)(dlg.getPageSize().Height(fp_PageSize::mm) * 10.0f);
+	pDM->dmPaperWidth  = (short)(dlg.getPageSize().Width(fp_PageSize::mm) * 10.0f);
+
+	GlobalUnlock(hDevMode);
+}
+
+static void _setDlgFromPSD(AP_Dialog_PageSetup& dlg, const PAGESETUPDLG& psd)
+{
+	const UT_Bool bPageIsInches = (psd.Flags & PSD_INTHOUSANDTHSOFINCHES) != 0;
+	const float pageScaleFactor	= bPageIsInches ? 1000.0f : 100.0f;
+	dlg.setPageUnits(bPageIsInches ? fp_PageSize::inch : fp_PageSize::mm);
+	dlg.setMarginUnits(dlg.getPageUnits());
+	dlg.setPageSize(fp_PageSize(float(psd.ptPaperSize.x) / pageScaleFactor,
+								float(psd.ptPaperSize.y) / pageScaleFactor,
+								dlg.getPageUnits()));
+
+	dlg.setMarginLeft(psd.rtMargin.left		/ pageScaleFactor);
+	dlg.setMarginTop(psd.rtMargin.top		/ pageScaleFactor);
+	dlg.setMarginRight(psd.rtMargin.right	/ pageScaleFactor);
+	dlg.setMarginBottom(psd.rtMargin.bottom	/ pageScaleFactor);
+
+	if (psd.hDevMode)
+	{
+		DEVMODE* pDM = (DEVMODE*)GlobalLock(psd.hDevMode);
+		UT_ASSERT(pDM);
+		if (pDM->dmFields & DM_ORIENTATION)
+		{
+			dlg.setPageOrientation(pDM->dmOrientation == DMORIENT_PORTRAIT ?
+														dlg.PORTRAIT : dlg.LANDSCAPE);
+		}
+		if (pDM->dmFields & DM_SCALE)
+		{
+			dlg.setPageScale((int)pDM->dmScale);
+		}
+		if (pDM->dmFields & DM_FORMNAME &&
+			pDM->dmFormName[0] &&
+			fp_PageSize::IsPredefinedName((const char*)pDM->dmFormName))
+		{
+			// override PAGESETUPDLG value
+			dlg.setPageSize(fp_PageSize((const char*)pDM->dmFormName));
+		}
+
+		GlobalUnlock(psd.hDevMode);
+	}
 }
 
 void AP_Win32Dialog_PageSetup::runModal(XAP_Frame *pFrame)
@@ -51,54 +174,11 @@ void AP_Win32Dialog_PageSetup::runModal(XAP_Frame *pFrame)
 
 	// Windows only supports mm or inches, why we only check for DIM_IN
 	UT_Bool bPageIsInches	= getPageUnits()	== fp_PageSize::inch;
-	UT_Bool bMarginIsInches	= getMarginUnits()	== fp_PageSize::inch;
-	float pageScaleFactor	= bPageIsInches   ? 1000.0f : 100.0f;
-	float marginScaleFactor	= bMarginIsInches ? 1000.0f : 100.0f;
-	fp_PageSize::Unit pageUnit   = bPageIsInches ? fp_PageSize::inch : fp_PageSize::mm;
 
-	const POINT ptPaperSize =
-	{
-		(LONG)(getPageSize().Width(pageUnit)	* pageScaleFactor),
-		(LONG)(getPageSize().Height(pageUnit)	* pageScaleFactor)
-	};
-	const RECT rcMargin =
-	{
-		(LONG)(getMarginLeft()		* marginScaleFactor),
-		(LONG)(getMarginTop()		* marginScaleFactor),
-		(LONG)(getMarginRight()		* marginScaleFactor),
-		(LONG)(getMarginBottom()	* marginScaleFactor)
-	};
+	const POINT ptPaperSize	= _getPaperSizeFromDlg(*this);
+	const RECT rcMargin		= _getMarginRectFromDlg(*this);
 
-	if (psd.hDevMode)
-	{
-		DEVMODE* pDM = (DEVMODE*)GlobalLock(psd.hDevMode);
-		UT_ASSERT(pDM);
-		if (pDM->dmFields & DM_ORIENTATION)
-		{
-			pDM->dmOrientation = getPageOrientation() == PORTRAIT ?
-									DMORIENT_PORTRAIT : DMORIENT_LANDSCAPE;
-		}
-		if (pDM->dmFields & DM_SCALE)
-		{
-			pDM->dmScale = (short)getPageScale();
-		}
-
-		pDM->dmFields &= ~(DM_PAPERSIZE | DM_FORMNAME);
-		pDM->dmPaperSize = 0;
-		pDM->dmFormName[0] = '\0';
-		const char* pszName = getPageSize().getPredefinedName();
-		if (pszName)
-		{
-			pDM->dmFields |= DM_FORMNAME;
-			lstrcpyn((char*)pDM->dmFormName, pszName, CCHFORMNAME);
-		}
-
-		// The following two are in tenths of mm
-		pDM->dmPaperLength = (short)(getPageSize().Height(fp_PageSize::mm) * 10.0f);
-		pDM->dmPaperWidth  = (short)(getPageSize().Width(fp_PageSize::mm) * 10.0f);
-
-		GlobalUnlock(psd.hDevMode);
-	}
+	_initDevModeFromDlg(psd.hDevMode, *this);
 
 	psd.hwndOwner				= pWin32Frame->getTopLevelWindow();
 	psd.Flags					= (bPageIsInches ? PSD_INTHOUSANDTHSOFINCHES : PSD_INHUNDREDTHSOFMILLIMETERS) |
@@ -113,58 +193,19 @@ void AP_Win32Dialog_PageSetup::runModal(XAP_Frame *pFrame)
 	psd.lpPageSetupTemplateName	= 0;
 	psd.hPageSetupTemplate		= 0;
 
-	// Display the Page Setup dialog
-	if (!PageSetupDlg(&psd))
+	const BOOL bDlgOK = PageSetupDlg(&psd);	// Display the Page Setup dialog
+	if (bDlgOK)
 	{
-		// user don't want to make changes
-		setAnswer(a_CANCEL);
-		if (psd.hDevMode)
-		{
-			GlobalFree(psd.hDevMode);
-		}
-		if (psd.hDevNames)
-		{
-			GlobalFree(psd.hDevNames);
-		}
-		return;
+		_setDlgFromPSD(*this, psd);	// Set data from the PAGESETUPDLG
+		setAnswer(a_OK);
 	}
-
-	// Set data from the PAGESETUPDLG
-	bPageIsInches = (psd.Flags & PSD_INTHOUSANDTHSOFINCHES) != 0;
-	pageScaleFactor = bPageIsInches ? 1000.0f : 100.0f;
-	setPageUnits(bPageIsInches ? fp_PageSize::inch : fp_PageSize::mm);
-	setMarginUnits(getPageUnits());
-	setPageSize(fp_PageSize(float(psd.ptPaperSize.x) / pageScaleFactor,
-							float(psd.ptPaperSize.y) / pageScaleFactor,
-							getPageUnits()));
-
-	setMarginLeft(psd.rtMargin.left		/ pageScaleFactor);
-	setMarginTop(psd.rtMargin.top		/ pageScaleFactor);
-	setMarginRight(psd.rtMargin.right	/ pageScaleFactor);
-	setMarginBottom(psd.rtMargin.bottom	/ pageScaleFactor);
+	else
+	{
+		setAnswer(a_CANCEL);
+	}
 
 	if (psd.hDevMode)
 	{
-		DEVMODE* pDM = (DEVMODE*)GlobalLock(psd.hDevMode);
-		UT_ASSERT(pDM);
-		if (pDM->dmFields & DM_ORIENTATION)
-		{
-			setPageOrientation(pDM->dmOrientation == DMORIENT_PORTRAIT ?
-													PORTRAIT : LANDSCAPE);
-		}
-		if (pDM->dmFields & DM_SCALE)
-		{
-			setPageScale((int)pDM->dmScale);
-		}
-		if (pDM->dmFields & DM_FORMNAME &&
-			pDM->dmFormName[0] &&
-			fp_PageSize::IsPredefinedName((const char*)pDM->dmFormName))
-		{
-			// override PAGESETUPDLG value
-			setPageSize(fp_PageSize((const char*)pDM->dmFormName));
-		}
-
-		GlobalUnlock(psd.hDevMode);
 		GlobalFree(psd.hDevMode);
 	}
 	if (psd.hDevNames)
@@ -172,5 +213,4 @@ void AP_Win32Dialog_PageSetup::runModal(XAP_Frame *pFrame)
 		GlobalFree(psd.hDevNames);
 	}
 
-	setAnswer(a_OK);
 }
