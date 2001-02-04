539bed96155912fa41d148748bb56f1894c10102
Dom Lachowicz <domlachowicz@gmail.com>
2001-02-04 18:37:25 +0000

    Reverted change that removed 'const' declaration from a function. Tinderbox turns green again
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3813 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/ev/xp/ev_Menu_Labels.h
+++ src/af/ev/xp/ev_Menu_Labels.h
@@ -69,7 +69,7 @@ public:
 	UT_Bool				setLabel(XAP_Menu_Id id,
 								 const char * szMenuLabel,
 								 const char * szStatusMsg);
-	EV_Menu_Label *		getLabel(XAP_Menu_Id id);
+	EV_Menu_Label *		getLabel(XAP_Menu_Id id) const;
 	const char *		getLanguage(void) const;
 
 protected:
