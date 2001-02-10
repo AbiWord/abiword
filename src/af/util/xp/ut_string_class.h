7802af152133fdb4aab6f98ab78d4beeb9de2ed1
Mike Nordell <tamlin@algonet.se>
2001-02-10 18:17:01 +0000

    New class UT_String
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@3886 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/util/xp/ut_string_class.h
@@ -0,0 +1,79 @@
+// ut_string_class.h
+//
+// A simple string class for use where templates are not
+// allowed.
+//
+#ifndef UT_STRING_CLASS_H
+#define UT_STRING_CLASS_H
+
+//
+// Copyright (C) 2001 Mike Nordell <tamlin@algonet.se>
+// 
+// This class is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This class is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+// 02111-1307, USA.
+//
+
+#include <stdlib.h>
+
+//!
+//	UT_String, a simple wrapper for zero terminated 'char' strings.
+//
+class UT_String
+{
+public:
+	UT_String();
+	UT_String(const char* sz, size_t n = 0 /* 0 == zero-terminate */);
+	UT_String(const UT_String& rhs);
+	~UT_String();
+
+	size_t		size() const;
+	bool		empty() const;
+
+	UT_String	substr(size_t iStart, size_t nChars) const;
+
+	UT_String&	operator=(const UT_String& rhs);
+	UT_String&	operator=(const char*      rhs);
+	UT_String&	operator+=(const UT_String& rhs);
+	UT_String&	operator+=(const char*      rhs);
+
+	void		swap(UT_String& rhs);
+
+	// The returned pointer is valid until the next non-const
+	// operation. You will _always_ get a legal pointer back,
+	// even if to an empty string.
+	const char* c_str() const;
+
+	// Convenience operator. Should possibly be removed
+	operator const char*() const { return c_str(); }
+
+private:
+	class UT_Stringbuf* pimpl;
+};
+
+// helpers
+bool operator==(const UT_String& s1, const UT_String& s2);
+bool operator==(const UT_String& s1, const char*      s2);
+bool operator==(const char*      s1, const UT_String& s2);
+bool operator!=(const UT_String& s1, const UT_String& s2);
+bool operator!=(const UT_String& s1, const char*      s2);
+bool operator!=(const char*      s1, const UT_String& s2);
+
+// strcmp ordering
+bool operator<(const UT_String& s1, const UT_String& s2);
+
+UT_String operator+(const UT_String& s1, const UT_String& s2);
+
+#endif	// UT_STRING_CLASS_H
+
---
+++ src/af/util/xp/ut_stringbuf.cpp
@@ -0,0 +1,163 @@
+// UT_Stringbuf.cpp
+
+// Copyright (C) 2001 Mike Nordell <tamlin@algonet.se>
+// 
+// This class is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This class is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+// 02111-1307, USA.
+//
+#include <stdlib.h>
+#include <string.h>	// memcpy
+#include "ut_stringbuf.h"
+
+
+// this class keeps zero terminated strings.
+// if size() != 0, capacity() is always at least size() + 1.
+
+//////////////////////////////////////////////////////////////////
+
+static const size_t g_nGrowExtraBytes = 4;
+
+
+UT_Stringbuf::UT_Stringbuf()
+:	m_psz(0),
+	m_pEnd(0),
+	m_size(0)
+{
+}
+
+UT_Stringbuf::UT_Stringbuf(const UT_Stringbuf& rhs)
+:	m_psz(new char_type[rhs.capacity()]),
+	m_pEnd(m_psz + rhs.size()),
+	m_size(rhs.capacity())
+{
+	memcpy(m_psz, rhs.m_psz, rhs.capacity());
+}
+
+UT_Stringbuf::UT_Stringbuf(const char_type* sz, size_t n)
+:	m_psz(new char_type[n+1]),
+	m_pEnd(m_psz + n),
+	m_size(n+1)
+{
+	memcpy(m_psz, sz, n);
+	m_psz[n] = 0;
+}
+
+UT_Stringbuf::~UT_Stringbuf()
+{
+	clear();
+}
+
+
+void UT_Stringbuf::operator=(const UT_Stringbuf& rhs)
+{
+	if (this != &rhs) {
+		clear();
+		assign(rhs.m_psz, rhs.size());
+	}
+}
+
+void UT_Stringbuf::assign(const char_type* sz, size_t n)
+{
+	if (n) {
+		if (n >= capacity()) {
+			grow_nocopy(n);
+		}
+		memcpy(m_psz, sz, n);
+		m_psz[n] = 0;
+		m_pEnd = m_psz + n;
+	} else {
+		clear();
+	}
+}
+
+void UT_Stringbuf::append(const char_type* sz, size_t n)
+{
+	if (!n) {
+		return;
+	}
+	if (!capacity()) {
+		assign(sz, n);
+		return;
+	}
+	const size_t nLen = size();
+	grow_copy(nLen + n);
+	memcpy(m_psz + nLen, sz, n);
+	m_psz[nLen + n] = 0;
+	m_pEnd += n;
+}
+
+void UT_Stringbuf::append(const UT_Stringbuf& rhs)
+{
+	append(rhs.m_psz, rhs.size());
+}
+
+
+static inline
+void my_ut_swap(UT_Stringbuf::char_type*&  a, UT_Stringbuf::char_type*&  b)
+	{ UT_Stringbuf::char_type*  t = a; a = b; b = t; }
+
+static inline
+void my_ut_swap(size_t& a, size_t& b)
+	{ size_t t = a; a = b; b = t; }
+
+void UT_Stringbuf::swap(UT_Stringbuf& rhs)
+{
+	my_ut_swap(m_psz , rhs.m_psz );
+	my_ut_swap(m_pEnd, rhs.m_pEnd);
+	my_ut_swap(m_size, rhs.m_size);
+}
+
+void UT_Stringbuf::clear()
+{
+	if (m_psz) {
+		delete[] m_psz;
+		m_psz = 0;
+		m_pEnd = 0;
+		m_size = 0;
+	}
+}
+
+void UT_Stringbuf::grow_nocopy(size_t n)
+{
+	++n;	// allow for zero termination
+	if (n > capacity()) {
+		const size_t nCurSize = size();
+		if (m_psz) {
+			delete[] m_psz;
+		}
+		n += g_nGrowExtraBytes;
+		m_psz  = new char_type[n];
+		m_pEnd = m_psz + nCurSize;
+		m_size = n;
+	}
+}
+
+void UT_Stringbuf::grow_copy(size_t n)
+{
+	++n;	// allow for zero termination
+	if (n > capacity()) {
+		n += g_nGrowExtraBytes;
+		char_type* p = new char_type[n];
+		const size_t nCurSize = size();
+		if (m_psz) {
+			memcpy(p, m_psz, size() + 1);
+			delete[] m_psz;
+		}
+		m_psz  = p;
+		m_pEnd = m_psz + nCurSize;
+		m_size = n;
+	}
+}
+
