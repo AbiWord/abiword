67284ec479b62f2520c57183047784dfa605686c
Martin Sevior <martines@unimelb.edu.au>
2001-02-23 01:41:18 +0000

    Gnome stuff. I forgot the first time.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4037 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/unix/gnome/ap_UnixGnomeDialog_Styles.cpp
@@ -0,0 +1,69 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include <stdlib.h>
+
+#include "ut_string.h"
+#include "ut_assert.h"
+#include "ut_debugmsg.h"
+#include "ap_Strings.h"
+#include "ut_dialogHelper.h"
+
+#include "ap_UnixGnomeDialog_Styles.h"
+
+
+AP_UnixGnomeDialog_Styles::AP_UnixGnomeDialog_Styles(XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id id) : AP_UnixDialog_Styles(pDlgFactory, id)
+{
+}
+
+AP_UnixGnomeDialog_Styles::~AP_UnixGnomeDialog_Styles(void)
+{
+}
+
+XAP_Dialog * AP_UnixGnomeDialog_Styles::static_constructor(XAP_DialogFactory *pDlgFactory, XAP_Dialog_Id id)
+{
+  AP_UnixGnomeDialog_Styles *p = new AP_UnixGnomeDialog_Styles(pDlgFactory, id);
+  return p;
+}
+
+GtkWidget * AP_UnixGnomeDialog_Styles::_constructWindow(void)
+{
+  	GtkWidget * windowStyles;
+
+	const XAP_StringSet * pSS = m_pApp->getStringSet();
+
+	windowStyles = gnome_dialog_new (pSS->getValue(AP_STRING_ID_DLG_Styles_StylesTitle), NULL);
+
+	_constructWindowContents(GNOME_DIALOG(windowStyles)->vbox);
+
+	// ok button
+	//
+	gnome_dialog_append_button(GNOME_DIALOG(windowStyles), GNOME_STOCK_BUTTON_OK);
+	m_wbuttonOk = GTK_WIDGET (g_list_last (GNOME_DIALOG (windowStyles)->buttons)->data);
+	GTK_WIDGET_SET_FLAGS (m_wbuttonOk, GTK_CAN_DEFAULT);
+
+	// cancel button
+	//
+	gnome_dialog_append_button(GNOME_DIALOG(windowStyles), GNOME_STOCK_BUTTON_CANCEL);
+	m_wbuttonCancel = GTK_WIDGET (g_list_last (GNOME_DIALOG (windowStyles)->buttons)->data);
+	GTK_WIDGET_SET_FLAGS (m_wbuttonCancel, GTK_CAN_DEFAULT);
+
+	_connectsignals();
+	return windowStyles;
+}
---
+++ src/wp/ap/unix/gnome/ap_UnixGnomeDialog_Styles.h
@@ -0,0 +1,49 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+#ifndef AP_UNIXGNOMEDIALOG_STYLES_H
+#define AP_UNIXGNOMEDIALOG_STYLES_H
+
+#include <gnome.h>
+#include "ap_Dialog_Styles.h"
+#include "ap_UnixDialog_Styles.h"
+
+#include "ut_types.h"
+#include "ut_string.h"
+
+class AP_UnixGnomeDialog_Styles: public AP_UnixDialog_Styles
+{
+public:
+	AP_UnixGnomeDialog_Styles(XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id id);
+	virtual ~AP_UnixGnomeDialog_Styles(void);
+
+	static XAP_Dialog *		static_constructor(XAP_DialogFactory *, XAP_Dialog_Id id);
+
+protected:
+
+	// private construction functions
+	virtual GtkWidget * _constructWindow(void);
+};
+
+#endif /* AP_UnixGnomeDialog_Styles_H */
+
+
+
+
+
+
