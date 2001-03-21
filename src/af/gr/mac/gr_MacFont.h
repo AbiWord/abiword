07b3cc38088c19048d228c1b6b46d1758e795644
Hubert Figuiere <hub@figuiere.net>
2001-03-21 22:56:44 +0000

    First Checked In.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4311 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/gr/mac/gr_MacFont.h
@@ -0,0 +1,46 @@
+/* AbiWord
+ * Copyright (C) 1998 AbiSource, Inc.
+ * Copyright (C) 2001 Hubert Figuiere
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+ 
+/* patform: MacOS 8/9 and MacOS X */
+ 
+ 
+
+#ifndef GR_MACFONT_h
+#define GR_MACFONT_h
+ 
+#include <QuickDraw.h>
+
+#include "ut_misc.h"
+#include "gr_Graphics.h"
+
+
+
+class GR_MacFont : public GR_Font
+{
+public:
+	GR_MacFont(int font, int face, int pointSize);
+private:
+	int m_font;
+	int m_face;
+	int m_pointSize;
+};
+
+
+#endif
