99b7892eb68a89a44896374501f061adc19810d6
Thomas Fletcher <thomasf@qnx.com>
2001-03-26 03:46:24 +0000

    Fixed some breakage in the App level
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4360 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/qnx/ap_QNXApp.cpp
+++ src/wp/ap/qnx/ap_QNXApp.cpp
@@ -1023,7 +1023,7 @@ bool AP_QNXApp::parseCommandLine(void)
 		{
 			// [filename]
 			if (to) {
- 				AP_Convert * conv = new AP_Convert();
+ 				AP_Convert * conv = new AP_Convert(getApp());
  				conv->setVerbose(verbose);
  				conv->convertTo(m_pArgs->m_argv[k], to);
  				delete conv;
