fe7fb7e8691bf6966bb3d1988538d9ccbbe957d6
Dom Lachowicz <domlachowicz@gmail.com>
2001-03-29 02:54:45 +0000

    Remove background color dialog from DEBUG
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4410 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Menu_Layouts_MainMenu.h
+++ src/wp/ap/xp/ap_Menu_Layouts_MainMenu.h
@@ -201,9 +201,7 @@ BeginLayout(Main,0)
 			MenuItem(AP_MENU_ID_ALIGN_RIGHT)
 			MenuItem(AP_MENU_ID_ALIGN_JUSTIFY)
 		EndSubMenu()
-#ifdef DEBUG
-                MenuItem(AP_MENU_ID_FMT_BACKGROUND)
-#endif
+	    MenuItem(AP_MENU_ID_FMT_BACKGROUND)
 		MenuItem(AP_MENU_ID_FMT_STYLE)
 	EndSubMenu()
 
