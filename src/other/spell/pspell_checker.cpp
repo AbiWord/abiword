f46208485bb5ee91dfb0cdc65b821db294ab80fd
Dom Lachowicz <domlachowicz@gmail.com>
2001-04-12 15:15:26 +0000

    Get this right..
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4509 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/other/spell/pspell_checker.cpp
+++ src/other/spell/pspell_checker.cpp
@@ -143,9 +143,9 @@ PSpellChecker::checkWord (const UT_UCSChar * szWord,
 	switch (pspell_manager_check(spell_manager, (char*)word8))
 	{
 	case 0:
-		ret = SpellChecker::LOOKUP_SUCCEEDED; break;
-	case 1:
 		ret = SpellChecker::LOOKUP_FAILED; break;
+	case 1:
+		ret = SpellChecker::LOOKUP_SUCCEEDED; break;
 	default:
 		ret = SpellChecker::LOOKUP_ERROR; break;
 	}
