8e70f42989733e2cf84480e9d8a969295459cf48
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2001-05-02 18:10:10 +0000

    undone earlier changes
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4773 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/tools/scripts/unix/ttfadmin.sh
+++ src/tools/scripts/unix/ttfadmin.sh
@@ -9,31 +9,18 @@ then
 	echo "NB: this script must be located in the same directory as"
 	echo "the program ttftool."
 	echo ""
-	echo "Usage: ttfadmin.sh directory [encoding] [brokencmap]"
-	echo "NB: parameters must be in given order"
+	echo "Usage: ttfadmin.sh directory [encoding]"
 	echo "run 'ttftool -e print' for a list of supported encodings"
 	echo "when the encoding is omitted fonts will be coded with"
 	echo "Adobe StandardEncoding"
-	echo "specify 'brokencmap' if you want to use -b option with"
-	echo "ttftool (run ttftool without options for explanation)"
 	echo "----------------------------------------------------------"
 	echo ""
 	exit 0
 fi
 
-if [ $(( $# > 1 )) = 1 ]
+if [ $(( $# == 2 )) = 1 ]
 then
-	if [ "$2" != "brokencmap" ]
-	then
-		ENCODING="-e $2"
-	else
-		BCMAP="-b"
-	fi
-fi
-
-if [ $(( $# > 2 )) = 1 ]
-then
-	BCMAP="-b"
+	ENCODING="-e $2"
 fi
 
 TTFTOOL=${0%ttfadmin.sh}ttftool
@@ -45,5 +32,5 @@ do
 	AFM=${dir%.ttf}.afm
 	UTOG=${dir%.ttf}.u2g
 	T42=${dir%.ttf}.t42
-	`$TTFTOOL -f $dir -a $AFM -p $T42 -u $UTOG $ENCODING $BCMAP`
+	`$TTFTOOL -f $dir -a $AFM -p $T42 -u $UTOG $ENCODING`
 done
