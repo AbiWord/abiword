4d9c0673a987ba0291a4af9fdb66e7c5d0178242
Pierre Abbat <phma@webjockey.net>
2001-05-09 20:09:32 +0000

    Slovenian translation by Andraz Tori sl-SI.strings changed to iso-8859-2
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@4835 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/xp/ap_Menu_LabelSet_sl-SI.h
@@ -0,0 +1,239 @@
+/* AbiWord
+ * Copyright (C) 1998-2000 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+ 
+// Slovenian translation provided by Andraz Tor <andraz.tori1@guest.arnes.si>
+
+
+/*****************************************************************
+******************************************************************
+** IT IS IMPORTANT THAT THIS FILE ALLOW ITSELF TO BE INCLUDED
+** MORE THAN ONE TIME.
+******************************************************************
+*****************************************************************/
+
+// We use the Win32 '&' character to denote a keyboard accelerator on a menu item.
+// If your platform doesn't have a way to do accelerators or uses a different
+// character, remove or change the '&' in your menu constructor code.
+
+// If the third argument is true, then this is the fall-back for
+// this language (named in the first argument).
+
+BeginSet(sl,SI,true)
+
+	MenuLabel(AP_MENU_ID__BOGUS1__,			NULL,				NULL)
+
+	//       (id,                       	szLabel,           	szStatusMsg)
+
+	MenuLabel(AP_MENU_ID_FILE,				"&Datoteka",			NULL)
+	MenuLabel(AP_MENU_ID_FILE_NEW,			"&Nova", 			"Ustvari nov dokument")	
+	MenuLabel(AP_MENU_ID_FILE_OPEN,			"&Odpri",			"Odpri obstojeè dokument")
+	MenuLabel(AP_MENU_ID_FILE_CLOSE,		"&Zapri", 			"Zapri dokument")
+	MenuLabel(AP_MENU_ID_FILE_SAVE,			"&Shrani", 			"Shrani dokument")
+	MenuLabel(AP_MENU_ID_FILE_SAVEAS,		"Shrani &kot", 		"Shrani dokument pod drugaènim imenom")
+	MenuLabel(AP_MENU_ID_FILE_IMPORT,		"&Uvozi",			"Uvozi dokument")
+	MenuLabel(AP_MENU_ID_FILE_EXPORT,       	"&Izvozi",			"Izvozi dokument")
+
+	MenuLabel(AP_MENU_ID_FILE_PAGESETUP,		"N&astavitev strani",		"Spremeni nastaviteve tiskanja")
+	MenuLabel(AP_MENU_ID_FILE_PRINT,		"Na&tisni",			"Tiskaj celoten dokument ali njegov del")
+	MenuLabel(AP_MENU_ID_FILE_PRINT_DIRECTLY,		"Tiskaj &neposredno",	"Tiskaj z uporabo internega gonilnika PostScript")
+	MenuLabel(AP_MENU_ID_FILE_PRINT_PREVIEW, 		"P&redogled tiskanja", "Predogled dokumenta pred tiskanjem")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_1,		"&1 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_2,		"&2 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_3,		"&3 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_4,		"&4 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_5,		"&5 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_6,		"&6 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_7,		"&7 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_8,		"&8 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_RECENT_9,		"&9 %s",			"Odpri ta dokument")
+	MenuLabel(AP_MENU_ID_FILE_EXIT,			"Iz&hod", 			"Zapri vsa okna programa in konèaj")
+
+	MenuLabel(AP_MENU_ID_EDIT,			"&Uredi",			NULL)
+	MenuLabel(AP_MENU_ID_EDIT_UNDO,			"&Razveljavi",			"Razveljavi urejanje")
+	MenuLabel(AP_MENU_ID_EDIT_REDO,			"&Obnovi",			"Obnovi prej razveljavljeno urejanje")
+	MenuLabel(AP_MENU_ID_EDIT_CUT,			"&Izre¾i",				"Izre¾i izbiro in jo daj na odlo¾i¹èe")
+	MenuLabel(AP_MENU_ID_EDIT_COPY,			"&Kopiraj",			"Kopiraj izbiro in jo daj na odlo¾i¹èe")
+	MenuLabel(AP_MENU_ID_EDIT_PASTE,		"&Prilepi",			"Vstavi vsebino odlo¾i¹èa")
+	MenuLabel(AP_MENU_ID_EDIT_PASTE_SPECIAL, 	"Prilepi neo&blikovano",  "Vstavi neoblikovano vsebino odlo¾i¹èa")
+	MenuLabel(AP_MENU_ID_EDIT_CLEAR,		"Poèi&sti",			"Zbri¹i izbiro")
+	MenuLabel(AP_MENU_ID_EDIT_SELECTALL,		"Izberi &vse",		"Izberi celoten dokument")
+	MenuLabel(AP_MENU_ID_EDIT_FIND,			"I&¹èi",			"Poi¹èi doloèeno besedilo")
+	MenuLabel(AP_MENU_ID_EDIT_REPLACE,		"&Zamenjaj",			"Zamenjaj doloèeno besedilo z drugim")
+	MenuLabel(AP_MENU_ID_EDIT_GOTO,			"Poj&di na",			"Prestavi kazalec na doloèeno mesto")
+	MenuLabel(AP_MENU_ID_EDIT_EDITHEADER,			"Uredi glavo",			"Uredi glavo trenutne strani")
+	MenuLabel(AP_MENU_ID_EDIT_EDITFOOTER,			"Uredi nogo",			"Uredi nogo trenutne strani")
+	
+	MenuLabel(AP_MENU_ID_VIEW,			"Po&gled",			NULL)
+	MenuLabel(AP_MENU_ID_VIEW_NORMAL, 		"O&bièajen", "Obièajen pogled")
+	MenuLabel(AP_MENU_ID_VIEW_WEB, 			"&Spletni pogled", "Spletni pogled")
+	MenuLabel(AP_MENU_ID_VIEW_PRINT, 		"Pogled &tiskanja", "Tiskalni¹ki pogled")
+	MenuLabel(AP_MENU_ID_VIEW_TOOLBARS,		"&Orodjarne",		NULL)
+	MenuLabel(AP_MENU_ID_VIEW_TB_STD,			"O&bièajne",		"Ka¾i/skrij obièajno orodjarno")
+	MenuLabel(AP_MENU_ID_VIEW_TB_FORMAT,			"&Oblikovanje",		"Ka¾i/skrij oblikovalsko orodjarno")
+	MenuLabel(AP_MENU_ID_VIEW_TB_EXTRA,			"&Dodatne",		"Ka¾i/skrij dodatno orodjarno")
+	MenuLabel(AP_MENU_ID_VIEW_RULER,		"&Ravnilo",			"Ka¾i/skrij ravnila")
+	MenuLabel(AP_MENU_ID_VIEW_STATUSBAR,		"Vrstica s&tanja",		"Ka¾i/skrij vrstico stanja")
+	MenuLabel(AP_MENU_ID_VIEW_SHOWPARA,		"Ka¾i o&dstavke",	"Ka¾i znake, ki oznaèujejo oblikovanje")
+	MenuLabel(AP_MENU_ID_VIEW_HEADFOOT,		"&Glava in noga",	"Uredi besedilo na vrhu ali dnu vsake strani")
+	MenuLabel(AP_MENU_ID_VIEW_FULLSCREEN, 		"Èez &cel zaslon", "Glej dokument èez cel zason")
+	MenuLabel(AP_MENU_ID_VIEW_ZOOM,			"&Poveèava",		"Poveèaj ali zmanj¹aj prikaz dokumenta")
+
+	MenuLabel(AP_MENU_ID_INSERT,			"&Vstavi",			NULL)
+	MenuLabel(AP_MENU_ID_INSERT_BREAK,		"&Prelom",			"Vstavi prelom strani, stolpca ali odseka")
+	MenuLabel(AP_MENU_ID_INSERT_PAGENO,		"©t&evilke strani",	"Vstavi ¹tevilke strani")
+	MenuLabel(AP_MENU_ID_INSERT_DATETIME,		"Datum in &uro",	"Vstavi datum in/ali uro")
+	MenuLabel(AP_MENU_ID_INSERT_FIELD,		"Po&lje",		"Vstavi preraèunano polje")
+	MenuLabel(AP_MENU_ID_INSERT_SYMBOL,		"Si&mbol",		"Vstavi simbol ali kak drug posebni znak")
+	MenuLabel(AP_MENU_ID_INSERT_PICTURE, 		"&Sliko", 	"Vstavi sliko")
+	MenuLabel(AP_MENU_ID_INSERT_GRAPHIC,			"&Iz datoteke",	"Vstavi obstojeèo sliko iz druge datoteke")
+
+	MenuLabel(AP_MENU_ID_FORMAT,			"&Oblikovanje",			NULL)
+	MenuLabel(AP_MENU_ID_FMT_LANGUAGE,		"&Jezik",		"Spremeni jezik izbranega besedila")
+	MenuLabel(AP_MENU_ID_FMT_FONT,			"P&isava",			"Spremeni pisavo izbranega besedila")
+	MenuLabel(AP_MENU_ID_FMT_PARAGRAPH,		"Odst&avek",		"Spremeni obliko izbranega odstavka")
+	MenuLabel(AP_MENU_ID_FMT_BULLETS,		"O&znaèevanje in o¹tevilèenje",	"Dodaj ali spremeni oznaèevanje in o¹tevilèenje izbranega odstavka")
+	MenuLabel(AP_MENU_ID_FMT_DOCUMENT,		"&Dokument",             "Nastavi lastnosti strani dokumenta kot so velikost strani in robovi")
+	MenuLabel(AP_MENU_ID_FMT_BORDERS,		"Okvirji in s&enèenja",		"Izbiri dodaj robove in senèenja")
+	MenuLabel(AP_MENU_ID_FMT_COLUMNS,		"&Stolpci",			"Spremeni ¹tevilo stolpcev")
+	MenuLabel(AP_MENU_ID_FMT_TOGGLECASE,   	"Spremeni &velikost", "Spremeni velikost izbranega besedila")
+	MenuLabel(AP_MENU_ID_FMT_BACKGROUND,   	"O&zadnje", "Spremeni barvo ozadja va¹ega besedila")
+	MenuLabel(AP_MENU_ID_FMT_STYLE,			"S&log",			"Doloèi ali uveljavi slog na izbiri")
+	MenuLabel(AP_MENU_ID_FMT_TABS,			"&Tabulatorji",			"Nastavi tabulatorska mesta")
+	MenuLabel(AP_MENU_ID_FMT_BOLD,			"&Krepko",			"Naredi izbiro krepko (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_ITALIC,		"K&urzivno",			"Naredi izbiro kurzivno (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_UNDERLINE,		"&Podèrtano",		"Naredi izbiro podèrtano (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_OVERLINE,		"Nad&èrtano",		"Naredi izbiro nadèrtano (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_STRIKE,		"P&reèrtano",			"Preèrtaj izbiro (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_TOPLINE,		"Èrta z&goraj",			"Èrta nad izbiro (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_BOTTOMLINE,	"Èrta spoda&j",		"Èrta pod izbiro (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_SUPERSCRIPT,	"&Nadpisano",		"Naredi izbiro nadpisano (preklopi)")
+	MenuLabel(AP_MENU_ID_FMT_SUBSCRIPT,		"P&odpisano",		"Naredi izbiro podpisano (preklopi)")
+
+	MenuLabel(AP_MENU_ID_TOOLS,			"&Orodja",			NULL)   
+	MenuLabel(AP_MENU_ID_TOOLS_SPELLING, "È&rkovalnik", NULL)
+	MenuLabel(AP_MENU_ID_TOOLS_SPELL,		"Preveri è&rkovanje",		"Preveri dokument za napaènim èrkovanjem")
+	MenuLabel(AP_MENU_ID_TOOLS_AUTOSPELL, "&Samodejno èrkovanje", "Samodejno preverjaj èrkovanje v dokumentu")
+	MenuLabel(AP_MENU_ID_TOOLS_WORDCOUNT,	"©tetje &besed",		"©tej ¹tevilo besed v dokumentu")
+	MenuLabel(AP_MENU_ID_TOOLS_OPTIONS,		"&Nastavitve",		"Nastavi nastavitve")
+	MenuLabel(AP_MENU_ID_TOOLS_LANGUAGE, "&Jezik", 				"Spremeni jezik izbranega besedila")
+	MenuLabel(AP_MENU_ID_TOOLS_PLUGINS, "&Vtièniki",			"Upravljaj z vtièniki")
+	MenuLabel(AP_MENU_ID_TOOLS_SCRIPTS, "S&kripte",				"Izvedi pomo¾ne skripte")
+
+	MenuLabel(AP_MENU_ID_ALIGN,				"&Poravnava",			NULL)
+	MenuLabel(AP_MENU_ID_ALIGN_LEFT,		"&Leva",			"Left-justify the paragraph")
+	MenuLabel(AP_MENU_ID_ALIGN_CENTER,		"&Sredi¹èna",			"Center-align the paragraph")
+	MenuLabel(AP_MENU_ID_ALIGN_RIGHT,		"&Desna",			"Right-justify the paragraph")
+	MenuLabel(AP_MENU_ID_ALIGN_JUSTIFY,		"&Obojestranska",			"Justify the paragraph")
+
+	MenuLabel(AP_MENU_ID_WEB, 			"&Splet", NULL)
+	MenuLabel(AP_MENU_ID_WEB_SAVEASWEB,    "Shrani kot &splet",		"Save the document as a web page")
+	MenuLabel(AP_MENU_ID_WEB_WEBPREVIEW,   "P&redogled sletne strani", "Preview the document as a web page")
+
+	MenuLabel(AP_MENU_ID_WINDOW,			"&Okno",			NULL)
+	MenuLabel(AP_MENU_ID_WINDOW_NEW,		"&Novo okno",		"Odpri ¹e eno okno")
+	MenuLabel(AP_MENU_ID_WINDOW_1,			"&1 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_2,			"&2 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_3,			"&3 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_4,			"&4 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_5,			"&5 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_6,			"&6 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_7,			"&7 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_8,			"&8 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_9,			"&9 %s",			"Dvigni to okno")
+	MenuLabel(AP_MENU_ID_WINDOW_MORE,		"&Veè oken",	"Ka¾i celoten seznam oken")
+
+	MenuLabel(AP_MENU_ID_HELP,			"&Pomoè",		NULL)
+	MenuLabel(AP_MENU_ID_HELP_CONTENTS,		"&Vsebina pomoèi",	"Ka¾i vsebino pomoèi")
+	MenuLabel(AP_MENU_ID_HELP_INDEX,		"&Kazalo pomoèi",		"Ka¾i kazalo pomoèi")
+	MenuLabel(AP_MENU_ID_HELP_CHECKVER,		"Ka¾i &razlièico",	"Poka¾i razlièico programa")
+	MenuLabel(AP_MENU_ID_HELP_SEARCH,		"&I¹èi po pomoèi",	"I¹èi po pomoèi za...")
+	MenuLabel(AP_MENU_ID_HELP_ABOUT,		"&O %s",		"Poka¾i razlièico in licenco in podatke o programu") 
+	MenuLabel(AP_MENU_ID_HELP_ABOUTOS,		"O O&dprtem programju",	"Poka¾i podatke o odprtem programju")
+
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_1,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_2,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_3,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_4,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_5,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_6,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_7,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_8,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_SUGGEST_9,	"%s",				"Spremeni na to predlagano èrkovanje")
+	MenuLabel(AP_MENU_ID_SPELL_IGNOREALL,	"&Prezri vse", 		"Prezri vse pojavitve te besede v dokumentu")
+	MenuLabel(AP_MENU_ID_SPELL_ADD,			"&Dodaj", 			"To besedo dodaj v prikrojen slovar")
+
+	/* autotext submenu labels */
+	MenuLabel(AP_MENU_ID_INSERT_AUTOTEXT, "&Samodejno besedilo", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_ATTN, "Opozorilo:", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING, "Zakljuèek:", "") 
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL, "Po¹tna navodila:", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_REFERENCE, "Reference:", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SALUTATION, "Pozdrav:", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SUBJECT, "Zadeva:", "")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL, "E-po¹ta:", "")
+
+	MenuLabel(AP_MENU_ID_AUTOTEXT_ATTN_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_ATTN_2, "%s", " ")
+	
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_2, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_3, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_4, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_5, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_6, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_7, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_8, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_9, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_10, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_11, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_CLOSING_12, "%s", " ")
+	
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_2, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_3, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_4, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_5, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_6, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_7, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_MAIL_8, "%s", " ")
+	
+	MenuLabel(AP_MENU_ID_AUTOTEXT_REFERENCE_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_REFERENCE_2, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_REFERENCE_3, "%s", " ")
+	
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SALUTATION_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SALUTATION_2, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SALUTATION_3, "%s", " ")
+
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SALUTATION_4, "%s", " ")
+
+	MenuLabel(AP_MENU_ID_AUTOTEXT_SUBJECT_1, "%s", " ")
+
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_1, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_2, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_3, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_4, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_5, "%s", " ")
+	MenuLabel(AP_MENU_ID_AUTOTEXT_EMAIL_6, "%s", " ")
+
+	// ... add others here ...
+
+	MenuLabel(AP_MENU_ID__BOGUS2__,			NULL,				NULL)
+
+EndSet()
---
+++ src/wp/ap/xp/ap_TB_LabelSet_sl-SI.h
@@ -0,0 +1,119 @@
+/* AbiWord
+ * Copyright (C) 1998-2001 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+
+// Slovenian translation provided by Andraz Tor <andraz.tori1@guest.arnes.si>
+
+/*****************************************************************
+******************************************************************
+** IT IS IMPORTANT THAT THIS FILE ALLOW ITSELF TO BE INCLUDED
+** MORE THAN ONE TIME.
+******************************************************************
+*****************************************************************/
+
+// Note: if the tooltip is blank, the status message will be used as the
+// Note: tooltip.  therefore, we probably don't need most tooltip strings
+// Note: here -- unless the status message is too long to look good in
+// Note: a tooltip.
+
+// Note: the icon field should not be localized unless absolutely necessary.
+// Note: the icon name here is to a specific icon (pixmap or bitmap or whatever)
+// Note: that will always be in the application.  if, for example, a big fat 'B'
+// Note: for BOLD doesn't make sense in another language, change the entry in
+// Note: the localization and add the icon to whatever table.
+
+// Note: if a tool item does not use an icon (like a combo box), use the
+// Note: constant "NoIcon" in that column.
+
+// If the third argument is true, then this is the fall-back for
+// this language (named in the first argument).
+
+BeginSet(sl,SI,true)
+
+	ToolbarLabel(AP_TOOLBAR_ID__BOGUS1__,		NULL,			NoIcon,					NULL,NULL)
+
+	//          (id, 		                    szLabel,		IconName,     			szToolTip,      szStatusMsg)
+
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Nova", 			tb_new_xpm,				NULL, "Ustvari nov dokument")	
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN,		"Odpri",			tb_open_xpm,			NULL, "Odpri obstojeè dokument")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE,		"Shrani", 		tb_save_xpm,			NULL, "Shrani dokument")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS,		"Shrani kot", 		tb_save_as_xpm,			NULL, "Shrani dokument pod drugaènim imenom")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT,		"Tiskaj",		tb_print_xpm,			NULL, "Natisni dokument")
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT_PREVIEW,	"Predogled tiskanja",	tb_print_preview_xpm, NULL, "Predogled dokumenta pred tiskanjem")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_UNDO,		"Razveljavi",			tb_undo_xpm,			NULL, "Razveljavi urejanje")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_REDO,		"Obnovi",			tb_redo_xpm,			NULL, "Obnovi urejanje")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_CUT,		"Izre¾i",			tb_cut_xpm,				NULL, "Izre¾i izbiro")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_COPY,		"Kopiraj",			tb_copy_xpm,			NULL, "Kopiraj izbiro")
+	ToolbarLabel(AP_TOOLBAR_ID_EDIT_PASTE,		"Prilepi",		tb_paste_xpm,			NULL, "Prilepi z odlo¾i¹èa")
+	ToolbarLabel(AP_TOOLBAR_ID_SPELLCHECK,		"Preveri èrkovanje",	tb_spellcheck_xpm,		NULL, "Preveri èrkovanje dokumenta")
+	ToolbarLabel(AP_TOOLBAR_ID_IMG,			"Vstavi sliko",	tb_insert_graphic_xpm,	NULL, "V dokument vstavi sliko")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_STYLE,		"Slog",		NoIcon,					NULL, "Slog")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_FONT,		"Pisava",			NoIcon,					NULL, "Pisava")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_SIZE,		"Velikost pisave",	NoIcon,					NULL, "Velikost pisave")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_BOLD,		"Krepko",			tb_text_bold_xpm,		NULL, "Krepko")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_ITALIC,		"Kurzivno",		tb_text_italic_xpm,		NULL, "Kurzivno")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_UNDERLINE,	"Podèrtano",	tb_text_underline_xpm,	NULL, "Podèrtano")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_OVERLINE,	"Nadèrtano",		tb_text_overline_xpm,	NULL, "Nadèrtano")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_STRIKE,		"Preèrtano",		tb_text_strikeout_xpm,	NULL, "Preèrtano")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_TOPLINE,		"Èrta zgoraj",		tb_text_topline_xpm,	NULL, "Èrta zgoraj")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_BOTTOMLINE,	"Èrta spodaj",		tb_text_bottomline_xpm,	NULL, "Èrta spodaj")
+	ToolbarLabel(AP_TOOLBAR_ID_HELP,		"Pomoè",			tb_help_xpm,			NULL, "Pomoè")
+
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_SUPERSCRIPT,	"Nadpisano",	tb_text_superscript_xpm,	NULL, "Nadpisano")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_SUBSCRIPT,	"Podpisano",	tb_text_subscript_xpm,		NULL, "Podpisano")
+	ToolbarLabel(AP_TOOLBAR_ID_INSERT_SYMBOL,	"Simbol",		tb_symbol_xpm,				NULL, "Vstavi simbol")
+
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_LEFT,		"Leva",			tb_text_align_left_xpm,		NULL, "Leva poravnava")
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_CENTER,	"Sredinska",		tb_text_center_xpm,			NULL, "Sredinska poravnava")
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_RIGHT,		"Desna",		tb_text_align_right_xpm,	NULL, "Desna poravnava")
+	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_JUSTIFY,	"Obojestranska",		tb_text_justify_xpm,		NULL, "Obojestranska poravnava")
+
+	ToolbarLabel(AP_TOOLBAR_ID_PARA_0BEFORE,	"Brez presledka pred",		tb_para_0before_xpm,	NULL, "Brez presledka pred odstavkom")
+	ToolbarLabel(AP_TOOLBAR_ID_PARA_12BEFORE,	"12 pik presledka pred",		tb_para_12before_xpm,	NULL, "12 pik presledka pred odstavkom")
+
+	ToolbarLabel(AP_TOOLBAR_ID_SINGLE_SPACE,	"Enojni medvrstièni prostor",	tb_line_single_space_xpm,	NULL, "Enojni medvrstièni prostor")
+	ToolbarLabel(AP_TOOLBAR_ID_MIDDLE_SPACE,	"1,5 medvrstiènega prostora",		tb_line_middle_space_xpm,	NULL, "1,5 medvrstièni prostor")
+	ToolbarLabel(AP_TOOLBAR_ID_DOUBLE_SPACE,	"Dvojni medvrstièni prostor",	tb_line_double_space_xpm,	NULL, "Dvojni medvrstièni prostor")
+
+	ToolbarLabel(AP_TOOLBAR_ID_1COLUMN,			"1 stolpec",			tb_1column_xpm,			NULL, "1 stolpec")
+	ToolbarLabel(AP_TOOLBAR_ID_2COLUMN,			"2 stolpca",		tb_2column_xpm,			NULL, "2 stolpca")
+	ToolbarLabel(AP_TOOLBAR_ID_3COLUMN,			"3 stolpci",		tb_3column_xpm,			NULL, "3 stolpci")
+
+	ToolbarLabel(AP_TOOLBAR_ID_VIEW_SHOWPARA,	"Ka¾i vse",			tb_view_showpara_xpm,		NULL, "Ka¾i/skrij")
+	ToolbarLabel(AP_TOOLBAR_ID_ZOOM,		"Poveèava",				NoIcon,					NULL, "Poveèaj")
+	ToolbarLabel(AP_TOOLBAR_ID_LISTS_BULLETS,	"Vrstiène oznake",			tb_lists_bullets_xpm,	NULL, "Vrstiène oznake")
+	ToolbarLabel(AP_TOOLBAR_ID_LISTS_NUMBERS,	"O¹tevilèenje",		tb_lists_numbers_xpm,	NULL, "O¹tevilèenjestrani")
+	ToolbarLabel(AP_TOOLBAR_ID_COLOR_FORE,		"Barva pisave",		tb_text_fgcolor_xpm,	NULL, "Barva pisave")
+	ToolbarLabel(AP_TOOLBAR_ID_COLOR_BACK,		"Osvetlitev",		tb_text_bgcolor_xpm,	NULL, "Osvetlitev")
+	ToolbarLabel(AP_TOOLBAR_ID_INDENT,		"Poveèaj zamik",	tb_text_indent_xpm, 	NULL, "Poveèaj zamik")
+	ToolbarLabel(AP_TOOLBAR_ID_UNINDENT,		"Zmanj¹aj zamik",	tb_text_unindent_xpm,	NULL, "Zmanj¹aj zamik")
+
+#ifdef ABI_OPT_JS
+	ToolbarLabel(AP_TOOLBAR_ID_SCRIPT_PLAY,		"Izvedi skripto",	tb_script_play_xpm,		NULL, "Izvedi skripto")
+#endif
+
+     // ... add others here ...
+#ifdef BIDI_ENABLED
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_DIR_OVERRIDE_LTR,	"Besedilo od leve proti desni",		tb_text_direction_ltr_xpm,	NULL, "Besedilo mora biti od leve proti desni")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_DIR_OVERRIDE_RTL,	"Besedilo od desne proti levi",		tb_text_direction_rtl_xpm,	NULL, "Besedilo mora biti od desne proti levi")
+	ToolbarLabel(AP_TOOLBAR_ID_FMT_DOM_DIRECTION,		"Usmerjenost odstavka",	tb_text_dom_direction_rtl_xpm,	NULL, "Spremeni prevladujoèo usmerjenost odstavka")
+#endif
+	ToolbarLabel(AP_TOOLBAR_ID__BOGUS2__,		NULL,			NoIcon,			NULL,NULL)
+
+EndSet()
