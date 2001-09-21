844268a91ba75fc895fcb7552af66487144de1b2
Michael Pritchett <mpritchett@svn.abisource.com>
2001-09-21 22:16:17 +0000

    Perl Scripting fix for Win32 - Thanks to Nikolaj Brandt Jensen for pointing this one out.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@5761 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/abi_defs.mk
+++ src/config/abi_defs.mk
@@ -669,7 +669,6 @@ endif
 ifdef ABI_OPT_PERL
     ifeq ($(OS_NAME), WIN32)
 		EXTRA_LIBS += $(shell perl -MExtUtils::Embed -e ldopts | sed 's|[a-zA-Z0-9~:\\]*msvcrt\.lib||g' | sed 's|\([-/]release\|[-/]nodefaultlib\|[-/]nologo\|[-/]machine:[a-zA-Z0-9]*\)||g' | sed 's|\\|\\\\\\\\|g')
-		EXTRA_LIBS += "C:\\\\Pritchett\\\\Compilers\\\\Perl\\\\lib\\\\CORE\\\\perl56.lib"
 		CFLAGS += -DABI_OPT_PERL $(shell perl -MExtUtils::Embed -e ccopts | sed 's/\(-O1\|-MD\)//g')
     else
 		EXTRA_LIBS += $(shell perl -MExtUtils::Embed -e ldopts)
