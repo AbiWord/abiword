a983ab7d36ddbd4436f569e5f14962f75d810fcd
Joaquin Cuenca Abela <joaquin@cuencaabela.com>
2001-10-18 08:48:22 +0000

    New UT_Map type.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@5917 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/util/xp/ut_map.cpp
@@ -0,0 +1,75 @@
+/* AbiWord
+ * Copyright (C) 2001 Joaquín Cuenca Abela
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "ut_map.h"
+#include "ut_pair.h"
+#include "ut_assert.h"
+
+bool ut_map_lexico_lesser(UT_RBTree::key_t x, UT_RBTree::key_t y)
+{
+	UT_Map::value_t x_ = static_cast<UT_Map::value_t> (x);
+	UT_Map::value_t y_ = static_cast<UT_Map::value_t> (y);
+	UT_ASSERT(x_);
+	UT_ASSERT(y_);
+	
+	return ut_lexico_lesser(x_->first(), y_->first());
+}
+
+bool ut_map_lexico_greater(UT_RBTree::key_t x, UT_RBTree::key_t y)
+{
+	UT_Map::value_t x_ = static_cast<UT_Map::value_t> (x);
+	UT_Map::value_t y_ = static_cast<UT_Map::value_t> (y);
+	UT_ASSERT(x_);
+	UT_ASSERT(y_);
+	
+	return ut_lexico_greater(x_->first(), y_->first());
+}
+
+bool ut_map_lexico_equal(UT_RBTree::key_t x, UT_RBTree::key_t y)
+{
+	UT_Map::value_t x_ = static_cast<UT_Map::value_t> (x);
+	UT_Map::value_t y_ = static_cast<UT_Map::value_t> (y);
+	UT_ASSERT(x_);
+	UT_ASSERT(y_);
+	
+	return ut_lexico_equal(x_->first(), y_->first());
+}
+
+///////////////////////////////////////////
+// UT_Map
+///////////////////////////////////////////
+bool lesser(UT_RBTree::key_t x, UT_RBTree::key_t y)
+{
+	UT_Map::value_t x_ = static_cast<UT_Map::value_t> (x);
+	UT_Map::value_t y_ = static_cast<UT_Map::value_t> (y);
+	UT_ASSERT(x_);
+	UT_ASSERT(y_);
+	
+	return x_->first() < y_->first();
+}
+
+UT_Map::UT_Map()
+	: m_rbtree(lesser)
+{
+}
+
+UT_Map::UT_Map(comparator comp)
+	: m_rbtree(comp)
+{
+}
---
+++ src/af/util/xp/ut_map.h
@@ -0,0 +1,73 @@
+/* AbiWord
+ * Copyright (C) 2001 Joaquín Cuenca Abela
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#ifndef UT_MAP_H
+#define UT_MAP_H
+
+#include "ut_rbtree.h"
+#include "ut_types.h"
+
+class UT_Pair;
+
+class ABI_EXPORT UT_Map
+{
+public:
+	// Please note that UT_RBTree::key_t is (conceptually)
+	// not the same as UT_Map::key_t.
+	typedef const void* key_t;
+	typedef const void* data_t;
+	typedef const UT_Pair* value_t;
+	typedef UT_RBTree::Iterator Iterator;
+	typedef UT_RBTree::comparator comparator;
+
+	// you should also note that until I come up with a wrapper
+	// for comparator, UT_Map & UT_Set are exactly the same one
+	// (but the default constructor).
+	UT_Map();
+	UT_Map(comparator comp);
+
+	bool insert(value_t item) { return m_rbtree.insert(item); }
+	void erase(Iterator& c) { m_rbtree.erase(c); }
+
+	Iterator find(key_t item) { return m_rbtree.find(item); }
+	Iterator find_if(key_t item, comparator pred) { return m_rbtree.find_if(item, pred); }
+
+	Iterator begin() { return m_rbtree.begin(); }
+	Iterator end() { return m_rbtree.end(); }
+
+	size_t size() { return m_rbtree.size(); }
+
+#ifdef DEBUG
+	void print() const { m_rbtree.print(); }
+	// This method should be const.  See UT_RBTree::checkInvariants comment
+	bool checkInvariants() { return m_rbtree.checkInvariants(); }
+#endif
+
+private:
+	UT_Map(const UT_Map&);
+	UT_Map& operator= (const UT_Map&);
+
+	UT_RBTree m_rbtree;
+};
+
+bool ut_map_lexico_lesser(UT_Map::key_t x, UT_Map::key_t y);
+bool ut_map_lexico_greater(UT_Map::key_t x, UT_Map::key_t y);
+bool ut_map_lexico_equal(UT_Map::key_t x, UT_Map::key_t y);
+
+#endif // UT_MAP_H
