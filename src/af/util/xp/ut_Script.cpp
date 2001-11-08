b74e8b8ca7368425c6436d518864c896ae0afb95
Dom Lachowicz <domlachowicz@gmail.com>
2001-11-08 18:43:39 +0000

    commit - new script factory - untested
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6073 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/util/xp/ut_Script.cpp
@@ -0,0 +1,274 @@
+/* AbiWord
+ * Copyright (C) 2001 Dom Lachowicz
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "ut_Script.h"
+#include "ut_string.h"
+#include "ut_debugmsg.h"
+#include "ut_assert.h"
+#include "ut_misc.h"
+
+/************************************************************************/
+/************************************************************************/
+
+UT_ScriptSniffer::UT_ScriptSniffer()
+  : m_type (-1)
+{
+}
+
+UT_ScriptSniffer::~UT_ScriptSniffer()
+{
+}
+
+/************************************************************************/
+/************************************************************************/
+
+UT_Script::UT_Script ()
+{
+}
+
+UT_Script::~UT_Script()
+{
+}
+
+/************************************************************************/
+/************************************************************************/
+
+UT_ScriptLibrary UT_ScriptLibrary::mInstance;
+
+UT_ScriptLibrary::UT_ScriptLibrary ()
+  : mSniffers (new UT_Vector (5))
+{
+}
+
+UT_ScriptLibrary::~UT_ScriptLibrary ()
+{
+  DELETEP(mSniffers);
+}
+
+UT_ScriptLibrary& UT_ScriptLibrary::instance ()
+{
+  return mInstance;
+}
+
+UT_Error UT_ScriptLibrary::execute ( const char * script )
+{
+  UT_Script * pScript = NULL;
+  UT_ScriptIdType scriptId = -1;
+
+  UT_Error err = UT_OK;
+
+  if ((err = constructScript(script, -1, &pScript, &scriptId)) == UT_OK)
+    {
+      pScript->execute ( script );
+      DELETEP ( pScript );
+    }
+
+  return err;
+}
+
+UT_uint32 UT_ScriptLibrary::getNumScripts () const
+{
+  return mSniffers->size ();
+}
+
+void UT_ScriptLibrary::registerScript ( UT_ScriptSniffer * s )
+{
+	UT_uint32 ndx = 0;
+	UT_Error err = mSniffers->addItem (s, &ndx);
+
+	UT_ASSERT(err == UT_OK);
+	UT_ASSERT(ndx >= 0);
+
+	s->setType(ndx+1);
+}
+
+void UT_ScriptLibrary::unregisterScript ( UT_ScriptSniffer * s )
+{
+  UT_uint32 ndx = s->getType(); // 1:1 mapping
+  
+  UT_ASSERT(ndx >= 0);
+  
+  mSniffers->deleteNthItem (ndx-1);
+  
+  // Refactor the indexes
+  UT_ScriptSniffer * pSniffer = 0;
+  UT_uint32 size  = mSniffers->size();
+  UT_uint32 i     = 0;
+  for( i = ndx-1; i < size; i++)
+    {
+      pSniffer = static_cast <UT_ScriptSniffer *>(mSniffers->getNthItem(i));
+      if (pSniffer)
+	pSniffer->setType(i+1);
+    }
+}
+
+void UT_ScriptLibrary::unregisterAllScripts ()
+{
+  UT_ScriptSniffer * pSniffer = 0;
+  UT_uint32 size = mSniffers->size();
+  
+  for (UT_uint32 i = 0; i < size; i++)
+    {
+      pSniffer = static_cast <UT_ScriptSniffer *>(mSniffers->getNthItem(i));
+      if (pSniffer)
+	pSniffer->unref();
+    }
+}
+
+UT_ScriptIdType	UT_ScriptLibrary::typeForContents(const char * szBuf,
+						  UT_uint32 iNumbytes)
+{
+  // we have to construct the loop this way because a
+  // given filter could support more than one file type,
+  // so we must query a match for all file types
+  UT_uint32 nrElements = getNumScripts();
+  
+  for (UT_uint32 k=0; k < nrElements; k++)
+    {
+      UT_ScriptSniffer * s = (UT_ScriptSniffer *)mSniffers->getNthItem (k);
+      if (s->recognizeContents(szBuf, iNumbytes))
+	{
+	  for (UT_sint32 a = 0; a < (int) nrElements; a++)
+	    {
+	      if (s->supportsType((UT_ScriptIdType) (a+1)))
+		return (UT_ScriptIdType) (a+1);
+	    }
+	  
+	  UT_ASSERT(UT_SHOULD_NOT_HAPPEN);
+	  // Hm... an importer recognizes the given data
+	  // but refuses to support any file type we request.
+	  return -1;
+	}
+    }
+  
+  // No filter recognizes this data
+  return -1;
+	
+}
+	
+UT_ScriptIdType	UT_ScriptLibrary::typeForSuffix(const char * szSuffix)
+{
+  if (!szSuffix)
+    return -1;
+	
+  // we have to construct the loop this way because a
+  // given filter could support more than one file type,
+  // so we must query a suffix match for all file types
+  UT_uint32 nrElements = getNumScripts();
+  
+  for (UT_uint32 k=0; k < nrElements; k++)
+    {
+      UT_ScriptSniffer * s = static_cast<UT_ScriptSniffer *>(mSniffers->getNthItem(k));
+      if (s->recognizeSuffix(szSuffix))
+	{
+	  for (UT_sint32 a = 0; a < (int) nrElements; a++)
+	    {
+	      if (s->supportsType(static_cast<UT_ScriptIdType>(a+1)))
+		return static_cast<UT_ScriptIdType>(a+1);
+	    }
+	  
+	  UT_ASSERT(UT_SHOULD_NOT_HAPPEN);
+	  // Hm... an importer has registered for the given suffix,
+	  // but refuses to support any file type we request.
+	  return -1;
+	}
+    }
+  
+  // No filter is registered for that extension
+  return -1;  
+}
+
+const char * UT_ScriptLibrary::suffixesForType(UT_ScriptIdType ieft)
+{
+  const char * szSuffixes = 0;
+  
+  // we have to construct the loop this way because a
+  // given filter could support more than one file type,
+  // so we must query a suffix match for all file types
+  UT_uint32 nrElements = getNumScripts();
+  
+  for (UT_uint32 k=0; k < nrElements; k++)
+    {
+      UT_ScriptSniffer * s = static_cast<UT_ScriptSniffer*>(mSniffers->getNthItem(k));
+      if (s->supportsType(ieft))
+	{
+	  const char *szDummy;
+	  UT_ScriptIdType ieftDummy;
+	  if (s->getDlgLabels(&szDummy,&szSuffixes,&ieftDummy))
+	    return szSuffixes;
+	  else
+	    UT_ASSERT(UT_SHOULD_NOT_HAPPEN);
+	}
+    }
+  
+  // The passed in filetype is invalid.
+  return 0;
+}
+	
+UT_Error UT_ScriptLibrary::constructScript(const char * szFilename,
+					   UT_ScriptIdType ieft,
+					   UT_Script ** ppscript, 
+					   UT_ScriptIdType * pieft)
+{
+  //bool bUseGuesswork = (ieft != -1);
+  
+  UT_ASSERT(ieft != -1 || (szFilename && *szFilename));
+  UT_ASSERT(ppscript);
+  
+  // no filter will support -1, so we try to detect
+  // from the contents of the file or the filename suffix
+  // the importer to use and assign that back to ieft.
+  // Give precedence to the file contents
+  if (ieft == -1 && szFilename && *szFilename)
+    {
+      char szBuf[4096];  // 4096 ought to be enough
+      int iNumbytes;
+      FILE *f;
+      // we must open in binary mode for UCS-2 compatibility
+      if ( ( f = fopen( szFilename, "rb" ) ) != (FILE *)0 )
+	{
+	  iNumbytes = fread(szBuf, 1, sizeof(szBuf), f);
+	  fclose(f);
+	  ieft = typeForContents(szBuf, iNumbytes);
+	}
+    }
+  if (ieft == -1 && szFilename && *szFilename)
+    {
+      ieft = typeForSuffix(UT_pathSuffix(szFilename));
+    }
+  
+  UT_ASSERT(ieft != -1);
+  
+  // tell the caller the type of importer they got
+  if (pieft != NULL) 
+    *pieft = ieft;
+  
+  // use the importer for the specified file type
+  UT_uint32 nrElements = getNumScripts();
+  
+  for (UT_uint32 k=0; k < nrElements; k++)
+    {
+      UT_ScriptSniffer * s = (UT_ScriptSniffer *)mSniffers->getNthItem (k);
+      if (s->supportsType(ieft))
+	return s->constructScript(ppscript);
+    }
+
+  // all has failed
+  return UT_ERROR;
+}
---
+++ src/af/util/xp/ut_Script.h
@@ -0,0 +1,114 @@
+/* AbiWord
+ * Copyright (C) 2001 Dom Lachowicz
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#ifndef UT_SCRIPT_H
+#define UT_SCRIPT_H
+
+#include "ut_types.h"
+#include "ut_vector.h"
+#include "ut_AbiObject.h"
+
+typedef UT_sint32 UT_ScriptIdType;
+
+class UT_Script;
+class UT_ScriptLibrary;
+
+class ABI_EXPORT UT_ScriptSniffer : public UT_AbiObject
+{
+	friend class UT_ScriptLibrary;
+	
+public:
+	UT_ScriptSniffer();
+	virtual ~UT_ScriptSniffer();
+	
+	// these you get for free
+	inline bool supportsType (UT_ScriptIdType type) {return m_type == type;}
+	inline UT_ScriptIdType getType() const {return m_type;}
+	
+	// these you must override these
+	virtual bool recognizeContents (const char * szBuf, 
+					UT_uint32 iNumbytes) = 0;
+	virtual bool recognizeSuffix (const char * szSuffix) = 0;
+	virtual bool getDlgLabels (const char ** szDesc,
+				   const char ** szSuffixList,
+				   UT_ScriptIdType * ft) = 0;
+	virtual UT_Error constructScript (UT_Script ** ppscript) = 0;
+	
+private:
+	// only UT_ScriptLibrary ever calls this
+	inline void setType (UT_ScriptIdType type) {m_type = type;}
+	UT_ScriptIdType m_type;
+};
+
+class ABI_EXPORT UT_Script
+{
+ public:
+  
+  virtual UT_Error execute ( const char * scriptName ) = 0;
+  virtual ~UT_Script ();
+
+ private:
+
+  UT_Script ();
+  UT_Script ( const UT_Script & ); // no impl
+  UT_Script& operator=( const UT_Script & ); // no impl
+};
+
+class ABI_EXPORT UT_ScriptLibrary
+{
+ public:
+
+  virtual ~UT_ScriptLibrary ();
+
+  static UT_ScriptLibrary& instance ();
+
+  bool	    enumerateDlgLabels(UT_uint32 ndx,
+			       const char ** pszDesc,
+			       const char ** pszSuffixList,
+			       UT_ScriptIdType * ft);
+
+  UT_Error execute ( const char * script );
+  
+  UT_uint32 getNumScripts () const;
+  void registerScript ( UT_ScriptSniffer * );
+  void unregisterScript ( UT_ScriptSniffer * );
+  void unregisterAllScripts ();
+
+ private:
+
+  UT_ScriptIdType	typeForContents(const char * szBuf,
+					UT_uint32 iNumbytes);
+	
+  UT_ScriptIdType	typeForSuffix(const char * szSuffix);
+  const char *          suffixesForType(UT_ScriptIdType ieft);
+	
+  UT_Error	constructScript(const char * szFilename,
+				UT_ScriptIdType ieft,
+				UT_Script ** ppscript, 
+				UT_ScriptIdType * pieft = NULL);
+  
+  UT_ScriptLibrary ();
+  UT_ScriptLibrary ( const UT_ScriptLibrary & );
+  UT_ScriptLibrary& operator=( const UT_ScriptLibrary & );
+
+  static UT_ScriptLibrary mInstance;
+  UT_Vector *mSniffers;
+};
+
+#endif /* UT_SCRIPT_H */
