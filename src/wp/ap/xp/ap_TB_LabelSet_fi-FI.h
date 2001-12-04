11a0cd59904dab74e90284563235f539e3cf376a
Pierre Abbat <phma@webjockey.net>
2001-12-04 04:31:10 +0000

    Finnish strings by Ismo Mäkinen
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6348 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TB_LabelSet_fi-FI.h
+++ src/wp/ap/xp/ap_TB_LabelSet_fi-FI.h
@@ -1,23 +1,24 @@
 /* AbiWord
- * Copyright (C) 1998-2000 AbiSource, Inc.
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
- * 02111-1307, USA.
- */
+* Copyright (C) 1998-2000 AbiSource, Inc.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+* 02111-1307, USA.
+*/
 
 // Finnish translations provided by Jarmo Karvonen <Jarmo.Karvonen@lpg.fi>
+// Updated by Ismo Mäkinen <ismtaol@luukku.com> - December 2001
 
 /*****************************************************************
 ******************************************************************
@@ -45,55 +46,83 @@
 
 BeginSetEnc(fi,FI,true,"iso-8859-1")
 
-	ToolbarLabel(AP_TOOLBAR_ID__BOGUS1__,		NULL,		NoIcon,			NULL,NULL)
+ToolbarLabel(AP_TOOLBAR_ID__BOGUS1__, NULL, NoIcon, NULL,NULL)
 
-	//          (id, 		                    szLabel,	IconName,     	szToolTip,      szStatusMsg)
+//          (id, szLabel, IconName, szToolTip, szStatusMsg)
 
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Uusi", 		tb_new_xpm,		NULL, "Luo uusi asiakirja")	
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN,		"Avaa",		tb_open_xpm,	NULL, "Avaa asiakirja")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE,		"Tallenna", 	tb_save_xpm,	NULL, "Tallenna asiakirja")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS,		"Tallenna Nimella", 	tb_save_as_xpm,	NULL, "Tallenna käsiteltävä asiakirja eri nimelle")
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT,		"Tulosta",	tb_print_xpm,	NULL, "Tulosta asiakirja")
+ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW, "Uusi", tb_new_xpm, NULL, "Luo uusi asiakirja")     
+ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN, "Avaa", tb_open_xpm, NULL, "Avaa asiakirja")
+ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE, "Tallenna", tb_save_xpm, NULL, "Tallenna asiakirja")
+ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS, "Tallenna nimellä", tb_save_as_xpm, NULL, "Tallenna käsiteltävä asiakirja eri nimellä")
+ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT, "Tulosta", tb_print_xpm, NULL, "Tulosta asiakirja")
+ToolbarLabel(AP_TOOLBAR_ID_FILE_PRINT_PREVIEW, "Esikatselu", tb_print_preview_xpm, NULL, "Tulostuksen esikatselu")
 
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_UNDO,		"Peru",		tb_undo_xpm,	NULL, "Peru muutokset")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_REDO,		"Uudelleen",		tb_redo_xpm,	NULL, "Palauta peruttu muokkaus")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_CUT,		"Leikkaa",		tb_cut_xpm,		NULL, "Leikkaa")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_COPY,		"Kopioi",		tb_copy_xpm,	NULL, "Kopioi")
-	ToolbarLabel(AP_TOOLBAR_ID_EDIT_PASTE,		"Liitä",	tb_paste_xpm,	NULL, "Liitä")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_UNDO, "Peru", tb_undo_xpm, NULL, "Peru muutokset")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_REDO, "Uudelleen", tb_redo_xpm, NULL, "Palauta peruttu muokkaus")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_CUT, "Leikkaa", tb_cut_xpm, NULL, "Leikkaa")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_COPY, "Kopioi", tb_copy_xpm, NULL, "Kopioi")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_PASTE, "Liitä", tb_paste_xpm, NULL, "Liitä")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_HEADER, "Ylätunniste", tb_edit_editheader_xpm, NULL, "Muokkaa ylätunnistetta")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_FOOTER, "Alatunniste", tb_edit_editfooter_xpm, NULL, "Muokkaa alatunnistetta")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_REMOVEHEADER, "Poista ylätunniste", tb_edit_removeheader_xpm, NULL, "Poista ylätunniste")
+ToolbarLabel(AP_TOOLBAR_ID_EDIT_REMOVEFOOTER, "Poista alatunniste", tb_edit_removefooter_xpm, NULL, "Poista alatunniste")
+ToolbarLabel(AP_TOOLBAR_ID_SPELLCHECK, "Oikoluku", tb_spellcheck_xpm, NULL, "Asiakirjan oikeinkirjoituksen tarkistus")
+ToolbarLabel(AP_TOOLBAR_ID_IMG, "Lisää kuva", tb_insert_graphic_xpm, NULL, "Lisää kuva asiakirjaan")
 
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_STYLE,		"Tyyli",	NoIcon,			NULL, "Tyyli")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_FONT,		"Fontti",		NoIcon,			NULL, "Fontti")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_SIZE,		"Fontin koko", NoIcon,		NULL, "Fontin koko")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_BOLD,		"Lihavoitu",		tb_text_bold_L_xpm,		NULL, "Lihavoitu")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_ITALIC,		"Kursivoitu",	tb_text_italic_K_xpm,	NULL, "Kursivoitu")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_UNDERLINE,	"Alleviivattu",tb_text_underline_A_xpm,	NULL, "Alleviivattu")
-        ToolbarLabel(AP_TOOLBAR_ID_FMT_OVERLINE,        "Ylleviivattu",tb_text_overline_xpm,        NULL, "Ylleviivattu")
-	ToolbarLabel(AP_TOOLBAR_ID_FMT_STRIKE,		"Yliviivattu",   tb_text_strikeout_Y_xpm,	NULL, "Yliviivattu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_STYLE, "Tyyli", NoIcon, NULL, "Tyyli")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_FONT, "Fontti", NoIcon, NULL, "Fontti")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_HYPERLINK, "+Hyperlinkki", tb_hyperlink, NULL, "Lisää hyperlinkki asiakirjaan")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_BOOKMARK, "+Kirjanmerkki", tb_anchor, NULL, "Lisää kirjanmerkki asiakirjaan")
 
-        ToolbarLabel(AP_TOOLBAR_ID_FMT_SUPERSCRIPT,     "Yläindeksi",  tb_text_superscript_xpm,        NULL, "Yläindeksi")
-        ToolbarLabel(AP_TOOLBAR_ID_FMT_SUBSCRIPT,       "Alaindeksi",    tb_text_subscript_xpm,          NULL, "Alaindeksi")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_SIZE, "Fontin koko", NoIcon, NULL, "Fontin koko")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_BOLD, "Lihavoitu", tb_text_bold_L_xpm, NULL, "Lihavoitu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_ITALIC, "Kursivoitu", tb_text_italic_K_xpm, NULL, "Kursivoitu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_UNDERLINE, "Alleviivattu",tb_text_underline_A_xpm, NULL, "Alleviivattu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_OVERLINE, "Ylleviivattu",tb_text_overline_xpm, NULL, "Ylleviivattu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_STRIKE, "Yliviivattu", tb_text_strikeout_Y_xpm, NULL, "Yliviivattu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_TOPLINE, "Yläviiva", tb_text_topline_xpm, NULL, "Valitun tekstirivin yläpuolelle viiva")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_BOTTOMLINE, "Alaviiva", tb_text_bottomline_xpm, NULL, "Valitun tekstirivin alapuolelle viiva")
+ToolbarLabel(AP_TOOLBAR_ID_HELP, "Ohje", tb_help_xpm, NULL, "Ohje")
 
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_LEFT,		"Vasen",		tb_text_align_left_xpm,		NULL, "Vasen tasaus")
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_CENTER,	"Keskitetty",	tb_text_center_xpm,	NULL, "Keskitetty tasaus")
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_RIGHT,		"Oikea",	tb_text_align_right_xpm,	NULL, "Oikea tasaus")
-	ToolbarLabel(AP_TOOLBAR_ID_ALIGN_JUSTIFY,	"Molemmat",	tb_text_justify_xpm,	NULL, "Molemmat reunat tasattu")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_SUPERSCRIPT, "Yläindeksi", tb_text_superscript_xpm, NULL, "Yläindeksi")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_SUBSCRIPT, "Alaindeksi", tb_text_subscript_xpm, NULL, "Alaindeksi")
+ToolbarLabel(AP_TOOLBAR_ID_INSERT_SYMBOL, "Lisää merkki", tb_symbol_xpm, NULL, "Lisää (erikois)merkki")
 
-	ToolbarLabel(AP_TOOLBAR_ID_PARA_0BEFORE,	"Ei yhtään ennen",		tb_para_0before_xpm,	NULL, "Riviväli ennen: ei väliä")
-	ToolbarLabel(AP_TOOLBAR_ID_PARA_12BEFORE,	"12 pt ennen",		tb_para_12before_xpm,	NULL, "Riviväli ennen: 12 pt")
+ToolbarLabel(AP_TOOLBAR_ID_ALIGN_LEFT, "Vasen", tb_text_align_left_xpm, NULL, "Vasen tasaus")
+ToolbarLabel(AP_TOOLBAR_ID_ALIGN_CENTER, "Keskitetty", tb_text_center_xpm, NULL, "Keskitetty tasaus")
+ToolbarLabel(AP_TOOLBAR_ID_ALIGN_RIGHT, "Oikea", tb_text_align_right_xpm, NULL, "Oikea tasaus")
+ToolbarLabel(AP_TOOLBAR_ID_ALIGN_JUSTIFY, "Tasattu", tb_text_justify_xpm, NULL, "Molemmat reunat tasattu")
 
-	ToolbarLabel(AP_TOOLBAR_ID_SINGLE_SPACE,	"Ykkös riviväli",	tb_line_single_space_xpm,	NULL, "Ykkös riviväli")
-	ToolbarLabel(AP_TOOLBAR_ID_MIDDLE_SPACE,	"1.5 riviväli",		tb_line_middle_space_xpm,	NULL, "1.5 riviväli")
-	ToolbarLabel(AP_TOOLBAR_ID_DOUBLE_SPACE,	"Kaksois riviväli",	tb_line_double_space_xpm,	NULL, "Kaksois riviväli")
+ToolbarLabel(AP_TOOLBAR_ID_PARA_0BEFORE, "Ei kappaleväliä", tb_para_0before_xpm, NULL, "Riviväli ennen kappaletta: ei väliä")
+ToolbarLabel(AP_TOOLBAR_ID_PARA_12BEFORE, "12 pt ennen", tb_para_12before_xpm, NULL, "Riviväli ennen kappaletta: 12 pt")
 
-	ToolbarLabel(AP_TOOLBAR_ID_1COLUMN,			"1 palsta",			tb_1column_xpm,			NULL, "1 palsta")
-	ToolbarLabel(AP_TOOLBAR_ID_2COLUMN,			"2 palstaa",		tb_2column_xpm,			NULL, "2 palstaa")
-	ToolbarLabel(AP_TOOLBAR_ID_3COLUMN,			"3 palstaa",		tb_3column_xpm,			NULL, "3 palstaa")
+ToolbarLabel(AP_TOOLBAR_ID_SINGLE_SPACE, "Ykkösriviväli", tb_line_single_space_xpm, NULL, "Riviväli 1")
+ToolbarLabel(AP_TOOLBAR_ID_MIDDLE_SPACE, " Riviväli 1,5", tb_line_middle_space_xpm, NULL, " Riviväli 1,5")
+ToolbarLabel(AP_TOOLBAR_ID_DOUBLE_SPACE, "Kakkosriviväli", tb_line_double_space_xpm, NULL, "Riviväli 2")
 
-	ToolbarLabel(AP_TOOLBAR_ID_ZOOM,			"Zoomi",		NoIcon,			NULL, "Zoomi")
-	ToolbarLabel(AP_TOOLBAR_ID_LISTS_BULLETS,               "Luettelo",              tb_lists_xpm,           NULL,           "Luettelo ja listamerkit")
+ToolbarLabel(AP_TOOLBAR_ID_1COLUMN, "1 palsta", tb_1column_xpm, NULL, "1 palsta")
+ToolbarLabel(AP_TOOLBAR_ID_2COLUMN, "2 palstaa", tb_2column_xpm, NULL, "2 palstaa")
+ToolbarLabel(AP_TOOLBAR_ID_3COLUMN, "3 palstaa", tb_3column_xpm, NULL, "3 palstaa")
+ToolbarLabel(AP_TOOLBAR_ID_VIEW_SHOWPARA, "Näytä kaikki", tb_view_showpara_xpm, NULL, "Näytä myöskin tulostumattomat merkit")
 
-	// ... add others here ...
+ToolbarLabel(AP_TOOLBAR_ID_ZOOM, "Zoomaus", NoIcon, NULL, "Näytön suurennussuhde")
+ToolbarLabel(AP_TOOLBAR_ID_LISTS_BULLETS, "Luettelo", tb_lists_xpm, NULL, "Luettelomerkit ja numerointi")
+ToolbarLabel(AP_TOOLBAR_ID_LISTS_NUMBERS, "Numerointi", tb_lists_numbers_xpm, NULL, "Valittujen rivien numerointi")
+ToolbarLabel(AP_TOOLBAR_ID_COLOR_FORE, "Tekstiväri", tb_text_fgcolor_xpm, NULL, "Fontin väri")
+ToolbarLabel(AP_TOOLBAR_ID_COLOR_BACK, "Korostus", tb_text_bgcolor_xpm, NULL, "Fontin taustaväri")
+ToolbarLabel(AP_TOOLBAR_ID_INDENT, "Suurenna sisennystä", tb_text_indent_xpm, NULL, "Suurenna sisennystä")
+ToolbarLabel(AP_TOOLBAR_ID_UNINDENT, "Pienennä sisennystä", tb_text_unindent_xpm, NULL, "Pienennä sisennystä")
+ToolbarLabel(AP_TOOLBAR_ID_SCRIPT_PLAY, "Aja skripti", tb_script_play_xpm, NULL, "Suorita skripti")
 
-	ToolbarLabel(AP_TOOLBAR_ID__BOGUS2__,		NULL,		NoIcon,			NULL,NULL)
+#ifdef BIDI_ENABLED
+ToolbarLabel(AP_TOOLBAR_ID_FMT_DIR_OVERRIDE_LTR, "Vasen-oikea", tb_text_direction_ltr_xpm, NULL, "Tekstin suunta vasemmalta oikealle")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_DIR_OVERRIDE_RTL, " Oikea-vasen", tb_text_direction_rtl_xpm, NULL, "Tekstin suunta oikealta vasemmalle")
+ToolbarLabel(AP_TOOLBAR_ID_FMT_DOM_DIRECTION, "Kappalesuunta ", tb_text_dom_direction_rtl_xpm, NULL, "Muuta kappaleen pääsuuntaa")
+#endif
+
+// ... add others here ...
+
+ToolbarLabel(AP_TOOLBAR_ID__BOGUS2__, NULL, NoIcon, NULL,NULL)
 
 EndSet()
+
