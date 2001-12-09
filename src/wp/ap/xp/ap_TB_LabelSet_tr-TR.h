4b4edb7ca2b16de5644a0b25ebc78a4d68060ab2
Pierre Abbat <phma@webjockey.net>
2001-12-09 12:32:15 +0000

    Remove null character
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6417 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TB_LabelSet_tr-TR.h
+++ src/wp/ap/xp/ap_TB_LabelSet_tr-TR.h
@@ -1,19 +1,19 @@
 /* AbiWord
  * Copyright (C) 1998-2001 AbiSource, Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
 
@@ -49,7 +49,7 @@ BeginSetEnc(tr,TR,true,"cp1254")
 
 	//          (id, 		                    szLabel,		IconName,     			szToolTip,      szStatusMsg)
 
-	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Yeni", 		tb_new_xpm,				NULL, "Yeni belge yarat")	
+	ToolbarLabel(AP_TOOLBAR_ID_FILE_NEW,		"Yeni", 		tb_new_xpm,				NULL, "Yeni belge yarat")
 	ToolbarLabel(AP_TOOLBAR_ID_FILE_OPEN,		"Aç",			tb_open_xpm,			NULL, "Mevcut bir belgeyi aç")
 	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVE,		"Kaydet", 		tb_save_xpm,			NULL, "Belgeyi kaydet")
 	ToolbarLabel(AP_TOOLBAR_ID_FILE_SAVEAS,		"Farklý kaydet",tb_save_as_xpm,			NULL, "Belgeyi farklý adla kaydet")
