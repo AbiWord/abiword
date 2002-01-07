d588463192ceaa8dec1bca61ea4f2003ba634c90
Dom Lachowicz <domlachowicz@gmail.com>
2002-01-07 16:10:27 +0000

    fix this up
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6629 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_mutex.h
+++ src/af/util/xp/ut_mutex.h
@@ -31,15 +31,18 @@ class UT_MutexImpl;
  */
 class UT_Mutex
 {
+  friend class UT_MutexImpl;
+  
  public:
   UT_Mutex ();
   ~UT_Mutex ();
 
+ private:
+  
+  // only called by UT_MutexImpl
   void lock ();
   void unlock ();
 
- private:
-  
   // no impls
   UT_Mutex (const UT_Mutex & other);
   UT_Mutex & operator=(const UT_Mutex & other);
