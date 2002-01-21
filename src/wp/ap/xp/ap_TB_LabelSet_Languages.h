f0f8b84ea858890f02571b0b43b9acb509109307
Pierre Abbat <phma@webjockey.net>
2002-01-21 02:39:47 +0000

    Fix "RTL" in Welsh strings
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6729 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TB_LabelSet_Languages.h
+++ src/wp/ap/xp/ap_TB_LabelSet_Languages.h
@@ -31,6 +31,7 @@
 #include "ap_TB_LabelSet_en-US.h"
 #include "ap_TB_LabelSet_ca-ES.h"
 #include "ap_TB_LabelSet_cs-CZ.h"
+#include "ap_TB_LabelSet_cy-GB.h"
 #include "ap_TB_LabelSet_da-DK.h"
 #include "ap_TB_LabelSet_de-DE.h"
 #include "ap_TB_LabelSet_en-AU.h"
