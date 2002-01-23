36ad6b61521b3d4892d03bceec5a0140e5bb2d11
Rui M. Silva Seabra <rms@1407.org>
2002-01-23 23:58:44 +0000

    Added or corrected GPL notice to files (as suggested by Tomas Frydrych)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6773 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TB_LabelSet_pt-PT.h
+++ src/wp/ap/xp/ap_TB_LabelSet_pt-PT.h
@@ -1,5 +1,5 @@
 /* AbiWord
- * Copyright (C) 1998-2000 AbiSource, Inc.
+ * Copyright (C) 2000-2002 Rui Miguel Seabra <rms@greymalkin.yi.org>
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
