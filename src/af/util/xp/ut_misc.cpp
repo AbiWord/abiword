e26fc28149ad39060f045a6219d41040c51e533a
Dom Lachowicz <domlachowicz@gmail.com>
2002-01-28 16:04:34 +0000

    fjf's html color-parsing code is here now
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6824 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_misc.cpp
+++ src/af/util/xp/ut_misc.cpp
@@ -129,6 +129,242 @@ bool UT_Rect::intersectsRect(const UT_Rect * pRect) const
 /*****************************************************************/
 /*****************************************************************/
 
+/* These are the colors defined in the SVG standard (I haven't checked the final recommendation for changes)
+ */
+struct colorToRGBMapping
+{
+  char * m_name;
+
+  unsigned char m_red;
+  unsigned char m_green;
+  unsigned char m_blue;
+};
+
+static struct colorToRGBMapping s_Colors[] =
+{
+	{ "aliceblue",			240, 248, 255 },
+	{ "antiquewhite",		250, 235, 215 },
+	{ "aqua",			  0, 255, 255 },
+	{ "aquamarine",			127, 255, 212 },
+	{ "azure",			240, 255, 255 },
+	{ "beige",			245, 245, 220 },
+	{ "bisque",			255, 228, 196 },
+	{ "black",			  0,   0,   0 },
+	{ "blanchedalmond",		255, 235, 205 },
+	{ "blue",			  0,   0, 255 },
+	{ "blueviolet",			138,  43, 226 },
+	{ "brown",			165,  42,  42 },
+	{ "burlywood",			222, 184, 135 },
+	{ "cadetblue",			 95, 158, 160 },
+	{ "chartreuse",			127, 255,   0 },
+	{ "chocolate",			210, 105,  30 },
+	{ "coral",			255, 127,  80 },
+	{ "cornflowerblue",		100, 149, 237 },
+	{ "cornsilk",			255, 248, 220 },
+	{ "crimson",			220,  20,  60 },
+	{ "cyan",			  0, 255, 255 },
+	{ "darkblue",			  0,   0, 139 },
+	{ "darkcyan",			  0, 139, 139 },
+	{ "darkgoldenrod",		184, 134,  11 },
+	{ "darkgray",			169, 169, 169 },
+	{ "darkgreen",			  0, 100,   0 },
+	{ "darkgrey",			169, 169, 169 },
+	{ "darkkhaki",			189, 183, 107 },
+	{ "darkmagenta",		139,   0, 139 },
+	{ "darkolivegreen",		 85, 107,  47 },
+	{ "darkorange",			255, 140,   0 },
+	{ "darkorchid",			153,  50, 204 },
+	{ "darkred",			139,   0,   0 },
+	{ "darksalmon",			233, 150, 122 },
+	{ "darkseagreen",		143, 188, 143 },
+	{ "darkslateblue",		 72,  61, 139 },
+	{ "darkslategray",		 47,  79,  79 },
+	{ "darkslategrey",		 47,  79,  79 },
+	{ "darkturquoise",		  0, 206, 209 },
+	{ "darkviolet",			148,   0, 211 },
+	{ "deeppink",			255,  20, 147 },
+	{ "deepskyblue",		  0, 191, 255 },
+	{ "dimgray",			105, 105, 105 },
+	{ "dimgrey",			105, 105, 105 },
+	{ "dodgerblue",			 30, 144, 255 },
+	{ "firebrick",			178,  34,  34 },
+	{ "floralwhite",		255, 250, 240 },
+	{ "forestgreen",		 34, 139,  34 },
+	{ "fuchsia",			255,   0, 255 },
+	{ "gainsboro",			220, 220, 220 },
+	{ "ghostwhite",			248, 248, 255 },
+	{ "gold",			255, 215,   0 },
+	{ "goldenrod",			218, 165,  32 },
+	{ "gray",			128, 128, 128 },
+	{ "grey",			128, 128, 128 },
+	{ "green",			  0, 128,   0 },
+	{ "greenyellow",		173, 255,  47 },
+	{ "honeydew",			240, 255, 240 },
+	{ "hotpink",			255, 105, 180 },
+	{ "indianred",			205,  92,  92 },
+	{ "indigo",			 75,   0, 130 },
+	{ "ivory",			255, 255, 240 },
+	{ "khaki",			240, 230, 140 },
+	{ "lavender",			230, 230, 250 },
+	{ "lavenderblush",		255, 240, 245 },
+	{ "lawngreen",			124, 252,   0 },
+	{ "lemonchiffon",		255, 250, 205 },
+	{ "lightblue",			173, 216, 230 },
+	{ "lightcoral",			240, 128, 128 },
+	{ "lightcyan",			224, 255, 255 },
+	{ "lightgoldenrodyellow",	250, 250, 210 },
+	{ "lightgray",			211, 211, 211 },
+	{ "lightgreen",			144, 238, 144 },
+	{ "lightgrey",			211, 211, 211 },
+	{ "lightpink",			255, 182, 193 },
+	{ "lightsalmon",		255, 160, 122 },
+	{ "lightseagreen",		 32, 178, 170 },
+	{ "lightskyblue",		135, 206, 250 },
+	{ "lightslategray",		119, 136, 153 },
+	{ "lightslategrey",		119, 136, 153 },
+	{ "lightsteelblue",		176, 196, 222 },
+	{ "lightyellow",		255, 255, 224 },
+	{ "lime",			  0, 255,   0 },
+	{ "limegreen",			 50, 205,  50 },
+	{ "linen",			250, 240, 230 },
+	{ "magenta",			255,   0, 255 },
+	{ "maroon",			128,   0,   0 },
+	{ "mediumaquamarine",		102, 205, 170 },
+	{ "mediumblue",			  0,   0, 205 },
+	{ "mediumorchid",		186,  85, 211 },
+	{ "mediumpurple",		147, 112, 219 },
+	{ "mediumseagreen",		 60, 179, 113 },
+	{ "mediumslateblue",		123, 104, 238 },
+	{ "mediumspringgreen",		  0, 250, 154 },
+	{ "mediumturquoise",		 72, 209, 204 },
+	{ "mediumvioletred",		199,  21, 133 },
+	{ "midnightblue",		 25,  25, 112 },
+	{ "mintcream",			245, 255, 250 },
+	{ "mistyrose",			255, 228, 225 },
+	{ "moccasin",			255, 228, 181 },
+	{ "navajowhite",		255, 222, 173 },
+	{ "navy",			  0,   0, 128 },
+	{ "oldlace",			253, 245, 230 },
+	{ "olive",			128, 128,   0 },
+	{ "olivedrab",			107, 142,  35 },
+	{ "orange",			255, 165,   0 },
+	{ "orangered",			255,  69,   0 },
+	{ "orchid",			218, 112, 214 },
+	{ "palegoldenrod",		238, 232, 170 },
+	{ "palegreen",			152, 251, 152 },
+	{ "paleturquoise",		175, 238, 238 },
+	{ "palevioletred",		219, 112, 147 },
+	{ "papayawhip",			255, 239, 213 },
+	{ "peachpuff",			255, 218, 185 },
+	{ "peru",			205, 133,  63 },
+	{ "pink",			255, 192, 203 },
+	{ "plum",			221, 160, 221 },
+	{ "powderblue",			176, 224, 230 },
+	{ "purple",			128,   0, 128 },
+	{ "red",			255,   0,   0 },
+	{ "rosybrown",			188, 143, 143 },
+	{ "royalblue",			 65, 105, 225 },
+	{ "saddlebrown",		139,  69,  19 },
+	{ "salmon",			250, 128, 114 },
+	{ "sandybrown",			244, 164,  96 },
+	{ "seagreen",			 46, 139,  87 },
+	{ "seashell",			255, 245, 238 },
+	{ "sienna",			160,  82,  45 },
+	{ "silver",			192, 192, 192 },
+	{ "skyblue",			135, 206, 235 },
+	{ "slateblue",			106,  90, 205 },
+	{ "slategray",			112, 128, 144 },
+	{ "slategrey",			112, 128, 144 },
+	{ "snow",			255, 250, 250 },
+	{ "springgreen",		  0, 255, 127 },
+	{ "steelblue",			 70, 130, 180 },
+	{ "tan",			210, 180, 140 },
+	{ "teal",			  0, 128, 128 },
+	{ "thistle",			216, 191, 216 },
+	{ "tomato",			255,  99,  71 },
+	{ "turquoise",			 64, 224, 208 },
+	{ "violet",			238, 130, 238 },
+	{ "wheat",			245, 222, 179 },
+	{ "white",			255, 255, 255 },
+	{ "whitesmoke",			245, 245, 245 },
+	{ "yellow",			255, 255,   0 },
+	{ "yellowgreen",		154, 205,  50 }
+};
+
+#ifdef __MRC__
+extern "C" {
+#endif
+
+static int color_compare (const void * a, const void * b)
+{
+  const char * name = (const char *) a;
+  const colorToRGBMapping * id = (const colorToRGBMapping *) b;
+		
+  return UT_stricmp (name, id->m_name);
+}
+
+#ifdef __MRC__
+};
+#endif
+
+static bool s_mapNameToColor (const char * name, char * buf7er)
+{
+  static const char hexval[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+
+  size_t length = sizeof (s_Colors) / sizeof (s_Colors[0]);
+
+  if ((name == 0) || (buf7er == 0)) return false; // Bad programmer! Bad!
+
+  if (name[0] == '#')
+    {
+      bool isValid = true;
+      for (int i = 0; i < 6; i++)
+	{
+	  switch (name[i+1])
+	    {
+	    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+	    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+	      buf7er[i] = name[i+1];
+	      break;
+	    case 'A': buf7er[i] = 'a'; break;
+	    case 'B': buf7er[i] = 'b'; break;
+	    case 'C': buf7er[i] = 'c'; break;
+	    case 'D': buf7er[i] = 'd'; break;
+	    case 'E': buf7er[i] = 'e'; break;
+	    case 'F': buf7er[i] = 'f'; break;
+	    default:
+	      isValid = false;
+	      break;
+	    }
+	  if (!isValid) break;
+	}
+      if (isValid)
+	buf7er[6] = 0;
+      else
+	buf7er[0] = 0;
+
+      return isValid;
+    }
+
+  colorToRGBMapping * id = (colorToRGBMapping *) bsearch (name, s_Colors, (int) length, sizeof (colorToRGBMapping), color_compare);
+
+  if (id == 0)
+    {
+      buf7er[0] = 0;
+      return false;
+    }
+
+  buf7er[0] = hexval[(id->m_red   >> 4) & 0x0f];
+  buf7er[1] = hexval[ id->m_red         & 0x0f];
+  buf7er[2] = hexval[(id->m_green >> 4) & 0x0f];
+  buf7er[3] = hexval[ id->m_green       & 0x0f];
+  buf7er[4] = hexval[(id->m_blue  >> 4) & 0x0f];
+  buf7er[5] = hexval[ id->m_blue        & 0x0f];
+  buf7er[6] = 0;
+
+  return true;
+}
+
 static int x_hexDigit(char c)
 {
 	if ((c>='0') && (c<='9'))
@@ -174,17 +410,19 @@ void UT_setColor(UT_RGBColor & col, unsigned char r, unsigned char g, unsigned c
 
 void UT_parseColor(const char *p, UT_RGBColor& c)
 {
-	if (strlen(p) == 6)
-	{
-		goto ParseHex;
-	}
-	if ((strlen(p) == 7) && (*p=='#'))
-	{
-		p++;
-		goto ParseHex;
-	}
+        UT_uint32 len = strlen (p);
 
-	// TODO consider accepting stock names (vga) colors, too?
+	char returned_color[7] = "";
+
+	if ( s_mapNameToColor ( p, returned_color ) )
+	  {
+	    goto ParseHex;
+	  }
+	else if ( 6 == len )
+	  {
+	    strncpy ( returned_color, p, 6 ) ;
+	    goto ParseHex;
+	  }
 
 	UT_DEBUGMSG(("String = %s \n",p));
 	UT_ASSERT(UT_NOT_IMPLEMENTED);
@@ -192,9 +430,9 @@ void UT_parseColor(const char *p, UT_RGBColor& c)
 	return;
 
  ParseHex:
-	c.m_red = x_hexDigit(p[0]) * 16 + x_hexDigit(p[1]);
-	c.m_grn = x_hexDigit(p[2]) * 16 + x_hexDigit(p[3]);
-	c.m_blu = x_hexDigit(p[4]) * 16 + x_hexDigit(p[5]);
+	c.m_red = x_hexDigit(returned_color[0]) * 16 + x_hexDigit(returned_color[1]);
+	c.m_grn = x_hexDigit(returned_color[2]) * 16 + x_hexDigit(returned_color[3]);
+	c.m_blu = x_hexDigit(returned_color[4]) * 16 + x_hexDigit(returned_color[5]);
 
 	return;
 }
