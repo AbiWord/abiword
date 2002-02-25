f53513c6c7ebcf229b22188cb924b085be6307c6
Dom Lachowicz <domlachowicz@gmail.com>
2002-02-25 13:14:48 +0000

    95% done now :-)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@6950 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_exp_WML.cpp
+++ src/wp/impexp/xp/ie_exp_WML.cpp
@@ -610,10 +610,7 @@ void s_WML_Listener::_outputData(const UT_UCSChar * data, UT_uint32 length)
 					UT_UCSChar c = XAP_EncodingManager::get_instance()->try_UToNative(*pData);
 					if (c==0 || c>255)
 					{
-						char localBuf[20];
-						char * plocal = localBuf;
-						sprintf(localBuf,"&#x%x;",*pData++);
-						sBuf += plocal;
+					  sBuf += UT_String_sprintf("&#x%x;",*pData++);
 					}
 					else
 					{
