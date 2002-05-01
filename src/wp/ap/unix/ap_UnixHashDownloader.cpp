fc0655dc07fe7fdd81ab2432d4029039ecdd21ff
Dom Lachowicz <domlachowicz@gmail.com>
2002-05-01 16:17:09 +0000

    more curl removal
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@7646 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/unix/ap_UnixHashDownloader.cpp
+++ src/wp/ap/unix/ap_UnixHashDownloader.cpp
@@ -1,5 +1,5 @@
 /* AbiWord
- * Copyright (C) 1998-2000 AbiSource, Inc.
+ * Copyright (C) 2002 Gabriel
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
