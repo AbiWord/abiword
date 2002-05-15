343e6f95d524cd8a415f4752b0f08f6884eb7213
Francis J Franklin <fjf@svn.abisource.com>
2002-05-15 19:03:00 +0000

    o 2 new classes (untested!): UT_UCS2_mbtowc & UT_UCS4_mbtowc
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@7859 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_mbtowc.cpp
+++ src/af/util/xp/ut_mbtowc.cpp
@@ -289,3 +289,199 @@ int UT_Mbtowc::mbtowc(wchar_t &wc,char mb)
 };
 
 #endif /* big if 0 */
+
+UT_UCS2_mbtowc::Converter::Converter (const char * from_charset) :
+  m_count(1),
+  m_cd(UT_iconv_open (ucs2Internal (), from_charset))
+{
+  // 
+}
+
+UT_UCS2_mbtowc::Converter::~Converter ()
+{
+  if (UT_iconv_isValid (m_cd)) UT_iconv_close (m_cd);
+}
+
+void UT_UCS2_mbtowc::Converter::initialize ()
+{
+  UT_iconv_reset (m_cd);
+}
+
+void UT_UCS2_mbtowc::initialize (bool clear)
+{
+  m_converter->initialize ();
+  if (clear) m_bufLen = 0;
+}
+
+UT_UCS2_mbtowc::UT_UCS2_mbtowc () :
+  m_converter(new Converter(XAP_EncodingManager::get_instance()->getNative8BitEncodingName())),
+  m_bufLen(0)
+{
+  // 
+}
+
+UT_UCS2_mbtowc::UT_UCS2_mbtowc (const char * from_charset) :
+  m_converter(new Converter(from_charset)),
+  m_bufLen(0)
+{
+  // 
+}
+
+UT_UCS2_mbtowc::UT_UCS2_mbtowc (const UT_UCS2_mbtowc & rhs) :
+  m_converter(rhs.m_converter),
+  m_bufLen(0)
+{
+  m_converter->ref ();
+}
+
+UT_UCS2_mbtowc::~UT_UCS2_mbtowc ()
+{
+  if (!m_converter->unref ()) delete m_converter;
+}
+
+void UT_UCS2_mbtowc::setInCharset (const char * from_charset)
+{
+  Converter * converter = new Converter(from_charset);
+  if (converter)
+    {
+      if (!m_converter->unref ()) delete m_converter;
+      m_converter = converter;
+    }
+}
+
+int UT_UCS2_mbtowc::mbtowc (UT_UCS2Char & wc, char mb)
+{
+  if(++m_bufLen > MY_MB_LEN_MAX)
+    {
+      initialize ();
+      return 0;
+    }
+  m_buf[m_bufLen-1] = mb;
+
+  const char * inptr = m_buf;
+
+  UT_UCS2Char ucs2;
+  char * outptr = reinterpret_cast<char *>(&ucs2);
+
+  size_t inlen = m_bufLen;
+  size_t outlen = sizeof (UT_UCS2Char);
+
+  const UT_iconv_t cd = m_converter->cd ();
+
+  size_t len = UT_iconv (const_cast<UT_iconv_t>(cd), &inptr, &inlen, &outptr, &outlen);
+  if (len != (size_t)-1)
+    {
+      wc = ucs2;
+      m_bufLen = 0;
+      return 1;
+    }
+  if (errno == EINVAL)
+    {
+      /* reset iconv, pointer might be messed up; need more chars...
+       */
+      initialize (false);
+    }
+  else
+    {
+      initialize (true); /* wrong seq */
+    }
+  return 0;
+}
+
+UT_UCS4_mbtowc::Converter::Converter (const char * from_charset) :
+  m_count(1),
+  m_cd(UT_iconv_open ("UCS_INTERNAL", from_charset))
+{
+  // 
+}
+
+UT_UCS4_mbtowc::Converter::~Converter ()
+{
+  if (UT_iconv_isValid (m_cd)) UT_iconv_close (m_cd);
+}
+
+void UT_UCS4_mbtowc::Converter::initialize ()
+{
+  UT_iconv_reset (m_cd);
+}
+
+void UT_UCS4_mbtowc::initialize (bool clear)
+{
+  m_converter->initialize ();
+  if (clear) m_bufLen = 0;
+}
+
+UT_UCS4_mbtowc::UT_UCS4_mbtowc () :
+  m_converter(new Converter(XAP_EncodingManager::get_instance()->getNative8BitEncodingName())),
+  m_bufLen(0)
+{
+  // 
+}
+
+UT_UCS4_mbtowc::UT_UCS4_mbtowc (const char * from_charset) :
+  m_converter(new Converter(from_charset)),
+  m_bufLen(0)
+{
+  // 
+}
+
+UT_UCS4_mbtowc::UT_UCS4_mbtowc (const UT_UCS4_mbtowc & rhs) :
+  m_converter(rhs.m_converter),
+  m_bufLen(0)
+{
+  m_converter->ref ();
+}
+
+UT_UCS4_mbtowc::~UT_UCS4_mbtowc ()
+{
+  if (!m_converter->unref ()) delete m_converter;
+}
+
+void UT_UCS4_mbtowc::setInCharset (const char * from_charset)
+{
+  Converter * converter = new Converter(from_charset);
+  if (converter)
+    {
+      if (!m_converter->unref ()) delete m_converter;
+      m_converter = converter;
+    }
+}
+
+int UT_UCS4_mbtowc::mbtowc (UT_UCS4Char & wc, char mb)
+{
+  if(++m_bufLen > MY_MB_LEN_MAX)
+    {
+      initialize ();
+      return 0;
+    }
+  m_buf[m_bufLen-1] = mb;
+
+  const char * inptr = m_buf;
+
+  UT_UCS4Char ucs4;
+  char * outptr = reinterpret_cast<char *>(&ucs4);
+
+  size_t inlen = m_bufLen;
+  size_t outlen = sizeof (UT_UCS4Char);
+
+  const UT_iconv_t cd = m_converter->cd ();
+
+  size_t len = UT_iconv (const_cast<UT_iconv_t>(cd), &inptr, &inlen, &outptr, &outlen);
+  if (len != (size_t)-1)
+    {
+      wc = ucs4;
+      m_bufLen = 0;
+      return 1;
+    }
+  if (errno == EINVAL)
+    {
+      /* reset iconv, pointer might be messed up; need more chars...
+       */
+      initialize (false);
+    }
+  else
+    {
+      initialize (true); /* wrong seq */
+    }
+  return 0;
+}
--- src/af/util/xp/ut_mbtowc.h
+++ src/af/util/xp/ut_mbtowc.h
@@ -56,6 +56,96 @@ public:
   UT_iconv_t cd;
 };
 
+class ABI_EXPORT UT_UCS2_mbtowc
+{
+ private:
+  class ABI_EXPORT Converter
+    {
+    public:
+      Converter (const char * from_charset);
+
+      ~Converter ();
+
+      void initialize ();
+
+      inline int   ref () { return ++m_count; }
+      inline int unref () { return --m_count; }
+
+      inline int count () const { return m_count; }
+
+      inline const UT_iconv_t cd () const { return m_cd; }
+
+    private:
+      int m_count;
+
+      UT_iconv_t m_cd;
+    };
+  Converter * m_converter;
+
+ public:
+  void initialize (bool clear = true);
+
+  UT_UCS2_mbtowc ();
+  UT_UCS2_mbtowc (const char * from_charset);
+  UT_UCS2_mbtowc (const UT_UCS2_mbtowc & v);
+
+  ~UT_UCS2_mbtowc();  
+
+  void setInCharset (const char * from_charset);
+
+  int mbtowc (UT_UCS2Char & wc, char mb);
+
+ private:
+  int m_bufLen;
+
+  char m_buf[MB_LEN_MAX];
+};
+
+class ABI_EXPORT UT_UCS4_mbtowc
+{
+ private:
+  class ABI_EXPORT Converter
+    {
+    public:
+      Converter (const char * from_charset);
+
+      ~Converter ();
+
+      void initialize ();
+
+      inline int   ref () { return ++m_count; }
+      inline int unref () { return --m_count; }
+
+      inline int count () const { return m_count; }
+
+      inline const UT_iconv_t cd () const { return m_cd; }
+
+    private:
+      int m_count;
+
+      UT_iconv_t m_cd;
+    };
+  Converter * m_converter;
+
+ public:
+  void initialize (bool clear = true);
+
+  UT_UCS4_mbtowc ();
+  UT_UCS4_mbtowc (const char * from_charset);
+  UT_UCS4_mbtowc (const UT_UCS4_mbtowc & v);
+
+  ~UT_UCS4_mbtowc();  
+
+  void setInCharset (const char * from_charset);
+
+  int mbtowc (UT_UCS4Char & wc, char mb);
+
+ private:
+  int m_bufLen;
+
+  char m_buf[MB_LEN_MAX];
+};
+
 #endif
 
 #endif // UT_MBTOWC_H
