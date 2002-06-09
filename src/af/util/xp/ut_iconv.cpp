41f6a60c96ce843e152cf15fa21a838240ac52ac
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-09 12:32:21 +0000

    cvs oops
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8220 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_iconv.cpp
+++ src/af/util/xp/ut_iconv.cpp
@@ -186,7 +186,7 @@ size_t  UT_iconv( UT_iconv_t cd, const char **inbuf,
   if ( !UT_iconv_isValid ( cd ) )
     return (size_t)-1;
 
-  const char ** buf = (const char**)(inbuf);
+
   ICONV_CONST char ** buf = (ICONV_CONST char**)(inbuf);
   return iconv( cd, buf, inbytesleft, outbuf, outbytesleft );
 }
