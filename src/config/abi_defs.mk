b90787037343f6112aada2a1ed6ad1bf38a58460
Johann Bjork <phearbear@svn.abisource.com>
2002-06-12 10:27:15 +0000

    Moved the ifeq(ABI_OPT_CURLHASH,1) from the unix specific past, as this now works on all platforms.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8287 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/abi_defs.mk
+++ src/config/abi_defs.mk
@@ -696,13 +696,13 @@ endif
 ifeq ($(ABI_NATIVE),unix)
 EXTRA_LIBS	+=	-L$(ABI_ROOT)/../popt/.libs -lpopt
 endif
+endif #ifeq(platform,unix)
 
 ifeq ($(ABI_OPT_CURLHASH),1)
 LIBCURL_CFLAGS  =       $(shell curl-config --cflags)
 LIBCURL_LIBS    =       $(shell curl-config --libs)
 CFLAGS          +=      $(LIBCURL_CFLAGS) -DHAVE_CURLHASH=1
 EXTRA_LIBS      +=      $(LIBCURL_LIBS)
-endif
 endif
 
 ##################################################################
