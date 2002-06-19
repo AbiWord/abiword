8d97c268c259eb8969d58497737d8ad17e8b74f6
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-19 19:56:21 +0000

    (3/15) Add another platform to the list.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8378 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/aix.mk
+++ src/config/platforms/aix.mk
@@ -23,78 +23,36 @@
 ## (Philippe.Defert@cern.ch)
 ##############################################################################
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
+# Here for syntactic purposes
+##
+PLATFORM_FLAGS		=
+PORT_FLAGS		=
+OS_CFLAGS		=
+##
+
+# Now we include some really common stuff.
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
+
+## IMPORTANT: Do not edit anything above here, or anything included above here.
+## 	      You can redefine anything and everything below this point.
 
 OS_ARCH		:= $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ | sed "s/\//-/")
 OS_ENDIAN	= BigEndian32
 
-# Define tools
-CC		= gcc
-CCC		= g++
-RANLIB		= ranlib
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
 # Compiler flags
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall -ansi -pedantic
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	= DBG
-else
-OPTIMIZER	= -O2 -Wall -ansi -pedantic
-DEFINES		=
-OBJ_DIR_SFX	= OBJ
-endif
-
-# Includes
-OS_INCLUDES		=
-G++INCLUDES		= -I/usr/include/g++
-
-# Compiler flags
-PLATFORM_FLAGS		= 
-PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE
-OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
-
-PLATFORM_FLAGS		+=
-PORT_FLAGS		+= 
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-GNOME_CONFIG    	= gnome-config
+PLATFORM_FLAGS		+= 
+PORT_FLAGS		+= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE
+OS_CFLAGS		+= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
 # Shared library flags
+# Are you kidding me?  This conflicts with IBM's documentation,
+# but I'll leave it for now Just In Case (tm).
 MKSHLIB			= 
-
-# Which links can this platform create.  Define one or
-# both of these options. (what can IRIX do?  someone
-# with an SGI set the right one and mail a patch)
-UNIX_CAN_BUILD_DYNAMIC=1
-UNIX_CAN_BUILD_STATIC=0
 
 # Compiler options for static and dynamic linkage
 DL_LIBS			=
 STATIC_FLAGS		= -static
 LDFLAGS			= -Wl,-bbigtoc
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 ##################################################################              
 ## ABIPKGDIR defines the directory containing the Makefile to use to            
