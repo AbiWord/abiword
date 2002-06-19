4413cee12678fc199e02aa722a65644e4aa812fd
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-19 20:11:33 +0000

    (4/15) Add another platform, as well as some newly tested optimizations.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8379 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/cygwin.mk
+++ src/config/platforms/cygwin.mk
@@ -23,124 +23,32 @@
 ## derived from linux makefile by Tom Newton <tomn@eeyore-mule.demon.co.uk>
 ## on 2/3/2000
 ##############################################################################
+# Here for syntactic purposes
+##
+PLATFORM_FLAGS		=
+PORT_FLAGS		=
+OS_CFLAGS		=
+##
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
+ABI_REQUIRE_PEER_ICONV=1
 
-OS_ARCH		:= $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ | sed "s/\//-/")
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 
-
-# Define architecture-specific flags (L. Kollar, 3 Nov 1998)
-# These are (probably) optional for your platform.
-
-# Compiler defaults should be fine for Intel.
-i386_ARCH_FLAGS		=
-
-# Jerry LeVan <levan@eagle.eku.edu> provided the PPC flags
-# Gary Thomas <gdt@linuxppc.org> suggests using -fno-schedule-insns2 
-# for some EGCS builds
-PPC_ARCH_FLAGS		= -fsigned-char -fno-schedule-insns2
-
-ALPHA_ARCH_FLAGS 	= 
-SPARC_ARCH_FLAGS 	= 
-
-# Define tools
-CC		= gcc
-CCC		= g++
-RANLIB		= ranlib
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= dll
-AR		= ar cr $@
-
-# Compiler flags
-# NOTE:  Using both "-Wall" and "-W" turn on every single darn message
-# NOTE:  GCC can throw, and it turns quiet compiles into raging rivers of
-# NOTE:  warnings.  -Wall includes the very useful warnings, -W includes
-# NOTE:  more stylistic warnings.  -pedantic just gets really picky about
-# NOTE:  ANSI things.
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall -W -ansi -pedantic
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	= DBG
-else
-# NOTE:  In some instances, GCC can only know about truly unused variables
-# NOTE:  when optimizations are enabled in the compilation.  For this reason,
-# NOTE:  building with optimizations may reveal further warnings not 
-# NOTE:  visible without any -O[number] option.
-OPTIMIZER	= -O2 -Wall -W -ansi -pedantic
-DEFINES		=
-OBJ_DIR_SFX	= OBJ
-endif
-
-# Includes
-OS_INCLUDES		= -I$(ABI_XX_ROOT)/../libiconv/include
-G++INCLUDES		= -I/usr/include/g++
+DLL_SUFFIX = dll
 
 # Compiler flags
 PLATFORM_FLAGS		= -pipe
 PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE -D__USE_XOPEN_EXTENDED
 OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
-# Architecture-specific flags
-ifeq ($(OS_ARCH), i386)
-PLATFORM_FLAGS		+= $(i386_ARCH_FLAGS)
-OS_ENDIAN		= LittleEndian32
-endif
-
-ifeq ($(OS_ARCH), ppc)
-PLATFORM_FLAGS		+= $(PPC_ARCH_FLAGS)
-OS_ENDIAN		= BigEndian32
-endif
-
-ifeq ($(OS_ARCH), alpha)
-PLATFORM_FLAGS		+= $(ALPHA_ARCH_FLAGS)
-OS_ENDIAN		= LittleEndian32
-endif
-
-ifeq ($(OS_ARCH), sparc)
-PLATFORM_FLAGS		+= $(SPARC_ARCH_FLAGS)
-OS_ENDIAN		= BigEndian32
-endif
-
-# Endian for M68K from Roman Hodek <Roman.Hodek@informatik.uni-erlangen.de>
-ifeq ($(OS_ARCH), m68k)
-PLATFORM_FLAGS		+= $(M68K_ARCH_FLAGS)
-OS_ENDIAN		= BigEndian32
-endif
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-GNOME_CONFIG    	= gnome-config
-
 # Shared library flags
 MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
 
-# Which links can this platform create.  Define one or
-# both of these options.
-# UNIX_CAN_BUILD_DYNAMIC=1
 UNIX_CAN_BUILD_STATIC=1
 
 # Compiler options for static and dynamic linkage
 DL_LIBS			= dl
 STATIC_FLAGS		= -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 ##################################################################
 ## ABIPKGDIR defines the directory containing the Makefile to use to
@@ -148,6 +56,6 @@ ABI_FE		= Unix
 ## This is relative to $(ABI_ROOT)/src/pkg
 
 ABIPKGDIR	= cygwin
-PSICONV_PLATFORM_DEFS= CFLAGS='-O2'
+PSICONV_PLATFORM_DEFS= CFLAGS='-O3 -fomit-frame-pointer'
 
 # End of cygwin defs
