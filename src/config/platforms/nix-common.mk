a3f48a68a9480d1e84944eef1dd479ef66548c79
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-19 11:03:45 +0000

    This is phase one (and rather cut down from that for safety purposes) of the giant diving make overhaul.  It should make things far easier to maintain, as well as more efficient.  Included is new crap, my patented compiler pacifier, optimization tweaks, ABI_OPT_TINY (much sexier in gcc3), a truckload o' fixes, gtk2 preparation crap from combined efforts between fjf and myself... Anyway, it's beautiful. Platform maintainers should NEVER modify nix-common, and ALWAYS modify BELOW the nix-common inclusion line.  That will insure 1) their stuff doesnt get stomped on and 2) they dont stomp on other people's stuff. More on the way, just some bestcase and worstcase scenarios here. -MG
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8370 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/config/platforms/nix-common.mk
@@ -0,0 +1,233 @@
+#! gmake
+
+## AbiSource Program Utilities
+## Copyright (C) 1998 AbiSource, Inc.
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+## 02111-1307, USA.
+
+##############################################################################
+## Common Unix(-like) platform defines
+##############################################################################
+
+## If a platform ever needs to differ in some way or other, and you have
+## doubts regarding the right conditional or if there is one, simply redefine
+## in that platform's .mk in this directory.  I'll keep a close eye on this to
+## make sure it doesn't get out of hand.  If it does, it'll be scrapped.  -MG
+
+##################################################################
+##################################################################
+## The main makefile and/or this file requires that OS_ARCH be set
+## to something to describe which chip that this OS is running on.
+## This can be used to change which tools are used and/or which
+## compiler/loader options are used.  It will probably also be used
+## in constructing the name object file destination directory.
+
+OS_ARCH		:= $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ | sed "s/\//-/")
+OS_REALARCH	:= $(shell uname -m)
+
+# Define architecture-specific flags (L. Kollar, 3 Nov 1998)
+# These are (probably) optional for your platform.
+
+# Compiler defaults should be fine for Intel.
+i386_ARCH_FLAGS		=
+
+# Jerry LeVan <levan@eagle.eku.edu> provided the PPC flags
+# Gary Thomas <gdt@linuxppc.org> suggests using -fno-schedule-insns2
+# for some EGCS builds
+# Those no longer are able to build our deps anyway, and thus there's no reason
+# not to gain the optimization of a second instruction scheduler pass.
+
+PPC_ARCH_FLAGS		= -fsigned-char
+
+ALPHA_ARCH_FLAGS 	=
+SPARC_ARCH_FLAGS 	=
+IA64_ARCH_FLAGS		=
+S390_ARCH_FLAGS		=
+
+# Define tools
+CC		= gcc
+CCC		= g++
+RANLIB		= ranlib
+
+# Suffixes
+OBJ_SUFFIX	= o
+LIB_SUFFIX	= a
+DLL_SUFFIX	= so
+AR		= ar cr $@
+
+# Compiler flags
+
+ifeq ($(ABI_OPT_PANGO),1)
+	OBJ_DIR_SFX	= PANGO_
+else
+	OBJ_DIR_SFX	=
+endif
+
+DEFINES		=
+OPTIMIZER	=
+
+ifeq ($(ABI_OPT_PROF),1)
+OPTIMIZER   	= -pg -fprofile-arcs -ftest-coverage
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)PRF_
+ABI_OPT_OPTIMIZE= 1
+ABI_OPT_DEBUG	= 0
+ABI_OPTIONS	+= Profile:On
+endif
+
+ifeq ($(ABI_OPT_OPTIMIZE),1)
+OPTIMIZER	+= -O3 -fomit-frame-pointer
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OPT_
+ABI_OPTIONS	+= Optimize:On
+ABI_OPT_DEBUG	= 0
+    ifeq ($(ABI_OPT_EXCLUSIVE_OPT),1)
+    OPTIMIZER := $(OPTIMIZER) -march=$(OS_REALARCH)
+    else
+    OPTIMIZER := $(OPTIMIZER) -mcpu=$(OS_REALARCH)
+    endif
+else
+    ifeq ($(ABI_OPT_TINY),1)
+    OPTIMIZER	= -Os -fno-default-inline -fno-inline
+    else
+    OPTIMIZER	= -O2
+    endif
+endif
+
+ifeq ($(ABI_OPT_DEBUG),1)
+OPTIMIZER	= -g -Wall -pedantic -Wno-long-long
+ABI_OPT_PACIFY_COMPILER = 1
+DEFINES		= -DDEBUG -UNDEBUG
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)DBG_
+endif
+
+
+ifeq ($(ABI_OPT_GNOME),2)
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)GNOME2_
+endif	
+ifeq ($(ABI_OPT_GNOME),1)
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)GNOME_
+endif	
+ifeq ($(ABI_OPT_PEER_EXPAT),1)
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)EXP_
+endif
+
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OBJ
+
+ifeq ($(ABI_OPT_WAY_TOO_MANY_WARNINGS),1)
+WARNFLAGS 	= -Weffc++
+else
+WARNFLAGS	=
+endif
+
+ifneq ($(ABI_OPT_PACIFY_COMPILER),1)
+WARNFLAGS	+= -Wall -ansi -pedantic
+else
+WARNFLAGS	+= -fpermissive
+endif
+
+# Includes
+OS_INCLUDES		=
+ifeq ($(ABI_REQUIRE_PEER_ICONV),1)
+OS_INCLUDES		+= -I$(ABI_ROOT)/../libiconv/include
+endif
+G++INCLUDES		= -I/usr/include/g++
+
+# TODO: This is true on about 1/10 platforms.  Why not use pkgconfig?
+ifeq ($(ABI_OPT_PANGO),1)
+OS_INCLUDES += -I/usr/local/include/pango-1.0 -I/usr/local/include/freetype2 -I/usr/local/include/glib-2.0
+endif
+
+# Architecture-specific flags
+ifeq ($(OS_ARCH), i386)
+PLATFORM_FLAGS		+= $(i386_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), ppc)
+PLATFORM_FLAGS		+= $(PPC_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+ifeq ($(OS_ARCH), alpha)
+PLATFORM_FLAGS		+= $(ALPHA_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), sparc)
+PLATFORM_FLAGS		+= $(SPARC_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+# Endian for ARM from Jim Studt <jim@federated.com>
+ifeq ($(OS_ARCH), arm)
+PLATFORM_FLAGS		+= $(ARM_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+# Endian for M68K from Roman Hodek <Roman.Hodek@informatik.uni-erlangen.de>
+ifeq ($(OS_ARCH), m68k)
+PLATFORM_FLAGS		+= $(M68K_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+ifeq ($(OS_ARCH), ia64)
+PLATFORM_FLAGS		+= $(IA64_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), s390)
+PLATFORM_FLAGS		+= $(S390_ARCH_FLAGS)
+OS_ENDIAN		= BigEndian32
+endif
+
+ifeq ($(OS_ARCH), parisc)
+PLATFORM_FLAGS      += $(HPPA_ARCH_FLAGS)
+OS_ENDIAN       = BigEndian32
+endif
+
+ifeq ($(OS_ARCH), parisc64)
+PLATFORM_FLAGS      += $(HPPA_ARCH_FLAGS)
+OS_ENDIAN       = BigEndian32
+endif
+
+ifeq ($(ABI_OPT_GTK),2) # GTK, not GNOME. -MG
+GLIB_CONFIG	= pkg-config glib-2.0
+GTK_CONFIG	= pkg-config gtk+-2.0
+# This is wrong.  So far the best option I've seen is gnome-desktop-2.0, but
+# that doesn't necessarily catch gnomeprint.  It also puts in libxml2 AFAICT.
+# We gotta find the best way to pull it all off real soon.  Until then, I'm commenting it. -MG
+# GNOME_CONFIG    = pkg-config gnome-2.0
+else
+GLIB_CONFIG	= glib-config
+GTK_CONFIG	= gtk-config
+GNOME_CONFIG    = gnome-config
+endif
+
+LIBXML_CONFIG	= xml2-config
+
+# Shared library flags
+MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
+#MKSHLIB			= g++ -shared -o  $(@:$(OBJDIR)/%.so=%.so)
+
+
+# Which links can this platform create.  Define one or
+# both of these options.
+UNIX_CAN_BUILD_DYNAMIC=1
+# Too many users with the wrong X Extension library, set to 0 as default.
+UNIX_CAN_BUILD_STATIC=0
+
+ABI_NATIVE	= unix
+ABI_FE		= Unix
+
--- src/config/platforms/openbsd.mk
+++ src/config/platforms/openbsd.mk
@@ -21,127 +21,39 @@
 ##############################################################################
 ## OpenBSD platform defines, courtesy of kstailey@kstailey.tzo.com
 ##############################################################################
+ABI_REQUIRE_PEER_ICONV = 1
+ABI_OPT_PEER_EXPAT=1 # I promise I'll fix this...sometime.
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
+# Here for syntactic purposes.
+##
+PLATFORM_FLAGS		=
+PORT_FLAGS		=
+OS_CFLAGS		=
+##
 
-OS_ARCH                := $(shell uname -m)
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 
-ABI_OPT_PEER_EXPAT=1 # See below
-
+# This sucks
 ifneq (,$(shell $(CC) -E - -dM </usr/include/machine/endian.h | grep BYTE_ORDER.*LITTLE_ENDIAN))
 OS_ENDIAN      = LittleEndian32
 else
 OS_ENDIAN      = BigEndian32
 endif
 
-# Define tools
-CC		= gcc
-CCC		= g++
-RANLIB		= ranlib
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
-DEFINES		=
-OPTIMIZER	= 
-
-ifeq ($(ABI_OPT_PROF),1)
-OPTIMIZER	= -pg -fprofile-arcs -ftest-coverage
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)PRF_
-ABI_OPT_OPTIMIZE= 1
-ABI_OPTIONS	+= Profile:On
-endif
-
-ifeq ($(ABI_OPT_OPTIMIZE),1)
-OPTIMIZER	+= -O3 -fomit-frame-pointer
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OPT_
-ABI_OPTIONS	+= Optimize:On
-ABI_OPT_DEBUG	= 0
-else
-OPTIMIZER	= -O2
-endif
-
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall -pedantic -Wno-long-long
-ABI_OPT_PACIFY_COMPILER = 1
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)DBG_
-endif
-
-
-ifeq ($(ABI_OPT_GNOME),1)
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)GNOME_
-endif
-ifeq ($(ABI_OPT_PEER_EXPAT),1)
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)EXP_
-endif
-
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OBJ
-
-ifeq ($(ABI_OPT_WAY_TOO_MANY_WARNINGS),1)
-WARNFLAGS	= -Weffc++
-else
-WARNFLAGS	=
-endif
-
-ifneq ($(ABI_OPT_PACIFY_COMPILER),1)
-WARNFLAGS	+= -Wall -ansi -pedantic
-endif
-
-ABI_REQUIRE_PEER_ICONV = 1
 # Includes
-ifeq ($(ABI_REQUIRE_PEER_ICONV),1)
-OS_INCLUDES	= -I$(ABI_ROOT)/../libiconv/include -I/usr/local/include
-else
-OS_INCLUDES	= -I/usr/local/include
-endif
-G++INCLUDES	= -I/usr/include/g++
+OS_INCLUDES	+= -I/usr/local/include
 
 # Compiler flags
-PLATFORM_FLAGS		= -pipe -DOPENBSD -DOpenBSD
-PORT_FLAGS		= -DHAVE_STRERROR
-OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
+PLATFORM_FLAGS		+= -pipe -DOPENBSD -DOpenBSD
+PORT_FLAGS		+= -DHAVE_STRERROR
+OS_CFLAGS		+= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
-PLATFORM_FLAGS		+= 
-PORT_FLAGS		+= 
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-ifeq ($(ABI_OPT_GNOME),1)
-GNOME_CONFIG    	= gnome-config
-endif
-# For now no libxml2 check, hardwire expat.
-
-# Shared library flags
+# Shared library flags redefinition
 MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
-
-# Which links can this platform create.  Define one or
-# both of these options.
-UNIX_CAN_BUILD_DYNAMIC=1
-UNIX_CAN_BUILD_STATIC=1
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 # Compiler options for static and dynamic linkage
 DL_LIBS			= 
 STATIC_FLAGS		=  -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
 
 # End of OpenBSD defs
 
