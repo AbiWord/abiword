2a5c50f96c1b92b3855e559eaf13f1a94983559b
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-20 17:29:17 +0000

    (10/15) This one might need another pass, but it should at least work.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8387 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/osf1.mk
+++ src/config/platforms/osf1.mk
@@ -23,60 +23,14 @@
 ## (Philippe.Defert@cern.ch)
 ##############################################################################
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
-
-OS_ARCH		:= $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ -e s/arm.*/arm/ -e s/sa110/arm/ | sed "s/\//-/")
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 OS_ENDIAN	= LittleEndian32
-
-# Define tools
-CC		= gcc
-CCC		= g++
-RANLIB		= ranlib
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
-# Compiler flags
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall -ansi -pedantic
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	= DBG
-else
-OPTIMIZER	= -O2 -Wall -ansi -pedantic
-DEFINES		=
-OBJ_DIR_SFX	= OBJ
-endif
-
-# Includes
-OS_INCLUDES		=
-G++INCLUDES		= -I/usr/include/g++
 
 # Compiler flags
 PLATFORM_FLAGS		= 
 PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE
 OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
-PLATFORM_FLAGS		+= 
-PORT_FLAGS		+= 
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-GNOME_CONFIG    	= gnome-config
-
-# Shared library flags
-MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
-
-# Which links can this platform create.  Define one or
-# both of these options.
 UNIX_CAN_BUILD_DYNAMIC=1
 UNIX_CAN_BUILD_STATIC=1
 
@@ -84,16 +38,6 @@ UNIX_CAN_BUILD_STATIC=1
 # ??? does Digital Unix need dl ???
 DL_LIBS			= 
 STATIC_FLAGS		= -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 ##################################################################              
 ## ABIPKGDIR defines the directory containing the Makefile to use to            
