dda93992a91952e46760307b72811f61c7ee6c71
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-20 16:36:45 +0000

    (6/15) A decent bit of stuff we still redefine past nix-common, but the gained compiler tuning is worth it.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8383 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/irix.mk
+++ src/config/platforms/irix.mk
@@ -22,42 +22,15 @@
 ## SGI IRIX platform defines
 ##############################################################################
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
-
-OS_ARCH		:= $(shell uname -m)
-
+ABI_OPT_PACIFY_COMPILER = 1 # Equivalent to the commented out options prior.
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 
 # Define tools
-CC		= gcc
-CCC		= g++
 RANLIB		= true
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
-# Compiler flags
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g # -Wall -ansi -pedantic
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	= DBG
-else
-OPTIMIZER	= -O2 # -Wall -ansi -pedantic
-DEFINES		=
-OBJ_DIR_SFX	= OBJ
-endif
 
 # Includes
 OS_INCLUDES		=
-#G++INCLUDES		= -I/usr/include/g++
+G++INCLUDES		=  # Commented prior, now we redefine NULL
 
 # Compiler flags
 PLATFORM_FLAGS		= -DIRIX
@@ -68,31 +41,12 @@ OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 # Architecture-specific flags
 OS_ENDIAN		= BigEndian32
 
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-GNOME_CONFIG    	= gnome-config
-
-# Shared library flags
-MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
-
-# Which links can this platform create.  Define one or
-# both of these options.
 UNIX_CAN_BUILD_DYNAMIC=1
 UNIX_CAN_BUILD_STATIC=0
 
 # Compiler options for static and dynamic linkage
 DL_LIBS			= dl
 STATIC_FLAGS		= -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 ##################################################################
 ## ABIPKGDIR defines the directory containing the Makefile to use to
