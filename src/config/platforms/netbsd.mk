e2944726cd98305adea09bf930002e37357735b5
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-20 17:22:51 +0000

    (8/15) Add netbsd to the fire, as well as a number of tweaks.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8385 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/netbsd.mk
+++ src/config/platforms/netbsd.mk
@@ -21,18 +21,8 @@
 ##############################################################################
 ## NetBSD platform defines, courtesy of John Wood (jyonw@asu.edu)
 ##############################################################################
-
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
-
-OS_ARCH		:= $(shell uname -m)
-
-ABI_OPT_PEER_EXPAT=1 # See below
+ABI_REQUIRE_PEER_ICONV = 1
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 
 ifneq (,$(shell $(CC) -E - -dM </usr/include/machine/endian.h | grep BYTE_ORDER.*LITTLE_ENDIAN))
 OS_ENDIAN	= LittleEndian32
@@ -40,120 +30,18 @@ else
 OS_ENDIAN	= BigEndian32
 endif
 
-# Define tools
-CC		= gcc
-CCC		= g++
-RANLIB		= ranlib
 
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
-# Compiler flags
-
-ifeq ($(ABI_OPT_PANGO),1)
-	OBJ_DIR_SFX	= PANGO_
-else
-	OBJ_DIR_SFX	= 
-endif
-
-DEFINES		= 
-OPTIMIZER	= 
-
-ifeq ($(ABI_OPT_PROF),1)
-OPTIMIZER	= -pg -fprofile-arcs -ftest-coverage
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)PRF_
-ABI_OPT_OPTIMIZE= 1
-ABI_OPTIONS	+= Profile:On
-endif
-
-ifeq ($(ABI_OPT_OPTIMIZE),1)
-OPTIMIZER	+= -O3 -fomit-frame-pointer
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OPT_
-ABI_OPTIONS	+= Optimize:On
-ABI_OPT_DEBUG	= 0
-else
-OPTIMIZER	= -O2
-endif
-
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall -pedantic -Wno-long-long
-ABI_OPT_PACIFY_COMPILER = 1
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)DBG_
-endif
-
-
-ifeq ($(ABI_OPT_GNOME),1)
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)GNOME_
-endif
-ifeq ($(ABI_OPT_PEER_EXPAT),1)
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)EXP_
-endif
-
-OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OBJ
-
-ifeq ($(ABI_OPT_WAY_TOO_MANY_WARNINGS),1)
-WARNFLAGS	= -Weffc++
-else
-WARNFLAGS	=
-endif
-
-ifneq ($(ABI_OPT_PACIFY_COMPILER),1)
-WARNFLAGS	+= -Wall -ansi -pedantic
-endif
-
-ABI_REQUIRE_PEER_ICONV = 1
 # Includes
-ifeq ($(ABI_REQUIRE_PEER_ICONV),1)
-OS_INCLUDES	= -I$(ABI_ROOT)/../libiconv/include -I/usr/local/include
-else
-OS_INCLUDES	= -I/usr/local/include
-endif
-G++INCLUDES	= -I/usr/include/g++
+OS_INCLUDES	+= -I/usr/local/include
 
 # Compiler flags
 PLATFORM_FLAGS		= -pipe -DNETBSD -DNetBSD
 PORT_FLAGS		= -DHAVE_STRERROR
 OS_CFLAGS		+= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
-PLATFORM_FLAGS		+= 
-PORT_FLAGS		+= 
-
-# Linker flags
-OS_DLLFLAGS		+=
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-ifeq ($(ABI_OPT_GNOME),1)
-GNOME_CONFIG    	= gnome-config
-endif
-# For now hardwire expat.  To be fixed soon.
-
-
-# Shared library flags
-MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
-
-# Which links can this platform create.  Define one or
-# both of these options.
-UNIX_CAN_BUILD_DYNAMIC=1
-UNIX_CAN_BUILD_STATIC=1
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
-
 # Compiler options for static and dynamic linkage
 DL_LIBS			= 
 STATIC_FLAGS		= -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
 
 ##################################################################
 ## ABIPKGDIR defines the directory containing the Makefile to use to
