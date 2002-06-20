e1f68f917d9150261867f002ccc24ba3af856d8f
Mark Gilbert <mgilbert@svn.abisource.com>
2002-06-20 16:44:15 +0000

    (7/15) Philip's stuff was rather close for the irix and irix64 platforms. A few variations though.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8384 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/irix64.mk
+++ src/config/platforms/irix64.mk
@@ -22,81 +22,27 @@
 ## SGI IRIX 64 platform defines, courtesy of Philippe Defert 
 ## (Philippe.Defert@cern.ch)
 ##############################################################################
+ABI_OPT_PACIFY_COMPILER	= 1 # About equivalent to prior definitions.
+include $(ABI_ROOT)/src/config/platforms/nix-common.mk
 
-##################################################################
-##################################################################
-## The main makefile and/or this file requires that OS_ARCH be set
-## to something to describe which chip that this OS is running on.
-## This can be used to change which tools are used and/or which
-## compiler/loader options are used.  It will probably also be used
-## in constructing the name object file destination directory.
-
-OS_ARCH		:= $(shell uname -m)
 OS_ENDIAN	= BigEndian32
 
-# Define tools
-CC		= gcc
-#CC		= cc
-CCC		= g++
-#CCC		= CC
-#RANLIB		= ranlib
 RANLIB		= true
-
-# Suffixes
-OBJ_SUFFIX	= o
-LIB_SUFFIX	= a
-DLL_SUFFIX	= so
-AR		= ar cr $@
-
-# Compiler flags
-ifeq ($(ABI_OPT_DEBUG),1)
-OPTIMIZER	= -g -Wall # -ansi -pedantic
-DEFINES		= -DDEBUG -UNDEBUG
-OBJ_DIR_SFX	= DBG
-else
-OPTIMIZER	= -O2 -Wall # -ansi -pedantic
-DEFINES		=
-OBJ_DIR_SFX	= OBJ
-endif
 
 # Includes
 OS_INCLUDES		=
-G++INCLUDES		= -I/usr/include/g++
 
 # Compiler flags
 PLATFORM_FLAGS		= 
 PORT_FLAGS		= -D_POSIX_SOURCE -D_BSD_SOURCE -DHAVE_STRERROR -D_XOPEN_SOURCE -DPATH_MAX=1024 -DSCANDIR_MISSING -DSETENV_MISSING
 OS_CFLAGS		= $(DSO_CFLAGS) $(PLATFORM_FLAGS) $(PORT_FLAGS)
 
-PLATFORM_FLAGS		+=
-PORT_FLAGS		+= 
-
-GLIB_CONFIG		= glib-config
-GTK_CONFIG		= gtk-config
-GNOME_CONFIG    	= gnome-config
-
-# Shared library flags
-MKSHLIB			= $(LD) $(DSO_LDOPTS) -soname $(@:$(OBJDIR)/%.so=%.so)
-
-# Which links can this platform create.  Define one or
-# both of these options. (what can IRIX do?  someone
-# with an SGI set the right one and mail a patch)
 UNIX_CAN_BUILD_DYNAMIC=1
 UNIX_CAN_BUILD_STATIC=0
 
 # Compiler options for static and dynamic linkage
 DL_LIBS			= dl
 STATIC_FLAGS		= -static
-
-ABI_NATIVE	= unix
-ABI_FE		= Unix
-
-##################################################################
-## Here you can choice if you want to use the gnome stuff.
-## Set ABI_OPT_GNOME to 1 (when invoking 'make') or set as
-## an environment variable.
-##
-## ABI_OPT_GNOME=1
 
 ##################################################################              
 ## ABIPKGDIR defines the directory containing the Makefile to use to            
