c3d3a18727750e0ba8be1c0aaed03f29dd2ff417
Johann Bjork <phearbear@svn.abisource.com>
2002-07-18 23:17:17 +0000

    Table stubs.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8622 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/qnx/ap_QNXDialog_InsertTable.cpp
@@ -0,0 +1,100 @@
+/* AbiWord
+ * Copyright (C) 2000 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ut_string.h"
+#include "ut_assert.h"
+#include "ut_debugmsg.h"
+
+#include "xap_App.h"
+#include "xap_QNXApp.h"
+#include "xap_QNXFrame.h"
+
+#include "ap_Strings.h"
+#include "ap_Dialog_Id.h"
+#include "ap_Dialog_Field.h"
+#include "ap_QNXDialog_Field.h"
+#include "ap_QNXDialog_InsertTable.h"
+#include "ut_qnxHelper.h"
+
+
+/*****************************************************************/
+
+XAP_Dialog * AP_QNXDialog_InsertTable::static_constructor(XAP_DialogFactory * pFactory,
+													 XAP_Dialog_Id id)
+{
+	AP_QNXDialog_InsertTable * p = new AP_QNXDialog_InsertTable(pFactory,id);
+	return p;
+}
+
+AP_QNXDialog_InsertTable::AP_QNXDialog_InsertTable(XAP_DialogFactory * pDlgFactory,
+											   XAP_Dialog_Id id)
+	: AP_Dialog_InsertTable(pDlgFactory,id)
+{
+}
+
+AP_QNXDialog_InsertTable::~AP_QNXDialog_InsertTable(void)
+{
+}
+
+void AP_QNXDialog_InsertTable::runModal(XAP_Frame * pFrame)
+{
+	UT_ASSERT(pFrame);
+	PtWidget_t *mainWindow = _constructWindow();
+	UT_ASSERT(mainWindow);
+	connectFocus(mainWindow,pFrame);
+	
+   // To center the dialog, we need the frame of its parent.
+    XAP_QNXFrame * pQNXFrame = static_cast<XAP_QNXFrame *>(pFrame);
+    UT_ASSERT(pQNXFrame);
+    
+    // Get the Window of the parent frame
+    PtWidget_t * parentWindow = pQNXFrame->getTopLevelWindow();
+    UT_ASSERT(parentWindow);
+    
+    // Center our new dialog in its parent and make it a transient
+    // so it won't get lost underneath
+    // Make it modal, and stick it up top
+	UT_QNXCenterWindow(parentWindow, mainWindow);
+	UT_QNXBlockWidget(parentWindow, 1);
+
+    // Show the top level dialog,
+	PtRealizeWidget(mainWindow);
+
+    // Run the event loop for this window.
+	int count;
+	count = PtModalStart();
+	done=0;
+	do {
+    		PtProcessEvent();
+	} while (!done);
+
+	PtModalEnd(MODAL_END_ARG(count));
+	UT_QNXBlockWidget(parentWindow,0);
+	PtDestroyWidget(mainWindow);
+}
+
+PtWidget_t * AP_QNXDialog_InsertTable::_constructWindow(void)
+{
+PtWidget_t *mainwindow;
+
+return mainwindow;
+}
