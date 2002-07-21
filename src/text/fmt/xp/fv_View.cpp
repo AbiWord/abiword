896ee1ab50d9c2e183e99bd8eccc7029fd7bcfda
Hubert Figuiere <hub@figuiere.net>
2002-07-21 08:42:34 +0000

    Backport fix for bug 3060 by tomasf
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-1-0-0-STABLE@8679 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fv_View.cpp
+++ src/text/fmt/xp/fv_View.cpp
@@ -9408,6 +9408,8 @@ UT_Error FV_View::cmdInsertHyperlink(const char * szName)
 
 	PT_DocPosition posStart = getPoint();
 	PT_DocPosition posEnd = posStart;
+	PT_DocPosition iPointOrig = posStart;
+	PT_DocPosition iAnchorOrig = m_iSelectionAnchor;
 	
 	if (!isSelectionEmpty())
 	{
@@ -9523,6 +9525,16 @@ UT_Error FV_View::cmdInsertHyperlink(const char * szName)
 	if(bRet)
 	{
 		bRet = m_pDoc->insertObject(posStart, PTO_Hyperlink, pAt, NULL);
+	}
+
+	if(bRet)
+	{
+		// because we have inserted two objects around the selection
+		// boundaries the original insetion point and selection anchor
+		// are now shifted, so we need to fix them
+		setPoint(iPointOrig+1);
+		m_iSelectionAnchor = iAnchorOrig + 1;
+	
 	}
 	
 	delete [] target;
