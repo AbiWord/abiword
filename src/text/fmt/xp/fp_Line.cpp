bd1b8e8969738c38c7fd6c4319aa78ae2fb54006
Patrick Lam <plam@lcs.mit.edu>
2002-07-21 20:05:44 +0000

    commit on behalf of msevior: less table background colour bleeding
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@8692 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fp_Line.cpp
+++ src/text/fmt/xp/fp_Line.cpp
@@ -63,40 +63,44 @@ UT_sint32 * fp_Line::s_pOldXs = NULL;
 UT_uint32   fp_Line::s_iOldXsSize = 0;
 UT_uint32	fp_Line::s_iClassInstanceCounter = 0;
 
-fp_Line::fp_Line(fl_SectionLayout * pSectionLayout) : 
-	fp_Container(FP_CONTAINER_LINE, pSectionLayout),
-	m_pBlock(NULL),
-	m_iWidth(0),
-	m_iMaxWidth(0),
-	m_iClearToPos(0),
-	m_iClearLeftOffset(0),
-	m_iHeight(0),
-	m_iScreenHeight(-1),
-	m_iAscent(0),
-	m_iDescent(0),
-	m_iX(0),
-	m_iY(-2000000), // So setY(0) triggers a clearscreen and redraw!
-#ifndef WITH_PANGO
-	m_iWidthLayoutUnits(0),
-	m_iMaxWidthLayoutUnits(0),
-	m_iHeightLayoutUnits(0),
-	m_iXLayoutUnits(0),
-	m_iYLayoutUnits(-20000000),
-#endif
-	//m_bRedoLayout(true),
-	m_bNeedsRedraw(false),
-	m_bMapDirty(true), //map that has not been initialized is dirty by deafault
-	m_iRunsRTLcount(0),
-	m_iRunsLTRcount(0),
-	m_bIsCleared(true),
-	m_bHasFootnoteRef(false)
+fp_Line::fp_Line(fl_SectionLayout * pSectionLayout) : fp_Container(FP_CONTAINER_LINE, pSectionLayout)
 {
+	m_pBlock = NULL;
+	m_iWidth = 0;
+	m_iMaxWidth = 0;
+	m_iClearToPos = 0;
+	m_iClearLeftOffset = 0;
+	m_iHeight = 0;
+
+	m_iScreenHeight = -1;
+	m_iAscent = 0;
+	m_iDescent = 0;
+	m_iX = 0;
+	m_iY = -2000000; // So setY(0) triggers a clearscreen and redraw!
+	m_iYLayoutUnits = -20000000;
+
+#ifndef WITH_PANGO
+	m_iHeightLayoutUnits = 0;
+	m_iWidthLayoutUnits = 0;
+	m_iMaxWidthLayoutUnits = 0;
+	m_iXLayoutUnits = 0;
+	m_iYLayoutUnits = 0;
+#endif
+
+	m_bNeedsRedraw = false;
+	//m_bRedoLayout = true;
+
 	if(!s_iClassInstanceCounter)
 	{
 		s_pOldXs = new UT_sint32[STATIC_BUFFER_INITIAL];
 		UT_ASSERT(s_pOldXs);
 		s_iOldXsSize = STATIC_BUFFER_INITIAL;
 	}
+
+
+	m_iRunsRTLcount = 0;
+	m_iRunsLTRcount = 0;
+	m_bMapDirty = true; //map that has not been initialized is dirty by deafault
 
 	#ifdef USE_STATIC_MAP
 	if(!s_pMapOfRunsL2V)
@@ -107,16 +111,19 @@ fp_Line::fp_Line(fl_SectionLayout * pSectionLayout) :
 		s_pEmbeddingLevels =  new FriBidiLevel[RUNS_MAP_SIZE];
 		s_iMapOfRunsSize = RUNS_MAP_SIZE;
 	}
+
     #else
 	m_pMapOfRunsL2V = new FriBidiStrIndex[RUNS_MAP_SIZE];
 	m_pMapOfRunsV2L = new FriBidiStrIndex[RUNS_MAP_SIZE];
 	m_pPseudoString    = new FriBidiChar[RUNS_MAP_SIZE];
 	m_pEmbeddingLevels =  new FriBidiLevel[RUNS_MAP_SIZE];
 	m_iMapOfRunsSize = RUNS_MAP_SIZE;
-    #endif
+	#endif
 
 	UT_ASSERT(s_pMapOfRunsL2V && s_pMapOfRunsV2L && s_pPseudoString && s_pEmbeddingLevels);
 
+	m_bNeedsRedraw = false;
+	m_bIsCleared = true;
 	++s_iClassInstanceCounter; // this tells us how many instances of Line are out there
 							   //we use this to decide whether the above should be
 							   //deleted by the destructor
@@ -282,13 +289,7 @@ bool fp_Line::removeRun(fp_Run* pRun, bool bTellTheRunAboutIt)
 		pRun->setLine(NULL);
 	}
 
-	// Might have to check for other footnotes on this line.
-	if (pRun->getType() == FPRUN_FIELD)
-	{
-		fp_FieldRun * fr = (fp_FieldRun*) pRun;
-		if (fr->getFieldType() == FPFIELD_endnote_ref)
-			_updateHasFootnoteRef();
-	}
+
 
 	UT_sint32 ndx = m_vecRuns.findItem(pRun);
 	UT_ASSERT(ndx >= 0);
@@ -306,13 +307,6 @@ void fp_Line::insertRunBefore(fp_Run* pNewRun, fp_Run* pBefore)
 	UT_ASSERT(pNewRun);
 	UT_ASSERT(pBefore);
 
-	if (pNewRun->getType() == FPRUN_FIELD)
-	{
-		fp_FieldRun * fr = (fp_FieldRun*) pNewRun;
-		if (fr->getFieldType() == FPFIELD_endnote_ref)
-			m_bHasFootnoteRef = true;
-	}
-
 	pNewRun->setLine(this);
 
 	UT_sint32 ndx = m_vecRuns.findItem(pBefore);
@@ -326,12 +320,6 @@ void fp_Line::insertRunBefore(fp_Run* pNewRun, fp_Run* pBefore)
 void fp_Line::insertRun(fp_Run* pNewRun)
 {
 	//UT_DEBUGMSG(("insertRun (line 0x%x, run 0x%x, type %d)\n", this, pNewRun, pNewRun->getType()));
-	if (pNewRun->getType() == FPRUN_FIELD)
-	{
-		fp_FieldRun * fr = (fp_FieldRun*) pNewRun;
-		if (fr->getFieldType() == FPFIELD_endnote_ref)
-			m_bHasFootnoteRef = true;
-	}
 
 	UT_ASSERT(m_vecRuns.findItem(pNewRun) < 0);
 	pNewRun->setLine(this);
@@ -344,12 +332,6 @@ void fp_Line::insertRun(fp_Run* pNewRun)
 void fp_Line::addRun(fp_Run* pNewRun)
 {
 	//UT_DEBUGMSG(("addRun (line 0x%x, run 0x%x, type %d)\n", this, pNewRun, pNewRun->getType()));
-	if (pNewRun->getType() == FPRUN_FIELD)
-	{
-		fp_FieldRun * fr = (fp_FieldRun*) pNewRun;
-		if (fr->getFieldType() == FPFIELD_endnote_ref)
-			m_bHasFootnoteRef = true;
-	}
 
 	UT_ASSERT(m_vecRuns.findItem(pNewRun) < 0);
 	pNewRun->setLine(this);
@@ -363,12 +345,6 @@ void fp_Line::addRun(fp_Run* pNewRun)
 void fp_Line::insertRunAfter(fp_Run* pNewRun, fp_Run* pAfter)
 {
 	//UT_DEBUGMSG(("insertRunAfter (line 0x%x, run 0x%x, type %d)\n", this, pNewRun, pNewRun->getType()));
-	if (pNewRun->getType() == FPRUN_FIELD)
-	{
-		fp_FieldRun * fr = (fp_FieldRun*) pNewRun;
-		if (fr->getFieldType() == FPFIELD_endnote_ref)
-			m_bHasFootnoteRef = true;
-	}
 
 	UT_ASSERT(m_vecRuns.findItem(pNewRun) < 0);
 	UT_ASSERT(pNewRun);
@@ -402,11 +378,12 @@ void fp_Line::remove(void)
 void fp_Line::mapXYToPosition(UT_sint32 x, UT_sint32 y, PT_DocPosition& pos,
 							  bool& bBOL, bool& bEOL)
 {
-	UT_uint32 count = m_vecRuns.getItemCount();
+	UT_sint32 count = m_vecRuns.getItemCount();
+	UT_ASSERT(count > 0);
 
 	FV_View* pView = getBlock()->getDocLayout()->getView();
 	bool bShowHidden = pView->getShowPara();
-	UT_uint32 i = 0;
+	UT_sint32 i = 0;
 	fp_Run* pFirstRun;
 	bool bHidden;
 	FPVisibility eHidden;
@@ -830,6 +807,7 @@ void fp_Line::clearScreen(void)
 			UT_sint32 height = getHeight();
 			// I have added the +1 to clear dirt after squiggles and
 			// revision underlines
+
 			pRun->getGraphics()->fillRect(pRun->getPageColor(),xoffLine - m_iClearLeftOffset, yoffLine, m_iClearToPos + m_iClearLeftOffset, height);
 //
 // Sevior: I added this for robustness.
@@ -1145,10 +1123,10 @@ void fp_Line::draw(GR_Graphics* pG)
 		// shortcircuit drawing if we're not included in the dirty region
 		UT_Rect runRect(da.xoff, da.yoff, pRun->getWidth(), pRun->getHeight());
 #ifdef DEBUG
-		flash(pG, runRect, UT_RGBColor(255, 255, 0)); // debug color only
+		flash(pG, runRect, UT_RGBColor(255, 255, 0));
 
 		if (pRect)
-			flash(pG, *pRect, UT_RGBColor(0, 255, 255)); // debug color only
+			flash(pG, *pRect, UT_RGBColor(0, 255, 255));
 		else
 			xxx_UT_DEBUGMSG(("pRect NULL\n"));
 #endif /* DEBUG */
@@ -1218,7 +1196,7 @@ void fp_Line::draw(dg_DrawArgs* pDA)
 
 		da.yoff += pRun->getY();
 		UT_Rect runRect(da.xoff, da.yoff - m_iAscent, pRun->getWidth(), pRun->getHeight());
-		// flash(pDA->pG, runRect, UT_RGBColor(0, 255, 0)); // debug color only
+		// flash(pDA->pG, runRect, UT_RGBColor(0, 255, 0));
 
 		if (pRect == NULL || pRect->intersectsRect(&runRect))
 			pRun->draw(&da);
@@ -2618,9 +2596,9 @@ void fp_Line::recalcMaxWidth(bool bDontClearIfNeeded)
 		else if(getContainer()->getContainerType() == FP_CONTAINER_CELL)
 		{
 			fp_CellContainer * pCell = (fp_CellContainer *) getContainer();
-			m_iClearToPos = (UT_sint32)(iMaxWidth + pCell->getRightPad() 
-										* SCALE_TO_SCREEN);
-			m_iClearLeftOffset = pSL->getLeftMargin() -1;
+			m_iClearToPos = iMaxWidth + pCell->getRightPad() * SCALE_TO_SCREEN;
+//			m_iClearLeftOffset =  pCell->getCellX(this) - pCell->getLeftPos() - 1;
+			m_iClearLeftOffset =  0;
 		}
 		else
 		{
@@ -2641,9 +2619,9 @@ void fp_Line::recalcMaxWidth(bool bDontClearIfNeeded)
 		else if(getContainer()->getContainerType() == FP_CONTAINER_CELL)
 		{
 			fp_CellContainer * pCell = (fp_CellContainer *) getContainer();
-			m_iClearToPos = (UT_sint32)(iMaxWidth + pCell->getRightPad() 
-										* SCALE_TO_SCREEN);
-			m_iClearLeftOffset = pSL->getLeftMargin() -1;
+			m_iClearToPos = iMaxWidth + pCell->getRightPad() * SCALE_TO_SCREEN;
+//			m_iClearLeftOffset =  pCell->getCellX(this) - pCell->getLeftPos() - 1;
+			m_iClearLeftOffset =  0;
 		}
 		else
 		{
@@ -3367,6 +3345,7 @@ fp_Run * fp_Line::getLastVisRun()
 
 	_createMapOfRuns();
 	UT_uint32 count = m_vecRuns.getItemCount();
+	UT_ASSERT(count > 0);
 	return((fp_Run *) m_vecRuns.getNthItem(s_pMapOfRunsV2L[count - 1]));
 }
 
@@ -3495,26 +3474,5 @@ void fp_Line::changeDirectionUsed(FriBidiCharType oldDir, FriBidiCharType newDir
 	{
 		m_bMapDirty = true;
 		_createMapOfRuns();
-	}
-}
-
-/*!
-    Scan through the runs on this line, checking for footnote anchor
-    fields.  Return true if so.
-*/
-void fp_Line::_updateHasFootnoteRef(void)
-{
-	m_bHasFootnoteRef = false;
-
-	UT_uint32 count = m_vecRuns.getItemCount();
-	for (UT_uint32 i = 0; i < count; i++)
-	{
-		fp_Run * r = (fp_Run *)m_vecRuns.getNthItem(i);
-		if (r->getType() == FPRUN_FIELD)
-		{
-			fp_FieldRun * fr = (fp_FieldRun*) r;
-			if (fr->getFieldType() == FPFIELD_endnote_ref)
-				m_bHasFootnoteRef = true;
-		}
 	}
 }
