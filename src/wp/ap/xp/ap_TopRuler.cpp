5749b7cb5b5bd00b30afe89f2d3952326ac82b36
Mark Gilbert <mgilbert@svn.abisource.com>
2002-07-25 16:45:06 +0000

    Fix a typo that broke the build
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-1-0-0-STABLE@8727 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_TopRuler.cpp
+++ src/wp/ap/xp/ap_TopRuler.cpp
@@ -1394,8 +1394,7 @@ bool AP_TopRuler::isMouseOverTab(UT_uint32 x, UT_uint32 y)
 	bool bRTLglobal;	
 	XAP_App::getApp()->getPrefsValueBool((XML_Char*)AP_PREF_KEY_DefaultDirectionRtl, &bRTLglobal);
 	
-	fl_BlockLayout * pBL = (static_cast<FV_View *>(m_pView))->getCurrentBloc
-k();
+	fl_BlockLayout * pBL = (static_cast<FV_View *>(m_pView))->getCurrentBlock();
 	UT_ASSERT (pBL);
 #if DEBUG
 	if (pBL == NULL) {
