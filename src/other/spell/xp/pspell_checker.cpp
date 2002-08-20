76b3f6228f52fe7b16474dde5bc68b2f4b269489
Mark Gilbert <mgilbert@svn.abisource.com>
2002-08-20 01:44:16 +0000

    I think I did this the same way....(-:  Yeah, fix another snprintf.  Was gonna fix the dler's but it seems someone committed before me.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@9006 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/other/spell/xp/pspell_checker.cpp
+++ src/other/spell/xp/pspell_checker.cpp
@@ -47,11 +47,11 @@ static void couldNotLoadDictionary ( const char * szLang )
   
   const XAP_StringSet * pSS    = pApp->getStringSet ();
   
-  char buf[1024]; // evil hardcoded buffer size
+  UT_String buf; // Boundless buffer
   const char * text = pSS->getValue (XAP_STRING_ID_DICTIONARY_CANTLOAD);
-  snprintf(buf, 1024, text, szLang);
-  
-  pFrame->showMessageBox (buf,
+  UT_String_sprintf(buf, text, szLang);
+  char *cbuf = buf.c_str();
+  pFrame->showMessageBox (cbuf,
 			  XAP_Dialog_MessageBox::b_O,
 			  XAP_Dialog_MessageBox::a_OK);
 }
