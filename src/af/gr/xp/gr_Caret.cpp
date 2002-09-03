87e2f6b49474b74827fb6bb50e7a377afcad8333
Patrick Lam <plam@lcs.mit.edu>
2002-09-03 16:55:21 +0000

    dom and my new improved caret class, makes the world wonderful
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@9119 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/gr/xp/gr_Caret.cpp
@@ -0,0 +1,213 @@
+/* An autonomous caret class.
+ *
+ * Author: Patrick Lam
+ *         Dom Lachowicz
+ * Inspired by Mike Nordell (tamlin@algonet.se)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#include "gr_Caret.h"
+#include "gr_Graphics.h"
+#include "ut_sleep.h"
+
+#include "ut_debugmsg.h"
+#include "ut_assert.h"
+
+#define CURSOR_BLINK_TIME 600 /* milliseconds */
+
+// The problem is that a complicated draw operation will be somewhat
+// inefficient & ugly, because the caret gets drawn/undrawn every time
+// one of the primitive draw operations takes place.  The solution
+// there is for the caret to only be reinstated after the whole draw
+// operation is done.  A good heuristic for that is to only draw the
+// caret after 10ms of no drawing activity.  This can be done by
+// scheduling the reinstating _blink for 10ms in the future; if
+// there's another draw that happens to take place before 10ms, then
+// your scheduled _blink gets cancelled.
+
+GR_Caret::GR_Caret(GR_Graphics * pG)
+	:   m_pClr(NULL),
+		m_pG(pG),
+		m_nDisableCount(1),
+		m_bCursorBlink(true),
+		m_bCursorIsOn(false),
+		m_bPositionSet(false),
+		m_bRecursiveDraw(false)
+{
+	UT_WorkerFactory::ConstructMode outMode = UT_WorkerFactory::NONE;
+	m_worker = static_cast<UT_Timer *>(UT_WorkerFactory::static_constructor
+		(s_work, this, UT_WorkerFactory::TIMER, outMode, pG));
+	UT_ASSERT(outMode == UT_WorkerFactory::TIMER);
+	m_worker->set(CURSOR_BLINK_TIME);
+}
+
+void GR_Caret::s_work(UT_Worker * _w)
+{
+	GR_Caret * c = static_cast<GR_Caret *>(_w->getInstanceData());
+
+// 	UT_DEBUGMSG(("blinking cursor %d\n", c->m_nDisableCount));
+
+	if (c->m_nDisableCount == 0)
+		c->_blink(false);
+}
+
+GR_Caret::~GR_Caret()
+{
+	m_worker->stop();
+}
+
+void GR_Caret::setCoords(UT_sint32 x, UT_sint32 y, UT_uint32 h,
+						 UT_sint32 x2, UT_sint32 y2, UT_uint32 h2,
+						 bool bPointDirection, UT_RGBColor * pClr)
+{
+	// if visible, then hide while we change positions.
+	if (m_bCursorIsOn)
+		_blink();
+
+	m_xPoint = x; m_yPoint = y; m_iPointHeight = h;
+	m_xPoint2 = x2; m_yPoint2 = y2; m_iPointHeight2 = h2;
+	m_bPointDirection = bPointDirection; m_pClr = pClr;
+	m_bPositionSet = true;
+
+	// now show the cursor, if it's enabled.
+	if (m_nDisableCount == 0)
+		_blink();
+}
+
+
+void GR_Caret::enable()
+{
+  	xxx_UT_DEBUGMSG(("GR_Caret::enable(), this=%p, count = %d\n", this, m_nDisableCount));
+
+	if (m_nDisableCount == 0)
+	{
+		// If the caret is already enabled, we re-draw the caret
+		// and don't touch m_nDisableCount.
+		if (m_bCursorIsOn)
+			_blink();
+		_blink();
+		return;
+	}
+
+	--m_nDisableCount;
+	// Check to see if we still have pending disables.
+	if (m_nDisableCount)
+		return;
+
+	// Always start out with the caret in drawn state
+// 	UT_ASSERT(!m_bCursorIsOn);
+	_blink();
+	m_worker->start();
+}
+
+void GR_Caret::disable(bool bNoMulti)
+{
+   	xxx_UT_DEBUGMSG(("GR_Caret::disable(), this=%p, count = %d\n", this, m_nDisableCount));
+	if (bNoMulti && (m_nDisableCount > 0))
+		return;
+
+	m_nDisableCount++;
+	if ((m_nDisableCount == 1) && m_bCursorIsOn)
+		_blink(true, true);
+
+	m_worker->stop();
+	// Caret should never be "on" when leaving disable().
+  	UT_ASSERT(m_bRecursiveDraw || !m_bPositionSet || !m_bCursorBlink ||
+			  (m_nDisableCount != 0) || !m_bCursorIsOn);
+}
+
+/** Determines whether Abi is going to blink the cursor or not.
+ * If not, then _blink() won't actually clear the cursor; it'll only draw. */
+void GR_Caret::setBlink(bool bBlink)
+{
+	m_bCursorBlink = bBlink;
+}
+
+void GR_Caret::_blink(bool bExplicit, bool bForceClear)
+{
+	if (m_bRecursiveDraw || !m_bPositionSet)
+		return;
+
+	// After any blink, we want there to be BLINK_TIME until next autoblink.
+	if (!bExplicit)
+	{ 
+		m_worker->stop(); m_worker->start();
+	}
+
+	UT_ASSERT(m_bCursorBlink);
+	if (m_bCursorBlink || (bForceClear && !m_bCursorIsOn))
+	{
+		m_bRecursiveDraw = true;
+		
+		xxx_UT_DEBUGMSG(("actually blinking at %d %d h=%d; cursorison will be %s\n", m_xPoint, m_yPoint, m_iPointHeight, !m_bCursorIsOn ? "true" : "false"));
+		m_bCursorIsOn = !m_bCursorIsOn;
+
+		UT_RGBColor oldColor; m_pG->getColor(oldColor);
+  		if (m_pClr)
+			m_pG->setColor(*m_pClr);
+		else
+			m_pG->setColor(UT_RGBColor(255,255,255));
+
+		m_pG->xorLine(m_xPoint-1, m_yPoint+1, m_xPoint-1, 
+					  m_yPoint + m_iPointHeight+1);
+		m_pG->xorLine(m_xPoint, m_yPoint+1, m_xPoint, 
+					  m_yPoint + m_iPointHeight+1);
+
+		if((m_xPoint != m_xPoint2) || (m_yPoint != m_yPoint2))
+		{
+			// #TF the caret will have a small flag at the top 
+			// indicating the direction of writing
+			if(m_bPointDirection)
+			{
+				m_pG->xorLine(m_xPoint-3, m_yPoint+1, m_xPoint-1, m_yPoint+1);
+				m_pG->xorLine(m_xPoint-2, m_yPoint+2, m_xPoint-1, m_yPoint+2);
+			}
+			else
+			{
+				m_pG->xorLine(m_xPoint+1, m_yPoint+1, m_xPoint+3, m_yPoint+1);
+				m_pG->xorLine(m_xPoint+1, m_yPoint+2, m_xPoint+2, m_yPoint+2);
+			}
+
+			// This is the second caret on ltr-rtl boundary
+			m_pG->xorLine(m_xPoint2-1, m_yPoint2+1, 
+						  m_xPoint2-1, m_yPoint2 + m_iPointHeight + 1);
+			m_pG->xorLine(m_xPoint2, m_yPoint2+1, 
+						  m_xPoint2, m_yPoint2 + m_iPointHeight + 1);
+
+			// This is the line that links the two carets
+			m_pG->xorLine(m_xPoint, m_yPoint + m_iPointHeight + 1, 
+						  m_xPoint2, m_yPoint2 + m_iPointHeight + 1);
+
+			if(m_bPointDirection)
+			{
+				m_pG->xorLine(m_xPoint2+1, m_yPoint2+1, 
+							  m_xPoint2+3, m_yPoint2+1);
+				m_pG->xorLine(m_xPoint2+1, m_yPoint2+2, 
+							  m_xPoint2+2, m_yPoint2+2);
+			}
+			else
+			{
+				m_pG->xorLine(m_xPoint2-3, m_yPoint2+1, 
+							  m_xPoint2-1, m_yPoint2+1);
+				m_pG->xorLine(m_xPoint2-2, m_yPoint2+2, 
+							  m_xPoint2-1, m_yPoint2+2);
+			}
+		}
+		m_pG->setColor(oldColor);
+		m_bRecursiveDraw = false;
+	}
+}
---
+++ src/af/gr/xp/gr_Caret.h
@@ -0,0 +1,115 @@
+/* Handle caret blinking.
+ *
+ * Author: Mike Nordell (tamlin@algonet.se)
+ *         Pat Lam
+ *         Dom Lachowicz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+#ifndef GR_CARET_H
+#define GR_CARET_H
+
+#include "ut_timer.h"
+#include "ut_misc.h"
+
+class GR_Graphics;
+
+class GR_Caret
+{
+	// provide access to _hide and _unhide for GR_Graphics.
+	friend class GR_Graphics;
+public:
+	explicit GR_Caret(GR_Graphics * pG);
+	~GR_Caret();
+	
+	void enable();
+	// Disable hides the caret if it's currently visible.
+	// The caret can be disabled multiple times, and just as many enable
+	// calls need to take place for the cursor to show up.
+	// If the cursor is already enabled, the enable call just makes the
+	// cursor get redrawn.
+	// if bNoMulti is true, then only one enable call needed to reverse this disable.
+	void disable(bool bNoMulti = false);
+	inline bool isEnabled() const { return m_nDisableCount == 0; }
+	
+	void setBlink(bool bBlink);
+
+	// When you call setCoords, the cursor is explicitly shown
+	// and the timer restarts from 0 for the next 500ms cycle.
+	void setCoords(UT_sint32 x, UT_sint32 y, UT_uint32 h,
+				   UT_sint32 x2 = 0, UT_sint32 y2 = 0, UT_uint32 h2 = 0,
+				   bool bPointDirection = false, UT_RGBColor * pClr = NULL);
+
+private:
+	// Before doing a draw operation, explicitly hide the cursor.
+// 	void _hide();
+	// After a draw, explicitly show the cursor (setting it to m_CursorIsOn).
+// 	void _show();
+	static void s_work(UT_Worker * w);
+
+	UT_sint32			m_xPoint;
+	UT_sint32			m_yPoint;
+	UT_uint32			m_iPointHeight;
+    // for bidi
+	UT_sint32			m_xPoint2;
+	UT_sint32			m_yPoint2;
+	UT_uint32			m_iPointHeight2;
+	bool				m_bPointDirection;
+	UT_RGBColor *		m_pClr;
+	GR_Graphics *		m_pG;
+
+	GR_Caret(const GR_Caret& rhs);			// no impl.
+	void operator=(const GR_Caret& rhs);	// no impl.
+	
+	void	_blink(bool bExplicit = true, bool bForceClear = false);
+	
+	UT_Timer *    m_worker;
+
+	// m_nDisableCount > 0 implies a disabled cursor.
+	// m_nDisableCount should never be negative.
+	UT_uint32	m_nDisableCount;
+	bool		m_bCursorBlink;
+	bool		m_bCursorIsOn;
+	bool		m_bPositionSet;
+	bool		m_bRecursiveDraw;
+};
+
+class GR_CaretDisabler
+{
+ public:
+
+	GR_CaretDisabler (GR_Caret * pCaret)
+		: m_pCaret(pCaret)
+		{
+			if (m_pCaret) m_pCaret->disable();
+		}
+
+	~GR_CaretDisabler ()
+		{
+			if (m_pCaret) m_pCaret->enable();
+		}
+
+private:
+	// no impls
+	GR_CaretDisabler ();
+	GR_CaretDisabler (const GR_CaretDisabler & other);
+	GR_CaretDisabler & operator=(const GR_CaretDisabler &other);
+	
+	GR_Caret * m_pCaret;
+};
+
+
+#endif // GR_CARET_H
