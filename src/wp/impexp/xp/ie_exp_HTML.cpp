d88e4402253fee48e6ee8161b61966344c3c8006
Dom Lachowicz <domlachowicz@gmail.com>
2002-09-17 01:13:08 +0000

    export UTF-8 text
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@9290 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_exp_HTML.cpp
+++ src/wp/impexp/xp/ie_exp_HTML.cpp
@@ -1408,7 +1408,7 @@ void s_HTML_Listener::_closeSpan(void)
 
 void s_HTML_Listener::_outputData(const UT_UCSChar * data, UT_uint32 length)
 {
-	UT_String sBuf;
+	UT_UTF8String sBuf;
 	const UT_UCSChar * pData;
 
 	if (!m_bInBlock)
@@ -1464,7 +1464,7 @@ void s_HTML_Listener::_outputData(const UT_UCSChar * data, UT_uint32 length)
 			}
 			else
 			{
-				sBuf += *pData;
+				sBuf.appendUCS4(pData, 1);
 				pData++;
 			}
 			break;
@@ -1521,56 +1521,12 @@ void s_HTML_Listener::_outputData(const UT_UCSChar * data, UT_uint32 length)
 			break;
 
 		default:
-			if (*pData > 0x007f)
-			{
-#ifdef IE_EXP_HTML_UTF8_OPTIONAL
-				// "try_nativeToU(0xa1) == 0xa1" looks dodgy to me (fjf)
-				if(XAP_EncodingManager::get_instance()->isUnicodeLocale() ||
-				   (XAP_EncodingManager::get_instance()->try_nativeToU(0xa1) == 0xa1))
-				{
-#endif /* IE_EXP_HTML_UTF8_OPTIONAL */
-					XML_Char * pszUTF8 = UT_encodeUTF8char(*pData++);
-					while (*pszUTF8)
-					{
-						sBuf += (char)*pszUTF8;
-						pszUTF8++;
-					}
-#ifdef IE_EXP_HTML_UTF8_OPTIONAL
-				}
-				else
-				{
-					/*
-					  Try to convert to native encoding and if
-					  character fits into byte, output raw byte. This
-					  is somewhat essential for single-byte non-latin
-					  languages like russian or polish - since
-					  tools like grep and sed can be used then for
-					  these files without any problem.
-					  Networks and mail transfers are 8bit clean
-					  these days.  - VH
-					*/
-					UT_UCSChar c = XAP_EncodingManager::get_instance()->try_UToNative(*pData);
-					if (c==0 || c>255)
-					{
-					  sBuf += UT_String_sprintf("&#x%x;",*pData++);
-					}
-					else
-					{
-						sBuf += (char)c;
-						pData++;
-					}
-				}
-#endif /* IE_EXP_HTML_UTF8_OPTIONAL */
-			}
-			else
-			{
-				sBuf += (char)*pData++;
-			}
-			break;
+		  sBuf.appendUCS4 (pData, 1);
+		  pData++;
 		}
 	}
 
-	m_pie->write(sBuf.c_str(),sBuf.size());
+	m_pie->write(sBuf.utf8_str(),sBuf.byteLength());
 }
 
 /*!	This function returns true if the name of the PD_Style which style is based
