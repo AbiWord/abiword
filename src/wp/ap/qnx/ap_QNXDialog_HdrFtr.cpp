16c0568f0b450dee700c9da1be055bda48bfc797
Johann Bjork <phearbear@svn.abisource.com>
2002-09-28 09:29:05 +0000

    Dialog redone, groups rocks.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@9406 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/qnx/ap_QNXDialog_HdrFtr.cpp
+++ src/wp/ap/qnx/ap_QNXDialog_HdrFtr.cpp
@@ -71,31 +71,10 @@ return Pt_CONTINUE;
 
 int ph_event_activate_updown(PtWidget_t *widget,AP_QNXDialog_HdrFtr *dlg,PtCallbackInfo_t *info)
 {
-dlg->UpDown(info->reason_subtype);
+dlg->RestartChanged();
 return Pt_CONTINUE;
 }
-void AP_QNXDialog_HdrFtr::UpDown(int updown)
-{
-char *textstr;
-char strnew[10];
-int number;
 
-if(updown==Pt_UPDOWN_UP)
-{
-	PtGetResource(m_RestartText,Pt_ARG_TEXT_STRING,&textstr,0);
-	number=atoi(textstr);
-	number++;
-	snprintf((char*)&strnew,10,"%d",number);
-	PtSetResource(m_RestartText,Pt_ARG_TEXT_STRING,&strnew,0);
-}else
-{
-	PtGetResource(m_RestartText,Pt_ARG_TEXT_STRING,&textstr,0);
-	number=atoi(textstr);
-	number--;
-	snprintf((char*)&strnew,10,"%d",number);
-	PtSetResource(m_RestartText,Pt_ARG_TEXT_STRING,&strnew,0);
-}
-}
 int ph_activate_restart( PtWidget_t *widget, AP_QNXDialog_HdrFtr * dlg, PtCallbackInfo_t *info)
 {
 UT_ASSERT(widget && dlg);
@@ -207,155 +186,82 @@ PtWidget_t *toggleFootEven;
 PtWidget_t *toggleFootFirst;
 PtWidget_t *toggleFootLast;
 PtWidget_t *toggleRestartNewSection;
-PtWidget_t *textRestartNumber;
 PtWidget_t *UpDown;
-PtWidget_t *Pane1;
 PtWidget_t *RestartLabel;
 
-	/* The window itself */
-	PhDim_t dim = { 496,355 };
-	PtArg_t args[] = {
-		Pt_ARG(Pt_ARG_DIM,&dim,0),
-		Pt_ARG(Pt_ARG_WINDOW_TITLE,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_Title).c_str(),0),
-		Pt_ARG(Pt_ARG_WINDOW_RENDER_FLAGS,0,ABI_MODAL_WINDOW_RENDER_FLAGS),
-		Pt_ARG(Pt_ARG_WINDOW_MANAGED_FLAGS,0,ABI_MODAL_WINDOW_MANAGE_FLAGS)
-		};
+	int n=0;
+	PtArg_t args[10];
 
-	 /* Top PtPane, lower */ 
-	 PhArea_t area1 = { { 0, 0 }, { 496, 322 } };
-	 PtArg_t args1[] = {
-		Pt_ARG( Pt_ARG_AREA, &area1, 0 ),
-		Pt_ARG( Pt_ARG_FLAGS, 256,256 ),
-		Pt_ARG( Pt_ARG_ANCHOR_FLAGS, 1440,8191 ),
-		};
-/* 'Header' PtPane */
-	 PhArea_t area2 = { { 3, 23 }, { 486, 95 } };
-	 PtArg_t args2[] = {
-		Pt_ARG( Pt_ARG_AREA, &area2, 0 ),
-		Pt_ARG( Pt_ARG_FLAGS, 256,256 ),
-		Pt_ARG( Pt_ARG_BEVEL_WIDTH, 1, 0 ),
-		};
-	/* PtToggle , diff head even page */
-	 PhArea_t area3 = { { 5, 5 }, { 466, 24 } };
-	 PtArg_t args3[] = {
-		Pt_ARG( Pt_ARG_AREA, &area3, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_HeaderEven).c_str(), 0 ),
-		};
-		/* PtToggle, diff head first page */
-	 PhArea_t area4 = { { 5, 31 }, { 469, 24 } };
-	 PtArg_t args4[] = {
-		Pt_ARG( Pt_ARG_AREA, &area4, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_HeaderFirst).c_str(), 0 ),
-		};
-	 /* PtToggle diff head last page */
-	 PhArea_t area5 = { { 5, 58 }, { 469, 24 } };
-	 PtArg_t args5[] = {
-		Pt_ARG( Pt_ARG_AREA, &area5, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_HeaderLast).c_str() , 0 ),
-		};
-	/* Head label  */
-	 PhArea_t area6 = { { 5, 0 }, { 206, 21 } };
-	 PtArg_t args6[] = {
-		Pt_ARG( Pt_ARG_AREA, &area6, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_HeaderFrame).c_str() , 0 ),
-		};
-		/* Footer label */
-	 PhArea_t area7 = { { 5, 133 }, { 308, 21 } };
-	 PtArg_t args7[] = {
-		Pt_ARG( Pt_ARG_AREA, &area7, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_FooterFrame).c_str() , 0 ),
-		};
- 	/*PtPane  Footer*/
-	 PhArea_t area8 = { { 3, 159 }, { 486, 95 } };
-	 PtArg_t args8[] = {
-		Pt_ARG( Pt_ARG_AREA, &area8, 0 ),
-		Pt_ARG( Pt_ARG_FLAGS, 256,256 ),
-		Pt_ARG( Pt_ARG_BEVEL_WIDTH, 1, 0 ),
-		};
-	/*PtToggle Diff foot first page */
-	 PhArea_t area9 = { { 5, 31 }, { 472, 24 } };
-	 PtArg_t args9[] = {
-		Pt_ARG( Pt_ARG_AREA, &area9, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_FooterFirst).c_str(), 0 ),
-		};
-	 /*PtToggle Diff foor last page */
-	 PhArea_t area10 = { { 5, 58 }, { 469, 24 } };
-	 PtArg_t args10[] = {
-		Pt_ARG( Pt_ARG_AREA, &area10, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_FooterLast).c_str(), 0 ),
-		};
-	/*PtToggle, diff foot on even page */
-	 PhArea_t area11 = { { 5, 5 }, { 472, 24 } };
-	 PtArg_t args11[] = {
-		Pt_ARG( Pt_ARG_AREA, &area11, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_FooterEven).c_str(), 0 ),
-		};
-	 /* PtToggle, restart page number on new section */
-	 PhArea_t area12 = { { 10, 282 }, { 268, 24 } };
-	 PtArg_t args12[] = {
-		Pt_ARG( Pt_ARG_AREA, &area12, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_RestartCheck).c_str(), 0 ),
-		};
-		/* PtLabel restart numbering at.. */
-	 PhArea_t area13 = { { 288, 285 }, { 139, 21 } };
-	 PtArg_t args13[] = {
-		Pt_ARG( Pt_ARG_AREA, &area13, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(AP_STRING_ID_DLG_HdrFtr_RestartNumbers).c_str() , 0 ),
-		};
-		/*PtText, where to restart numbering.  */
-	 PhArea_t area14 = { { 432, 280 }, { 37, 27 } };
-	 PtArg_t args14[] = {
-		Pt_ARG( Pt_ARG_AREA, &area14, 0 ),
-		Pt_ARG( Pt_ARG_TEXT_STRING,"0",0),
-		Pt_ARG( Pt_ARG_EDIT_MASK, "####", 0 ),
-		};
-		/*PtUpDown */
-	 PhArea_t area15 = { { 468, 280 }, { 18, 27 } };
-	 PtArg_t args15[] = {
-		Pt_ARG( Pt_ARG_AREA, &area15, 0 ),
-		};
-	/*PtPane for OK buttons. */
-	 PhArea_t area16 = { { 0, 321 }, { 496, 33 } };
-	 PtArg_t args16[] = {
-		Pt_ARG( Pt_ARG_AREA, &area16, 0 ),
-		Pt_ARG( Pt_ARG_FLAGS, 258,1334445470 ),
-		Pt_ARG( Pt_ARG_FILL_COLOR, 0xc0c0c0, 0 ),
-		Pt_ARG( Pt_ARG_BASIC_FLAGS, 67056,4194303 ),
-		};
-		/* PtButton Cancel */
-	 PhArea_t area17 = { { 320, 2 }, { 82, 27 } };
-	 PtArg_t args17[] = {
-		Pt_ARG( Pt_ARG_AREA, &area17, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING,pSS->getValueUTF8(XAP_STRING_ID_DLG_Cancel).c_str() , 0 ),
-		Pt_ARG( Pt_ARG_ANCHOR_FLAGS, 48,8191 ),
-		};
-		/* PtButton OK*/
-	 PhArea_t area18 = { { 408, 2 }, { 82, 27 } };
-	 PtArg_t args18[] = {
-		Pt_ARG( Pt_ARG_AREA, &area18, 0 ),
-Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(XAP_STRING_ID_DLG_OK).c_str(), 0 ),
-		};
-
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_Title),0);
+	PtSetArg(&args[n++],Pt_ARG_WINDOW_RENDER_FLAGS,Pt_FALSE,ABI_MODAL_WINDOW_RENDER_FLAGS);
 	mainWindow=PtCreateWidget( PtWindow,NULL,sizeof(args) / sizeof(PtArg_t),args);
-	Pane1=PtCreateWidget( PtPane, NULL, sizeof(args1) / sizeof(PtArg_t), args1 );
-		PtCreateWidget( PtPane, NULL, sizeof(args2) / sizeof(PtArg_t), args2 );
-	toggleHeadEven=PtCreateWidget( PtToggleButton, NULL, sizeof(args3) / sizeof(PtArg_t), args3 );
-	toggleHeadFirst=PtCreateWidget( PtToggleButton, NULL, sizeof(args4) / sizeof(PtArg_t), args4 );
-	toggleHeadLast=PtCreateWidget( PtToggleButton, NULL, sizeof(args5) / sizeof(PtArg_t), args5 );
-		PtCreateWidget( PtLabel, Pane1, sizeof(args6) / sizeof(PtArg_t), args6 );
-		PtCreateWidget( PtLabel,Pane1, sizeof(args7) / sizeof(PtArg_t), args7 );
-		PtCreateWidget( PtPane, Pane1, sizeof(args8) / sizeof(PtArg_t), args8 );
-	toggleFootFirst=PtCreateWidget( PtToggleButton, NULL, sizeof(args9) / sizeof(PtArg_t), args9 );
-	toggleFootLast=PtCreateWidget( PtToggleButton, NULL, sizeof(args10) / sizeof(PtArg_t), args10 );
-	toggleFootEven=PtCreateWidget( PtToggleButton, NULL, sizeof(args11) / sizeof(PtArg_t), args11 );
-	toggleRestartNewSection=PtCreateWidget( PtToggleButton, Pane1, sizeof(args12) / sizeof(PtArg_t), args12 );
-	RestartLabel=PtCreateWidget( PtLabel, Pane1, sizeof(args13) / sizeof(PtArg_t), args13 );
-	textRestartNumber=PtCreateWidget( PtText, Pane1, sizeof(args14) / sizeof(PtArg_t), args14 );
-	UpDown=PtCreateWidget( PtUpDown, Pane1, sizeof(args15) / sizeof(PtArg_t), args15 );
-		PtCreateWidget( PtPane, mainWindow, sizeof(args16) / sizeof(PtArg_t), args16 );
-	PtButton_cancel=PtCreateWidget( PtButton, NULL, sizeof(args17) / sizeof(PtArg_t), args17 );
-	PtButton_ok=PtCreateWidget( PtButton, NULL, sizeof(args18) / sizeof(PtArg_t), args18 );
+	n=0;
 
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ORIENTATION,Pt_GROUP_VERTICAL,0);
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ROWS_COLS,4,0);
+	PtSetArg(&args[n++],Pt_ARG_GROUP_FLAGS,Pt_TRUE,Pt_GROUP_EQUAL_SIZE_HORIZONTAL);
+	PtWidget_t *mgroup = PtCreateWidget(PtGroup,Pt_DEFAULT_PARENT,n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ORIENTATION,Pt_GROUP_VERTICAL,0);
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ROWS_COLS,3,0);
+	PtWidget_t *headgrp = PtCreateWidget(PtGroup,Pt_DEFAULT_PARENT,n,args);
+	pretty_group(headgrp,_(AP,DLG_HdrFtr_HeaderFrame));
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_HeaderEven),0);
+	toggleHeadEven=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT,n,args );
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_HeaderFirst),0);
+	toggleHeadFirst=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT,n,args);
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_HeaderLast),0);
+	toggleHeadLast=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT,n,args );
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ORIENTATION,Pt_GROUP_VERTICAL,0);
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ROWS_COLS,3,0);
+	PtWidget_t *footgrp = PtCreateWidget(PtGroup,mgroup,n,args);
+	pretty_group(footgrp,_(AP,DLG_HdrFtr_FooterFrame));
+
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_FooterEven),0);
+	toggleFootEven=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT, n,args);
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_FooterFirst),0);
+	toggleFootFirst=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT, n,args);
+	n=0;
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_FooterLast),0);
+	toggleFootLast=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT, n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ROWS_COLS,3,0);
+	PtCreateWidget(PtGroup,mgroup,n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_RestartCheck),0);
+	toggleRestartNewSection=PtCreateWidget( PtToggleButton, Pt_DEFAULT_PARENT,n,args); 
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(AP,DLG_HdrFtr_RestartNumbers),0);
+	RestartLabel=PtCreateWidget( PtLabel, Pt_DEFAULT_PARENT, n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_NUMERIC_MAX,10000,0);
+	PtSetArg(&args[n++],Pt_ARG_NUMERIC_MIN,0,0);
+	UpDown=PtCreateWidget( PtNumericInteger, Pt_DEFAULT_PARENT,n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_GROUP_ROWS_COLS,2,0);
+	PtCreateWidget(PtGroup,mgroup,n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(XAP,DLG_OK),0);
+	PtButton_ok=PtCreateWidget( PtButton, Pt_DEFAULT_PARENT, n,args);
+	n=0;
+
+	PtSetArg(&args[n++],Pt_ARG_TEXT_STRING,_(XAP,DLG_Cancel),0);
+	PtButton_cancel=PtCreateWidget( PtButton, Pt_DEFAULT_PARENT,n,args);
+	n=0;
 	
 	PtAddCallback(PtButton_cancel,Pt_CB_ACTIVATE,ph_event_cancel,this);
 	PtAddCallback(PtButton_ok,Pt_CB_ACTIVATE,ph_event_ok,this);
@@ -367,7 +273,7 @@ Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(XAP_STRING_ID_DLG_OK).c_str(), 0 )
 	PtAddCallback(toggleFootEven,Pt_CB_ACTIVATE,ph_FtrEven,this);
 	PtAddCallback(toggleFootFirst,Pt_CB_ACTIVATE,ph_FtrFirst,this);
 	PtAddCallback(toggleFootLast,Pt_CB_ACTIVATE,ph_FtrLast,this);
-	PtAddCallback(UpDown,Pt_CB_ACTIVATE,ph_event_activate_updown,this);
+	PtAddCallback(UpDown,Pt_CB_NUMERIC_CHANGED,ph_event_activate_updown,this);
 
 	m_wHdrFtr[HdrEven]= toggleHeadEven;
 	m_wHdrFtr[HdrFirst]= toggleHeadFirst;
@@ -377,7 +283,6 @@ Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(XAP_STRING_ID_DLG_OK).c_str(), 0 )
 	m_wHdrFtr[FtrLast]= toggleFootLast;
 	m_RestartLabel=RestartLabel;
 	m_RestartUpDown= UpDown;
-	m_RestartText=textRestartNumber;
 	m_RestartToggle=toggleRestartNewSection;
 	/* Set up the default state */
 	
@@ -386,7 +291,6 @@ Pt_ARG( Pt_ARG_TEXT_STRING, pSS->getValueUTF8(XAP_STRING_ID_DLG_OK).c_str(), 0 )
 	} else {
 		PtSetResource(m_RestartLabel,	Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
 		PtSetResource(m_RestartUpDown,Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
-		PtSetResource(m_RestartText,Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
 	}
 
 	UT_sint32 j = (UT_sint32) HdrEven;
@@ -419,24 +323,22 @@ setValue(which,value,true);
 
 void AP_QNXDialog_HdrFtr::RestartChanged(void)
 {
-char *text;
+int *num;
 long * flags;
 UT_sint32 RestartValue;
 
-PtGetResource(m_RestartText,Pt_ARG_TEXT_STRING,&text,0);
+PtGetResource(m_RestartUpDown,Pt_ARG_NUMERIC_VALUE,&num,0);
 PtGetResource(m_RestartToggle,Pt_ARG_FLAGS,&flags,0);
 
-RestartValue=atoi(text);
+RestartValue=*num;
 if(Pt_SET & *flags)
 	{
 		PtSetResource(m_RestartLabel,	Pt_ARG_FLAGS,Pt_FALSE,Pt_GHOST|Pt_BLOCKED);
 		PtSetResource(m_RestartUpDown,Pt_ARG_FLAGS,Pt_FALSE,Pt_GHOST|Pt_BLOCKED);
-		PtSetResource(m_RestartText,Pt_ARG_FLAGS,Pt_FALSE,Pt_GHOST|Pt_BLOCKED);
 		setRestart(true,RestartValue,true);
 	}else {
 		PtSetResource(m_RestartLabel,	Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
 		PtSetResource(m_RestartUpDown,Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
-		PtSetResource(m_RestartText,Pt_ARG_FLAGS,Pt_TRUE,Pt_GHOST|Pt_BLOCKED);
 		setRestart(false,RestartValue,true);
 	}
 }
