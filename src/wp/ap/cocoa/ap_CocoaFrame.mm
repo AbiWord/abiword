df334495b6ca3333816eeba74fd7d07c94f0a6ec
Hubert Figuiere <hub@figuiere.net>
2002-11-02 18:14:20 +0000

    Fix build (loc system changes by Jordi)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@9636 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/cocoa/ap_CocoaFrame.mm
+++ src/wp/ap/cocoa/ap_CocoaFrame.mm
@@ -390,9 +390,9 @@ bool AP_CocoaFrame::initialize()
 
 	if (!XAP_CocoaFrame::initialize(AP_PREF_KEY_KeyBindings,AP_PREF_DEFAULT_KeyBindings,
 								   AP_PREF_KEY_MenuLayout, AP_PREF_DEFAULT_MenuLayout,
-								   AP_PREF_KEY_MenuLabelSet, AP_PREF_DEFAULT_MenuLabelSet,
+								   AP_PREF_KEY_StringSet, AP_PREF_DEFAULT_StringSet,
 								   AP_PREF_KEY_ToolbarLayouts, AP_PREF_DEFAULT_ToolbarLayouts,
-								   AP_PREF_KEY_ToolbarLabelSet, AP_PREF_DEFAULT_ToolbarLabelSet))
+								   AP_PREF_KEY_StringSet, AP_PREF_DEFAULT_StringSet))
 		return false;
 
 #ifndef ABI_OPT_WIDGET
