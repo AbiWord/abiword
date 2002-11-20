5799164c1dde6259d01f0ad66e4a12ce9d40cdcf
Mark Gilbert <mgilbert@svn.abisource.com>
2002-11-20 03:23:27 +0000

    Yesterday (day before?) I unb0rkified the spell checker by removing some premature frees.  These were leaving the dialog to reference uninitialized data (aka garbage).  Now, those frees were put in place to fix a mem leak problem (a rather significant one, especially when lots of spelling mistakes were made).  With this commit I've replugged the leaks.  The four sources of leakage iirc were preword word postword and suggest (suggest is just a small array used temporarily in building the list of suggested replacements, each suggested replacement for each misspelled word occupies it at some point or another). Better late than never I suppose. Keepin' it clean, green, and ungrammarific -MG
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-1-0-0-STABLE@9790 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/unix/ap_UnixDialog_Spell.cpp
+++ src/wp/ap/unix/ap_UnixDialog_Spell.cpp
@@ -398,28 +398,26 @@ void AP_UnixDialog_Spell::_showMisspelledWord(void)
    // insert start of sentence
    p = _getPreWord();
    gchar * preword = (gchar*) _convertToMB(p);
-   //FREEP(mbword);
    FREEP(p);
    gtk_text_insert(GTK_TEXT(m_textWord), NULL, NULL, NULL,
 		   preword, strlen(preword));
-
+   FREEP(preword);
    // insert misspelled word (in highlight color)
    p = _getCurrentWord();
    gchar * word = (gchar*) _convertToMB(p);
-   //FREEP(mbword);
    FREEP(p);
 
    gtk_text_insert(GTK_TEXT(m_textWord) , NULL, &m_highlight, NULL,
 		   word, strlen(word));
-   
+   FREEP(word);
    // insert end of sentence
    p = _getPostWord();
    gchar * postword = (gchar*) _convertToMB(p);
-   //FREEP(mbword);
    FREEP(p);
    gtk_text_insert(GTK_TEXT(m_textWord), NULL, NULL, NULL,
 		   postword, strlen(postword));
 
+   FREEP(postword);
    // TODO: set scroll position so misspelled word is centered
 
    gtk_text_thaw( GTK_TEXT(m_textWord) );   
@@ -430,8 +428,8 @@ void AP_UnixDialog_Spell::_showMisspelledWord(void)
    
    for (UT_uint32 i = 0; i < m_Suggestions->getItemCount(); i++) {
       suggest[0] = (gchar*) _convertToMB((UT_UCSChar*)m_Suggestions->getNthItem(i));
-      //FREEP(mbword);
       gtk_clist_append( GTK_CLIST(m_clistSuggestions), suggest);
+      FREEP(suggest[0]);
    }
    
    if (!m_Suggestions->getItemCount()) {
@@ -452,7 +450,6 @@ void AP_UnixDialog_Spell::_showMisspelledWord(void)
 
       // select first on the list; signal handler should update our entry box
       gtk_clist_select_row(GTK_CLIST(m_clistSuggestions), 0, 0);
-
    }
    
    gtk_clist_thaw(GTK_CLIST(m_clistSuggestions) );
@@ -595,7 +592,7 @@ void AP_UnixDialog_Spell::event_ReplacementChanged()
 // make a multibyte encoded version of a string
 char * AP_UnixDialog_Spell::_convertToMB(UT_UCSChar *wword)
 {
-	char *mbword = (char *) malloc (UT_UCS_strlen(wword)*2);
+	mbword = (char *) malloc (UT_UCS_strlen(wword)*2);
 	UT_UCS_strcpy_to_char(mbword,wword);
 	return mbword;
 }
