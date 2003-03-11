8b16db1ad9f338cf1e5b97d164f162074920e614
Mark Gilbert <mgilbert@svn.abisource.com>
2003-03-11 04:44:58 +0000

    More build fixage for mingw32.  As it only supports win-fe builds, it should not include nix-common defs, even though they are in some areas coincidal.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@10844 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/config/platforms/mingw32.mk
+++ src/config/platforms/mingw32.mk
@@ -22,6 +22,8 @@
 ## Mingw platform defines
 ##############################################################################
 
+# *** WARNING: THESE DEFS WIRE WIN_FE, NO CHOICE, AND ASSUME A NATIVE WIN32 RUNTIME ENVIRONMENT.  ***
+
 # Here for syntactic purposes.
 ##
 PLATFORM_FLAGS	=
@@ -30,14 +32,121 @@ OS_CFLAGS		=
 ##
 
 # mingw doesn't like -ansi in compiling wv
-ABI_OPT_PACIFY_COMPILER=1
+ABI_OPT_PACIFY_COMPILER=1 # There were changes in wv, not sure we need this any more
 # *** We do not support ancient mingw versions of the gcc2 kind ***
 
-# Now we include some really common stuff.
-include $(ABI_ROOT)/src/config/platforms/nix-common.mk
-# I highly suggest that here we redefine the compiler, archiver.
-# Just need to find a consistent solution, possibly of conditionals,
-# that works across mingw installations that have a high internal variance.
+ifndef $(OS_ARCH)
+OS_ARCH		:= $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc32/ -e s/sparc32/sparc/ -e s/arm.*/arm/ -e s/sa110/arm/ | sed "s/\//-/")
+endif
+ifndef $(OS_REALARCH)
+OS_REALARCH	:= $(shell uname -m)
+endif
+
+ALPHA_ARCH_FLAGS 	=
+IA64_ARCH_FLAGS		=
+
+# Define tools
+CC		= gcc
+CCC		= g++
+RANLIB		= ranlib
+
+# Suffixes
+OBJ_SUFFIX	= o
+LIB_SUFFIX	= a
+DLL_SUFFIX	= so # We dont use actual dlls, for disting anyway.  We could though, but given the windows using church secretary, I think current system is safer.
+AR		= ar cr $@
+
+OBJ_DIR_SFX	=
+DEFINES		=
+OPTIMIZER	=
+
+ifdef ABI_OPT_PROF
+    ifeq ($(ABI_OPT_PROF),1)
+    OPTIMIZER   	= -pg
+    OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)PRF_
+    ABI_OPT_OPTIMIZE= 1
+    ABI_OPT_DEBUG	= 0
+    ABI_OPTIONS	+= Profile:On
+    endif
+    ifeq ($(ABI_OPT_PROF),2)
+    OPTIMIZER   	= -pg -g -fprofile-arcs -ftest-coverage
+    OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)PRF_
+    ABI_OPT_OPTIMIZE= 1
+    ABI_OPT_DEBUG	= 0
+    ABI_OPTIONS	+= Profile:On
+    endif
+	# Level 3 is deprecated in most areas, for the systems used by the people who'd use it.
+endif
+
+ifeq ($(ABI_OPT_OPTIMIZE),1)
+OPTIMIZER	+= -O3
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OPT_
+ABI_OPTIONS	+= Optimize:On
+ABI_OPT_DEBUG	= 0
+    ifeq ($(ABI_OPT_EXCLUSIVE_OPT),1)
+    OPTIMIZER := $(OPTIMIZER) -march=$(OS_REALARCH)
+    else
+    OPTIMIZER := $(OPTIMIZER) -mcpu=$(OS_REALARCH)
+    endif
+else
+    ifeq ($(ABI_OPT_TINY),1)
+    OPTIMIZER	= -Os -fno-default-inline -fno-inline
+    else
+    OPTIMIZER	= -O2
+    endif
+endif
+
+ifeq ($(ABI_OPT_DEBUG),1)
+OPTIMIZER	= -g
+DEFINES		= -DDEBUG
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)DBG_
+endif
+ifeq ($(ABI_OPT_DEBUG),2)
+OPTIMIZER	= -g3
+DEFINES		= -DDEBUG -DUT_DEBUG -DFMT_TEST -DUT_TEST -DPT_TEST -UNDEBUG
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)DBG_
+endif
+
+OBJ_DIR_SFX	:= $(OBJ_DIR_SFX)OBJ
+
+ifneq ($(ABI_OPT_PACIFY_COMPILER),1)
+WARNFLAGS	+= -Wall -ansi -pedantic
+else
+WARNFLAGS	+= -fpermissive -w
+endif
+
+# Includes
+OS_INCLUDES		=
+ifeq ($(ABI_REQUIRE_PEER_ICONV),1)
+OS_INCLUDES		+= -I$(ABI_ROOT)/../libiconv/include
+endif
+
+# TODO: This is true on about 1/10 platforms.  Why not use pkgconfig?
+ifeq ($(ABI_OPT_PANGO),1)
+OS_INCLUDES += -I/usr/local/include/pango-1.0 -I/usr/local/include/freetype2 -I/usr/local/include/glib-2.0
+endif
+
+# Architecture-specific flags
+ifeq ($(OS_ARCH), i386)
+PLATFORM_FLAGS		+= $(i386_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), alpha)
+PLATFORM_FLAGS		+= $(ALPHA_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+ifeq ($(OS_ARCH), ia64)
+PLATFORM_FLAGS		+= $(IA64_ARCH_FLAGS)
+OS_ENDIAN		= LittleEndian32
+endif
+
+# LIBXML_CONFIG	= xml2-config   # yeah, so, we don't use this yet.  may or may not before 2.0
+
+# Which links can this platform create.  Define one not both of these options.
+UNIX_CAN_BUILD_DYNAMIC=0
+UNIX_CAN_BUILD_STATIC=1 # I'm still not totally decided really...I'll need to experiment some more.
 
 # Define tools - resource compiler
 RC		= windres
@@ -48,11 +157,6 @@ EXE_SUFFIX = .exe
 # Compiler flags
 # requires the commctrl.dll from ie4.0 or greater
 DEFINES		+= -D_WIN32_IE=0x0400 -DSUPPORTS_UT_IDLE
-G++INCLUDES=
-GLIB_CONFIG=
-GTK_CONFIG=
-GNOME_CONFIG=
-LIBXML_CONFIG=
 OS_CFLAGS += -fvtable-thunks  # required for compiling ole dragndrop
 
 # Shared library flags
