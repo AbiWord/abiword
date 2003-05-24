c7d61a024ec731758b6b90db378d83e3aa660d7a
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2003-05-24 12:48:19 +0000

    set text-alignment to right for RTL paragraphs
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@11437 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_imp_Text.cpp
+++ src/wp/impexp/xp/ie_imp_Text.cpp
@@ -243,9 +243,9 @@ bool IE_Imp_Text::_insertSpan(UT_GrowBuf &b)
 				UT_String props("dom-dir:");
 				
 				if(FRIBIDI_IS_RTL(type))
-					props += "rtl";
+					props += "rtl;text-align:right";
 				else
-					props += "ltr";
+					props += "ltr;text-align:left";
 
 				propsArray[1] = props.c_str();
 				
