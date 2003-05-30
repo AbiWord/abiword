c82d418cdddfb51e4f6fa8c133f676ec0273db99
J M Maurer <uwog@uwog.net>
2003-05-30 22:40:27 +0000

    Some copyright information
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@11522 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Dialog_FormatTable.cpp
+++ src/wp/ap/xp/ap_Dialog_FormatTable.cpp
@@ -1,6 +1,6 @@
 /* AbiWord
  * Copyright (C) 1998 AbiSource, Inc.
- * 
+ * Copyright (C) 2003 Marc Maurer
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
