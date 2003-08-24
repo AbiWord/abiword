3296a58d20a0dbf4e593d003d4bb6b5b09a012ba
Jordi Mas <jmas@softcatala.org>
2003-08-24 18:58:37 +0000

    win32 build fix
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@12148 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_imp_MsWord_97.cpp
+++ src/wp/impexp/xp/ie_imp_MsWord_97.cpp
@@ -3401,8 +3401,8 @@ UT_Error IE_Imp_MsWord_97::_handleImage (Blip * b, long width, long height)
     unsigned long uncomprLen, comprLen;
     comprLen = size;
     uncomprLen = b->blip.metafile.m_cb;
-    unsigned char *uncompr = new unsigned char[uncomprLen];
-    int err = uncompress (uncompr, &uncomprLen, data, comprLen);
+    unsigned char *uncompr = new unsigned char[uncomprLen];    
+    int err = uncompress (uncompr, &uncomprLen, reinterpret_cast<const unsigned char *>(data), comprLen);
     if (err != Z_OK)
       {
 	UT_DEBUGMSG(("Could not uncompress image\n"));
