1817eed3eca560a38a35f9bff74b3ad97ac83178
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2003-11-17 09:35:43 +0000

    remove incorrect assert
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@12829 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/ev/win/ev_Win32Toolbar.cpp
+++ src/af/ev/win/ev_Win32Toolbar.cpp
@@ -1048,7 +1048,13 @@ bool EV_Win32Toolbar::_refreshItem(AV_View * pView, const EV_Toolbar_Action * pA
 				bool bGrayed = EV_TIS_ShouldBeGray(tis);
 				bool bString = EV_TIS_ShouldUseString(tis);
 
-				UT_return_val_if_fail(szState, true);
+				// szState can be legitimately NULL, for instance in
+				// the font combo box if the selection spans across
+				// text using more than one font. In that case, we
+				// will for now just leave the contexnts of the window
+				// as they are
+				if(!szState)
+					return true;
 
 				HWND hwndCombo = _getControlWindow(id);
 				UT_return_val_if_fail(hwndCombo, true);
