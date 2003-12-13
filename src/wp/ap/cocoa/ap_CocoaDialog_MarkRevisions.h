fec1ba3dcd897264f2904b8d35db79359ffc4af2
Hubert Figuiere <hub@figuiere.net>
2003-12-13 18:00:08 +0000

    3 new dialogs 	-list revisions 	-mark revisions 	-image properties
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@13050 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/cocoa/ap_CocoaDialog_MarkRevisions.h
+++ src/wp/ap/cocoa/ap_CocoaDialog_MarkRevisions.h
@@ -1,6 +1,7 @@
 /* AbiWord
  * Copyright (C) 1998-2000 AbiSource, Inc.
  * Copyright (C) 2002 Tomas Frydrych <tomas@frydrych.uklinux.net>
+ * Copyright (C) 2003 Hubert Figuiere
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,27 +19,54 @@
  * 02111-1307, USA.
  */
 
-#ifndef AP_COCOADIALOG_STUB_H
-#define AP_COCOADIALOG_STUB_H
+#ifndef AP_COCOADIALOG_MARKREVISIONS_H
+#define AP_COCOADIALOG_MARKREVISIONS_H
 
 #include "ap_Dialog_MarkRevisions.h"
 
 class XAP_CocoaFrame;
+@class AP_CocoaDialog_MarkRevisionsController;
+
 
 /*****************************************************************/
 
 class AP_CocoaDialog_MarkRevisions: public AP_Dialog_MarkRevisions
 {
 public:
-	AP_CocoaDialog_MarkRevisions(XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id id);
+	AP_CocoaDialog_MarkRevisions(XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id dlgid);
 	virtual ~AP_CocoaDialog_MarkRevisions(void);
 
 	virtual void			runModal(XAP_Frame * pFrame);
 
-	static XAP_Dialog *		static_constructor(XAP_DialogFactory *, XAP_Dialog_Id id);
+	static XAP_Dialog *		static_constructor(XAP_DialogFactory *, XAP_Dialog_Id dlgid);
+	void event_OK();
+	void event_Cancel();
+	void event_FocusToggled () ;
 
-protected:
-
+private:
+	AP_CocoaDialog_MarkRevisionsController	*m_dlg;
 };
+
+
+@interface AP_CocoaDialog_MarkRevisionsController : NSWindowController <XAP_CocoaDialogProtocol>
+{
+    IBOutlet NSButton *_cancelBtn;
+    IBOutlet NSTextField *_commentData;
+	IBOutlet NSTextField *_label1;
+    IBOutlet NSTextField *_label2;
+    IBOutlet NSButton *_okBtn;
+    IBOutlet NSButton *_radio1;
+    IBOutlet NSButton *_radio2;
+	AP_CocoaDialog_MarkRevisions *_xap;
+}
+- (IBAction)cancelAction:(id)sender;
+- (IBAction)okAction:(id)sender;
+- (IBAction)radio1Action:(id)sender;
+- (IBAction)radio2Action:(id)sender;
+
+- (NSString*)comment;
+- (int)toggled;
+- (void)setItems2Enabled:(bool)state;
+@end
 
 #endif /* AP_COCOADIALOG_MARKREVISIONS_H */
--- src/wp/ap/cocoa/ap_CocoaDialog_MarkRevisions.mm
+++ src/wp/ap/cocoa/ap_CocoaDialog_MarkRevisions.mm
@@ -1,7 +1,7 @@
 /* AbiWord
  * Copyright (C) 2000 AbiSource, Inc.
- * Copyright (C) 2001-2002 Hubert Figuiere
  * Copyright (C) 2002 Tomas Frydrych <tomas@frydrych.uklinux.net>
+ * Copyright (C) 2001-2003 Hubert Figuiere
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -31,6 +31,7 @@
 #include "xap_App.h"
 #include "xap_CocoaApp.h"
 #include "xap_CocoaFrame.h"
+#include "xap_CocoaDialog_Utilities.h"
 
 #include "ap_Strings.h"
 #include "ap_Dialog_Id.h"
@@ -40,15 +41,16 @@
 /*****************************************************************/
 
 XAP_Dialog * AP_CocoaDialog_MarkRevisions::static_constructor(XAP_DialogFactory * pFactory,
-													 XAP_Dialog_Id id)
+													 XAP_Dialog_Id dlgid)
 {
-	AP_CocoaDialog_MarkRevisions * p = new AP_CocoaDialog_MarkRevisions(pFactory,id);
+	AP_CocoaDialog_MarkRevisions * p = new AP_CocoaDialog_MarkRevisions(pFactory,dlgid);
 	return p;
 }
 
 AP_CocoaDialog_MarkRevisions::AP_CocoaDialog_MarkRevisions(XAP_DialogFactory * pDlgFactory,
-										 XAP_Dialog_Id id)
-	: AP_Dialog_MarkRevisions(pDlgFactory,id)
+										 XAP_Dialog_Id dlgid)
+	: AP_Dialog_MarkRevisions(pDlgFactory,dlgid),
+		m_dlg(nil)
 {
 }
 
@@ -56,10 +58,35 @@ AP_CocoaDialog_MarkRevisions::~AP_CocoaDialog_MarkRevisions(void)
 {
 }
 
+void AP_CocoaDialog_MarkRevisions::event_OK ()
+{
+	m_answer = AP_Dialog_MarkRevisions::a_OK;
+	setComment2 ([[m_dlg comment] UTF8String]);
+	[NSApp stopModal];
+}
+
+void AP_CocoaDialog_MarkRevisions::event_Cancel ()
+{
+	m_answer = AP_Dialog_MarkRevisions::a_CANCEL ;
+	[NSApp stopModal];
+}
+
+void AP_CocoaDialog_MarkRevisions::event_FocusToggled ()
+{
+	bool second_active = false;
+	int toggled = [m_dlg toggled];
+	
+	if ((toggled == 2) || (getRadio1Label() == NULL)) {
+		second_active = true;
+	}
+	[m_dlg setItems2Enabled:second_active];
+}
+
+
 void AP_CocoaDialog_MarkRevisions::runModal(XAP_Frame * pFrame)
 {
-	UT_ASSERT(pFrame);
-
+	/* comment below should be re-read after analysing the UNIX code  -- Hub*/
+	
 	/*
 	   This is the only function you need to implement, and the MarkRevisions
 	   dialogue should look like this:
@@ -97,6 +124,126 @@ void AP_CocoaDialog_MarkRevisions::runModal(XAP_Frame * pFrame)
 
 
 	*/
+	m_dlg = [[AP_CocoaDialog_MarkRevisionsController alloc] initFromNib];
+	
+	[m_dlg setXAPOwner:this];
 
-	UT_ASSERT(UT_NOT_IMPLEMENTED);
+	NSWindow * window = [m_dlg window];
+	UT_ASSERT(window);
+
+	event_FocusToggled();
+
+	[NSApp runModalForWindow:window];
+
+	[m_dlg discardXAP];
+	[m_dlg close];
+	[m_dlg release];
+	m_dlg = nil;
 }
+
+
+@implementation AP_CocoaDialog_MarkRevisionsController
+
+
+- (id)initFromNib
+{
+	self = [super initWithWindowNibName:@"ap_CocoaDialog_MarkRevisions"];
+	return self;
+}
+
+-(void)discardXAP
+{
+	_xap = NULL; 
+}
+
+-(void)dealloc
+{
+	[super dealloc];
+}
+
+- (void)setXAPOwner:(XAP_Dialog *)owner
+{
+	_xap = dynamic_cast<AP_CocoaDialog_MarkRevisions*>(owner);
+}
+
+-(void)windowDidLoad
+{
+	if (_xap) {
+		char * str;
+		char * str2;
+		const XAP_StringSet *pSS = XAP_App::getApp()->getStringSet();
+		LocalizeControl([self window], pSS, AP_STRING_ID_DLG_MarkRevisions_Title);
+		LocalizeControl(_okBtn, pSS, XAP_STRING_ID_DLG_OK);
+		LocalizeControl(_cancelBtn, pSS, XAP_STRING_ID_DLG_Cancel);
+		str = _xap->getRadio1Label();
+		if (str) {
+			[_radio1 setTitle:[NSString stringWithUTF8String:str]];
+			[_radio2 setTitle:[NSString stringWithUTF8String:_xap->getRadio2Label()]];
+			str2 = _xap->getComment1();
+			if (str2) {
+				[_label1 setStringValue:[NSString stringWithUTF8String:str2]];
+				FREEP(str2);	/* getComment1() is allocated */
+			}
+			FREEP(str);
+		}
+		else {
+			[_radio1 removeFromSuperview];
+			_radio1 = nil;
+			[_radio2 removeFromSuperview];
+			_radio2 = nil;
+			[_label1 removeFromSuperview];
+			_label1 = nil;
+		}
+		[_label2 setStringValue:[NSString stringWithUTF8String:_xap->getComment2Label()]];
+	}
+}
+
+
+- (IBAction)cancelAction:(id)sender
+{
+	_xap->event_Cancel();
+}
+
+- (IBAction)okAction:(id)sender
+{
+	_xap->event_OK();
+}
+
+- (IBAction)radio1Action:(id)sender
+{
+	[_radio2 setState:NSOffState];
+	_xap->event_FocusToggled();
+}
+
+- (IBAction)radio2Action:(id)sender
+{
+	[_radio1 setState:NSOffState];
+	_xap->event_FocusToggled();
+}
+
+- (NSString*)comment
+{
+	return [_commentData stringValue];
+}
+
+- (int)toggled
+{
+	if ([_radio1 state] == NSOnState) {
+		return 1;
+	}
+	else {
+		return 2;
+	}
+}
+
+/*!
+	Toggle enabling of bottom part of the dialog.
+ */
+- (void)setItems2Enabled:(bool)state
+{
+	[_label2 setEnabled:state];
+	[_commentData setEnabled:state];
+}
+
+
+@end
