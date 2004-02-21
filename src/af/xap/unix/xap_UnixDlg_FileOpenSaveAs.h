27eb72d9d355ab7894f2906bce3f90666dee7e6d
J M Maurer <uwog@uwog.net>
2004-02-21 23:48:59 +0000

    Replace GtkFileSelection with GtkFileChooser
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-GSFGTK24-PORT@13456 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/unix/xap_UnixDlg_FileOpenSaveAs.h
+++ src/af/xap/unix/xap_UnixDlg_FileOpenSaveAs.h
@@ -45,7 +45,7 @@ public:
 
 protected:
 
-	bool					_run_gtk_main(XAP_Frame * pFrame, void * pFSvoid,
+	bool					_run_gtk_main(XAP_Frame * pFrame,
 										  bool bCheckWritePermission,
 										  GtkWidget * filetypes_pulldown);
 	void 					_notifyError_OKOnly(XAP_Frame * pFrame,
@@ -56,7 +56,7 @@ protected:
 	bool 				_askOverwrite_YesNo(XAP_Frame * pFrame,
 												const char * fileName);
 
-	GtkFileSelection * m_FS;
+	GtkFileChooser * m_FC;
 	GtkWidget * m_preview;
 private:
 	bool				m_bExport;
