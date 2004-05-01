2da21ec8faca2a57fc2f8fa4ef41d4eefaa5705a
Mikey Cooper <mikey@bluey.com>
2004-05-01 17:14:24 +0000

    Set boundaries for scroll-wheel zooming at 10% - 500%.  Fixes bug 6559.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@14031 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_EditMethods.cpp
+++ src/wp/ap/xp/ap_EditMethods.cpp
@@ -7479,7 +7479,7 @@ Defun1(zoomIn)
 	UT_return_val_if_fail(pFrame, false);
 	
 	pFrame->raise();
-	UT_uint32 newZoom = pFrame->getZoomPercentage() + 10;
+	UT_uint32 newZoom = UT_MIN(pFrame->getZoomPercentage() + 10, 500);
 	UT_String tmp (UT_String_sprintf("%d",newZoom));
 	XAP_App * pApp = pFrame->getApp();
 	UT_return_val_if_fail(pApp, false);
@@ -7504,7 +7504,7 @@ Defun1(zoomOut)
 	
 	pFrame->raise();
 	
-	UT_uint32 newZoom = pFrame->getZoomPercentage() - 10;
+	UT_uint32 newZoom = UT_MAX(pFrame->getZoomPercentage() - 10, 10);
 	UT_String tmp (UT_String_sprintf("%d",newZoom));
 	XAP_App * pApp = pFrame->getApp();
 	UT_return_val_if_fail(pApp, false);
