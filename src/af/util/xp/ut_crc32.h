251cf54c38c9acefb004bb2232a21a8e14b71d9e
Mikey Cooper <mikey@bluey.com>
2004-06-30 05:13:38 +0000

    Remove MAC-style EOL characters.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@14278 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_crc32.h
+++ src/af/util/xp/ut_crc32.h
@@ -15,21 +15,21 @@ const UT_uint32 CRC32_NEGL = 0xffffffffL;
 class UT_CRC32
 {
 public:
-	enum {DIGESTSIZE = 4};
-	UT_CRC32();
-	void Fill(const char *input);
-	void Fill(const char *input, UT_uint32 length);
-	void Fill(const unsigned char *input, UT_uint32 length);
-	UT_uint32 DigestSize() const {return DIGESTSIZE;}
+        enum {DIGESTSIZE = 4};
+        UT_CRC32();
+        void Fill(const char *input);
+        void Fill(const char *input, UT_uint32 length);
+        void Fill(const unsigned char *input, UT_uint32 length);
+        UT_uint32 DigestSize() const {return DIGESTSIZE;}
 
-	void UpdateByte(unsigned char b) {m_crc = m_tab[CRC32_INDEX(m_crc) ^ b] ^ CRC32_SHIFTED(m_crc);}
-	unsigned char GetCrcByte(UT_uint32 i) const {return ((unsigned char *)&(m_crc))[i];}
-	UT_uint32 GetCRC32(void) const { return m_crc;}
+        void UpdateByte(unsigned char b) {m_crc = m_tab[CRC32_INDEX(m_crc) ^ b] ^ CRC32_SHIFTED(m_crc);}
+        unsigned char GetCrcByte(UT_uint32 i) const {return ((unsigned char *)&(m_crc))[i];}
+        UT_uint32 GetCRC32(void) const { return m_crc;}
 private:
-	void Reset() {m_crc = CRC32_NEGL;}
-	
-	static const UT_uint32 m_tab[256];
-	UT_uint32  m_crc;
+        void Reset() {m_crc = CRC32_NEGL;}
+        
+        static const UT_uint32 m_tab[256];
+        UT_uint32  m_crc;
 };
 
 
