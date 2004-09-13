81e8526f58b38520344059db5652c1b516fa5d36
J M Maurer <uwog@uwog.net>
2004-09-13 17:49:31 +0000

    Fix bug 7365: "Paragraph Dialog: line spacing ambiguos" (Robert Staudinger)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@14754 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/unix/ap_UnixDialog_Paragraph.cpp
+++ src/wp/ap/unix/ap_UnixDialog_Paragraph.cpp
@@ -1103,10 +1103,12 @@ void AP_UnixDialog_Paragraph::_syncControls(tControl changed, bool bAll /* = fal
 		case indent_NONE:
 			// clear the spin control
 			gtk_entry_set_text(GTK_ENTRY(m_spinbuttonBy), "");
+			gtk_widget_set_sensitive(m_spinbuttonBy, FALSE);
 			break;
 
 		default:
 			// set the spin control
+			gtk_widget_set_sensitive(m_spinbuttonBy, TRUE);
 			gtk_entry_set_text(GTK_ENTRY(m_spinbuttonBy), _getSpinItemValue(id_SPIN_SPECIAL_INDENT));
 			break;
 		}
@@ -1132,10 +1134,12 @@ void AP_UnixDialog_Paragraph::_syncControls(tControl changed, bool bAll /* = fal
 		case spacing_DOUBLE:
 			// clear the spin control
 			gtk_entry_set_text(GTK_ENTRY(m_spinbuttonAt), "");
+			gtk_widget_set_sensitive(m_spinbuttonAt, FALSE);
 			break;
 
 		default:
 			// set the spin control
+			gtk_widget_set_sensitive(m_spinbuttonAt, TRUE);
 			gtk_entry_set_text(GTK_ENTRY(m_spinbuttonAt), _getSpinItemValue(id_SPIN_SPECIAL_SPACING));
 			break;
 		}
