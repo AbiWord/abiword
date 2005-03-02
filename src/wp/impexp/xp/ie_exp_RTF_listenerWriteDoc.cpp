6ed9b2cf91c7c1572a54c8653c6aec6eb8ba2cc7
J M Maurer <uwog@uwog.net>
2005-03-02 00:00:54 +0000

    Backport: Fix extra lines of RTF export (bug 5627) fix from Birch Knutson
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-2-0-STABLE@16037 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_exp_RTF_listenerWriteDoc.cpp
+++ src/wp/impexp/xp/ie_exp_RTF_listenerWriteDoc.cpp
@@ -3954,6 +3954,7 @@ bool s_RTF_ListenerWriteDoc::populateStrux(PL_StruxDocHandle sdh,
 			m_sdh = sdh;
 			_rtf_open_section(pcr->getIndexAP());
 			m_bInBlock = false;
+			m_bBlankLine = true;
 			return true;
 		}
 
