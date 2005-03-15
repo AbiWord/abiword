fd3fabb2df42e787aa4656a2b2e5397d07b43dbb
J M Maurer <uwog@uwog.net>
2005-03-15 19:26:45 +0000

    Add virtual destructors to classes with virtual functions
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@16120 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/xp/xav_Listener.h
+++ src/af/xap/xp/xav_Listener.h
@@ -96,6 +96,8 @@ typedef UT_uint32 AV_ChangeMask;
 class ABI_EXPORT AV_Listener
 {
 public:
+	virtual ~AV_Listener() {}
+
 	virtual bool		notify(AV_View * pView, const AV_ChangeMask mask) = 0;
 	virtual AV_ListenerType    getType(void) = 0;
 };
