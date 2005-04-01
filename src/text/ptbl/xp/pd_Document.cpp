1dc13788d202748fbbd0944a2df8b6a55a25704b
Martin Sevior <martines@unimelb.edu.au>
2005-04-01 04:32:33 +0000

    Fix 8323 crash when typing after endnote reference.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@16217 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/ptbl/xp/pd_Document.cpp
+++ src/text/ptbl/xp/pd_Document.cpp
@@ -1727,6 +1727,10 @@ bool PD_Document::isEndFootnoteAtPos(PT_DocPosition pos)
     {
 		pf = pf->getPrev();
     }
+	if(pf->getPos() < pos)
+	{
+		return false;
+	}
 	bool b = m_pPieceTable->isEndFootnote(pf);
 	if(b)
 	{
