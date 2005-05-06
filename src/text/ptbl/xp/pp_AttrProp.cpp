3d578bec5f85305f59aaadb1961de6465a317926
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2005-05-06 19:40:56 +0000

    fix cause of 8234
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@16404 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/ptbl/xp/pp_AttrProp.cpp
+++ src/text/ptbl/xp/pp_AttrProp.cpp
@@ -1233,11 +1233,11 @@ bool PP_AttrProp::isEquivalent(const PP_AttrProp * pAP2) const
 			return false;
 
 		// ignore property attribute
-		if(0 != UT_strcmp(pValue, PT_PROPS_ATTRIBUTE_NAME))
+		if(0 == UT_strcmp(pValue, PT_PROPS_ATTRIBUTE_NAME))
 			continue;
 
 		// handle revision attribute correctly
-		if(0 != UT_strcmp(pValue, PT_REVISION_ATTRIBUTE_NAME))
+		if(0 == UT_strcmp(pValue, PT_REVISION_ATTRIBUTE_NAME))
 		{
 			// requires special treatment
 			PP_RevisionAttr r1(pValue);
@@ -1304,11 +1304,11 @@ bool PP_AttrProp::isEquivalent(const XML_Char ** attrs, const XML_Char ** props)
 			return false;
 
 		// ignore property attribute
-		if(0 != UT_strcmp(pValue, PT_PROPS_ATTRIBUTE_NAME))
+		if(0 == UT_strcmp(pValue, PT_PROPS_ATTRIBUTE_NAME))
 			continue;
 
 		// handle revision attribute correctly
-		if(0 != UT_strcmp(pValue, PT_REVISION_ATTRIBUTE_NAME))
+		if(0 == UT_strcmp(pValue, PT_REVISION_ATTRIBUTE_NAME))
 		{
 			// requires special treatment
 			PP_RevisionAttr r1(pValue);
