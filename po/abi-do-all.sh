5e408c5f7065364f32624301c2f280e21548c8b0
J M Maurer <uwog@uwog.net>
2005-06-11 00:23:37 +0000

    Convert all .po's automatically
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@16759 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- po/abi-do-all.sh
+++ po/abi-do-all.sh
@@ -2,35 +2,8 @@
 
 ./update.pl --pot
 
-./update.pl gl-ES
-./update.pl nb-NO
-./update.pl sv-SE
-./update.pl de-DE
-./update.pl hu-HU
-./update.pl uk-UA 
-./update.pl ca-ES 
-./update.pl nl-NL 
-./update.pl id-ID 
-./update.pl pl-PL 
-./update.pl zh-CN 
-./update.pl cs-CZ 
-./update.pl es-ES 
-./update.pl it-IT      
-./update.pl pt-BR 
-./update.pl zh-TW 
-./update.pl cy-GB 
-./update.pl fi-FI 
-./update.pl lt-LT 
-./update.pl pt-PT 
-./update.pl da-DK 
-./update.pl fr-FR 
-./update.pl nn-NO 
-./update.pl ru-RU 
-./update.pl bg-BG 
-./update.pl tr-TR
-./update.pl de-CH
-./update.pl ta-IN
-./update.pl qu-BO
-./update.pl ast-ES
-./update.pl ku
-
+for I in `ls *.po` ;
+do
+    I=`echo "$I" | cut -d . -f1`
+    ./update.pl $I
+done
