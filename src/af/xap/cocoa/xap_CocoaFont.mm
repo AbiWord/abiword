c71c4ba9e37c0c6a01fb462ebb37ee1517592627
Francis J Franklin <fjf@svn.abisource.com>
2005-06-13 08:03:50 +0000

    o fix for Bug 8858
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-2-0-STABLE@16783 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/cocoa/xap_CocoaFont.mm
+++ src/af/xap/cocoa/xap_CocoaFont.mm
@@ -404,7 +404,7 @@ UT_UCS4Char XAP_CocoaFont::remapChar(UT_UCS4Char charCode, RemapFont rf)
 
 							NSFont * font = [NSFont fontWithName:fontName size:[NSFont smallSystemFontSize]];
 
-							NSDictionary * attr = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+							NSDictionary * attr = font ? [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName] : [NSDictionary dictionary];
 
 							[m_FontNames             addObject:fontName];
 							[m_FontMembers           addObject:memberName];
@@ -441,7 +441,7 @@ UT_UCS4Char XAP_CocoaFont::remapChar(UT_UCS4Char charCode, RemapFont rf)
 
 					NSFont * font = [NSFont fontWithName:fontName size:[NSFont systemFontSize]];
 
-					NSDictionary * attr = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+					NSDictionary * attr = font ? [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName] : [NSDictionary dictionary];
 
 					m_AttributedFontFamilyName = [[NSAttributedString alloc] initWithString:m_FontFamilyName attributes:attr];
 				}
