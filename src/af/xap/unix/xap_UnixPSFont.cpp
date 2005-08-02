3e9cfe024cc0c035f7f13373a728dfb1a791cd5f
J M Maurer <uwog@uwog.net>
2005-08-02 20:10:27 +0000

    Adapt to new api
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@17110 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/unix/xap_UnixPSFont.cpp
+++ src/af/xap/unix/xap_UnixPSFont.cpp
@@ -100,7 +100,7 @@ UT_sint32 PSFont::measureUnremappedCharForCache(UT_UCSChar cChar) const
 // rec.top = distance from the origin to the top of the glyph
 // rec.height = total height of the glyph
 //
-bool  PSFont::glyphBox(UT_UCS4Char glyph_index,UT_Rect & rec) const
+bool  PSFont::glyphBox(UT_UCS4Char glyph_index, UT_Rect & rec, GR_Graphics * pG)
 {
 	XftFaceLocker locker(m_hFont->getLayoutXftFont(GR_CharWidthsCache::CACHE_FONT_SIZE));
 	FT_Face pFace = locker.getFace();
