531d9697734df2dc53e15f91cd699f59f25d62b6
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2005-09-09 20:47:58 +0000

    do modeless cleanup on close
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@17298 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/win/ap_Win32Dialog_Latex.cpp
+++ src/wp/ap/win/ap_Win32Dialog_Latex.cpp
@@ -87,7 +87,8 @@ void AP_Win32Dialog_Latex::event_Insert(void)
 
 void AP_Win32Dialog_Latex::event_Close(void)
 {
-	m_answer = AP_Dialog_Latex::a_CANCEL;	
+	m_answer = AP_Dialog_Latex::a_CANCEL;
+	modeless_cleanup();
 	destroyWindow();
 }
 
