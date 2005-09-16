5e011d1199823a9bf3b049002890ee55e2df2ef6
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2005-09-16 14:14:07 +0000

    Allow user to select a printer without printing document.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@17346 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/win/xap_Win32Dlg_Print.cpp
+++ src/af/xap/win/xap_Win32Dlg_Print.cpp
@@ -30,7 +30,13 @@
 #include "gr_Win32Graphics.h"
 
 /*****************************************************************/
-/*
+// The only time the hook gets the custom data is during the WM_INITDIALOG message, so we
+// need to store it somewhere in a static var for the lifetime of the dlg
+// This static variable gets set to NULL both by the constructor and the destructor to
+// ensure that we are not accessing some stale value in the hook (since this is a
+// persistent dlg there should be one instance only at any time).
+XAP_Win32Dialog_Print * XAP_Win32Dialog_Print::s_pThis = NULL;
+
 static UINT CALLBACK s_PrintHookProc(
   HWND hdlg,      // handle to the dialog box window
   UINT uiMsg,     // message identifier
@@ -38,35 +44,70 @@ static UINT CALLBACK s_PrintHookProc(
   LPARAM lParam   // message parameter
   )
 {
-	// the only thing we want to do at the moment is to hide the
-	// selection radio if it is disabled
 	if(uiMsg == WM_INITDIALOG)
 	{
 		PRINTDLG * pDlgInfo = (PRINTDLG*)lParam;
-#if 0
+		XAP_Win32Dialog_Print::s_pThis = (XAP_Win32Dialog_Print*)pDlgInfo->lCustData;
+
+		// reset the 'closed' flag which indicates that the dialog should be considered
+		// 'closed' rather than aborted
+		XAP_Win32Dialog_Print::s_pThis->setClosed(false);
+
+		// hide the selection radio since we do not support selection printing
 		if(pDlgInfo->Flags & PD_NOSELECTION)
 		{
 			HWND wPrintSelectionRadio = GetDlgItem(hdlg, rad2);
 			ShowWindow(wPrintSelectionRadio, SW_HIDE);
 		}
-#endif
-#if 0
-		// notify layout of currently selected printer
-		GR_Win32Graphics* pG=(GR_Win32Graphics*)XAP_App::getApp()->getLastFocussedFrame()->getCurrentView()->getGraphics();
-		UT_return_val_if_fail( pG, 0 );
-		pG->setPrintDC(pDlgInfo->hDC);
-	}
-	else
-	{
-		UT_DEBUGMSG(("PrintDlg notification: 0x%x\n", uiMsg));
-#endif
-	}
-	
 
-	// we want to respond to changes to printer selection ...
+		// remember the original printer selected; we use this to decide if upon closure
+		// we should notify associated views to rebuild to reflect the new font metrics
+		UT_uint32 iPrinter = SendDlgItemMessage(hdlg, cmb4, CB_GETCURSEL, 0, 0);
+		XAP_Win32Dialog_Print::s_pThis->setOrigPrinter(iPrinter);
+		XAP_Win32Dialog_Print::s_pThis->setNewPrinter(iPrinter);
+	}
+	else if(uiMsg == WM_COMMAND)
+	{
+		// if the command indicates that the user changed printer, we change the Cancel
+		// button to Close; make sure we have valid this pointer.
+		if((int)LOWORD(wParam) == cmb4 && HIWORD(wParam) == CBN_SELCHANGE && XAP_Win32Dialog_Print::s_pThis)
+		{
+			XAP_Win32Dialog_Print::s_pThis->setNewPrinter(SendDlgItemMessage(hdlg, cmb4, CB_GETCURSEL, 0, 0));
+
+			XAP_App*              pApp        = XAP_App::getApp();
+			const XAP_StringSet*  pSS         = pApp->getStringSet();
+			
+			if(XAP_Win32Dialog_Print::s_pThis->getNewPrinter() != XAP_Win32Dialog_Print::s_pThis->getOrigPrinter())
+			{
+				SetDlgItemText(hdlg,IDCANCEL,pSS->getValue(XAP_STRING_ID_DLG_Close));
+			}
+			else
+			{
+				// the user set the printer back to what it used to be -- revert back to
+				// cancel button
+				SetDlgItemText(hdlg,IDCANCEL,pSS->getValue(XAP_STRING_ID_DLG_Cancel));
+			}
+		}
+		else if((int)LOWORD(wParam) == IDCANCEL && HIWORD(wParam) == 0)
+		{
+			// the user presed the Cancel/Close button; see if it is Cancel or Close
+			if(XAP_Win32Dialog_Print::s_pThis->getNewPrinter() != XAP_Win32Dialog_Print::s_pThis->getOrigPrinter())
+			{
+				// Different printer is selected to the one we started with, this is Close
+				// scenario -- eat this message and simulate click on the OK button
+				// instead so that the default dlg procedure fills the PRINTDLG struct
+				// with the data we need; set the m_bClosed flag, so we can differentiate
+				// this from the user pressing the OK button
+				XAP_Win32Dialog_Print::s_pThis->setClosed(true);
+				PostMessage(hdlg, WM_COMMAND, IDOK, 0);
+
+				return 1; // eat the message
+			}
+		}
+	}
 	
 	return 0;
-}*/
+}
 
 XAP_Dialog * XAP_Win32Dialog_Print::static_constructor(XAP_DialogFactory * pFactory,
 													 XAP_Dialog_Id id)
@@ -76,14 +117,19 @@ XAP_Dialog * XAP_Win32Dialog_Print::static_constructor(XAP_DialogFactory * pFact
 }
 
 XAP_Win32Dialog_Print::XAP_Win32Dialog_Print(XAP_DialogFactory * pDlgFactory,
-										   XAP_Dialog_Id id)
-	: XAP_Dialog_Print(pDlgFactory,id)
+										   XAP_Dialog_Id id):
+	XAP_Dialog_Print(pDlgFactory,id),
+	m_iOrigPrinter(0),
+	m_iNewPrinter(0),
+	m_bClosed(false)
 {
 	m_pPersistPrintDlg = (PRINTDLG *)UT_calloc(1,sizeof(PRINTDLG));
 	UT_ASSERT(m_pPersistPrintDlg);
 	
 	memset(m_pPersistPrintDlg,0,sizeof(m_pPersistPrintDlg));
 	m_pPersistPrintDlg->lStructSize = sizeof(*m_pPersistPrintDlg);
+
+	s_pThis = NULL;
 }
 
 XAP_Win32Dialog_Print::~XAP_Win32Dialog_Print(void)
@@ -96,6 +142,8 @@ XAP_Win32Dialog_Print::~XAP_Win32Dialog_Print(void)
 			GlobalFree(m_pPersistPrintDlg->hDevNames);
 		free(m_pPersistPrintDlg);
 	}
+
+	s_pThis = NULL;
 }
 
 GR_Graphics * XAP_Win32Dialog_Print::getPrinterGraphicsContext(void)
@@ -145,9 +193,8 @@ void XAP_Win32Dialog_Print::runModal(XAP_Frame * pFrame)
 	m_pPersistPrintDlg->nToPage		= (WORD)m_nLastPage;
 	m_pPersistPrintDlg->nMinPage	= (WORD)m_nFirstPage;
 	m_pPersistPrintDlg->nMaxPage	= (WORD)m_nLastPage;
-	m_pPersistPrintDlg->Flags		= PD_ALLPAGES | PD_RETURNDC /*| PD_ENABLEPRINTHOOK*/;
-	//m_pPersistPrintDlg->lpfnPrintHook   = s_PrintHookProc;
-	// we do not need this at the moment, but one day it will come handy in the hook procedure
+	m_pPersistPrintDlg->Flags		= PD_ALLPAGES | PD_RETURNDC | PD_ENABLEPRINTHOOK;
+	m_pPersistPrintDlg->lpfnPrintHook   = s_PrintHookProc;
 	m_pPersistPrintDlg->lCustData       = (DWORD)this;
 		
 	if (!m_bEnablePageRange)
@@ -213,10 +260,17 @@ void XAP_Win32Dialog_Print::runModal(XAP_Frame * pFrame)
 		GR_Win32Graphics* pG=(GR_Win32Graphics*)XAP_App::getApp()->getLastFocussedFrame()->getCurrentView()->getGraphics();
 		UT_return_if_fail( pG );
 		pG->setPrintDC(m_pPersistPrintDlg->hDC);
+
+		// we need to differentiate between actual OK being pressed by user and simulated
+		// by our hook procedure -- in the later case we want to return a_CANCEL so that
+		// the document does not get sent to the printer
+		if(m_bClosed)
+			m_answer = a_CANCEL;
 	}
 	else
 	{
-		UT_DEBUGMSG(("Printer dialog failed: reason=0x%x\n", CommDlgExtendedError()));
+		// The user clicked the cancel button and did not change the printer, there is
+		// nothing for us to do
 		m_answer = a_CANCEL;
 	}
 
