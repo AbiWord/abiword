6295534a19fc0de3fd00c60df2836523bd1029ea
J M Maurer <uwog@uwog.net>
2006-05-17 22:18:08 +0000

    Add new hr-HR translation (Renato PaviÄiÄ, Martin Srebotnjak)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@18322 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- po/abi-port-all.sh
+++ po/abi-port-all.sh
@@ -20,6 +20,7 @@ echo ""
 ./abi-extract.pl -N zh-TW 
 ./abi-extract.pl -N cy-GB 
 ./abi-extract.pl -N fi-FI 
+./abi-extract.pl -N hr-HR 
 ./abi-extract.pl -N lt-LT 
 ./abi-extract.pl -N pt-PT 
 ./abi-extract.pl -N da-DK 
