3febddb3e3a42d25150ab972d75fac2d19f83568
J M Maurer <uwog@uwog.net>
2006-06-16 19:29:02 +0000

    Backport: Initialize two variables to prevent valgrind warnings. (sum1)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-4-0-STABLE@18388 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Dialog_Stylist.cpp
+++ src/wp/ap/xp/ap_Dialog_Stylist.cpp
@@ -46,6 +46,7 @@
 
 AP_Dialog_Stylist::AP_Dialog_Stylist(XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id id)
 	: XAP_Dialog_Modeless(pDlgFactory,id),
+	  m_pDoc(NULL),
 	  m_bIsModal(false),
 	  m_pAutoUpdater(0),
 	  m_iTick(0),
