6fc6fb9c87307c42679c491487dbce05ed367c01
J M Maurer <uwog@uwog.net>
2006-06-23 22:27:55 +0000

    Backport: Prevent crashes caused by inserting embed or math runs into TOCs (fixes Bug 9721), and correct some comments. (sum1)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-4-0-STABLE@18419 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fl_BlockLayout.cpp
+++ src/text/fmt/xp/fl_BlockLayout.cpp
@@ -4876,6 +4876,13 @@ bool	fl_BlockLayout::_doInsertTabRun(PT_BlockOffset blockOffset)
 
 bool	fl_BlockLayout::_doInsertMathRun(PT_BlockOffset blockOffset,PT_AttrPropIndex indexAP, PL_ObjectHandle oh)
 {
+	if(isContainedByTOC())
+	{
+		fp_Run * pDumRun = new fp_DummyRun(this,blockOffset);
+		xxx_UT_DEBUGMSG(("Inserting a dummy run instead of mathrun at %d \n",blockOffset));
+		return _doInsertRun(pDumRun);
+	}
+
 	fp_Run * pNewRun = NULL;
 	pNewRun = new fp_MathRun(this,blockOffset,indexAP,oh);
 	UT_ASSERT(pNewRun); // TODO check for outofmem
@@ -4886,6 +4893,13 @@ bool	fl_BlockLayout::_doInsertMathRun(PT_BlockOffset blockOffset,PT_AttrPropInde
 
 bool	fl_BlockLayout::_doInsertEmbedRun(PT_BlockOffset blockOffset,PT_AttrPropIndex indexAP, PL_ObjectHandle oh)
 {
+	if(isContainedByTOC())
+	{
+		fp_Run * pDumRun = new fp_DummyRun(this,blockOffset);
+		xxx_UT_DEBUGMSG(("Inserting a dummy run instead of embedrun at %d \n",blockOffset));
+		return _doInsertRun(pDumRun);
+	}
+
 	fp_Run * pNewRun = NULL;
 	pNewRun = new fp_EmbedRun(this,blockOffset,indexAP,oh);
 	UT_ASSERT(pNewRun); // TODO check for outofmem
@@ -4926,7 +4940,7 @@ bool	fl_BlockLayout::_doInsertImageRun(PT_BlockOffset blockOffset, FG_Graphic* p
 	if(isContainedByTOC())
 	{
 		fp_Run * pDumRun = new fp_DummyRun(this,blockOffset);
-		xxx_UT_DEBUGMSG(("Inserting a dummy run instead of iamge at %d \n",blockOffset));
+		xxx_UT_DEBUGMSG(("Inserting a dummy run instead of image at %d \n",blockOffset));
 		return _doInsertRun(pDumRun);
 	}
 
@@ -5001,7 +5015,7 @@ bool	fl_BlockLayout::_doInsertFieldRun(PT_BlockOffset blockOffset, const PX_Chan
 		if(isContainedByTOC())
 		{
 			fp_Run * pDumRun = new fp_DummyRun(this,blockOffset);
-			xxx_UT_DEBUGMSG(("Inserting a dummy run instead of endnote_ref at %d \n",blockOffset));
+			xxx_UT_DEBUGMSG(("Inserting a dummy run instead of footnote_anchor at %d \n",blockOffset));
 			return _doInsertRun(pDumRun);
 		}
 		pNewRun = new fp_FieldFootnoteAnchorRun(this,   blockOffset, 1);
@@ -5023,7 +5037,7 @@ bool	fl_BlockLayout::_doInsertFieldRun(PT_BlockOffset blockOffset, const PX_Chan
 		if(isContainedByTOC())
 		{
 			fp_Run * pDumRun = new fp_DummyRun(this,blockOffset);
-			xxx_UT_DEBUGMSG(("Inserting a dummy run instead of endnote_ref at %d \n",blockOffset));
+			xxx_UT_DEBUGMSG(("Inserting a dummy run instead of endnote_anchor at %d \n",blockOffset));
 			return _doInsertRun(pDumRun);
 		}
 		pNewRun = new fp_FieldEndnoteAnchorRun(this,   blockOffset, 1);
