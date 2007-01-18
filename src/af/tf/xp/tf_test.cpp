7d0c86e30aa71521b1b2b390b9b8b4b699b857fc
Hubert Figuiere <hub@figuiere.net>
2007-01-18 03:28:01 +0000

    fix build of unit tests
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@19780 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/tf/xp/tf_test.cpp
+++ src/af/tf/xp/tf_test.cpp
@@ -1,6 +1,23 @@
-/*
- */
+/* -*- mode: C++; tab-width: 4; c-basic-offset: 4; -*- */
 
+/* AbiWord
+ * Copyright (c) 2004,2005 Hubert Figuiere
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 /* Original Copyright notice, as part of WvStreams, LGPL licensed */
 /*
  * Worldvisions Weaver Software:
@@ -13,12 +30,13 @@
 //#include "config.h"
 #include <stdio.h>
 #include <string.h>
-#include <g_try_malloc.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <signal.h>
 
 #include <cstdlib>
+
+#include <glib.h>
 
 #ifdef HAVE_VALGRIND_MEMCHECK_H
 # include <valgrind/memcheck.h>
