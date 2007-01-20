696db95fe09cc212d3dd39125dc2d4eb49b9a01b
Tomas Frydrych <dr.tomas@yahoo.co.uk>
2007-01-20 11:34:50 +0000

    use glib for utf8/ucs4 conversions
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@19815 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_stringbuf.cpp
+++ src/af/util/xp/ut_stringbuf.cpp
@@ -22,6 +22,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <glib.h>
+
 #include "ut_string.h"
 #include "ut_stringbuf.h"
 #include "ut_string_class.h"
@@ -1304,69 +1306,17 @@ bool UT_UTF8Stringbuf::UTF8Iterator::sync ()
  */
 UT_UCS4Char UT_UCS4Stringbuf::UTF8_to_UCS4 (const char *& buffer, size_t & length)
 {
-	UT_UCS4Char ucs4;
+	UT_UCS4Char ucs4 = g_utf8_get_char_validated (buffer, length);
 
-	while (true) {
-		ucs4 = 0;
-		if (length == 0) break;
+	if ((UT_sint32)ucs4 == -1 || (UT_sint32)ucs4 == -2)
+		return 0;
 
-		unsigned char c = static_cast<unsigned char>(*buffer);
-		buffer++;
-		length--;
+	const char * p = g_utf8_next_char (buffer);
+	UT_uint32 diff = p - buffer;
 
-		if ((c & 0x80) == 0) { // ascii, single-byte sequence
-			ucs4 = static_cast<UT_UCS4Char>(c);
-			break;
-		}
-		if ((c & 0xc0) == 0x80) { // hmm, continuing byte - let's just ignore it
-			continue;
-		}
+	buffer += diff;
+	length -= diff;
 
-		/* we have a multi-byte sequence...
-		 */
-		size_t seql;
-
-		if ((c & 0xe0) == 0xc0) {
-			seql = 2;
-			ucs4 = static_cast<UT_UCS4Char>(c & 0x1f);
-		}
-		else if ((c & 0xf0) == 0xe0) {
-			seql = 3;
-			ucs4 = static_cast<UT_UCS4Char>(c & 0x0f);
-		}
-		else if ((c & 0xf8) == 0xf0) {
-			seql = 4;
-			ucs4 = static_cast<UT_UCS4Char>(c & 0x07);
-		}
-		else if ((c & 0xfc) == 0xf8) {
-			seql = 5;
-			ucs4 = static_cast<UT_UCS4Char>(c & 0x03);
-		}
-		else if ((c & 0xfe) == 0xfc) {
-			seql = 6;
-			ucs4 = static_cast<UT_UCS4Char>(c & 0x01);
-		}
-		else { // or perhaps we don't :-( - whatever it is, let's just ignore it
-			continue;
-		}
-
-		if (length < seql - 1) { // huh? broken sequence perhaps? anyway, let's just ignore it
-			continue;
-		}
-
-		bool okay = true;
-		for (size_t i = 1; i < seql; i++) {
-			c = static_cast<unsigned char>(*buffer);
-			buffer++;
-			length--;
-			if ((c & 0xc0) != 0x80) { // not a continuing byte? grr!
-				okay = false;
-				break;
-			}
-			ucs4 = ucs4 << 6 | static_cast<UT_UCS4Char>(c & 0x3f);
-		}
-		if (okay) break;
-	}
 	return ucs4;
 }
 
@@ -1374,16 +1324,7 @@ UT_UCS4Char UT_UCS4Stringbuf::UTF8_to_UCS4 (const char *& buffer, size_t & lengt
  */
 int UT_UCS4Stringbuf::UTF8_ByteLength (UT_UCS4Char u)
 {
-	if ((u & 0x7fffffff) != u) return -1; // UCS-4 is only 31-bit!
-
-	if (u == 0) return 0; // end-of-string
-
-	if ((u & 0x7fffff80) == 0) return 1;
-	if ((u & 0x7ffff800) == 0) return 2;
-	if ((u & 0x7fff0000) == 0) return 3;
-	if ((u & 0x7fe00000) == 0) return 4;
-	if ((u & 0x7c000000) == 0) return 5;
-	return 6;
+	return g_unichar_to_utf8 (u, NULL);
 }
 
 /* appends to the buffer the UTF-8 sequence corresponding to the UCS-4 value;
@@ -1392,55 +1333,17 @@ int UT_UCS4Stringbuf::UTF8_ByteLength (UT_UCS4Char u)
  */
 bool UT_UCS4Stringbuf::UCS4_to_UTF8 (char *& buffer, size_t & length, UT_UCS4Char ucs4)
 {
-	int seql = UT_UCS4Stringbuf::UTF8_ByteLength (ucs4);
-	if (seql < 0) return false;
-	if (seql == 0) {
-		if (length == 0) return false;
-		*buffer++ = 0;
-		length--;
-		return true;
-	}
+	gchar buf [6];
+	int seql = g_unichar_to_utf8 (ucs4, &buf[0]);
+	
 	if (length < static_cast<unsigned>(seql)) return false;
-	length -= seql;
 
-	switch (seql) {
-	case 1:
-		*buffer++ = static_cast<char>(static_cast<unsigned char>(ucs4 & 0x7f));
-		break;
-	case 2:
-		*buffer++ = static_cast<char>(0xc0 | static_cast<unsigned char>((ucs4 >> 6) & 0x1f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>(ucs4 & 0x3f));
-		break;
-	case 3:
-		*buffer++ = static_cast<char>(0xe0 | static_cast<unsigned char>((ucs4 >> 12) & 0x0f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 6) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>(ucs4 & 0x3f));
-		break;
-	case 4:
-		*buffer++ = static_cast<char>(0xf0 | static_cast<unsigned char>((ucs4 >> 18) & 0x07));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 12) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 6) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>(ucs4 & 0x3f));
-		break;
-	case 5:
-		*buffer++ = static_cast<char>(0xf8 | static_cast<unsigned char>((ucs4 >> 24) & 0x03));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 18) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 12) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 6) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>(ucs4 & 0x3f));
-		break;
-	case 6:
-		*buffer++ = static_cast<char>(0xfc | static_cast<unsigned char>((ucs4 >> 30) & 0x01));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 24) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 18) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 12) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>((ucs4 >> 6) & 0x3f));
-		*buffer++ = static_cast<char>(0x80 | static_cast<unsigned char>(ucs4 & 0x3f));
-		break;
-	default: // huh?
-		UT_ASSERT(UT_SHOULD_NOT_HAPPEN);
-		break;
+	length -= seql;
+	for (int i = 0; i < seql; ++i)
+	{
+		*buffer++ = buf[i];
 	}
+	
 	return true;
 }
 
