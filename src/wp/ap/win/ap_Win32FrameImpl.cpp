AbiWord/abiword

commit ad3ccdb41440f634f6d3b411bb4bf7c8c3f76c40

Fix https://dev.laptop.org/ticket/3253 : don't assume there is only one cursor at any given time
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@21936 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/wp/ap/win/ap_Win32FrameImpl.cpp
@@ -24,6 +24,7 @@
 #include "ut_Win32Timer.h"
 #include "ut_Win32OS.h"
 #include "gr_Win32Graphics.h"
+#include "gr_Painter.h"
 #include "ap_Win32TopRuler.h"
 #include "ap_Win32LeftRuler.h"
 #include "ap_Win32StatusBar.h"
@@ -1344,7 +1345,7 @@ LRESULT CALLBACK AP_Win32FrameImpl::_DocumentWndProc(HWND hwnd, UINT iMsg, WPARA
 			FV_View * pFV = static_cast<FV_View *>(pView);
 			GR_Graphics * pG = pFV->getGraphics();
 
-			GR_CaretDisabler caretDisabler(pG->getCaret());
+			GR_Painter caretDisablerPainter(m_pG); // not an elegant way to disable all carets, but it works beautifully - MARCM
 			
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);

src/af/gr/qnx/gr_QNXGraphics.cpp
@@ -29,6 +29,7 @@
 #include "gr_QNXGraphics.h"
 #include "gr_QNXImage.h"
 #include "gr_CharWidths.h"
+#include "gr_Painter.h"
 
 #include "ut_debugmsg.h"
 #include "ut_assert.h"
@@ -733,7 +734,7 @@ void GR_QNXGraphics::scroll(UT_sint32 dx, UT_sint32 dy)
 	PhPoint_t offset;
 	UT_sint32 newY = getPrevYOffset() + dy;
 	UT_sint32 newX = getPrevXOffset() + dx;
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(m_pG); // not an elegant way to disable all carets, but it works beautifully - MARCM
 	UT_sint32 ddx = -tdu(newX - getPrevXOffset());
 	UT_sint32 ddy = -tdu(newY - getPrevYOffset());
 	setPrevYOffset(newY);
@@ -800,7 +801,7 @@ void GR_QNXGraphics::scroll(UT_sint32 x_dest, UT_sint32 y_dest,
 						  UT_sint32 x_src, UT_sint32 y_src,
 						  UT_sint32 width, UT_sint32 height)
 {
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(this); // not an elegant way to disable all carets, but it works beautifully - MARCM
 	PhPoint_t shift,offset;
 	PhRect_t rect;
 	PhRect_t dst_rect;

src/af/gr/unix/gr_UnixPangoGraphics.cpp
@@ -23,6 +23,7 @@
 #endif
 
 #include "gr_UnixPangoGraphics.h"
+#include "gr_Painter.h"
 
 #include "xap_App.h"
 #include "xap_Prefs.h"
@@ -493,7 +494,7 @@ GR_Graphics *   GR_UnixPangoGraphics::graphicsAllocator(GR_AllocInfo& info)
 
 void GR_UnixPangoGraphics::scroll(UT_sint32 dx, UT_sint32 dy)
 {
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(this); // not an elegant way to disable all carets, but it works beautifully - MARCM
 	UT_sint32 oldDY = tdu(getPrevYOffset());
 	UT_sint32 oldDX = tdu(getPrevXOffset());
 	UT_sint32 newY = getPrevYOffset() + dy;
@@ -741,7 +742,7 @@ void GR_UnixPangoGraphics::scroll(UT_sint32 x_dest, UT_sint32 y_dest,
 						  UT_sint32 x_src, UT_sint32 y_src,
 						  UT_sint32 width, UT_sint32 height)
 {
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(this); // not an elegant way to disable all carets, but it works beautifully - MARCM
    	gdk_draw_drawable(_getDrawable(), m_pGC, _getDrawable(), tdu(x_src), tdu(y_src),
    				  tdu(x_dest), tdu(y_dest), tdu(width), tdu(height));
 }

src/af/gr/win/gr_Win32Graphics.cpp
@@ -29,6 +29,7 @@
 
 #include "gr_Win32Graphics.h"
 #include "gr_Win32Image.h"
+#include "gr_Painter.h"
 #include <xap_Win32App.h>
 
 #include <xap_Win32Res_Cursors.rc2>
@@ -1027,7 +1028,7 @@ void GR_Win32Graphics::scroll(UT_sint32 dx, UT_sint32 dy)
 	{
 		return;
 	}
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(this); // not an elegant way to disable all carets, but it works beautifully - MARCM
 
 	setExposePending(true);
 	ScrollWindowEx(m_hwnd, ddx, ddy, NULL, NULL, NULL, 0, SW_INVALIDATE);
@@ -1049,7 +1050,7 @@ void GR_Win32Graphics::scroll(UT_sint32 x_dest, UT_sint32 y_dest,
 	r.right = r.left + width;
 	r.bottom = r.top + height;
 	
-	GR_CaretDisabler caretDisabler(getCaret());
+	GR_Painter caretDisablerPainter(this); // not an elegant way to disable all carets, but it works beautifully - MARCM
 	
 	setExposePending(true);
 	ScrollWindowEx(m_hwnd, (x_dest - x_src), (y_dest - y_src),

src/text/fmt/xp/fv_View_protected.cpp
@@ -5953,7 +5953,7 @@ bool FV_View::_charInsert(const UT_UCSChar * text, UT_uint32 count, bool bForce)
 	// of the line upon character insertion, but it's the wrong thing to
 	// do.  The right thing to do is to either delay calculation, or to
 	// not make the wrong number come up; disabling the caret is wrong. -PL
- 	GR_CaretDisabler caretDisabler(m_pG->getCaret());
+	GR_Painter caretDisablerPainter(m_pG); // not an elegant way to disable all carets, but it works beautifully - MARCM
 
 	// Signal PieceTable Change
 	_saveAndNotifyPieceTableChange();

commit 46d981df33031a8726f6dfbc114260bbcdfc8d6b

Fix win32 build (breakage caused by r21936).
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@21947 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/wp/ap/win/ap_Win32FrameImpl.cpp
@@ -1345,7 +1345,7 @@ LRESULT CALLBACK AP_Win32FrameImpl::_DocumentWndProc(HWND hwnd, UINT iMsg, WPARA
 			FV_View * pFV = static_cast<FV_View *>(pView);
 			GR_Graphics * pG = pFV->getGraphics();
 
-			GR_Painter caretDisablerPainter(m_pG); // not an elegant way to disable all carets, but it works beautifully - MARCM
+			GR_Painter caretDisablerPainter(pG); // not an elegant way to disable all carets, but it works beautifully - MARCM
 			
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);