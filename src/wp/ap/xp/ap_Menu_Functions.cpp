2d9d9ab6d7eef5cfd7835abc4a4f9db43a6c5df2
J M Maurer <uwog@uwog.net>
2007-10-18 12:37:59 +0000

    Prevent a glib-critical warning; backport
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@22128 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Menu_Functions.cpp
+++ src/wp/ap/xp/ap_Menu_Functions.cpp
@@ -115,11 +115,10 @@ Defun_EV_GetMenuItemComputedLabel_Fn(ap_GetLabel_Recent)
 
 		const char * szURI = pPrefs->getRecent(ndx);
 		char *szRecent = g_filename_from_uri(szURI, NULL, NULL);
-		char *szFile = g_path_get_basename (szRecent);
+		UT_UTF8String szFile(szRecent ? g_path_get_basename (szRecent) : "");
 
-		snprintf(buf, PATH_MAX, szFormat, szFile);
+		snprintf(buf, PATH_MAX, szFormat, szFile.utf8_str());
 		g_free (szRecent);
-		g_free (szFile);
 		return buf;
 	}
 
