c4dcf8faafba5f9ab009a6f394a68b646da7a92f
Rob Staudinger <robert.staudinger@gmail.com>
2008-01-02 14:00:58 +0000

    Copy toplevel files
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/buildsys@22404 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ link-sources.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# abiword tree
+for l in `find . -name sources.list | grep -v '^./plugins'`; do 
+	d=`dirname $l`
+	cd=`echo $d | sed -e 's|/[[:alnum:]-]*|/\.\.|g'`
+	if [ -f "$d/sources.base" ]; then
+		srcd=`cat $d/sources.base`
+	else
+		srcd="$d"
+	fi
+	for f in `cat $l`; do
+		(echo $d/$f && cd $d && ln -sf $cd/../abiword/$srcd/$f)
+	done
+done
+
+# plugins tree
+for l in `find ./plugins -name sources.list`; do 
+	d=`dirname $l`
+	cd=`echo $d | sed -e 's|/[[:alnum:]-]*|/\.\.|g'`
+	srcd=`cat $d/sources.base`
+	for f in `cat $l`; do
+		(echo $d/$f && cd $d && ln -sf $cd/../abiword-plugins/$srcd/$f)
+	done
+done
+
