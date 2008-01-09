2ffab5521ad30258dc4429fec186fb8733429230
Rob Staudinger <robert.staudinger@gmail.com>
2008-01-09 09:31:18 +0000

    + Fix use of aclocal, as suggested by hub (makes autoreconf work) + Find a new home for the win32 manifest to not clutter up $(top_srcdir) so much + Prune splash screen relics from the buildsys, the splash had been removed from trunk before the merge
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@22621 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- autogen.sh
+++ autogen.sh
@@ -27,6 +27,26 @@ find plugins -name Makefile.am | sed  's|.am$||g' > plugin-makefiles.m4
 # create plugin configuration
 find plugins -name plugin.m4 | xargs cat > plugin-configure.m4
 
+automake --version | perl -ne 'if (/\(GNU automake\) (([0-9]+).([0-9]+))/) {print; if ($2 < 1 || ($2 == 1 && $3 < 4)) {exit 1;}}'
+if [ $? -ne 0 ]; then
+    echo "* * * error: you need automake 1.4 or later.  Please upgrade."
+    exit 1
+fi
+
+# find extra m4 files provided by plugins and symlink them
+for f in ` find ./plugins -name '*.m4' | grep -v 'plugin\.m4'`; do
+    ln -sf $f
+done
+
+# Produce aclocal.m4, so autoconf gets the automake macros it needs
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I ."
+echo "+ Creating aclocal.m4: aclocal $ACLOCAL_FLAGS"
+aclocal $ACLOCAL_FLAGS 2>> autogen.err || {
+    echo ""
+    echo "* warning: possible errors while running aclocal - check autogen.err"
+    echo ""
+}
+
 echo "+ Running libtoolize --force --copy"
 libtoolize --force --copy || {
     echo "* * * error: libtoolize failed"
@@ -38,49 +58,16 @@ uname | grep -i '^mingw32'
 if [ $? -eq 0 ]; then
     echo "+ Win32 detected, skipping autoheader"
 else
-    echo "+ Running autoheader"
-    autoheader || {
-        echo "* * * error: autoheader failed"
-        exit 1
+    echo "+ Running autoheader ..."
+    autoheader configure.in 2>> autogen.err || {
+        echo ""
+        echo "* warning: possible errors while running automake - check autogen.err"
+        echo ""
     }
 fi
 
-automake --version | perl -ne 'if (/\(GNU automake\) (([0-9]+).([0-9]+))/) {print; if ($2 < 1 || ($2 == 1 && $3 < 4)) {exit 1;}}'
-
-if [ $? -ne 0 ]; then
-    echo "* * * error: you need automake 1.4 or later.  Please upgrade."
-    exit 1
-fi
-
-# Scan for definements set by the build system
-#
-echo "+ Running autoheader ..."
-
-autoheader configure.in 2>> autogen.err || {
-    echo ""
-    echo "* warning: possible errors while running automake - check autogen.err"
-    echo ""
-}
-
-# find extra m4 files provided by plugins
-for f in ` find ./plugins -name '*.m4' | grep -v 'plugin\.m4'`; do
-    f=`dirname $f`
-    ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $f"
-done
-
-# Produce aclocal.m4, so autoconf gets the automake macros it needs
-# 
-echo "+ Creating aclocal.m4: aclocal $ACLOCAL_FLAGS"
-
-aclocal $ACLOCAL_FLAGS 2>> autogen.err || {
-    echo ""
-    echo "* warning: possible errors while running aclocal - check autogen.err"
-    echo ""
-}
-
 # Produce all the `Makefile.in's and create neat missing things
 # like `install-sh', etc.
-# 
 echo "+ Running automake ..."
 
 automake --add-missing --copy 2>> autogen.err || {
