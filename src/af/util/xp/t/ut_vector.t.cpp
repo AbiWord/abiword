d400345f8daa5454bb0d6b25ffbb2b11821c65c3
Hubert Figuiere <hub@figuiere.net>
2008-01-11 00:35:24 +0000

    fix compilation of unit test. (no backport)
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@22664 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/t/ut_vector.t.cpp
+++ src/af/util/xp/t/ut_vector.t.cpp
@@ -1,3 +1,21 @@
+/* AbiSource Applications
+ * Copyright (C) 2006 Hubert Figuiere
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 
 
 #include "tf_test.h"
@@ -7,7 +25,7 @@
 
 TFTEST_MAIN("UT_GenericVector basics")
 {
-	UT_GenericVector<char *> v;
+	UT_GenericVector<const char *> v;
 
 	TFPASS(v.getItemCount() == 0);
 	v.addItem("foo");
