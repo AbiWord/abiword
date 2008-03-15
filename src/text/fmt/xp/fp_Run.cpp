4902bfec82749d3e187e406650530cabb0067895
J M Maurer <uwog@uwog.net>
2008-03-15 14:51:38 +0000

    Merged revisions 23112 via svnmerge from svn+ssh://svn.abisource.com/svnroot/abiword/trunk
    ........
      r23112 | msevior | 2008-03-15 15:42:43 +0100 (Sat, 15 Mar 2008) | 4 lines
      
      
      Fix Bug 11475 Underlined fields do no print WYSIWYG. Yes I know this fix makes no sense. I blame the compiler. Please backport to 2.6
    ........
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-6-0-STABLE@23113 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fp_Run.cpp
+++ src/text/fmt/xp/fp_Run.cpp
@@ -1510,7 +1510,7 @@ bool fp_Run::_recalcWidth(void)
 
 void fp_Run::drawDecors(UT_sint32 xoff, UT_sint32 yoff, GR_Graphics * pG)
 {
-
+	UT_DEBUGMSG(("drawDecors xoff %d \n",xoff));
 	/*
 	  Upon entry to this function, yoff is the TOP of the run,
 	  NOT the baseline.
@@ -4309,7 +4309,8 @@ void fp_FieldRun::_defaultDraw(dg_DrawArgs* pDA)
 //
 	UT_sint32 yTopOfRun = pDA->yoff - getAscent()-1; // Hack to remove
 	                                                 //character dirt
-	drawDecors( xoff, yTopOfRun,pG);
+	UT_DEBUGMSG(("xoff infield before drawdecors %d \n",pDA->xoff));
+	drawDecors( pDA->xoff, yTopOfRun,pG);
 
 }
 
