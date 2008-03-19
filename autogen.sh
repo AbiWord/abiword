d185a94c01d96ee7a734fd3d6181502d740bff50
Rob Staudinger <robert.staudinger@gmail.com>
2008-03-19 13:35:11 +0000

    Preaching water, drinking wine.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@23207 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- autogen.sh
+++ autogen.sh
@@ -89,3 +89,10 @@ cd $olddir
 
 conf_flags="--enable-maintainer-mode"
 
+if test x$NOCONFIGURE = x; then
+  echo Running $srcdir/configure $conf_flags "$@" ...
+  $srcdir/configure $conf_flags "$@" \
+  && echo Now type \`make\' to compile. || exit 1
+else
+  echo Skipping configure process.
+fi
