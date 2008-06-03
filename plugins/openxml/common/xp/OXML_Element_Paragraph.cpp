7b44a8a2fd3ec2d1a2b985537b1527d76e44d0c0
Firat Kiyak <firatkiyak@gmail.com>
2008-06-03 19:06:38 +0000

    common data model extended for OXML_Element_Paragraph object
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2008ooxml@24134 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ plugins/openxml/common/xp/OXML_Element_Paragraph.cpp
@@ -0,0 +1,53 @@
+/* -*- mode: C++; tab-width: 4; c-basic-offset: 4; -*- */
+
+/* AbiSource
+ * 
+ * Copyright (C) 2008 Firat Kiyak <firatkiyak@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+// Class definition include
+#include <OXML_Element_Paragraph.h>
+
+// AbiWord includes
+#include <ut_types.h>
+#include <ut_string.h>
+#include <pd_Document.h>
+
+OXML_Element_Paragraph::OXML_Element_Paragraph(std::string id) : 
+	OXML_Element(id, P_TAG, BLOCK)
+{
+}
+
+OXML_Element_Paragraph::~OXML_Element_Paragraph()
+{
+
+}
+
+UT_Error OXML_Element_Paragraph::serialize(IE_Exp_OpenXML* exporter)
+{
+	//TODO Whenever we are ready to export
+	return this->serializeChildren(exporter);
+}
+
+UT_Error OXML_Element_Paragraph::addToPT(PD_Document * pDocument)
+{
+	//TODO Move the OXML_Element addToPT case P_TAG code here
+	//the importer needs to be updated to create OXML_Element_Paragraph object
+	//instead of generic OXML_Element object for representing a paragraph
+	return OXML_Element::addToPT(pDocument);
+}
