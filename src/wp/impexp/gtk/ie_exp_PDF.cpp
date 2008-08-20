aa3a7bb54a9c94d9c4f70c046f8e67f6d11bdc9e
Dom Lachowicz <domlachowicz@gmail.com>
2008-08-20 18:34:24 +0000

    don't use temporary files in the cairo-based exporter; include cairo-based exporter unconditionally; re-add PS exporter; stub out support for a SVG exporter
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2008cairo-integration@24827 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/gtk/ie_exp_PDF.cpp
+++ src/wp/impexp/gtk/ie_exp_PDF.cpp
@@ -32,6 +32,12 @@
 #include "ut_misc.h"
 
 #include <cairo-pdf.h>
+#include <cairo-ps.h>
+
+#if 0
+#include <cairo-svg.h>
+#endif
+
 #include <gsf/gsf-output-memory.h>
 #include <gsf/gsf-input-stdio.h>
 #include <glib/gstdio.h>
@@ -42,38 +48,42 @@
 /*****************************************************************/
 /*****************************************************************/
 
-class ABI_EXPORT IE_Exp_PDF : public IE_Exp
+static cairo_status_t
+ie_exp_cairo_write_func (void *closure, const unsigned char *data, unsigned int length)
+{
+	if (!gsf_output_write((GsfOutput*)closure, length, data))
+		return CAIRO_STATUS_WRITE_ERROR;
+    return CAIRO_STATUS_SUCCESS;
+}
+
+class ABI_EXPORT IE_Exp_Cairo : public IE_Exp
 {
 public:
   typedef enum
     {
       BACKEND_PS,
-      BACKEND_PDF
+      BACKEND_PDF,
+	  BACKEND_SVG
     } Format;
   
-  IE_Exp_PDF::Format mFormat;
+  IE_Exp_Cairo::Format mFormat;
   
-  IE_Exp_PDF(PD_Document * pDocument, IE_Exp_PDF::Format format)
+  IE_Exp_Cairo(PD_Document * pDocument, IE_Exp_Cairo::Format format)
     : IE_Exp(pDocument), mFormat(format)
   {
   }
   
-  virtual ~IE_Exp_PDF()
+  virtual ~IE_Exp_Cairo()
   {
   }
 
   virtual UT_Error _writeDocument(void)
   {
-    UT_Error exit_status = UT_ERROR;
-
     cairo_t *cr = NULL;
     cairo_surface_t *surface = NULL;
     GR_CairoPrintGraphics *print_graphics = NULL;
     FL_DocLayout *pDocLayout = NULL;
     FV_View *printView = NULL;
-    char *filename = NULL;
-	GError *error = NULL;
-    int fd;
 
     std::set<UT_uint32> pages;
     const std::string & pages_prop = getProperty ("pages");
@@ -89,15 +99,20 @@ public:
     height = getDoc()->m_docPageSize.Height (DIM_IN);
     portrait = getDoc()->m_docPageSize.isPortrait();
 
-	fd = g_file_open_tmp(NULL, &filename, &error);
-	if (error) {
-		/* TODO msgbox */
-		g_critical(error->message);
-		g_error_free(error), error = NULL;
-		goto exit_writeDocument;
-	}
-    close(fd);
-	surface = cairo_pdf_surface_create(filename, width * 96, height * 96);
+	if (BACKEND_PDF == mFormat)
+		surface = cairo_pdf_surface_create_for_stream(ie_exp_cairo_write_func, getFp(), width * 96, height * 96);
+	else if (BACKEND_PS == mFormat)
+		surface = cairo_ps_surface_create_for_stream(ie_exp_cairo_write_func, getFp(), width * 96, height * 96);
+	else if (BACKEND_SVG == mFormat)
+		{
+			// surface = cairo_svg_surface_create_for_stream(ie_exp_cairo_write_func, getFp(), width * 96, height * 96);
+			return UT_ERROR;
+		}
+	else
+		{
+			return UT_ERROR;
+		}
+
 	cr = cairo_create(surface);
 	cairo_surface_destroy(surface), surface = NULL;
 
@@ -112,95 +127,59 @@ public:
 
     if (!pages_prop.empty())
       {
-	char **page_descriptions = g_strsplit(pages_prop.c_str(), ",", -1);
+		  char **page_descriptions = g_strsplit(pages_prop.c_str(), ",", -1);
 	
-	int i = 0;
-	while (page_descriptions[i] != NULL)
-	  {
-	    char *description = page_descriptions[i];
-	    i++;
+		  int i = 0;
+		  while (page_descriptions[i] != NULL)
+			  {
+				  char *description = page_descriptions[i];
+				  i++;
 	    
-	    int start_page, end_page;
+				  int start_page, end_page;
 	    
-	    if (2 == sscanf(description, "%d-%d", &start_page, &end_page))
-	      {
-	      }
-	    else if (1 == sscanf(description, "%d", &start_page))
-	      {
-		end_page = start_page;
-	      }
-	    else
-	      {
-		// invalid page specification
-		continue;
-	      }
+				  if (2 == sscanf(description, "%d-%d", &start_page, &end_page))
+					  {
+					  }
+				  else if (1 == sscanf(description, "%d", &start_page))
+					  {
+						  end_page = start_page;
+					  }
+				  else
+					  {
+						  // invalid page specification
+						  continue;
+					  }
 	    
-	    for (int pageno = start_page; pageno <= end_page; pageno++)
-	      {
-		if ((pageno > 0) && (pageno <= (int)pDocLayout->countPages()))
-		  pages.insert(pageno);
-	      }
-	  }
+				  for (int pageno = start_page; pageno <= end_page; pageno++)
+					  {
+						  if ((pageno > 0) && (pageno <= (int)pDocLayout->countPages()))
+							  pages.insert(pageno);
+					  }
+			  }
 	
-	g_strfreev(page_descriptions);
+		  g_strfreev(page_descriptions);
       }
     
     if (pages.empty())
       {
-	for (UT_uint32 i = 1; i <= pDocLayout->countPages(); i++)
-	  {
-	    pages.insert(i);
-	  }
+		  for (UT_uint32 i = 1; i <= pDocLayout->countPages(); i++)
+			  {
+				  pages.insert(i);
+			  }
       }    
-
+	
     s_actuallyPrint (getDoc(), print_graphics,
-		     printView, getFileName(), 
-		     1, true, 
-		     pDocLayout->getWidth(), pDocLayout->getHeight() / pDocLayout->countPages(), 
-		     pages);
-
+					 printView, getFileName(), 
+					 1, true, 
+					 pDocLayout->getWidth(), pDocLayout->getHeight() / pDocLayout->countPages(), 
+					 pages);
+	
     DELETEP(print_graphics);
 
-    // copy filename back into getFp()
-    if(_copyFile(filename))
-      exit_status = UT_OK;
-    
-  exit_writeDocument:
-    if(filename)
-      {
-	// clean up temporary file
-	g_remove(filename);
-	g_free (filename);
-      }
-    
     DELETEP(pDocLayout);
     DELETEP(printView);
     DELETEP(print_graphics);
-    return exit_status;
-  }
-
-private:
-  bool _copyFile(const char * filename)
-  {
-    GsfInput * printed_file = gsf_input_stdio_new(filename, NULL);
-    if(printed_file)
-      {
-	size_t remaining = gsf_input_size(printed_file);
-	guint8 buf[1024];
-	
-	while(remaining > 0)
-	  {
-	    size_t nread = MIN(remaining, sizeof(buf));
-	    gsf_output_write(getFp(), nread, gsf_input_read(printed_file, nread, buf));
-	    remaining -= nread;
-	  }
-	
-	g_object_unref(G_OBJECT(printed_file));
-
-	return true;
-      }
-
-    return false;
+    return UT_OK;
   }
 };
 
@@ -241,7 +220,52 @@ bool IE_Exp_PS_Sniffer::getDlgLabels (const char ** szDesc,
 UT_Error IE_Exp_PS_Sniffer::constructExporter (PD_Document * pDocument,
 					       IE_Exp ** ppie)
 {
-  *ppie = new IE_Exp_PDF(pDocument, IE_Exp_PDF::BACKEND_PS);
+  *ppie = new IE_Exp_Cairo(pDocument, IE_Exp_Cairo::BACKEND_PS);
+  return UT_OK;
+}
+
+/*****************************************************************/
+/*****************************************************************/
+
+IE_Exp_SVG_Sniffer::IE_Exp_SVG_Sniffer()
+: IE_ExpSniffer("image/svg+xml", false)
+{
+}
+
+IE_Exp_SVG_Sniffer::~IE_Exp_SVG_Sniffer ()
+{
+}
+  
+UT_Confidence_t IE_Exp_SVG_Sniffer::supportsMIME (const char * szMIME)
+{
+  if(!g_ascii_strcasecmp(szMIME, "image/svg+xml") ||
+	 !g_ascii_strcasecmp(szMIME, "image/svg") ||
+	 !g_ascii_strcasecmp(szMIME, "image/svg-xml") ||
+	 !g_ascii_strcasecmp(szMIME, "image/vnd.adobe.svg+xml") ||
+	 !g_ascii_strcasecmp(szMIME, "text/xml-svg"))
+    return UT_CONFIDENCE_PERFECT;
+  return UT_CONFIDENCE_ZILCH;
+}
+  
+bool IE_Exp_SVG_Sniffer::recognizeSuffix (const char * szSuffix)
+{
+  return !g_ascii_strcasecmp(szSuffix,".svg");
+}
+
+bool IE_Exp_SVG_Sniffer::getDlgLabels (const char ** szDesc,
+				      const char ** szSuffixList,
+				      IEFileType * ft)
+{
+  *szDesc = "Scalable Vector Graphics (.svg)";
+  *szSuffixList = "*.svg";
+  *ft = getFileType();
+  return true;
+}
+
+UT_Error IE_Exp_SVG_Sniffer::constructExporter (PD_Document * pDocument,
+					       IE_Exp ** ppie)
+{
+  *ppie = new IE_Exp_Cairo(pDocument, IE_Exp_Cairo::BACKEND_SVG);
   return UT_OK;
 }
 
@@ -282,7 +306,7 @@ bool IE_Exp_PDF_Sniffer::getDlgLabels (const char ** szDesc,
 UT_Error IE_Exp_PDF_Sniffer::constructExporter (PD_Document * pDocument,
 					       IE_Exp ** ppie)
 {
-  *ppie = new IE_Exp_PDF(pDocument, IE_Exp_PDF::BACKEND_PDF);
+  *ppie = new IE_Exp_Cairo(pDocument, IE_Exp_Cairo::BACKEND_PDF);
   return UT_OK;
 }
 
