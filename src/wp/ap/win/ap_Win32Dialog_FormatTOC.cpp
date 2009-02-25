AbiWord/abiword

commit f8761b13b9734a205f17960c6c3862cfd2923ff3

Fix the win32 build.
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@25741 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/wp/ap/win/ap_Win32Dialog_FormatTOC.cpp
@@ -605,17 +605,14 @@ void AP_Win32Dialog_FormatTOC_Layout::_onInitDialog()
 	
 
 	/* Now the Page Numbering style */
-	const UT_GenericVector<const gchar*> * vecTypeList = AP_Dialog_FormatFootnotes::getFootnoteTypeLabelList();
-	const UT_GenericVector<const gchar*> * vecPropList = getContainer()->getVecLabelPropValue();
-	UT_sint32 nTypes = vecTypeList->getItemCount();
+	const FootnoteTypeDesc * vecTypeList = AP_Dialog_FormatFootnotes::getFootnoteTypeLabelList();
 	UT_UTF8String * sProp = NULL;	
 	UT_UTF8String  val;
-	int j;
 	
 	sProp = new UT_UTF8String("toc-page-type");		
-	for (j=0; j< nTypes; j++)
+	for (; vecTypeList->n !=  _FOOTNOTE_TYPE_INVALID; vecTypeList++)
 	{
-		const char * szVal = static_cast<const char *>(vecTypeList->getNthItem(j));
+		const char * szVal = vecTypeList->label;
 		item = SendDlgItemMessage(getHandle(), AP_RID_DIALOG_FORMATTOC_LAYOUTDETAILS_COMBO_NUMTYPE, 
 			CB_ADDSTRING, 0, (LPARAM) szVal);
 			
@@ -632,10 +629,10 @@ void AP_Win32Dialog_FormatTOC_Layout::_onInitDialog()
 	/* Tab Type styles */
 	const UT_GenericVector<const gchar*> * vecLabels = getContainer()->getVecTABLeadersLabel();
 	const UT_GenericVector<const gchar*> * vecProps = getContainer()->getVecTABLeadersProp();
-	nTypes = vecLabels->getItemCount();
-	sProp = NULL;	
+	UT_sint32 nTypes = vecLabels->getItemCount();
 	sProp = new UT_UTF8String("toc-tab-leader");	
-	for(j=0; j< nTypes; j++)
+
+	for(UT_sint32 j=0; j< nTypes; j++)
 	{
 		const char * szLab = static_cast<const char *>(vecLabels->getNthItem(j));
 		UT_DEBUGMSG(("Got label %s for item %d \n",szLab,j));		
@@ -782,8 +779,8 @@ void AP_Win32Dialog_FormatTOC_Layout::saveCtrlsValuesForDetailsLevel ()
 	{	
 		UT_String sNum;		
 
-		const UT_GenericVector<const gchar*> * vecPropList = getContainer()->getVecLabelPropValue();
-		const char * szVal = static_cast<const char *>(vecPropList->getNthItem(nSelected));
+		const FootnoteTypeDesc * footnoteTypeList = AP_Dialog_FormatFootnotes::getFootnoteTypeLabelList();
+		const char * szVal = footnoteTypeList[nSelected].label;
 		sProp = static_cast<char *> ("toc-page-type");
 		sVal = static_cast<const char *> (szVal);
 		

src/wp/ap/win/ap_Win32Dialog_FormatFootnotes.cpp
@@ -136,20 +136,19 @@ BOOL AP_Win32Dialog_FormatFootnotes::_onInitDialog(HWND hWnd, WPARAM /*wParam*/,
 	int nItem;
 	int nDefF = 0;
 	int nDefE = 0;
-	const UT_GenericVector<const gchar*>* pType = getFootnoteTypeLabelList();
-	UT_return_val_if_fail( pType, 0 );
+	const FootnoteTypeDesc * footnoteTypeList = getFootnoteTypeLabelList();
 
-	for(UT_uint32 i = 0; i < pType->getItemCount(); ++i)
+	for(UT_uint32 i = 0; footnoteTypeList->n !=  _FOOTNOTE_TYPE_INVALID; footnoteTypeList++, i++)
 	{
 		 nItem = SendDlgItemMessage(hWnd, AP_RID_DIALOG_FORMATFOOTNOTES_COMBO_FSTYLE, CB_ADDSTRING, 0,
-									(LPARAM)pType->getNthItem(i));
+									(LPARAM)footnoteTypeList->label);
 		 SendDlgItemMessage(hWnd, AP_RID_DIALOG_FORMATFOOTNOTES_COMBO_FSTYLE, CB_SETITEMDATA, nItem, (LPARAM)i);
 
 		 if (i==(UT_uint32)getFootnoteType())
 		  	nDefF = i;			
 
 		 nItem = SendDlgItemMessage(hWnd, AP_RID_DIALOG_FORMATFOOTNOTES_COMBO_ESTYLE, CB_ADDSTRING, 0,
-									(LPARAM)pType->getNthItem(i));
+									(LPARAM)footnoteTypeList->label);
 		 SendDlgItemMessage(hWnd, AP_RID_DIALOG_FORMATFOOTNOTES_COMBO_ESTYLE, CB_SETITEMDATA, nItem, (LPARAM)i);
 		 
 		 if (i==(UT_uint32)getEndnoteType())

commit 041c3a349fc49ef6443c694cd8c91a7449767441

Store the property instead of the label (fixes a regression introduced in r25741).
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@25742 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/wp/ap/win/ap_Win32Dialog_FormatTOC.cpp
@@ -780,7 +780,7 @@ void AP_Win32Dialog_FormatTOC_Layout::saveCtrlsValuesForDetailsLevel ()
 		UT_String sNum;		
 
 		const FootnoteTypeDesc * footnoteTypeList = AP_Dialog_FormatFootnotes::getFootnoteTypeLabelList();
-		const char * szVal = footnoteTypeList[nSelected].label;
+		const char * szVal = footnoteTypeList[nSelected].prop;
 		sProp = static_cast<char *> ("toc-page-type");
 		sVal = static_cast<const char *> (szVal);
 		