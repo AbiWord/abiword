e546255884a229af6ff280bd1c09ca1984ccd5b1
J M Maurer <uwog@uwog.net>
2009-03-04 18:15:22 +0000

    Merged revisions 25828 via svnmerge from svn+ssh://svn.abisource.com/svnroot/abiword/trunk
    ........
      r25828 | uwog | 2009-03-03 20:14:05 +0100 (Tue, 03 Mar 2009) | 18 lines
      
      Relicense ut_go_file.h and ut_go_file.c to be GPLv2+.
      
      These files originated from goffice's go_file.h and go_file.c files, and before that from gnumeric. All contributors to these files have given me written permission (by email) to relicense their contributions to these files to GPLv2+:
      
      Dominic Lachowicz
      Stepan Kasal
      Jean Br?\195?\169fort
      Jon K Hellan
      Morten Welinder
      Ivan Yat-Cheung Wong
      Eduardo Lima
      Jody Goldberg
      Robert Staudinger
      "sum1"
      
      The RedHat copyright on these files covers code that was released under the LGPLv2+ license, and is thus compatible with the GPLv2+ licence.
    ........
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-6-0-STABLE@25840 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_go_file.cpp
+++ src/af/util/xp/ut_go_file.cpp
@@ -6,8 +6,9 @@
  * Copyright (C) 2003, Red Hat, Inc.
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 2 of the GNU General Public
- * License as published by the Free Software Foundation.
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,8 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  */
 
 #ifdef HAVE_CONFIG_H
