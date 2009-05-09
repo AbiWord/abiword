a57b7bb7967d7e233880b9413993bbf56f029fd7
Firat Kiyak <firatkiyak@gmail.com>
2009-05-09 19:40:25 +0000

    OpenXML importer table listener initial code added
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2009oox@26230 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ plugins/openxml/imp/xp/OXMLi_ListenerState_Table.cpp
@@ -0,0 +1,56 @@
+/* -*- mode: C++; tab-width: 4; c-basic-offset: 4; -*- */
+
+/* AbiSource
+ * 
+ * Copyright (C) 2009 Firat Kiyak <firatkiyak@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+// Class definition include
+#include <OXMLi_ListenerState_Table.h>
+
+// Internal includes
+#include <OXML_Document.h>
+#include <OXML_FontManager.h>
+
+// AbiWord includes
+#include <ut_assert.h>
+#include <ut_misc.h>
+
+// External includes
+#include <string>
+
+OXMLi_ListenerState_Table::OXMLi_ListenerState_Table():
+	OXMLi_ListenerState()
+{
+
+}
+
+void OXMLi_ListenerState_Table::startElement (OXMLi_StartElementRequest * /*rqst*/)
+{
+	//TODO: add functionality here
+}
+
+void OXMLi_ListenerState_Table::endElement (OXMLi_EndElementRequest * /*rqst*/)
+{
+	//TODO: add functionality here
+}
+
+void OXMLi_ListenerState_Table::charData (OXMLi_CharDataRequest * /*rqst*/)
+{
+	//TODO: add functionality here
+}
