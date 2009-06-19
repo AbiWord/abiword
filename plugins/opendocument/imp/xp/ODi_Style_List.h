7daa5b7c9d4298156d9c4a1c5634b35b0c652165
Hubert Figuiere <hub@figuiere.net>
2009-06-19 08:00:22 +0000

    missing implementation I mistakenly removed.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@26897 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/opendocument/imp/xp/ODi_Style_List.h
+++ plugins/opendocument/imp/xp/ODi_Style_List.h
@@ -57,7 +57,7 @@ public:
     void endElement (const gchar* pName,
                              ODi_ListenerStateAction& rAction);
                              
-    void charData (const gchar* pBuffer, int length);
+    void charData (const gchar* /*pBuffer*/, int /*length*/) { };
     
     void defineAbiList(PD_Document* pDocument);
 
