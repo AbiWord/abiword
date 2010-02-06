0c805e24630fd8946e2f41d623f1432e095bda6b
J M Maurer <uwog@uwog.net>
2010-02-06 14:27:09 +0000

    Recorded merge of revisions 28584 via svnmerge from svn+ssh://uwog@svn.abisource.com/svnroot/abiword/trunk
    ........
      r28584 | sum1 | 2010-01-03 18:21:42 +0100 (Sun, 03 Jan 2010) | 1 line
      
      Bump the copyright date to 2010. Can be backported.
    ........
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-8-0-STABLE@28638 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/xp/xap_Dlg_About.h
+++ src/af/xap/xp/xap_Dlg_About.h
@@ -36,7 +36,7 @@
 
 #define XAP_ABOUT_TITLE "About %s"
 #define XAP_ABOUT_DESCRIPTION "%s is an Open Source application licensed under the GNU GPL.\nYou are free to redistribute this application."
-#define XAP_ABOUT_COPYRIGHT "Copyright (C) 1998-2009 Dom Lachowicz, and others"
+#define XAP_ABOUT_COPYRIGHT "Copyright (C) 1998-2010 Dom Lachowicz, and others"
 #define XAP_ABOUT_GPL "%s is available for use under the the terms\nof the GNU General Public License"
 #define XAP_ABOUT_VERSION "Version: %s"
 #define XAP_ABOUT_BUILD "Build options: %s"
