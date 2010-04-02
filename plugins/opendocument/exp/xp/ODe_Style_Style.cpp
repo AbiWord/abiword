54b679c3a78426017d0d968ff9040f0f418585c3
J M Maurer <uwog@uwog.net>
2010-04-02 19:46:13 +0000

    Recorded merge of revisions 28754 via svnmerge from svn+ssh://uwog@svn.abisource.com/svnroot/abiword/trunk
    ........
      r28754 | uwog | 2010-03-28 16:47:54 +0200 (Sun, 28 Mar 2010) | 3 lines
      
      Add a LocaleTransactor (sum1), and fix a memory leak. Please backport.
    ........
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-2-8-0-STABLE@28779 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/opendocument/exp/xp/ODe_Style_Style.cpp
+++ plugins/opendocument/exp/xp/ODe_Style_Style.cpp
@@ -1101,6 +1101,7 @@ fetchAttributesFromAbiProps(const PP_AttrProp& rAP) {
                     
                     tabStop.m_type = "char";
                     tabStop.m_char.appendUCS4(pDecimalStr);
+                    FREEP(pDecimalStr);
                     break;
                 }
                 case FL_TAB_NONE: // huh, a tab that is no tab?!
@@ -1112,6 +1113,7 @@ fetchAttributesFromAbiProps(const PP_AttrProp& rAP) {
             }
             
             // style:position
+            UT_LocaleTransactor t(LC_NUMERIC, "C");
             double pos = (double)pTabStop->getPosition() / UT_LAYOUT_RESOLUTION;
             tabStop.m_position = UT_UTF8String_sprintf("%fin", pos);
 
