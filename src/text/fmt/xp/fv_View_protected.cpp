1cfb84a7b34ac2a08c39d70b0ecbd16e15702e09
Martin Sevior <martines@unimelb.edu.au>
2010-04-15 10:02:31 +0000

    Fix selection bug.
    Nice catch from dzan
    
    
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@28839 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/fmt/xp/fv_View_protected.cpp
+++ src/text/fmt/xp/fv_View_protected.cpp
@@ -3615,7 +3615,7 @@ void FV_View::_extSel(UT_uint32 iOldPoint)
 	getEditableBounds(false,posBOD);
 	getEditableBounds(true,posEOD);
 	if(dNewPoint < posBOD || dNewPoint > posEOD || dOldPoint < posBOD
-	   || dNewPoint > posEOD)
+	   || dOldPoint > posEOD)
 	{
 		return;
 	}
