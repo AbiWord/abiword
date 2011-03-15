d385551d06fe9240c3d0d8b05b2d787ae6db203d
Fridrich Strba <fridrich.strba@bluewin.ch>
2011-03-15 14:58:36 +0000

    Fix bmp plugin build with libpng 1.5.x
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@29665 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/bmp/xp/ie_impGraphic_BMP.cpp
+++ plugins/bmp/xp/ie_impGraphic_BMP.cpp
@@ -191,7 +191,7 @@ UT_Error IE_ImpGraphic_BMP::_convertGraphic(UT_ByteBuf * pBB)
 
 	/* Clean Up Memory Used */
 		
-	FREEP(m_pPNGInfo->palette);
+	// FREEP(m_pPNGInfo->palette);
 	DELETEP(pBB);
 	png_destroy_write_struct(&m_pPNG, &m_pPNGInfo);
    
@@ -313,7 +313,7 @@ UT_Error IE_ImpGraphic_BMP::Initialize_PNG()
 	 * the normal method of doing things with libpng).  REQUIRED unless you
 	 * set up your own error handlers in the png_create_read_struct() earlier.
 	 */
-	if (setjmp(m_pPNG->jmpbuf))
+	if (setjmp(png_jmpbuf(m_pPNG)))
 	{
 		/* Free all of the memory associated with the png_ptr and info_ptr */
 		png_destroy_write_struct(&m_pPNG, &m_pPNGInfo);
@@ -332,7 +332,7 @@ UT_Error IE_ImpGraphic_BMP::Initialize_PNG()
 	UT_Error IE_ImpGraphic_BMP::Convert_BMP_Pallet(UT_ByteBuf* pBB)
 	{
 		/* Reset error handling for libpng */
-		if (setjmp(m_pPNG->jmpbuf))
+		if (setjmp(png_jmpbuf(m_pPNG)))
 		{
 			png_destroy_write_struct(&m_pPNG, &m_pPNGInfo);
 			return UT_ERROR;
@@ -372,7 +372,7 @@ UT_Error IE_ImpGraphic_BMP::Initialize_PNG()
 UT_Error IE_ImpGraphic_BMP::Convert_BMP(UT_ByteBuf* pBB)
 {
 	/* Reset error handling for libpng */
-	if (setjmp(m_pPNG->jmpbuf))
+	if (setjmp(png_jmpbuf(m_pPNG)))
 	{
 		png_destroy_write_struct(&m_pPNG, &m_pPNGInfo);
 		return UT_ERROR;
