26c3b1c6804b815d21399c412ca08549d1a4ba5d
Fridrich Strba <fridrich.strba@bluewin.ch>
2011-03-15 14:58:06 +0000

    Allow building garble plugin with libpng 1.5.x
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@29664 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/garble/xp/abiword-garble-png.cpp
+++ plugins/garble/xp/abiword-garble-png.cpp
@@ -79,7 +79,7 @@ bool abiword_document::garble_png( void*& data, size_t& size ) {
 		png_set_strip_alpha( png_ptr );
 		png_set_interlace_handling( png_ptr );
 		png_set_bgr( png_ptr );
-		rowbytes = info_ptr->rowbytes;
+		rowbytes = png_get_rowbytes(png_ptr, info_ptr);
 		png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
 	}
 
