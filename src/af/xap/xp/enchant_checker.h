de313d03bda353a0758f6ec61b8c8a92e291ae13
chenxiajian <chenxiajian1985@gmail.com>
2011-05-12 07:31:59 +0000

    corrcorrect the copyright
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2011hyphenation@29748 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/xp/enchant_checker.h
+++ src/af/xap/xp/enchant_checker.h
@@ -1,5 +1,5 @@
 /* AbiSuite
- * Copyright (C) 2003 Dom Lachowicz
+ * Copyright (C) 2012 chenxiajian <chenxiajian1985@gmail.com> 
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
