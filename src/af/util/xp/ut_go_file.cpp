fbaca6fb588bb767b773f6ff57b5a91dfb7fe589
J M Maurer <uwog@uwog.net>
2011-07-04 15:13:11 +0000

    Don't break on unix with older glib version
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@29932 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_go_file.cpp
+++ src/af/util/xp/ut_go_file.cpp
@@ -446,7 +446,11 @@ UT_go_filename_simplify (const char *filename, UT_GODotDot dotdot,
 					isdir = TRUE;
 					break;
 				case UT_GO_DOTDOT_TEST: {
+#if GLIB_CHECK_VERSION(2,26,0) || defined(G_OS_WIN32)
 					GStatBuf statbuf;
+#else
+					struct stat statbuf;
+#endif
 					char savec = *q;
 					/*
 					 * Terminate the path so far so we can
@@ -1390,7 +1394,12 @@ UT_go_file_exists (char const *uri)
 
 	return FALSE;
 #else
+
+#if GLIB_CHECK_VERSION(2,26,0) || defined(G_OS_WIN32)
 	GStatBuf file_stat;
+#else
+	struct stat file_stat;
+#endif
 	char *filename = UT_go_filename_from_uri (uri);
 	int result = filename ? g_stat (filename, &file_stat) : -1;
 
@@ -1597,7 +1606,11 @@ UT_go_file_get_date (char const *uri, UT_GOFileDateType type)
 
 	gnome_vfs_file_info_unref (file_info);
 #else
+#if GLIB_CHECK_VERSION(2,26,0) || defined(G_OS_WIN32)
 	GStatBuf file_stat;
+#else
+	struct stat file_stat;
+#endif
 	char *filename = UT_go_filename_from_uri (uri);
 	int result = filename ? g_stat (filename, &file_stat) : -1;
 
