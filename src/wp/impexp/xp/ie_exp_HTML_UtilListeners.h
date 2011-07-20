dd9bb826ba65d9aee702c26602d2206905074eef
Volodymyr Rudyj <vladimir.rudoy@gmail.com>
2011-07-20 04:53:21 +0000

    Extracted helper listener classes - header and footer listeners, bookmark listener and placed into separate file. Added copyright notices
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2011-html-refactoring@30067 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/impexp/xp/ie_exp_HTML_UtilListeners.h
@@ -0,0 +1,146 @@
+#ifndef IE_EXP_HTML_UTILLISTENERS_H
+#define	IE_EXP_HTML_UTILLISTENERS_H
+
+
+#ifdef TH_SKIP_REST
+#undef TH_SKIP_REST
+#endif
+#define TH_SKIP_REST  1
+
+#ifdef TH_SKIP_THIS
+#undef TH_SKIP_THIS
+#endif
+#define TH_SKIP_THIS  2
+
+#include "ie_exp_HTML.h"
+#include "ie_exp_HTML_MainListener.h"
+
+class ABI_EXPORT s_TemplateHandler : public UT_XML::ExpertListener
+{
+public:
+    s_TemplateHandler(PD_Document * pDocument, IE_Exp_HTML * pie);
+
+    ~s_TemplateHandler();
+
+    /* Implementation of ExpertListener
+     */
+    void StartElement(const gchar * name, const gchar ** atts);
+    void EndElement(const gchar * name);
+    void CharData(const gchar * buffer, int length);
+    void ProcessingInstruction(const gchar * target, const gchar * data);
+    void Comment(const gchar * data);
+    void StartCdataSection();
+    void EndCdataSection();
+    void Default(const gchar * buffer, int length);
+
+private:
+    void _handleMetaTag(const char * key, UT_UTF8String & value);
+    void _handleMeta();
+
+    bool echo() const;
+    bool condition(const gchar * data) const;
+
+    PD_Document * m_pDocument;
+    IE_Exp_HTML * m_pie;
+
+    bool m_cdata;
+    bool m_empty;
+
+    UT_UTF8String m_utf8;
+    UT_UTF8String m_root;
+    typedef std::map<std::string, std::string> hash_type;
+    hash_type m_hash;
+    UT_NumberStack m_mode;
+};
+
+class ABI_EXPORT s_HTML_HdrFtr_Listener : public PL_Listener
+{
+friend class IE_Exp_HTML_MainListener;
+public:
+s_HTML_HdrFtr_Listener(PD_Document * pDocument, IE_Exp_HTML * pie, PL_Listener * pHTML_Listener);
+
+~s_HTML_HdrFtr_Listener();
+
+bool populate(PL_StruxFmtHandle sfh,
+              const PX_ChangeRecord * pcr);
+
+bool populateStrux(PL_StruxDocHandle sdh,
+                   const PX_ChangeRecord * pcr,
+                   PL_StruxFmtHandle * psfh);
+
+//See note in _writeDocument
+//bool 	startOfDocument ();
+bool endOfDocument();
+
+bool change(PL_StruxFmtHandle sfh,
+            const PX_ChangeRecord * pcr);
+
+bool insertStrux(PL_StruxFmtHandle sfh,
+                 const PX_ChangeRecord * pcr,
+                 PL_StruxDocHandle sdh,
+                 PL_ListenerId lid,
+                 void (*pfnBindHandles) (PL_StruxDocHandle sdhNew,
+                 PL_ListenerId lid,
+                 PL_StruxFmtHandle sfhNew));
+
+bool signal(UT_uint32 iSignal);
+void doHdrFtr(bool bHeader);
+private:
+PD_DocumentRange * m_pHdrDocRange;
+PD_DocumentRange * m_pFtrDocRange;
+PD_Document * m_pDocument;
+PL_Listener * m_pHTML_Listener;
+};
+
+class ABI_EXPORT s_HTML_Bookmark_Listener : public PL_Listener
+{
+public:
+s_HTML_Bookmark_Listener(PD_Document* pDoc, IE_Exp_HTML * pie);
+bool populate(PL_StruxFmtHandle sfh,
+              const PX_ChangeRecord * pcr);
+// Not used
+
+bool populateStrux(PL_StruxDocHandle /*sdh*/,
+                   const PX_ChangeRecord * /*pcr*/,
+                   PL_StruxFmtHandle * /*psfh*/)
+{
+    return true;
+}
+// Not used
+
+bool change(PL_StruxFmtHandle /*sfh*/,
+            const PX_ChangeRecord * /*pcr*/)
+{
+    return true;
+}
+// Not used
+
+bool insertStrux(PL_StruxFmtHandle /*sfh*/,
+                 const PX_ChangeRecord * /*pcr*/,
+                 PL_StruxDocHandle /*sdh*/,
+                 PL_ListenerId /*lid*/,
+                 void (*/*pfnBindHandles*/) (PL_StruxDocHandle sdhNew,
+                 PL_ListenerId lid,
+                 PL_StruxFmtHandle sfhNew))
+{
+    return true;
+}
+// Not used
+
+bool signal(UT_uint32 /*iSignal*/)
+{
+    return true;
+}
+
+inline std::map<UT_UTF8String, UT_UTF8String> getBookmarks() const
+{
+    return m_bookmarks;
+}
+private:
+std::map<UT_UTF8String, UT_UTF8String> m_bookmarks;
+PD_Document * m_pDoc;
+IE_Exp_HTML * m_pie;
+
+};
+#endif	/* IE_EXP_HTML_UTILLISTENERS_H */
+
