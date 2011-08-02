7118816d34ff1751a24e5a26f01bd0c26d23ec85
Volodymyr Rudyj <vladimir.rudoy@gmail.com>
2011-08-02 06:27:56 +0000

    Moved TagWriter and OutputWriter classes to ie_exp_HTML_util.h
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2011-html-refactoring@30082 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_exp_HTML_util.cpp
+++ src/wp/impexp/xp/ie_exp_HTML_util.cpp
@@ -177,3 +177,204 @@ char * s_removeWhiteSpace (const char * text, UT_UTF8String & utf8str,
 	}
 	return 0;
 }
+
+IE_Exp_HTML_OutputWriter::IE_Exp_HTML_OutputWriter(GsfOutput* output):
+m_output(output)
+{
+   
+}
+
+void IE_Exp_HTML_OutputWriter::write(const gchar* data, size_t size)
+{
+    gsf_output_write(m_output, size, (const guint8*)data);
+}
+
+void IE_Exp_HTML_OutputWriter::write(const std::string& str)
+{
+    write(str.c_str(), strlen(str.c_str()));
+}
+
+IE_Exp_HTML_TagWriter::IE_Exp_HTML_TagWriter(IE_Exp_HTML_OutputWriter* pOutputWriter):
+m_pOutputWriter(pOutputWriter),
+    m_bXmlModeEnabled(false),
+    m_bCurrentTagIsSingle(false),
+    m_bAttributesWritten(false),
+        m_bDataWritten(false),
+    m_bInComment(false),
+    m_buffer("")
+{
+    
+}
+
+void IE_Exp_HTML_TagWriter::openTag(const std::string& tagName, bool isInline, bool isSingle)
+{
+    if (m_bInComment)
+    {
+        UT_ASSERT("Trying to open tag inside comment\n");
+        return;
+    }
+        
+    if ((m_tagStack.size() > 0) && m_bCurrentTagIsSingle)
+    {
+        closeTag();
+    } else
+    {
+        _closeAttributes();
+    }
+    m_bCurrentTagIsSingle = isSingle;
+    m_bAttributesWritten = false;
+    m_bDataWritten = false;
+    m_tagStack.push_back(tagName);
+    m_inlineFlagStack.push_back(isInline);
+   
+    
+    
+    if (!isInline)
+    {
+        std::string indent = "";
+        for (size_t i = 0; i < m_tagStack.size() - 1; i++)
+        {
+            indent += "    ";
+        }
+        
+        m_buffer += indent;
+    }
+    m_buffer += "<" + tagName;
+    
+    UT_DEBUGMSG(("Opened tag: %s\n", tagName.c_str())); 
+}
+
+void IE_Exp_HTML_TagWriter::addAttribute(const std::string& name, const std::string& value)
+{
+    if (m_bInComment)
+    {
+        UT_ASSERT("Trying to add attribute inside comment\n");
+        return;
+    }
+    m_buffer += " " + name + "=\"" + value + "\""; 
+}
+
+void IE_Exp_HTML_TagWriter::_closeAttributes()
+{
+    if (m_tagStack.size() ==  0)
+    {
+        return;
+    }
+    
+    if (m_bInComment)
+    {
+        UT_ASSERT("Trying to close attribute list inside comment\n");
+        return;
+    }
+        
+    if (!m_bAttributesWritten)
+    {
+        if (m_bXmlModeEnabled)
+        {
+                m_buffer += " />";
+        } else
+        {
+            m_buffer += ">";
+        }
+        
+        if (!m_inlineFlagStack.back())
+        {
+            m_buffer += "\n";
+        }
+        
+        m_bAttributesWritten = true;
+    }
+}
+
+void IE_Exp_HTML_TagWriter::writeData(const std::string& data)
+{
+    if (!m_bInComment)
+    {
+        _closeAttributes();
+    }
+    m_buffer += data;
+    m_bDataWritten = true;
+}
+
+void IE_Exp_HTML_TagWriter::closeTag()
+{
+    if (m_bInComment)
+    {
+        UT_ASSERT("Trying to close tag inside comment\n");
+        return;
+    }
+    
+    if (m_tagStack.size() == 0)
+    {
+        UT_ASSERT("Trying to close unopened tag\n");
+        return;
+    }
+    _closeAttributes();
+    
+    if (!m_bCurrentTagIsSingle)
+    {
+
+        if (m_bDataWritten && !m_inlineFlagStack.back())
+        {
+            std::string indent = "";
+            for (size_t i = 0; i < m_tagStack.size() - 1; i++)
+            {
+                indent += "    ";
+            }
+            m_buffer += "\n" + indent;
+        }
+        
+        m_buffer += "</" + m_tagStack.back() + ">";
+        if (!m_inlineFlagStack.back())
+        {
+            m_buffer += "\n";
+        }
+    } else
+    {
+        m_bCurrentTagIsSingle = false;
+    }
+    
+    UT_DEBUGMSG(("Closed tag: %s\n", m_tagStack.back().c_str()));
+    m_tagStack.pop_back();
+    m_inlineFlagStack.pop_back();
+    
+    flush();
+    
+    
+}
+
+void IE_Exp_HTML_TagWriter::openComment()
+{
+    if (m_bInComment)
+    {
+        UT_ASSERT("Trying to create nested comment\n");
+        return;
+    }
+    m_bInComment = true;
+    m_buffer += "<!-- ";
+}
+
+void IE_Exp_HTML_TagWriter::closeComment()
+{
+    if (!m_bInComment)
+    {
+        UT_ASSERT("Trying to close unopened comment\n");
+        return;
+    }
+    m_bInComment = false;
+    m_buffer += " -->";
+}
+
+void IE_Exp_HTML_TagWriter::flush()
+{
+    if (m_buffer.length() > 0)
+    {
+        m_pOutputWriter->write(m_buffer);
+        m_buffer = "";
+    }
+}
+
+void IE_Exp_HTML_TagWriter::enableXmlMode(bool enable)
+{
+    m_bXmlModeEnabled = enable;
+}
--- src/wp/impexp/xp/ie_exp_HTML_util.h
+++ src/wp/impexp/xp/ie_exp_HTML_util.h
@@ -22,8 +22,15 @@
 #ifndef IE_EXP_HTML_UTIL_H
 #define IE_EXP_HTML_UTIL_H
 
+// External includes
+#include <vector>
+#include <string>
+#include <gsf/gsf-output.h>
+
+// Abiword includes
 #include <ut_string_class.h>
 #include <ut_types.h>
+#include <ut_debugmsg.h>
 
 #define MYEOL "\n"
 
@@ -46,6 +53,52 @@ UT_UTF8String s_string_to_url (const UT_UTF8String & str);
 bool is_CSS (const char * prop_name, const char ** prop_default = 0);
 char * s_removeWhiteSpace (const char * text, UT_UTF8String & utf8str,
 								  bool bLowerCase = true);
+
+
+/**
+ * Utility class that allows write character data or UTF8 strings
+ */
+class IE_Exp_HTML_OutputWriter
+{
+public:
+    IE_Exp_HTML_OutputWriter(GsfOutput *output);
+    void write(const gchar * data, size_t size);
+    void write(const std::string &str);
+    
+private:
+    GsfOutput *m_output;
+};
+
+/**
+ * Utility class that gives simple interface to create HTML and XML documents
+ */
+class IE_Exp_HTML_TagWriter
+{
+public:
+    IE_Exp_HTML_TagWriter(IE_Exp_HTML_OutputWriter *pOutputWriter);
+    void openTag(const std::string &tagName, bool isInline = false, bool isSingle = false);
+    void addAttribute(const std::string &name, const std::string &value);
+    void writeData(const std::string &data);
+    void closeTag();
+    void flush();
+    
+    void openComment();
+    void closeComment();
+    
+    void enableXmlMode(bool enable = true);
+private:
+    
+    inline void _closeAttributes();
+    std::vector<std::string> m_tagStack;
+    std::vector<bool> m_inlineFlagStack;
+    IE_Exp_HTML_OutputWriter *m_pOutputWriter;
+    bool m_bXmlModeEnabled;
+    bool m_bCurrentTagIsSingle;
+    bool m_bAttributesWritten;
+    bool m_bDataWritten;
+    bool m_bInComment;
+    std::string m_buffer;
+};
 
 
 
