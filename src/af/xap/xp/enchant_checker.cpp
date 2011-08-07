e30319952eae911c250dfd68eb2179af916c5b84
chenxiajian <chenxiajian1985@gmail.com>
2011-08-07 15:31:34 +0000

    fix the bug that influence the path for SpellChecker
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2011hyphenation@30121 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/xp/enchant_checker.cpp
+++ src/af/xap/xp/enchant_checker.cpp
@@ -64,7 +64,7 @@ EnchantChecker::EnchantChecker()
 		std::string ispell_hyphenation_path = std::string(ispell_hyphenation_path1) + ";" + std::string(ispell_hyphenation_path2);
 
 		enchant_broker_set_param(s_enchant_broker,  "enchant.ispell.dictionary.path", ispell_path.c_str());
-		enchant_broker_set_param(s_enchant_broker,  "enchant.ispell.hyphenation.dictionary.path", ispell_path.c_str());
+		//enchant_broker_set_param(s_enchant_broker,  "enchant.ispell.hyphenation.dictionary.path", ispell_path.c_str());
 
 		g_free(ispell_path1);
 		g_free(ispell_path2);
@@ -73,7 +73,7 @@ EnchantChecker::EnchantChecker()
 
 		gchar* myspell_path = g_build_filename (XAP_App::getApp()->getAbiSuiteLibDir(), "dictionary", "myspell", NULL);
 		enchant_broker_set_param(s_enchant_broker,  "enchant.myspell.dictionary.path", myspell_path);
-		enchant_broker_set_param(s_enchant_broker,  "enchant.myspell.hyphenation.dictionary.path", myspell_path);
+		//enchant_broker_set_param(s_enchant_broker,  "enchant.myspell.hyphenation.dictionary.path", myspell_path);
 		g_free(myspell_path);
 #endif
 	}
