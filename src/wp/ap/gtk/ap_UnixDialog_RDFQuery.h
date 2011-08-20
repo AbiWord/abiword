ff3979b3a237b11e61a6bb4897b0d2559fe62b2a
Ben Martin <monkeyiq@users.sourceforge.net>
2011-08-20 06:57:30 +0000

    Forgot to svn add these before.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@30265 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/wp/ap/gtk/ap_UnixDialog_RDFQuery.h
@@ -0,0 +1,90 @@
+/* AbiWord
+ * Copyright (C) 2011 AbiSource, Inc.
+ * Copyright (C) Ben Martin
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#ifndef AP_UNIXDIALOG_RDFQUERY_H
+#define AP_UNIXDIALOG_RDFQUERY_H
+
+#include "ap_Dialog_RDFQuery.h"
+#include "fv_View.h"
+
+class XAP_UnixFrame;
+
+
+
+class AP_UnixDialog_RDFQuery: public AP_Dialog_RDFQuery
+{
+public:
+	AP_UnixDialog_RDFQuery (XAP_DialogFactory * pDlgFactory, XAP_Dialog_Id id);
+	virtual ~AP_UnixDialog_RDFQuery (void);
+
+	static XAP_Dialog *static_constructor (XAP_DialogFactory *, 
+										   XAP_Dialog_Id id);
+
+	virtual void runModeless 	   (XAP_Frame *pFrame);
+	virtual void notifyActiveFrame (XAP_Frame *pFrame);
+	virtual void activate	 	   (void);
+	virtual void destroy 		   (void);
+
+    void onExecuteClicked();
+    void onShowAllClicked();
+
+
+	const GtkWidget *getWindow (void) { return m_wDialog; }
+
+    virtual void clear();
+    virtual void addStatement( const PD_RDFStatement& st );
+    virtual void addBinding( std::map< std::string, std::string >& b );
+    virtual void setStatus( const std::string& msg );
+    virtual void setQueryString( const std::string& sparql );
+    virtual void setupBindingsView( std::map< std::string, std::string >& b );
+
+protected:
+
+	void constuctWindow 	  (XAP_Frame *pFrame);
+	void updateWindow		  (void);
+
+private: 
+
+    enum 
+    {
+        C_SUBJ_COLUMN = 0,
+        C_PRED_COLUMN,
+        C_OBJ_COLUMN,
+		C_COLUMN_COUNT,
+        C_COLUMN_ARRAY_SIZE = 1024
+	};
+    
+    GtkTreeViewColumn* w_cols[C_COLUMN_ARRAY_SIZE];
+    
+	GtkWidget *m_wDialog;
+	GtkWidget *m_btClose;
+    GtkWidget *m_btExecute;
+    GtkWidget *m_btShowAll;
+    GtkWidget *m_query;
+	GtkTreeView*  m_resultsView;
+	GtkTreeStore* m_resultsModel;
+    GtkWidget *m_status;
+
+};
+
+#endif /* AP_UNIXDIALOG_RDFQUERY_H */
+
+
+
