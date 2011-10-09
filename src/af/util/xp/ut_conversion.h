5180448fa02144745594714dda73c901ada78061
Ben Martin <monkeyiq@users.sourceforge.net>
2011-10-09 11:41:10 +0000

    added
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@30362 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ src/af/util/xp/ut_conversion.h
@@ -0,0 +1,82 @@
+/* -*- mode: C++; tab-width: 4; c-basic-offset: 4; -*- */
+
+/* AbiSource Program Utilities
+ * Copyright (C) 2010 AbiSource, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
+ * 02111-1307, USA.
+ */
+
+#ifndef UT_CONVERSION_H
+#define UT_CONVERSION_H
+
+#include "pp_AttrProp.h"
+class PP_Revision;
+
+#include <string>
+#include <sstream>
+
+template < typename ClassName >
+static ClassName toType( const char* s )
+{
+    ClassName ret = 0;
+    std::stringstream ss;
+    ss << s;
+    ss >> ret;
+    return ret;
+}
+template < typename ClassName >
+static ClassName toType( std::string s )
+{
+    ClassName ret = 0;
+    std::stringstream ss;
+    ss << s;
+    ss >> ret;
+    return ret;
+}
+
+template < typename T >
+static std::string tostr( T v )
+{
+    std::stringstream ss;
+    ss << v;
+    return ss.str();
+}
+
+
+template < typename T >
+T UT_getAttributeTyped( const PP_AttrProp *pAP,
+                        const gchar* name,
+                        T def )
+{
+    const gchar * pAttrValue = NULL;
+    if( pAP->getAttribute( name, pAttrValue ))
+    {
+        return toType<T>( pAttrValue );
+    }
+    return def;
+}
+template < typename T >
+T UT_getAttributeTyped( const PP_Revision* pAP,
+                        const gchar* name,
+                        T def )
+{
+    return UT_getAttributeTyped( (const PP_AttrProp*)pAP, name, def );
+}
+
+    
+
+
+#endif // reinclude protection
