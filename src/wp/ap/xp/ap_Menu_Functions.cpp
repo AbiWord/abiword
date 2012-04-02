80afbb23609abcf8c58295ef1e1634e9e64c5ff7
Kathiravelu Pradeeban <kk.pradeeban@gmail.com>
2012-04-02 03:47:32 +0000

    Insert Textbox was disabled in the Normal and Web Mode. Patch from Aaditya (id=13312), as this seems to be a bug. Committing as discussed over the irc. Pls revert, if this was disabled intentionally.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@30755 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Menu_Functions.cpp
+++ src/wp/ap/xp/ap_Menu_Functions.cpp
@@ -1362,11 +1362,6 @@ Defun_EV_GetMenuItemState_Fn(ap_GetState_InsTextBox)
 	ABIWORD_VIEW;
 	UT_return_val_if_fail (pView, EV_MIS_Gray);
 
-        if((pView->getViewMode() == VIEW_NORMAL) || (pView->getViewMode() == VIEW_WEB))
-	{
-            return EV_MIS_Gray;
-        }
-
         return EV_MIS_ZERO;
 }
 
