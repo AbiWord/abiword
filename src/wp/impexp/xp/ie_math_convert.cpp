50a93f9ee4cd79347d776cea5645cbb3c5b0eb6a
Prashant Bafna <appu.bafna_gmail.com>
2012-07-17 11:31:50 +0000

    cosmetic changes to ie_math_convert
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2012math@31517 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_math_convert.cpp
+++ src/wp/impexp/xp/ie_math_convert.cpp
@@ -31,54 +31,56 @@ static xsltStylesheet * cur = NULL;
 
 bool convertMathMLtoLaTeX(const UT_UTF8String & sMathML, UT_UTF8String & sLaTeX)
 {
-	//static xsltStylesheet *cur = NULL;
-	xmlDocPtr doc, res;
-	xmlChar * pLatex = NULL;
-	int len;
-	
-	if (sMathML.empty())
-		// Nothing has failed, but we have nothing to do anyway
-		return false;
+    //static xsltStylesheet *cur = NULL;
+    xmlDocPtr doc, res;
+    xmlChar * pLatex = NULL;
+    int len;
+
+    if (sMathML.empty())
+        // Nothing has failed, but we have nothing to do anyway
+	return false;
+    if (!cur)
+    {
+	UT_UTF8String path(XAP_App::getApp()->getAbiSuiteLibDir());
+	path += "/xsltml/mmltex.xsl";
+			
+	cur = xsltParseStylesheetFile((const xmlChar *)(path.utf8_str()));
 	if (!cur)
 	{
-		UT_UTF8String path(XAP_App::getApp()->getAbiSuiteLibDir());
-		path += "/xsltml/mmltex.xsl";
-				
-		cur = xsltParseStylesheetFile((const xmlChar *)(path.utf8_str()));
-		if (!cur)
-		{
-			UT_DEBUGMSG(("convertMathMLtoLaTeX: Parsing stylesheet failed\n"));
-			return false;
-		}
+            UT_DEBUGMSG(("convertMathMLtoLaTeX: Parsing stylesheet failed\n"));
+	    return false;
 	}
-	// bad bad bad, apparently on MacOS X, the system libxml2 take a non-const here.
-	doc = xmlParseDoc((xmlChar*)(sMathML.utf8_str()));
-	if (!doc)
-	{
-		xxx_UT_DEBUGMSG(("convertMathMLtoLaTeX: Parsing MathML document failed\n"));
-		return false;
-	}	
+    }
 	
-	res = xsltApplyStylesheet(cur, doc, NULL);
-	if (!res)
-	{
-		xxx_UT_DEBUGMSG(("convertMathMLtoLaTeX: Applying stylesheet failed\n"));
-		xmlFreeDoc(doc);
-		return false;
-	}
+    // bad bad bad, apparently on MacOS X, the system libxml2 take a non-const here.
+    doc = xmlParseDoc((xmlChar*)(sMathML.utf8_str()));
+    if (!doc)
+    {
+        xxx_UT_DEBUGMSG(("convertMathMLtoLaTeX: Parsing MathML document failed\n"));
+	return false;
+    }	
 	
-	if (xsltSaveResultToString(&pLatex, &len, res, cur) != 0)
-	{
-		xmlFreeDoc(res);
-		xmlFreeDoc(doc);
-		return false;
-	}
-	sLaTeX.assign((const char*)pLatex, len);
+    res = xsltApplyStylesheet(cur, doc, NULL);
+    if (!res)
+    {
+        xxx_UT_DEBUGMSG(("convertMathMLtoLaTeX: Applying stylesheet failed\n"));
+	xmlFreeDoc(doc);
+	return false;
+    }
 	
-	g_free(pLatex);
+    if (xsltSaveResultToString(&pLatex, &len, res, cur) != 0)
+    {
 	xmlFreeDoc(res);
 	xmlFreeDoc(doc);
-	return true;
+	return false;
+    }
+    
+    sLaTeX.assign((const char*)pLatex, len);
+	
+    g_free(pLatex);
+    xmlFreeDoc(res);
+    xmlFreeDoc(doc);
+    return true;
 }
 
 // Function to convert the generated LaTeX to the equation form acceptable in the editor
@@ -86,37 +88,37 @@ bool convertMathMLtoLaTeX(const UT_UTF8String & sMathML, UT_UTF8String & sLaTeX)
 bool convertLaTeXtoEqn(const UT_UTF8String & sLaTeX,UT_UTF8String & eqnLaTeX)
 {
 
-	if (sLaTeX.empty())
-		return false;
+    if (sLaTeX.empty())
+	return false;
 
-	if(strcmp((sLaTeX.substr(1,2)).utf8_str(),"\\[")==0 && strcmp((sLaTeX.substr(sLaTeX.size()-2,2)).utf8_str(),"\\]")==0)
-	{
-		UT_sint32 i=3;
-		UT_sint32 j=sLaTeX.size()-3;
+    if(strcmp((sLaTeX.substr(1,2)).utf8_str(),"\\[")==0 && strcmp((sLaTeX.substr(sLaTeX.size()-2,2)).utf8_str(),"\\]")==0)
+    {
+        UT_sint32 i=3;
+	UT_sint32 j=sLaTeX.size()-3;
 
-		while((strcmp((sLaTeX.substr(i,1)).utf8_str(),"\n")==0 || strcmp((sLaTeX.substr(i,1)).utf8_str(),"\t")==0) && i< sLaTeX.size()-2)
-		{
-			i++;
-		}
-		while((strcmp((sLaTeX.substr(j,1)).utf8_str(),"\n")==0 || strcmp((sLaTeX.substr(j,1)).utf8_str(),"\t")==0) && j> 2)
-		{
-			j--;
-		}
+	while((strcmp((sLaTeX.substr(i,1)).utf8_str(),"\n")==0 || strcmp((sLaTeX.substr(i,1)).utf8_str(),"\t")==0) && i< sLaTeX.size()-2)
+	{
+	    i++;
+	}
+	while((strcmp((sLaTeX.substr(j,1)).utf8_str(),"\n")==0 || strcmp((sLaTeX.substr(j,1)).utf8_str(),"\t")==0) && j> 2)
+	{
+	    j--;
+	}
 
-		eqnLaTeX = sLaTeX.substr(i,(j-i)+1);
-		return true;
-	}
-	else if(strcmp((sLaTeX.substr(0,1)).utf8_str(),"$")==0 && strcmp((sLaTeX.substr(sLaTeX.size()-1,1)).utf8_str(),"$")==0)
-	{
-		eqnLaTeX = sLaTeX.substr(1,sLaTeX.size()-2);
-		return true;
-	}
-	else
-	{
-		// No Conversion Required
-		eqnLaTeX = sLaTeX;
-		return true;
-	}
+	eqnLaTeX = sLaTeX.substr(i,(j-i)+1);
+	return true;
+    }
+    else if(strcmp((sLaTeX.substr(0,1)).utf8_str(),"$")==0 && strcmp((sLaTeX.substr(sLaTeX.size()-1,1)).utf8_str(),"$")==0)
+    {
+	eqnLaTeX = sLaTeX.substr(1,sLaTeX.size()-2);
+	return true;
+    }
+    else
+    {
+	// No Conversion Required
+	eqnLaTeX = sLaTeX;
+	return true;
+    }
 
 }
 
@@ -141,8 +143,6 @@ bool convertOMMLtoMathML(const std::string & pOMML, std::string & pMathML)
         path+= "/omml_xslt/omml2mml.xsl";
         
         // TO DO : add the post build event to the msvc project to copy the omml_xslt folder from openxml plugin to debug/release, after the gsoc2012math branch is merged with trunk
-
-        //path = "../../plugins/openxml/common/omml_xslt/omml2mml.xsl";
 
         cur2 = xsltParseStylesheetFile((const xmlChar *)(path.c_str()));
         if(!cur2)
