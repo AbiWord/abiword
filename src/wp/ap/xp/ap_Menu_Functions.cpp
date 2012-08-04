3f09ecea99c1d1dfc83a2af89d9b0592c493ffb2
Ingo Brueckl <ib@wupperonline.de>
2012-08-04 20:38:24 +0000

    Fix bug with not functional revisions feature.
    The restriction to !pView->isMarkRevisions() seems to be a remnant from
    the time where this feature wasn't yet implemented (see r13380). There
    is no reason for the restriction.
    
    This closes bugzilla bug 11514.
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@31671 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/xp/ap_Menu_Functions.cpp
+++ src/wp/ap/xp/ap_Menu_Functions.cpp
@@ -1630,10 +1630,8 @@ Defun_EV_GetMenuItemState_Fn(ap_GetState_RevisionPresent)
 	ABIWORD_VIEW;
 	UT_return_val_if_fail (pView, EV_MIS_Gray);
 
-	if(pView->isMarkRevisions())
+	if(!pView->doesSelectionContainRevision())
 		return EV_MIS_Gray;
-    else if(!pView->doesSelectionContainRevision())
-        return EV_MIS_Gray;
 
 	return EV_MIS_ZERO;
 }
