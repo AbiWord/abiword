25946c4dee9bc3e2c0953c272fdf5b9899c596b1
Ingo Brueckl <ib@wupperonline.de>
2012-08-15 20:31:58 +0000

    Change the abi-update-all.sh script.
    Make option --stat the default for calling update.pl unless 'fuzzy' is
    given as argument (which will make the script behave as before then).
    
    This should generate correct stats for the translation web page now.
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@31745 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- po/abi-update-all.sh
+++ po/abi-update-all.sh
@@ -2,8 +2,13 @@
 
 ./update.pl --pot
 
+OPT=--stat
+test "$1" = "fuzzy" && OPT=--dist 
+
 for I in *.po;
 do
     I=`echo "$I" | cut -d . -f1`
-    ./update.pl --dist $I
+    ./update.pl $OPT $I
 done
+
+echo "Use 'abi-update-all.sh fuzzy' for fuzzy matching of msgmerge."
