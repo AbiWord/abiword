AbiWord/abiword

commit 39b79f5d7c81f29c4907af5f6bbae5baf83e1814

More warning fixes (non-Debug)
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@30427 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/af/xap/gtk/xap_UnixClipboard.cpp
@@ -282,6 +282,7 @@ bool XAP_UnixClipboard::_getDataFromFakeClipboard(T_AllowGet tFrom, const char**
 }
 
 
+#if DEBUG
 static void allTargets(GtkClipboard * /*clipboard*/,
                                                          GdkAtom *atoms,
                                                          gint n_atoms,
@@ -295,6 +296,7 @@ static void allTargets(GtkClipboard * /*clipboard*/,
       UT_DEBUGMSG((" Found Atom %s on clipboard \n", gdk_atom_name(Atom)));
   }
 }
+#endif
 
 bool XAP_UnixClipboard::_getDataFromServer(T_AllowGet tFrom, const char** formatList,
 										   void ** ppData, UT_uint32 * pLen,
@@ -305,7 +307,11 @@ bool XAP_UnixClipboard::_getDataFromServer(T_AllowGet tFrom, const char** format
 	  return false;
 
 	GtkClipboard * clipboard = gtkClipboardForTarget (tFrom);
+#if DEBUG
 	gtk_clipboard_request_targets(clipboard,( GtkClipboardTargetsReceivedFunc) allTargets, this);
+#else
+	gtk_clipboard_request_targets(clipboard, NULL, NULL);
+#endif
 	UT_GenericVector<GdkAtom> atoms ;
 	for(int atomCounter = 0; formatList[atomCounter]; atomCounter++)
 		atoms.addItem(gdk_atom_intern(formatList[atomCounter],FALSE));

src/af/ev/gtk/ev_UnixMenu.cpp
@@ -660,9 +660,9 @@ bool EV_UnixMenu::synthesizeMenu(GtkWidget * wMenuRoot, bool isPopup)
 #else
 
 	// make sure our last item on the stack is the one we started with
-	GtkWidget * wDbg = NULL;
-	wDbg = stack.top();
+	GtkWidget * wDbg = stack.top();
 	stack.pop();
+	UT_UNUSED(wDbg);
 	UT_ASSERT(wDbg == wMenuRoot);
 
 	// we also have to bind the top level window to our

src/af/xap/gtk/xap_UnixApp.cpp
@@ -76,6 +76,7 @@ XAP_UnixApp::XAP_UnixApp(const char * szAppName)
 	  m_szTmpFile(NULL)
 {
 	int fc_inited = FcInit();
+	UT_UNUSED(fc_inited); // TODO actually deal with the error here
 	UT_ASSERT(fc_inited);
 
 	_setAbiSuiteLibDir();

src/af/xap/xp/xap_App.cpp
@@ -368,9 +368,9 @@ bool XAP_App::initialize(const char * szKeyBindingsKey, const char * szKeyBindin
 	bool bResult;
 	bResult = m_pInputModes->createInputMode(szBindings,pBindingMap);
 	UT_ASSERT(bResult);
-	bool bResult2;
-	bResult2 = m_pInputModes->setCurrentMap(szBindings);
-	UT_ASSERT(bResult2);
+	bResult = m_pInputModes->setCurrentMap(szBindings);
+	UT_ASSERT(bResult);
+	UT_UNUSED(bResult); // TODO deal with the error
 
 	// check if the prefs are set to use specific graphics class
 	const char * pszGraphics = NULL;

src/af/xap/xp/xap_Frame.cpp
@@ -687,6 +687,7 @@ void XAP_FrameImpl::_createToolbars(void)
 		
 		m_vecToolbars.addItem(pToolbar);
 	}
+	UT_UNUSED(bResult); // TODO deal with the result
 }
 
 UT_sint32 XAP_Frame::findToolbarNr(EV_Toolbar * pTB)

src/af/xap/xp/xap_InputModes.cpp
@@ -59,7 +59,9 @@ bool XAP_InputModes::createInputMode(const char * szName,
 	b1 = (m_vecEventMaps.addItem(pEEM) == 0);
 	bool b2;
 	b2 = (m_vecNames.addItem(szDup) == 0);
-    UT_ASSERT(b1 && b2);
+	UT_ASSERT(b1 && b2);
+	UT_UNUSED(b1);
+	UT_UNUSED(b2);
 
 	return true;
 }

commit 2ee81dd2b3842d8991eb4e805992f4c67236b716

Fix crash with copy & paste.
Don't provide a NULL pointer as callback function.

It's a regression from r30427.

This closes bugzilla bug 13329.


git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@31748 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/af/xap/gtk/xap_UnixClipboard.cpp
@@ -309,8 +309,6 @@ bool XAP_UnixClipboard::_getDataFromServer(T_AllowGet tFrom, const char** format
 	GtkClipboard * clipboard = gtkClipboardForTarget (tFrom);
 #if DEBUG
 	gtk_clipboard_request_targets(clipboard,( GtkClipboardTargetsReceivedFunc) allTargets, this);
-#else
-	gtk_clipboard_request_targets(clipboard, NULL, NULL);
 #endif
 	UT_GenericVector<GdkAtom> atoms ;
 	for(int atomCounter = 0; formatList[atomCounter]; atomCounter++)