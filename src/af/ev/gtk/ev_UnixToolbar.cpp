1a3eddb9e42d169a084afbe39b7935fca93452b1
Ingo Brueckl <ib@wupperonline.de>
2012-09-05 14:50:55 +0000

    Cosmetic: Adjust indent.
    Additionally, add some blank lines.
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@31888 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/ev/gtk/ev_UnixToolbar.cpp
+++ src/af/ev/gtk/ev_UnixToolbar.cpp
@@ -1553,29 +1553,39 @@ bool EV_UnixToolbar::repopulateStyles(void)
 // Now make a new one.
 //
 	gint items = v->getItemCount();
-		GtkTreeIter iter;
-		GtkListStore *list = gtk_list_store_new(1, G_TYPE_STRING);
-		for (gint m=0; m < items; m++) {
-			const char * sz = v->getNthItem(m);
-			UT_UTF8String sLoc;
-				pt_PieceTable::s_getLocalisedStyleName(sz, sLoc);
-				sz = sLoc.utf8_str();
-			gtk_list_store_append(list, &iter);
-			gtk_list_store_set(list, &iter, 0, sz, -1);
-		}
-			GtkTreeSortable *sort;
-			sort = GTK_TREE_SORTABLE(list);
-			gtk_tree_sortable_set_sort_column_id(sort, 0, GTK_SORT_ASCENDING);
-		gboolean itering = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(list), &iter);
-		while (itering)
-		{
-			gchar *entry;
-			gtk_tree_model_get(GTK_TREE_MODEL(list), &iter, 0, &entry, -1);
-			gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo), entry);
-			g_free(entry);
-			itering = gtk_tree_model_iter_next(GTK_TREE_MODEL(list), &iter);
-		}
-		g_object_unref(G_OBJECT(list));
+
+	GtkTreeIter iter;
+	GtkListStore *list = gtk_list_store_new(1, G_TYPE_STRING);
+
+	for (gint m=0; m < items; m++) {
+		UT_UTF8String sLoc;
+		const char * sz = v->getNthItem(m);
+
+		pt_PieceTable::s_getLocalisedStyleName(sz, sLoc);
+		sz = sLoc.utf8_str();
+		gtk_list_store_append(list, &iter);
+		gtk_list_store_set(list, &iter, 0, sz, -1);
+	}
+
+	GtkTreeSortable *sort;
+	sort = GTK_TREE_SORTABLE(list);
+	gtk_tree_sortable_set_sort_column_id(sort, 0, GTK_SORT_ASCENDING);
+
+	gboolean itering = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(list), &iter);
+
+	while (itering)
+	{
+		gchar *entry;
+
+		gtk_tree_model_get(GTK_TREE_MODEL(list), &iter, 0, &entry, -1);
+		gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo), entry);
+
+		g_free(entry);
+
+		itering = gtk_tree_model_iter_next(GTK_TREE_MODEL(list), &iter);
+	}
+
+	g_object_unref(G_OBJECT(list));    
 
 	wd->m_blockSignal = wasBlocked;
 
