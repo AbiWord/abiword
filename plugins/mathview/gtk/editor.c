3eb62bcee35ffae74619065b153e963c33f597cd
Abhishek Ravi <abhishekravi1992@gmail.com>
2013-08-17 18:03:27 +0000

    Symbol picker for advanced editor
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/gsoc2013lasem@33381 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/mathview/gtk/editor.c
+++ plugins/mathview/gtk/editor.c
@@ -1,73 +1,240 @@
 #include <gtk/gtk.h>
+#include <glib.h>
 
+typedef enum {GREEK,MATH,OTHER} Category; //to be expanded
 
+struct SymbolButton
+{
+	gchar* unicode;
+	gchar* itex;
+	gchar* tooltip_text;
+	Category cat;
+	GtkWidget *button;
+};
+
+typedef struct 
+{
+	GtkWidget  *view;
+	GtkTextBuffer *buffer;
+	struct SymbolButton *s;
+}data;
+
+data d;
 void insert_into_doc(GtkWidget *widget, gpointer view)
 {
-  
+
 }
 
-void insert_symbol(GtkWidget *widget, gpointer view)
+void set_data(GtkWidget *widget, gpointer d1)
 {
-  gtk_entry_append_text(GTK_ENTRY(view),"\\alpha");
+	d.s = d1;
+}
+
+
+void insert_symbol(GtkWidget *widget, data *d)
+{
+	gtk_text_buffer_insert_at_cursor(d->buffer,d->s->itex,-1);
 }
 
 void close_window(GtkWidget *widget)
 {
-  gtk_main_quit();
+	gtk_main_quit();
 }
+
+gboolean is_new_category(gchar* line)
+{
+	if(strncmp(line,"GREEK",5)==0)
+		return TRUE;
+	if(strncmp(line,"MATH",4)==0)
+		return TRUE;
+	if(strncmp(line,"OTHER",5)==0)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+Category get_category(gchar* line)
+{
+	if(strncmp(line,"GREEK",5)==0)
+		return GREEK;
+	if(strncmp(line,"MATH",4)==0)
+		return MATH;
+	if(strncmp(line,"OTHER",5)==0)
+		return OTHER;
+	else
+		return OTHER;
+}
+
+char *category_heading(Category c)
+{
+    char *headings[] = {"Greek Letters","Math symbols","Others" };
+    return headings[c];
+}
+
+
+void load_list(GPtrArray *list)
+{
+	char filename[] = "symbols.txt";
+	char ** toks;
+	const char delim[] = ";";
+  	Category current=OTHER;
+	FILE *file = fopen ( filename, "r" );
+	 if (file != NULL) 
+  	 {
+   	 	char line [1000];
+		gchar* copy;
+   	 	while(fgets(line,sizeof line,file)!= NULL) 
+   	 	{	
+			copy = g_strdup(line);
+			toks = strsep(&copy,"|");
+			
+			if(is_new_category(line))
+				current = get_category(line);
+			else
+			{
+				struct SymbolButton *s = g_new0(struct SymbolButton, 1);
+						
+				//gchar* a = "\u2135";
+				//printf("%s %s Test : %d\n",a,toks, strncmp(a,toks,1));
+			
+				gchar *label_name = g_strdup((gchar*)toks);
+				toks++;
+				toks=strsep(&toks,";");
+				s->button = gtk_button_new_with_label(label_name);			
+				s->itex = g_strdup((gchar*)toks);						
+				s->cat = current;
+			
+				gtk_widget_set_size_request(s->button, 30, 30);
+				g_ptr_array_add(list, s);
+}
+  	 	}
+
+  		fclose(file);
+  	}
+  	else
+  	{
+   		 perror(filename);
+  	}
+}
+
 
 int main(int argc, char** argv) 
 {
 
-  GtkWidget *view;
-  GtkWidget *window;
-  GtkWidget *frame;
-  GtkWidget *alpha;
-  GtkWidget *insert;
-  GtkWidget *close;
+	GtkWidget *view;
+	GtkWidget *window;
+	GtkWidget *frame;
+	GtkWidget *latex_label;
+	GtkWidget *insert;
+	GtkWidget *close;
+	GtkWidget *notebook;
+	GtkWidget *label;
+	GtkWidget *grid;		
 
-  gtk_init(&argc, &argv);
+	GtkTextBuffer *buffer;
+  	GtkTextIter start, end;
+  	GtkTextIter iter;
+	
+	GPtrArray *list;
 
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-  gtk_window_set_default_size(GTK_WINDOW(window), 500, 360);
-  gtk_window_set_title(GTK_WINDOW(window), "Advanced Editor");
+	gtk_init(&argc, &argv);
 
-  frame = gtk_fixed_new();
-  gtk_container_add(GTK_CONTAINER(window), frame);
-
-
-  alpha = gtk_button_new_with_label("a");
-  gtk_widget_set_size_request(alpha, 40, 40);
-  gtk_fixed_put(GTK_FIXED(frame), alpha, 60, 80);
-
-  insert = gtk_button_new_with_label("Insert");
-  gtk_widget_set_size_request(insert, 80, 35);
-  gtk_fixed_put(GTK_FIXED(frame), insert, 50, 300);
-
-  close = gtk_button_new_with_label("Close");
-  gtk_widget_set_size_request(close, 80, 35);
-  gtk_fixed_put(GTK_FIXED(frame), close, 240, 300);
-
-  view = gtk_entry_new ();
-  gtk_fixed_put(GTK_FIXED(frame), view, 50, 50); 
-
-  gtk_widget_show_all(window);
-
-  g_signal_connect(window, "destroy",
-      G_CALLBACK (gtk_main_quit), NULL);
-
-  g_signal_connect(alpha, "clicked", 
-      G_CALLBACK(insert_symbol), view);
-
-  g_signal_connect(insert, "clicked", 
-      G_CALLBACK(insert_into_doc), view);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_window_set_default_size(GTK_WINDOW(window), 500, 360);
+	gtk_window_set_title(GTK_WINDOW(window), "Advanced Editor");
 
 
-  g_signal_connect(close, "clicked", 
-      G_CALLBACK(close_window), view);
+	frame = gtk_fixed_new();
+	gtk_container_add(GTK_CONTAINER(window), frame);
 
-  gtk_main();
+	list =  g_ptr_array_new ();  
+            
+	//Load entities
+	load_list(list);
 
-  return 0;
+	latex_label = gtk_label_new("Latex Equation :");
+	gtk_widget_set_size_request(latex_label, 100, 20);
+	gtk_fixed_put(GTK_FIXED(frame), latex_label, 20, 20);
+
+	insert = gtk_button_new_with_label("Insert");
+	gtk_widget_set_size_request(insert, 80, 35);
+	gtk_fixed_put(GTK_FIXED(frame), insert, 50, 300);
+
+	close = gtk_button_new_with_label("Close");
+	gtk_widget_set_size_request(close, 80, 35);
+	gtk_fixed_put(GTK_FIXED(frame), close, 240, 300);
+	
+
+	view = gtk_text_view_new();
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (view), GTK_WRAP_WORD);
+	
+	GtkWidget* scrolledwindow = gtk_scrolled_window_new(NULL, NULL);
+	gtk_container_set_border_width (GTK_CONTAINER (scrolledwindow), 5);
+   	gtk_scrolled_window_set_policy((GtkScrolledWindow*)scrolledwindow,GTK_POLICY_NEVER,GTK_POLICY_AUTOMATIC);
+	gtk_container_add(GTK_CONTAINER(scrolledwindow), view);
+	
+
+	gtk_fixed_put(GTK_FIXED(frame), scrolledwindow, 30, 50);
+	gtk_widget_set_size_request(scrolledwindow,450,80);
+	buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+
+
+
+	d.view = view; 
+	d.buffer = buffer;
+	notebook = gtk_notebook_new ();
+	gtk_notebook_set_tab_pos (GTK_NOTEBOOK (notebook), GTK_POS_TOP);
+	gtk_fixed_put(GTK_FIXED(frame), notebook, 20, 140);
+
+	int j=0,pos=0;
+	Category prev=GREEK;
+
+	grid = gtk_grid_new();
+	for(j=0;j<5;j++)
+	{
+		struct SymbolButton *s = g_ptr_array_index(list,j);
+		if(s->cat!=prev)
+		{	
+			//display the current grid
+			label = gtk_label_new (category_heading(prev));
+			gtk_widget_show (grid);
+			gtk_notebook_append_page (GTK_NOTEBOOK (notebook), grid, label);			
+			//New category elements in new grid/page			
+			grid = gtk_grid_new();
+			prev = s->cat;
+			pos=0;
+		}
+		gtk_widget_set_size_request(s->button, 30, 30);
+		gtk_grid_attach(GTK_GRID (grid),s->button,pos++,0,1,1);
+	}	
+	//display last category
+	label = gtk_label_new (category_heading(prev));
+	gtk_widget_show (grid);
+	gtk_notebook_append_page (GTK_NOTEBOOK (notebook), grid, label);			
+
+	gtk_widget_show_all(window);
+
+	g_signal_connect(window, "destroy",
+			G_CALLBACK (gtk_main_quit), NULL);
+
+	g_signal_connect(insert, "clicked", 
+			G_CALLBACK(insert_into_doc), view);
+
+	g_signal_connect(close, "clicked", 
+			G_CALLBACK(close_window), view);
+	
+
+	gint i;
+	for(i=0;i<5;i++)
+	{
+		struct SymbolButton *s = g_ptr_array_index(list,i);
+		g_signal_connect(s->button, "clicked", 
+				G_CALLBACK(set_data), s);
+		g_signal_connect(s->button, "clicked", 
+				G_CALLBACK(insert_symbol), &d);
+	}
+	
+	gtk_main();
+	return 0;
 }
--- plugins/mathview/plugin.m4
+++ plugins/mathview/plugin.m4
@@ -1,27 +1,30 @@
-lasem
-dnl Only 0.4.10, or later will work
-for ver in 0.6 0.4 ; do
-  if test "x$lasem" = x; then
-    if pkg-config --exists lasem-$ver; then
-      lasem=lasem-$ver
+lasem=
+
+if test "$enable_mathview" != ""; then
+  dnl Only 0.4.1, or later will work
+  for ver in 0.6 0.4 ; do
+    if test "x$lasem" = x; then
+      if pkg-config --exists lasem-$ver; then
+        lasem="lasem-$ver"
+      fi
     fi
+  done
+  if test "x$lasem" = x; then
+    test "$enable_mathview" = "auto" && AC_MSG_WARN([mathview plugin: dependencies not satisfied - $mathview_pkgs])
   fi
-done
-if test "x$lasem" = x; then
-  # Not important.  Things will fail below.
-  lasem=lasem-0.4
 fi
-mathview_pkgs='$lasem-0.4 >= 0.4.1'
-AC_CHECK_FUNCS(lsm)
+mathview_pkgs="$lasem >= 0.4.1"
+
+if test "$enable_mathview" = "yes" || \
+   test "x$lasem" != x; then
+
+if test "$enable_mathview_builtin" = "yes"; then
+AC_MSG_ERROR([mathview plugin: static linking not supported])
+fi
+
 PKG_CHECK_MODULES(MATHVIEW,[ $mathview_pkgs ])
 
 test "$enable_mathview" = "auto" && PLUGINS="$PLUGINS mathview"
-
-MATHVIEW_CFLAGS="$MATHVIEW_CFLAGS"'${PLUGIN_CFLAGS}'
-MATHVIEW_LIBS="$MATHVIEW_LIBS "'${PLUGIN_LIBS}'
-
-AC_SUBST([MATHVIEW_CFLAGS])
-AC_SUBST([MATHVIEW_LIBS])
 
 dnl check for lsm_itex_to_mathml()
 saved_CFLAGS=$CFLAGS
@@ -32,63 +35,12 @@ AC_CHECK_FUNCS(lsm_itex_to_mathml)
 CFLAGS=$saved_CFLAGS
 LIBS=$saved_CLIBS
 
-
-
-
-mathview_deps="no"
-
-
-# test hashmap availablity
-HASHMAP_CFLAGS=""
-AC_LANG(C++)
-AC_CHECK_HEADER(hash_map,
-[
-	HASHMAP_CFLAGS="-DHAVE_HASH_MAP"
-], [
-	AC_CHECK_HEADER(ext/hash_map,
-	[
-		HASHMAP_CFLAGS="-DHAVE_EXT_HASH_MAP"
-	], [
-		AC_MSG_WARN([mathview plugin: dependencies not satisfied - missing 'hash_map' or 'ext/hash_map' header])
-	])
-])
-AC_LANG(C)
-
-if test "$HASHMAP_CFLAGS" != ""; then
-
-if test "$enable_mathview" != ""; then
-
-PKG_CHECK_EXISTS([ $mathview_pkgs ], 
-[
-	mathview_deps="yes"
-], [
-	test "$enable_mathview" = "auto" && AC_MSG_WARN([mathview plugin: dependencies not satisfied - $mathview_pkgs])
-])
-
-fi
-
-if test "$enable_mathview" = "yes" || \
-   test "$mathview_deps" = "yes"; then
-
-if test "$enable_mathview_builtin" = "yes"; then
-AC_MSG_ERROR([mathview plugin: static linking not supported])
-fi
-
-PKG_CHECK_MODULES(MATHVIEW,[ $mathview_pkgs ])
-
-test "$enable_mathview" = "auto" && PLUGINS="$PLUGINS mathview"
-
-MATHVIEW_CFLAGS="$MATHVIEW_CFLAGS $HASHMAP_CFLAGS "'${PLUGIN_CFLAGS}'
+MATHVIEW_CFLAGS="$MATHVIEW_CFLAGS "'${PLUGIN_CFLAGS}'
 MATHVIEW_LIBS="$MATHVIEW_LIBS "'${PLUGIN_LIBS}'
 
 fi
 
-fi
-
-# need to unconditionally test, for `make distcheck'
-AM_PROG_LEX
-AC_PROG_YACC
-
 AC_SUBST([MATHVIEW_CFLAGS])
 AC_SUBST([MATHVIEW_LIBS])
+
 
