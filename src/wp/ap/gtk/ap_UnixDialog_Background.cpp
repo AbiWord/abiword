2098e921caef34abf728a3b12f804d63e43114cf
Jean Brefort <jean.brefort@normalesup.org>
2014-05-02 14:50:09 +0000

    Reversed previous patch
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@34395 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/gtk/ap_UnixDialog_Background.cpp
+++ src/wp/ap/gtk/ap_UnixDialog_Background.cpp
@@ -50,9 +50,6 @@ static void s_color_cleared(GtkWidget * /*btn*/, AP_UnixDialog_Background * dlg)
 }
 
 static void s_color_changed(GtkWidget * csel,
-#if GTK_CHECK_VERSION(3,4,0)
-                GdkRGBA *,
-#endif
 			    AP_UnixDialog_Background * dlg)
 {
 	UT_ASSERT(csel && dlg);
