155b08171fe873aec85b959867d3c5a740241c71
Hubert Figuiere <hub@figuiere.net>
2014-11-16 00:21:48 +0000

    Gtk background dialog: Remove GtkAlignment which is deprecated. Gtk background dialog: Bump Gtk requirement to 3.4. Use GtkColorChooser
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35022 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/gtk/ap_UnixDialog_Background.cpp
+++ src/wp/ap/gtk/ap_UnixDialog_Background.cpp
@@ -24,7 +24,6 @@
 // This header defines some functions for Unix dialogs,
 // like centering them, measuring them, etc.
 #include "xap_UnixDialogHelper.h"
-#include "xap_Gtk2Compat.h"
 
 #include "xap_App.h"
 #include "xap_UnixApp.h"
@@ -50,18 +49,12 @@ static void s_color_cleared(GtkWidget * /*btn*/, AP_UnixDialog_Background * dlg)
 }
 
 static void s_color_changed(GtkWidget * csel,
-#if GTK_CHECK_VERSION(3,4,0)
-                GdkRGBA *,
-#endif
+                            GdkRGBA *color,
 			    AP_UnixDialog_Background * dlg)
 {
 	UT_ASSERT(csel && dlg);
-  
-	GtkColorChooser * w = GTK_COLOR_CHOOSER(csel);
-	GdkRGBA cur;
 
-	gtk_color_chooser_get_rgba (w, &cur);
-	UT_RGBColor * rgbcolor = UT_UnixGdkColorToRGBColor(cur);
+	UT_RGBColor * rgbcolor = UT_UnixGdkColorToRGBColor(*color);
 	UT_HashColor hash_color;
 	dlg->setColor (hash_color.setColor(*rgbcolor) + 1); // return with # prefix
 	delete rgbcolor;
@@ -112,7 +105,7 @@ GtkWidget * AP_UnixDialog_Background::_constructWindow (void)
 
 	const XAP_StringSet * pSS = m_pApp->getStringSet();
 	std::string s;
-	
+
 	if(isForeground())
 	{
 		pSS->getValueUTF8(AP_STRING_ID_DLG_Background_TitleFore,s);
@@ -151,9 +144,6 @@ void AP_UnixDialog_Background::_constructWindowContents (GtkWidget * parent)
 	gtk_container_add (GTK_CONTAINER(parent), vbox);
 
 	colorsel = gtk_color_chooser_widget_new();
-#if !GTK_CHECK_VERSION(3,4,0)
-	gtk_color_selection_set_has_palette (GTK_COLOR_SELECTION (colorsel), TRUE);
-#endif
 	gtk_color_chooser_set_use_alpha(GTK_COLOR_CHOOSER(colorsel), false);
 	gtk_widget_show (colorsel);
 	gtk_container_add (GTK_CONTAINER(vbox), colorsel);
@@ -173,40 +163,33 @@ void AP_UnixDialog_Background::_constructWindowContents (GtkWidget * parent)
 //
 // Button to clear background color
 //
-	GtkWidget * alignment = NULL;
 	GtkWidget * clearColor = NULL;
 	std::string s;
-	
+
 	if(!isForeground())
 	{
 		const XAP_StringSet * pSS = m_pApp->getStringSet();
 		if(isHighlight())
 		{
 			pSS->getValueUTF8 (AP_STRING_ID_DLG_Background_ClearHighlight,s);
-			clearColor = gtk_button_new_with_label (s.c_str());
 		}
 		else
 		{
 			pSS->getValueUTF8 (AP_STRING_ID_DLG_Background_ClearClr,s);
-			clearColor = gtk_button_new_with_label (s.c_str());
 		}
-		gtk_widget_show(clearColor);
-
-		alignment = gtk_alignment_new (1.0, 0.5, 0.0, 0.0);
-		gtk_widget_show(alignment);
-		gtk_container_add (GTK_CONTAINER (alignment), clearColor);
-		gtk_box_pack_start (GTK_BOX (vbox), alignment, FALSE, FALSE, 0);
-
+		clearColor
+		  = gtk_widget_new(GTK_TYPE_BUTTON, "label", s.c_str(),
+				   "xalign", 1.0, "yalign", 0.5, NULL);
+		GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+		gtk_box_pack_end(GTK_BOX(hbox), clearColor, FALSE, FALSE, 0);
+		gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
 		g_signal_connect(G_OBJECT(clearColor), "clicked",
 						G_CALLBACK(s_color_cleared),
 						(gpointer) this);
+		gtk_widget_show_all(vbox);
 	}
 	g_signal_connect (G_OBJECT(colorsel),
-#if GTK_CHECK_VERSION(3,4,0)
 			  "color-activated",
-#else
-			  "color-changed",
-#endif
 			  G_CALLBACK(s_color_changed),
 			  (gpointer) this);
 }
@@ -225,17 +208,10 @@ void AP_UnixDialog_Background::colorCleared(void)
 {
 	setColor(NULL);
 	GdkRGBA gcolor;
-#if !GTK_CHECK_VERSION(3,0,0)
-	gcolor.pixel = 0;
-	gcolor.red = 0xffff;
-	gcolor.blue = 0xffff;
-	gcolor.green = 0xffff;
-#else
 	gcolor.red = 1.0;
 	gcolor.blue = 1.0;
 	gcolor.green = 1.0;
 	gcolor.alpha = 1.0;
-#endif
 	gtk_color_chooser_set_rgba (GTK_COLOR_CHOOSER(m_wColorsel),
                                     &gcolor);
 }
