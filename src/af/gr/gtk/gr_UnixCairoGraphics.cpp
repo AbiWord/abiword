63d81d04e5eaecfd5df3378fcc5a83a60697ef55
Hubert Figuiere <hub@figuiere.net>
2014-11-16 01:07:59 +0000

    Gtk Graphics: fix a PangoFontDesc memory leak introduced in ABI-3-0-0-STABLE@34954
    Cherry-pick from trunk@35024.
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35025 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/gr/gtk/gr_UnixCairoGraphics.cpp
+++ src/af/gr/gtk/gr_UnixCairoGraphics.cpp
@@ -241,7 +241,7 @@ GR_Font * GR_UnixCairoGraphics::getGUIFont(void)
 		gtk_widget_path_append_type (path, GTK_TYPE_WINDOW);
 		gtk_style_context_set_path(tempCtxt, path);
 		gtk_widget_path_free(path);
-		const PangoFontDescription* fontDesc;
+		PangoFontDescription* fontDesc;
 		gtk_style_context_get(tempCtxt, GTK_STATE_FLAG_NORMAL, "font", &fontDesc, NULL);
 		const char *guiFontName = pango_font_description_get_family(fontDesc);
 #else
@@ -265,6 +265,7 @@ GR_Font * GR_UnixCairoGraphics::getGUIFont(void)
 		m_pPFontGUI = new GR_PangoFont(guiFontName, 11.0, this, s.utf8_str(), true);
 
 #if GTK_CHECK_VERSION(3,0,0)
+		pango_font_description_free(fontDesc);
 		g_object_unref(G_OBJECT(tempCtxt));
 #else
 		g_object_unref(G_OBJECT(tempStyle));
