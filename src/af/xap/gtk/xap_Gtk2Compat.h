ced182aaa0971fc60f31a161fac67b5344cdc8a4
Hubert Figuiere <hub@figuiere.net>
2014-11-16 06:19:56 +0000

    Bug 13568 - Remove Gtk2 compatibility helpers.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35030 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/gtk/xap_Gtk2Compat.h
+++ src/af/xap/gtk/xap_Gtk2Compat.h
@@ -62,295 +62,6 @@
 #endif
 
 
-// Gtk2 compatibility for GSEAL
-
-#if !GTK_CHECK_VERSION(2,14,0)
-inline GdkWindow* gtk_widget_get_window(GtkWidget* w)
-{
-  return w->window;
-}
-
-inline const guchar *gtk_selection_data_get_data(const GtkSelectionData *s)
-{
-	return s->data;
-}
-
-inline GdkAtom gtk_selection_data_get_target(const GtkSelectionData *s)
-{
-  return s->target;
-}
-
-inline gint gtk_selection_data_get_length(const GtkSelectionData *s)
-{
-  return s->length;
-}
-
-inline GtkWidget* gtk_dialog_get_action_area(GtkDialog* dialog)
-{
-  return dialog->action_area;
-}
-
-inline GtkWidget* gtk_dialog_get_content_area(GtkDialog* dialog)
-{
-  return dialog->vbox;
-}
-
-inline GtkWidget* gtk_color_selection_dialog_get_color_selection(GtkColorSelectionDialog* d)
-{
-  return d->colorsel;
-}
-
-inline void gtk_adjustment_configure(GtkAdjustment *adjustment,
-				     gdouble value, gdouble lower,
-				     gdouble upper, gdouble step_increment,
-				     gdouble page_increment, gdouble page_size)
-{
-  adjustment->value = value;
-  adjustment->lower = lower;
-  adjustment->upper = upper;
-  adjustment->step_increment = step_increment;
-  adjustment->page_increment = page_increment;
-  adjustment->page_size = page_size;
-  g_signal_emit_by_name(G_OBJECT(adjustment), "changed");
-}
-#endif
-
-#if !GTK_CHECK_VERSION(2,18,0)
-inline void gtk_widget_get_allocation(GtkWidget* widget, GtkAllocation* alloc)
-{
-  *alloc = widget->allocation;
-}
-
-inline gboolean gtk_widget_has_focus(GtkWidget* widget)
-{
-  return GTK_WIDGET_HAS_FOCUS(widget);
-}
-
-inline gboolean gtk_widget_has_grab(GtkWidget* widget)
-{
-  return GTK_WIDGET_HAS_GRAB(widget);
-}
-
-inline gboolean gtk_widget_is_toplevel(GtkWidget* widget)
-{
-  return GTK_WIDGET_TOPLEVEL(widget);
-}
-
-inline gboolean gtk_widget_get_double_buffered(GtkWidget* widget)
-{
-  return GTK_WIDGET_DOUBLE_BUFFERED(widget);
-}
-
-inline void gtk_widget_set_can_default(GtkWidget* w, gboolean can_default)
-{
-  if (can_default) {
-    GTK_WIDGET_SET_FLAGS(w, GTK_CAN_DEFAULT);
-  }
-  else {
-    GTK_WIDGET_UNSET_FLAGS(w, GTK_CAN_DEFAULT);
-  }
-}
-
-inline void gtk_widget_set_can_focus(GtkWidget* w, gboolean can_focus)
-{
-  if (can_focus) {
-    GTK_WIDGET_SET_FLAGS(w, GTK_CAN_FOCUS);
-  }
-  else {
-    GTK_WIDGET_UNSET_FLAGS(w, GTK_CAN_FOCUS);
-  }
-}
-
-inline gboolean gtk_widget_get_sensitive(GtkWidget *widget)
-{
-  return GTK_WIDGET_SENSITIVE(widget);
-}
-
-inline gboolean gtk_widget_get_visible(GtkWidget *widget)
-{
-  return GTK_WIDGET_VISIBLE(widget);
-}
-
-inline void gtk_widget_set_visible(GtkWidget *widget, gboolean visible)
-{
-  visible ? gtk_widget_show(widget) : gtk_widget_hide(widget);
-}
-#endif
-
-#if !GTK_CHECK_VERSION(2,20,0)
-inline void gtk_widget_get_requisition(GtkWidget* widget, GtkRequisition* requisition)
-{
-  *requisition = widget->requisition;
-}
-
-inline gboolean gtk_widget_get_mapped(GtkWidget* w)
-{
-  return GTK_WIDGET_MAPPED(w);
-}
-
-inline gboolean gtk_widget_get_realized(GtkWidget* w)
-{
-  return GTK_WIDGET_REALIZED(w);
-}
-#endif
-
-#if !GTK_CHECK_VERSION(2,24,0)
-
-#define GTK_COMBO_BOX_TEXT GTK_COMBO_BOX
-#define GtkComboBoxText    GtkComboBox
-
-inline GtkWidget* gtk_combo_box_text_new()
-{
-  return gtk_combo_box_new_text();
-}
-
-inline GtkWidget* gtk_combo_box_text_new_with_entry()
-{
-  return gtk_combo_box_entry_new_text();
-}
-
-inline void gtk_combo_box_text_append_text(GtkComboBox* combo, const gchar* text)
-{
-  gtk_combo_box_append_text(combo, text);
-}
-
-inline gchar* gtk_combo_box_text_get_active_text(GtkComboBox* combo)
-{
-  return gtk_combo_box_get_active_text(combo);
-}
-
-inline void gtk_combo_box_text_remove(GtkComboBox* combo, gint position)
-{
-  gtk_combo_box_remove_text(combo, position);
-}
-
-inline GtkWidget * gtk_combo_box_new_with_model_and_entry(GtkTreeModel *model)
-{
-  return gtk_combo_box_entry_new_with_model(model,0);
-}
-
-inline GdkDisplay * gdk_window_get_display (GdkWindow *window)
-{
-  g_return_val_if_fail (GDK_IS_WINDOW (window), NULL);
-  return gdk_drawable_get_display (GDK_DRAWABLE (window));
-}
-#endif
-
-#if !GTK_CHECK_VERSION(3,0,0)
-
-// UGLY
-typedef GdkColor GdkRGBA;
-
-
-inline GtkWidget* gtk_color_chooser_dialog_new(const gchar* title,
-					       GtkWindow* parent)
-{
-  GtkWidget* w = gtk_color_selection_dialog_new (title);
-  if(parent)
-    gtk_window_set_transient_for(GTK_WINDOW(w), parent);
-  return w;
-}
-
-// in Gtk 3 we use GtkRGBA.
-inline void gtk_color_selection_get_current_rgba (GtkColorSelection *colorsel,
-                                                  GdkColor *color)
-{
-  gtk_color_selection_get_current_color(colorsel, color);
-}
-
-inline void gtk_color_selection_set_current_rgba (GtkColorSelection *colorsel,
-                                                  GdkColor *color)
-{
-  gtk_color_selection_set_current_color(colorsel, color);
-}
-
-inline void gdk_rgba_free(GdkColor* color)
-{
-  gdk_color_free(color);
-}
-
-inline void gtk_color_button_set_rgba(GtkColorButton* button,
-				     const GdkColor* color)
-{
-  gtk_color_button_set_color(button, color);
-}
-
-inline void gtk_color_button_get_rgba(GtkColorButton* button,
-				     GdkColor* color)
-{
-  gtk_color_button_get_color(button, color);
-}
-
-inline GtkWidget* gtk_box_new(GtkOrientation orientation, gint spacing)
-{
-  if(orientation == GTK_ORIENTATION_VERTICAL) {
-    return gtk_vbox_new(FALSE, spacing);
-  }
-  return gtk_hbox_new(FALSE, spacing);
-}
-
-inline GtkWidget* gtk_scrollbar_new(GtkOrientation orientation, GtkAdjustment* adj)
-{
-  if(orientation == GTK_ORIENTATION_VERTICAL) {
-    return gtk_vscrollbar_new(adj);
-  }
-  return gtk_hscrollbar_new(adj);
-}
-
-inline GtkWidget* gtk_separator_new(GtkOrientation orientation)
-{
-  if(orientation == GTK_ORIENTATION_VERTICAL) {
-    return gtk_vseparator_new();
-  }
-  return gtk_hseparator_new();
-}
-
-inline void gtk_combo_box_text_remove_all(GtkComboBoxText *combo_box)
-{
-  gtk_list_store_clear(GTK_LIST_STORE(gtk_combo_box_get_model(GTK_COMBO_BOX(combo_box))));
-}
-
-inline void gtk_widget_get_preferred_size(GtkWidget *widget,
-					  GtkRequisition *minimum_size,
-					  GtkRequisition * /*natural_size*/)
-{
-	return gtk_widget_size_request(widget, minimum_size);
-}
-
-#endif
-
-#if !GTK_CHECK_VERSION(3,4,0)
-
-#define GTK_COLOR_CHOOSER GTK_COLOR_SELECTION
-
-typedef GtkColorSelection GtkColorChooserWidget;
-typedef GtkColorSelection GtkColorChooser;
-
-inline GtkWidget* gtk_color_chooser_widget_new()
-{
-  return gtk_color_selection_new();
-}
-
-inline void gtk_color_chooser_set_use_alpha(GtkColorSelection* chooser,
-					    gboolean opacity)
-{
-  gtk_color_selection_set_has_opacity_control(chooser,opacity);
-}
-
-inline void gtk_color_chooser_get_rgba(GtkColorSelection *chooser,
-				       GdkRGBA *color)
-{
-  gtk_color_selection_get_current_rgba (chooser, color);
-}
-
-inline void gtk_color_chooser_set_rgba(GtkColorSelection *chooser,
-				       GdkRGBA *color)
-{
-  gtk_color_selection_set_current_rgba (chooser, color);
-}
-
-#endif
-
 #if GTK_CHECK_VERSION(3,0,0)
 
 typedef GdkWindow GdkDrawable;
@@ -362,22 +73,14 @@ typedef GdkWindow GdkDrawable;
 inline void XAP_gtk_color_button_set_rgba(GtkColorButton* colorbtn,
 				     const GdkRGBA* color)
 {
-#if GTK_CHECK_VERSION(3,4,0)
   gtk_color_chooser_set_rgba(GTK_COLOR_CHOOSER(colorbtn), color);
-#else
-  gtk_color_button_set_rgba (colorbtn, color);
-#endif
 }
 
 // wrapper because gtk_color_button_get_rgba is deprecated.
 inline void XAP_gtk_color_button_get_rgba(GtkColorButton* colorbtn,
 					  GdkRGBA* color)
 {
-#if GTK_CHECK_VERSION(3,4,0)
   gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(colorbtn), color);
-#else
-  gtk_color_button_get_rgba (colorbtn, color);
-#endif
 }
 
 
@@ -388,7 +91,6 @@ GdkGrabStatus XAP_gdk_keyboard_grab(GdkWindow *window,
 				    gboolean owner_events,
 				    guint32 time_)
 {
-#if GTK_CHECK_VERSION(3,0,0)
 	GdkDeviceManager *manager
 		= gdk_display_get_device_manager(gdk_display_get_default());
 	GdkDevice *device = gdk_device_manager_get_client_pointer (manager);
@@ -400,9 +102,6 @@ GdkGrabStatus XAP_gdk_keyboard_grab(GdkWindow *window,
 				GDK_ALL_EVENTS_MASK,
 				NULL,
 				time_);
-#else
-	return gdk_keyboard_grab(window, owner_events, time_);
-#endif
 }
 
 inline
@@ -413,7 +112,6 @@ GdkGrabStatus XAP_gdk_pointer_grab(GdkWindow *window,
 				   GdkCursor *cursor,
 				   guint32 time_)
 {
-#if GTK_CHECK_VERSION(3,0,0)
 	GdkDeviceManager *manager
 		= gdk_display_get_device_manager(gdk_display_get_default());
 	GdkDevice *device = gdk_device_manager_get_client_pointer (manager);
@@ -422,10 +120,6 @@ GdkGrabStatus XAP_gdk_pointer_grab(GdkWindow *window,
 				GDK_OWNERSHIP_WINDOW,
 				owner_events, event_mask,
 				cursor, time_);
-#else
-	return gdk_pointer_grab(window, owner_events, event_mask,
-				confine_to, cursor, time_);
-#endif
 }
 
 
@@ -433,28 +127,20 @@ GdkGrabStatus XAP_gdk_pointer_grab(GdkWindow *window,
 inline
 void XAP_gdk_keyboard_ungrab(guint32 t)
 {
-#if GTK_CHECK_VERSION(3,0,0)
 	GdkDeviceManager *manager
 		= gdk_display_get_device_manager(gdk_display_get_default());
 	GdkDevice *device = gdk_device_manager_get_client_pointer (manager);
 	gdk_device_ungrab (gdk_device_get_associated_device(device),
 			   t);
-#else
-	gdk_keyboard_ungrab(t);
-#endif
 }
 
 inline
 void XAP_gdk_pointer_ungrab(guint32 t)
 {
-#if GTK_CHECK_VERSION(3,0,0)
 	GdkDeviceManager *manager
 		= gdk_display_get_device_manager(gdk_display_get_default());
 	GdkDevice *device = gdk_device_manager_get_client_pointer (manager);
 	gdk_device_ungrab (device, t);
-#else
-	gdk_pointer_ungrab(t);
-#endif
 }
 
 #endif
