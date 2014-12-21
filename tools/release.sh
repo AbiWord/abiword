d258e1655137d5357cf8c2f67704a1ab116072e2
Hubert Figuiere <hub@figuiere.net>
2014-12-21 04:28:12 +0000

    Better working release script.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35052 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- tools/release.sh
+++ tools/release.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+BRANCH="branches/ABI-3-0-0-STABLE"
 RELEASE="3.0.1"
-SVNUSER="hub"
+
+# check for a svn checkout
+svn info > /dev/null 2>&1 || echo "Must be in a SVN checkout"
 
 mkdir abiword-release-dir-$RELEASE
 cd abiword-release-dir-$RELEASE
 
-svn copy -m "Tag release $RELEASE" svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword/trunk svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword/tags/release-$RELEASE
-svn copy -m "Tag release $RELEASE" svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword-docs/trunk svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword-docs/tags/release-$RELEASE
-svn copy -m "Tag release $RELEASE" svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword-msvc2008/trunk svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword-msvc2008/tags/release-$RELEASE
+svn copy -m "Tag release $RELEASE" ^/abiword/$BRANCH ^/abiword/tags/release-$RELEASE
+svn copy -m "Tag release $RELEASE" ^/abiword-docs/$BRANCH ^/abiword-docs/tags/release-$RELEASE
+svn copy -m "Tag release $RELEASE" ^/abiword-msvc2008/$BRANCH ^/abiword-msvc2008/tags/release-$RELEASE
 
-svn export svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword/tags/release-$RELEASE abiword-$RELEASE
-svn export svn+ssh://$SVNUSER@svn.abisource.com/svnroot/abiword-docs/tags/release-$RELEASE abiword-docs-$RELEASE
+svn export ^/abiword/tags/release-$RELEASE abiword-$RELEASE
+svn export ^/abiword-docs/tags/release-$RELEASE abiword-docs-$RELEASE
 
 cd abiword-$RELEASE
 ./autogen.sh && make distcheck
