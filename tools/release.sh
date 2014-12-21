eaa1a4213f2b99274218f1007fac462fc98334e7
Hubert Figuiere <hub@figuiere.net>
2014-12-21 04:34:21 +0000

    Safer release script.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35053 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- tools/release.sh
+++ tools/release.sh
@@ -2,19 +2,26 @@
 
 BRANCH="branches/ABI-3-0-0-STABLE"
 RELEASE="3.0.1"
+RELEASE_DIR="abiword-release-dir-$RELEASE"
 
 # check for a svn checkout
 svn info > /dev/null 2>&1 || echo "Must be in a SVN checkout"
 
-mkdir abiword-release-dir-$RELEASE
-cd abiword-release-dir-$RELEASE
+if [ -d "$RELEASE_DIR" ] ; then
+	echo "Unclean release. Directory $RELEASE_DIR exists."
+	exit 2
+fi
+
+mkdir $RELEASE_DIR
 
 svn copy -m "Tag release $RELEASE" ^/abiword/$BRANCH ^/abiword/tags/release-$RELEASE
 svn copy -m "Tag release $RELEASE" ^/abiword-docs/$BRANCH ^/abiword-docs/tags/release-$RELEASE
 svn copy -m "Tag release $RELEASE" ^/abiword-msvc2008/$BRANCH ^/abiword-msvc2008/tags/release-$RELEASE
 
-svn export ^/abiword/tags/release-$RELEASE abiword-$RELEASE
-svn export ^/abiword-docs/tags/release-$RELEASE abiword-docs-$RELEASE
+svn export ^/abiword/tags/release-$RELEASE $RELEASE_DIR/abiword-$RELEASE
+svn export ^/abiword-docs/tags/release-$RELEASE $RELEASE_DIR/abiword-docs-$RELEASE
+
+cd $RELEASE_DIR
 
 cd abiword-$RELEASE
 ./autogen.sh && make distcheck
