fa7c1bb18294965891aa6eb9362b568e5dd4b234
Hubert Figuiere <hub@figuiere.net>
2015-04-02 03:08:33 +0000

    Provide bool PD_URI::operator<(const PD_URI& b) const This fixes building on FreeBSD with their C++ library.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35084 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/text/ptbl/xp/pd_DocumentRDF.h
+++ src/text/ptbl/xp/pd_DocumentRDF.h
@@ -68,6 +68,7 @@ class ABI_EXPORT PD_URI
     bool isValid() const;
     bool operator==(const PD_URI& b) const;
     bool operator==(const std::string& b) const;
+    bool operator<(const PD_URI& b) const;
 
     virtual bool read( std::istream& ss );
     virtual bool write( std::ostream& ss ) const;
