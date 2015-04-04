739cc383d39f4f82f9e3890aaa2728a171528a79
Hubert Figuiere <hub@figuiere.net>
2015-04-04 14:16:47 +0000

    Bug 13712 - Fixed SIP collab backend build failure.
    Cherry-picked from trunk@35089 by jbrefort.
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35090 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- plugins/collab/backends/sipsimple/xp/SIPSimpleBuddy.h
+++ plugins/collab/backends/sipsimple/xp/SIPSimpleBuddy.h
@@ -52,10 +52,10 @@ public:
 
 	virtual const DocTreeItem* getDocTreeItems() const
 	{
-		const vector<DocHandle*>& docHandles = getDocHandles();
+		const std::vector<DocHandle*>& docHandles = getDocHandles();
 		DocTreeItem* first = 0;
 		DocTreeItem* prev = 0;
-		for (vector<DocHandle*>::const_iterator pos = docHandles.begin(); pos != docHandles.end(); pos++)
+		for (std::vector<DocHandle*>::const_iterator pos = docHandles.begin(); pos != docHandles.end(); pos++)
 		{
 			DocTreeItem* item = new DocTreeItem();
 			item->m_type = DOCTREEITEM_TYPE_DOCUMENT;
