5623a94c756cb780f5ae45093b4db73d632763ed
Hubert Figuiere <hub@figuiere.net>
2016-04-12 02:55:47 +0000

    Bug 13770 - Require C++11 from now on.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35197 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/ap/gtk/ap_UnixApp.cpp
+++ src/wp/ap/gtk/ap_UnixApp.cpp
@@ -829,7 +829,7 @@ static bool is_so (const char *file) {
 	if (len < (strlen(G_MODULE_SUFFIX) + 2)) // this is ".so" and at least one char for the filename
 		return false;
 	const char *suffix = file+(len-3);
-	if(0 == strcmp (suffix, "."G_MODULE_SUFFIX))
+	if(0 == strcmp (suffix, "." G_MODULE_SUFFIX))
 		return true;
 	return false;
 }
