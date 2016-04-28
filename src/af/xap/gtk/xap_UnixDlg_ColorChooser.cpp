429c86345db654e69a4b8e2f43252dc804f3b3aa
Hubert Figuiere <hub@figuiere.net>
2016-04-28 01:24:18 +0000

    Forgot this auto_ptr<>.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35243 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/xap/gtk/xap_UnixDlg_ColorChooser.cpp
+++ src/af/xap/gtk/xap_UnixDlg_ColorChooser.cpp
@@ -54,5 +54,5 @@ std::unique_ptr<UT_RGBColor> XAP_UnixDlg_RunColorChooser(GtkWindow* parent,
 
 	// do not propagate further
 	gtk_widget_destroy (colordlg);
-	return std::auto_ptr<UT_RGBColor>(rgb);
+	return std::unique_ptr<UT_RGBColor>(rgb);
 }
