3925a2594fbbad53f9dc88967366607e511ebc64
Hubert Figuiere <hub@figuiere.net>
2016-10-21 00:38:05 +0000

    Revert "Bug 13767 - Properly queue drawing to work on Wayland"
    This reverts commit b789426d7b613981b39ad3a849134dc40e76ab15.
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35354 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/gr/gtk/gr_UnixCairoGraphics.h
+++ src/af/gr/gtk/gr_UnixCairoGraphics.h
@@ -102,8 +102,6 @@ public:
 								 UT_sint32 x, UT_sint32 y,
 								 UT_sint32 w, UT_sint32 h);
 #endif
-	virtual void      flush(void);
-
 protected:
 	virtual void		_resetClip(void);
 	static void		widget_size_allocate (GtkWidget        *widget,
