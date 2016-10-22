abf88e1a8b441a0d678924503f2b53267dd5da52
Hubert Figuiere <hub@figuiere.net>
2016-10-22 19:56:12 +0000

    Bug 13819 - Remove nullptr as we don't require C++11
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35366 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/gr/gtk/gr_UnixCairoGraphics.cpp
+++ src/af/gr/gtk/gr_UnixCairoGraphics.cpp
@@ -180,7 +180,7 @@ void GR_UnixCairoGraphics::init3dColors(GtkWidget* /*w*/)
 	if (m_styleBg) {
 		g_object_unref(m_styleBg);
 	}
-	m_styleBg = XAP_GtkStyle_get_style(nullptr, "GtkButton"); // "button"
+	m_styleBg = XAP_GtkStyle_get_style(NULL, "GtkButton"); // "button"
 	// guess colours
 	// WHITE
 	GdkRGBA rgba2;
@@ -627,7 +627,7 @@ void GR_UnixCairoGraphics::fillRect(GR_Color3D c, UT_sint32 x, UT_sint32 y,
 		_setProps();
 		cairo_save (m_cr);
 
-		GtkStyleContext *context = nullptr;
+		GtkStyleContext *context = NULL;
 		switch(c) {
 		case GR_Graphics::CLR3D_Background:
 			context = m_styleBg;
--- src/wp/ap/xp/ap_Dialog_Spell.cpp
+++ src/wp/ap/xp/ap_Dialog_Spell.cpp
@@ -251,7 +251,7 @@ bool AP_Dialog_Spell::nextMisspelledWord(void)
 					UT_return_val_if_fail (m_Suggestions, false);
 
 					// get suggestions from spelling engine
-					const UT_GenericVector<UT_UCSChar*> *cpvEngineSuggestions = nullptr;
+					const UT_GenericVector<UT_UCSChar*> *cpvEngineSuggestions = NULL;
 
 					if (checker->checkWord(m_pWord, m_iWordLength) == SpellChecker::LOOKUP_FAILED)
 					{
