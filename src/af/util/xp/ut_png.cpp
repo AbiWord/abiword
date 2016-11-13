8731fa4fe511502b711b81ff28694389da11fa7a
Hubert Figuiere <hub@figuiere.net>
2016-11-13 15:14:06 +0000

    Bug 13827 - Fix overflow read in PNG
    Based on a patch by Artem Smotrakov <artem.smotrakov@gmail.com>
    
    Sample file: http://bugzilla.abisource.com/attachment.cgi?id=5830
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@35368 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/util/xp/ut_png.cpp
+++ src/af/util/xp/ut_png.cpp
@@ -28,6 +28,7 @@
 
 #include "ut_assert.h"
 #include "ut_bytebuf.h"
+#include "ut_debugmsg.h"
 
 struct _bb
 {
@@ -40,6 +41,19 @@ static void _png_read(png_structp png_ptr, png_bytep data, png_size_t length)
 	struct _bb* p = static_cast<struct _bb*>(png_get_io_ptr(png_ptr));
 	const UT_Byte* pBytes = p->pBB->getPointer(0);
 
+	// make sure that we don't read outside of pBytes
+	if (p->iCurPos >= p->pBB->getLength() - length) {
+		UT_WARNINGMSG(("PNG: Reading past buffer bounds. cur = %u, buflen = %u, length = %lu\n",
+					   p->iCurPos, p->pBB->getLength(), length));
+		length = p->pBB->getLength() - p->iCurPos;
+		if (length == 0) {
+			UT_WARNINGMSG(("PNG: Truncating to ZERO length.\n"));
+			png_error(png_ptr, "Premature end of buffer");
+			return;
+		} else {
+			UT_WARNINGMSG(("PNG: Truncating to %lu.\n", length));
+		}
+	}
 	memcpy(data, pBytes + p->iCurPos, length);
 	p->iCurPos += length;
 }
