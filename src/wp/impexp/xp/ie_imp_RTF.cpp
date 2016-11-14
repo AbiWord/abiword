2527fe2d9703183cede26139e23ccd6ceccca845
Hubert Figuiere <hub@figuiere.net>
2016-11-14 04:08:49 +0000

    Bug 13826 - Part 3: Fix out of bounds read of basedon style
    Adapted from trunk@35372
    
    Sample documents:
    http://bugzilla.abisource.com/attachment.cgi?id=5821
    http://bugzilla.abisource.com/attachment.cgi?id=5820
    http://bugzilla.abisource.com/attachment.cgi?id=5819
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35378 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/wp/impexp/xp/ie_imp_RTF.cpp
+++ src/wp/impexp/xp/ie_imp_RTF.cpp
@@ -12068,7 +12068,13 @@ bool IE_Imp_RTF::HandleStyleDefinition(void)
 					// must not mix static and dynamically allocated strings in the same
 					// array, otherwise there is no way we can g_free it !!!
 					//attribs[attribsCount++] = g_strdup(static_cast<const char *>(m_styleTable[istyle]));
-					attribs[attribsCount++] = m_styleTable[istyle].c_str();
+					if (istyle >= 0 && static_cast<UT_uint32>(istyle) < m_styleTable.size()) {
+						attribs[attribsCount++] = m_styleTable[istyle].c_str();
+					} else {
+						UT_WARNINGMSG(("RTF: basedon by style index out of bounds: %d. max %lu.\n",
+									   istyle, m_styleTable.size()));
+						attribs[attribsCount++] = NULL;
+					}
 					UT_return_val_if_fail( attribsCount < PT_MAX_ATTRIBUTES * 2,false );
 				}
 				else
