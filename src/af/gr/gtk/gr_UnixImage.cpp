9ec47e948f504d6f344abad5dd63ad8eb7dea65d
Hubert Figuiere <hub@figuiere.net>
2016-12-08 02:03:03 +0000

    Don't delete GError but use g_error_free instead
    Cherry-pick from trunk@35380.
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/branches/ABI-3-0-0-STABLE@35393 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- src/af/gr/gtk/gr_UnixImage.cpp
+++ src/af/gr/gtk/gr_UnixImage.cpp
@@ -244,12 +244,13 @@ bool GR_UnixImage::saveToPNG(const char * szFile)
         UT_return_val_if_fail(m_image,false);
 
 	GError * error = NULL;
-	gboolean res = gdk_pixbuf_save (m_image, szFile, "png", &error,NULL);
-	if(res != FALSE)
-	{
+	gboolean res = gdk_pixbuf_save (m_image, szFile, "png", &error, NULL);
+	if (res != FALSE) {
+		if (error) {
+			g_error_free (error);
+		}
 		return true;
 	}
-	delete error;
 	return false;
 
 }
