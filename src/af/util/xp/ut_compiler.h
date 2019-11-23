a65fab84f2665a2701534a38e85eb7beedc4731e
Hubert Figuiere <hub@figuiere.net>
2019-11-22 21:52:42 -0500

    Properly fix ut_compiler to support clang better

--- src/af/util/xp/ut_compiler.h
+++ src/af/util/xp/ut_compiler.h
@@ -1,30 +1,30 @@
 
 
 // a bunch of macro for compiler control.
-// TODO FIXME, currently tied to gcc.
+// TODO FIXME, currently tied to gcc and clang.
 
 #pragma once
 
 #if defined(__clang__)
-#define ABI_W_PUSH \
-_Pragma("clang diagnostic pop")
+# define ABI_W_PUSH \
+_Pragma("clang diagnostic push")
 #else
-#define ABI_W_PUSH \
-_Pragma("GCC diagnostic pop")
+# define ABI_W_PUSH \
+_Pragma("GCC diagnostic push")
 #endif
 
 #define ABI_W_NO_CONST_QUAL \
-_Pragma("GCC diagnostic push") \
+ABI_W_PUSH \
 _Pragma("GCC diagnostic ignored \"-Wcast-qual\"")
 
 #define ABI_W_NO_DEPRECATED \
-_Pragma("GCC diagnostic push") \
+ABI_W_PUSH \
 _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
 
 #if defined(__clang__)
-#define ABI_W_POP \
+# define ABI_W_POP \
 _Pragma("clang diagnostic pop")
 #else
-#define ABI_W_POP \
+# define ABI_W_POP \
 _Pragma("GCC diagnostic pop")
 #endif
